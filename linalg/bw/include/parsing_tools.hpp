5f541681c8e90771e7dc52de0f8d5e8b4bb602d5
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2005-08-16 12:50:34 +0200

    Started conversion to simpler code in cxx.

--- linalg/bw/include/parsing_tools.hpp
+++ linalg/bw/include/parsing_tools.hpp
@@ -9,10 +9,12 @@ template<typename T> struct wanted_item;
 template<>
 struct wanted_item<char> {
 	char c;
-	wanted_item(char c) : c(c) {}
+	bool fail_if_not;
+	wanted_item(char c, bool b = true) : c(c), fail_if_not(b) {}
 	std::istream& operator>>(std::istream& is) const {
 		if (is.peek() != c) {
-			is.setstate(std::ios::failbit);
+			if (fail_if_not)
+				is.setstate(std::ios::failbit);
 		} else {
 			is.ignore(1);
 		}
@@ -23,14 +25,20 @@ struct wanted_item<char> {
 template<unsigned int sz>
 struct wanted_item<char [sz]> {
 	const char * c;
-	wanted_item(const char c[]) : c(c) {}
+	bool fail_if_not;
+	wanted_item(const char c[], bool b = true) : c(c), fail_if_not(b) {}
 	std::istream& operator>>(std::istream& is) const {
 		is >> std::ws;
 		const char * ptr = c;
 		for( ; *ptr && isspace(*ptr) ; ptr++);
 		for( ; *ptr ; ptr++) {
 			if (is.peek() != *ptr) {
-				is.setstate(std::ios::failbit);
+				/* Beware ; the gobbled characters are
+				 * _NOT_ pushed back into the stream
+				 * (just because we can't)
+				 */
+				if (fail_if_not)
+					is.setstate(std::ios::failbit);
 				return is;
 			}
 			is.ignore(1);
@@ -39,17 +47,10 @@ struct wanted_item<char [sz]> {
 	}
 };
 
-inline bool try_match(std::istream& is, char c)
-{
-	if (is.peek() == c) {
-		is.ignore(1);
-		return true;
-	}
-	return false;
-}
-
 template<typename T>
 inline wanted_item<T> wanted(const T& c) { return wanted_item<T>(c); }
+template<typename T>
+inline wanted_item<T> optional(const T& c) { return wanted_item<T>(c,false); }
 
 namespace std {
 	template<typename T>
@@ -57,6 +58,15 @@ namespace std {
 	operator>>(std::istream& is, const wanted_item<T>& w) {
 		return w.operator>>(is);
 	}
+}
+
+inline bool try_match(std::istream& is, char c)
+{
+	if (is.peek() == c) {
+		is.ignore(1);
+		return true;
+	}
+	return false;
 }
 
 inline bool startswith(const std::string& msg, const std::string& prefix)
