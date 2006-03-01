32bf7e8fd877a30772b2ba4b4ebcc2b8a8bedcdb
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2006-03-01 18:16:08 +0100

    Changed to include the new mpfq layer.

--- linalg/bw/include/gmp-hacks.h
+++ linalg/bw/include/gmp-hacks.h
@@ -33,35 +33,44 @@
 
 /* Useful for the lazy boyz */
 
-#define MPZ_INIT_SET_MPN(DST, SRC, NLIMBS)			\
-	BEGIN_BLOCK						\
-	ALLOC(DST) = (NLIMBS);				\
-	SIZ(DST) = (NLIMBS);					\
+#define MPZ_GROW_ALLOC(DST, NLIMBS)					\
+	BEGIN_BLOCK							\
+	if (ALLOC(DST) < (int) (NLIMBS)) {				\
+		ALLOC(DST) = (NLIMBS);					\
+		PTR(DST)=(mp_limb_t *) realloc(PTR(DST),		\
+				(NLIMBS) * sizeof(mp_limb_t));		\
+	}								\
+	END_BLOCK
+
+#define MPZ_INIT_SET_MPN(DST, SRC, NLIMBS)				\
+	BEGIN_BLOCK							\
+	ALLOC(DST) = (NLIMBS);						\
+	SIZ(DST) = (NLIMBS);						\
 	PTR(DST) = (mp_limb_t *)malloc((NLIMBS) * sizeof(mp_limb_t));	\
-	memcpy(PTR(DST),(SRC),(NLIMBS) * sizeof(mp_limb_t));	\
-	MPN_NORMALIZE(PTR(DST),SIZ(DST));		\
+	memcpy(PTR(DST),(SRC),(NLIMBS) * sizeof(mp_limb_t));		\
+	MPN_NORMALIZE(PTR(DST),SIZ(DST));				\
 	END_BLOCK
 	
-#define MPZ_SET_MPN(DST, SRC, NLIMBS)				\
-	BEGIN_BLOCK						\
-	if (ALLOC(DST) < (NLIMBS)) {			\
-		ALLOC(DST) = (NLIMBS);			\
-		PTR(DST)=(mp_limb_t *)realloc(PTR(DST),(NLIMBS)*sizeof(mp_limb_t)); \
-	}							\
-	SIZ(DST) = (NLIMBS);					\
-	memcpy(PTR(DST),(SRC),(NLIMBS) * sizeof(mp_limb_t));	\
-	MPN_NORMALIZE(PTR(DST),SIZ(DST));		\
+#define MPZ_SET_MPN(DST, SRC, NLIMBS)					\
+	BEGIN_BLOCK							\
+	MPZ_GROW_ALLOC(DST, NLIMBS);					\
+	SIZ(DST) = (NLIMBS);						\
+	memcpy(PTR(DST),(SRC),(NLIMBS) * sizeof(mp_limb_t));		\
+	MPN_NORMALIZE(PTR(DST),SIZ(DST));				\
 	END_BLOCK
 	   
-#define MPN_SET_MPZ(DST,NLIMBS,SRC)				\
-	BEGIN_BLOCK						\
-	memcpy((DST),PTR(SRC),SIZ(SRC) * sizeof(mp_limb_t));	\
+#define MPN_SET_MPZ(DST,NLIMBS,SRC)					\
+	BEGIN_BLOCK							\
+	memcpy((DST),PTR(SRC),SIZ(SRC) * sizeof(mp_limb_t));		\
 	memset((DST)+SIZ(SRC),0,((NLIMBS)-SIZ(SRC)) * sizeof(mp_limb_t));\
 	END_BLOCK
 
-#define MPN_ADD_MPZ(DST,NLIMBS,SRC)				\
-	BEGIN_BLOCK						\
-	mpn_add((DST), (DST), (NLIMBS), PTR(SRC), SIZ(SRC));	\
+/* This one is rather ill-designed, hence the ASSERTS as a caution */
+#define MPN_ADD_MPZ(DST,NLIMBS,SRC)					\
+	BEGIN_BLOCK							\
+	ASSERT(SIZ(SRC) > 0);						\
+	ASSERT((NLIMBS) > SIZ(SRC));					\
+	mpn_add((DST), (DST), (NLIMBS), PTR(SRC), SIZ(SRC));		\
 	END_BLOCK
 
 
