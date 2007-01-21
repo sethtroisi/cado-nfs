0eb9f84973e53a3b22ee24d71773e52ad0900ea0
Paul Zimmermann <Paul.Zimmermann@loria.fr>
2007-01-21 20:37:45 +0000

    first (dummy) version of polynomial selection
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cado/trunk@22 de4796e2-0b1e-0410-91b4-86bc23549317

---
+++ polyselect/polyselect.c
@@ -0,0 +1,400 @@
+/* polyselect1 - polynomial selection (naive implementation)
+
+   Usage: polyselect < c80 > c80.poly
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h> /* for ULONG_MAX */
+#include "cado.h"
+
+static void
+usage ()
+{
+  fprintf (stderr, "Usage: polyselect < in > out\n\n");
+  fprintf (stderr, "       in  - input file (number to factor)\n");
+  fprintf (stderr, "       out - output file (polynomials)\n");
+  exit (1);
+}
+
+static void
+init (cado_input in)
+{
+  mpz_init (in->n);
+  in->degree = 0;
+}
+
+static void
+clear (cado_input in)
+{
+  mpz_clear (in->n);
+}
+
+struct sd {
+  size_t s;
+  unsigned long d;
+};
+
+static struct sd default_degrees[DEFAULT_DEGREES_LENGTH] = DEFAULT_DEGREES;
+
+/* default degree, when not given by user */
+static int
+default_degree (mpz_t n)
+{
+  size_t s = mpz_sizeinbase (n, 10);
+  int i;
+
+  for (i = 0; s > default_degrees[i].s; i++);
+  return default_degrees[i].d;
+}
+
+static struct sd default_rlim_table[DEFAULT_RLIM_LENGTH] = DEFAULT_RLIM;
+static struct sd default_alim_table[DEFAULT_ALIM_LENGTH] = DEFAULT_ALIM;
+
+/* default degree, when not given by user */
+static unsigned long
+default_rlim (mpz_t n)
+{
+  size_t s = mpz_sizeinbase (n, 10);
+  int i;
+
+  for (i = 0; s > default_rlim_table[i].s; i++);
+  return default_rlim_table[i].d;
+}
+
+/* default degree, when not given by user */
+static unsigned long
+default_alim (mpz_t n)
+{
+  size_t s = mpz_sizeinbase (n, 10);
+  int i;
+
+  for (i = 0; s > default_alim_table[i].s; i++);
+  return default_alim_table[i].d;
+}
+
+static struct sd default_lpbr_table[DEFAULT_LPBR_LENGTH] = DEFAULT_LPBR;
+static struct sd default_lpba_table[DEFAULT_LPBA_LENGTH] = DEFAULT_LPBA;
+
+/* default rational large prime bound, when not given by user */
+static unsigned long
+default_lpbr (mpz_t n)
+{
+  size_t s = mpz_sizeinbase (n, 10);
+  int i;
+
+  for (i = 0; s > default_lpbr_table[i].s; i++);
+  return default_lpbr_table[i].d;
+}
+
+/* default algebraic large prime bound, when not given by user */
+static unsigned long
+default_lpba (mpz_t n)
+{
+  size_t s = mpz_sizeinbase (n, 10);
+  int i;
+
+  for (i = 0; s > default_lpba_table[i].s; i++);
+  return default_lpba_table[i].d;
+}
+
+static struct sd default_mfbr_table[DEFAULT_MFBR_LENGTH] = DEFAULT_MFBR;
+static struct sd default_mfba_table[DEFAULT_MFBA_LENGTH] = DEFAULT_MFBA;
+
+/* default rational factor-residual bound, when not given by user */
+static unsigned long
+default_mfbr (mpz_t n)
+{
+  size_t s = mpz_sizeinbase (n, 10);
+  int i;
+
+  for (i = 0; s > default_mfbr_table[i].s; i++);
+  return default_mfbr_table[i].d;
+}
+
+/* default algebraic factor-residual bound, when not given by user */
+static unsigned long
+default_mfba (mpz_t n)
+{
+  size_t s = mpz_sizeinbase (n, 10);
+  int i;
+
+  for (i = 0; s > default_mfba_table[i].s; i++);
+  return default_mfba_table[i].d;
+}
+
+struct sf {
+  size_t s;
+  double f;
+};
+
+static struct sf default_rlambda_table[DEFAULT_RLAMBDA_LENGTH] = DEFAULT_RLAMBDA;
+static struct sf default_alambda_table[DEFAULT_ALAMBDA_LENGTH] = DEFAULT_ALAMBDA;
+
+/* default rational lambda, when not given by user */
+static double
+default_rlambda (mpz_t n)
+{
+  size_t s = mpz_sizeinbase (n, 10);
+  int i;
+
+  for (i = 0; s > default_rlambda_table[i].s; i++);
+  return default_rlambda_table[i].f;
+}
+
+/* default algebraic lambda, when not given by user */
+static double
+default_alambda (mpz_t n)
+{
+  size_t s = mpz_sizeinbase (n, 10);
+  int i;
+
+  for (i = 0; s > default_alambda_table[i].s; i++);
+  return default_alambda_table[i].f;
+}
+
+static struct sd default_qint_table[DEFAULT_QINT_LENGTH] = DEFAULT_QINT;
+
+/* default rational factor-residual bound, when not given by user */
+static unsigned long
+default_qint (mpz_t n)
+{
+  size_t s = mpz_sizeinbase (n, 10);
+  int i;
+
+  for (i = 0; s > default_qint_table[i].s; i++);
+  return default_qint_table[i].d;
+}
+
+static void
+init_poly (cado_poly p, cado_input in)
+{
+  int i;
+
+  strcpy (p->name, "");
+  mpz_init_set (p->n, in->n);
+  p->degree = in->degree;
+  if (p->degree == 0)
+    p->degree = default_degree (p->n);
+  p->f = (mpz_t*) malloc ((p->degree + 1) * sizeof (mpz_t));
+  for (i = 0; i <= p->degree; i++)
+    mpz_init (p->f[i]);
+  p->g = (mpz_t*) malloc (2 * sizeof (mpz_t));
+  mpz_init (p->g[0]);
+  mpz_init (p->g[1]);
+  strcpy (p->type, "gnfs");
+}
+
+static void
+clear_poly (cado_poly p)
+{
+  int i;
+
+  mpz_clear (p->n);
+  for (i = 0; i <= p->degree; i++)
+    mpz_clear (p->f[i]);
+  free (p->f);
+  mpz_clear (p->g[0]);
+  mpz_clear (p->g[1]);
+  free (p->g);
+  strcpy (p->type, "gnfs");
+}
+
+static void
+parse_error (int c)
+{
+  if (c == 0)
+    {
+      fprintf (stderr, "Error, no field read\n");
+      exit (1);
+    }
+  if (c == EOF)
+    {
+      fprintf (stderr, "Error, end of file\n");
+      exit (1);
+    }
+}
+
+/* read a string of characters [a-z]+, returns the number of characters read */
+static int
+read_string (char *s)
+{
+  int c, l = 0;
+  
+  while (1)
+    {
+      c = getchar ();
+      if (c == EOF)
+	return c;
+      if (islower (c) == 0)
+	{
+	  ungetc (c, stdin);
+	  s[l] = '\0';
+	  return l;
+	}
+      s[l++] = c;
+    }
+}
+
+static void
+parse_input (cado_input in)
+{
+  int c;
+  char s[256];
+
+  while ((c = getchar ()) != EOF)
+    {
+      /* skip blank characters and lines */
+      while (isspace (c) && c != EOF)
+	c = getchar ();
+
+      if (c == EOF)
+	break;
+      
+      if (c != '#') /* comment line */
+	{
+	  ungetc (c, stdin);
+	  c = read_string (s);
+	  if (c == 0 || c == EOF)
+	    parse_error (c);
+
+	  /* read possible spaces or tabs */
+	  while (isblank (c = getchar ()))
+	    printf ("read '%c'\n", c);
+
+	  /* read ':' */
+	  if (c != ':')
+	    {
+	      fprintf (stderr, "Error, ':' expected\n");
+	      exit (1);
+	    }
+
+	  if (strcmp (s, "n") == 0)
+	    {
+	      if (mpz_inp_str (in->n, stdin, 10) == 0)
+		{
+		  fprintf (stderr, "Error after n:\n");
+		  exit (1);
+		}
+	    }
+	  else if (strcmp (s, "deg") == 0)
+	    {
+	      c = scanf ("%d", &(in->degree));
+	      if (c == 0 || c == EOF)
+		parse_error (c);
+	    }
+	  else
+	    {
+	      fprintf (stderr, "Error, unrecognized field: %s\n", s);
+	      exit (1);
+	    }
+	}
+
+      /* read to end of line */
+      while (!feof (stdin) && c != '\n')
+	c = getchar ();
+    }
+
+  if (mpz_cmp_ui (in->n, 0) == 0)
+    {
+      fprintf (stderr, "Error, no input number read\n");
+      exit (1);
+    }
+}
+
+/* round to nearest, assume d > 0 */
+static void
+mpz_ndiv_qr (mpz_t q, mpz_t r, mpz_t n, mpz_t d)
+{
+  int s;
+  mpz_fdiv_qr (q, r, n, d); /* round towards -inf */
+  mpz_mul_2exp (r, r, 1);
+  s = mpz_cmp (r, d);
+  mpz_div_2exp (r, r, 1);
+  if (s > 0)
+    {
+      mpz_add_ui (q, q, 1);
+      mpz_sub (r, r, d);
+    }
+}
+
+/* very naive method, just to check the interface with other modules */
+void
+generate_poly (cado_poly out)
+{
+  int i, d = out->degree;
+
+  mpz_root (out->g[0], out->n, d + 1);
+  mpz_ndiv_qr (out->f[1], out->f[0], out->n, out->g[0]);
+  for (i = 1; i < d; i++)
+    mpz_ndiv_qr (out->f[i+1], out->f[i], out->f[i], out->g[0]);
+  mpz_neg (out->g[0], out->g[0]);
+  mpz_set_ui (out->g[1], 1);
+  out->skew = 1.0;
+  out->rlim = default_rlim (out->n);
+  out->alim = default_alim (out->n);
+  out->lpbr = default_lpbr (out->n);
+  out->lpba = default_lpba (out->n);
+  out->mfbr = default_mfbr (out->n);
+  out->mfba = default_mfba (out->n);
+  out->rlambda = default_rlambda (out->n);
+  out->alambda = default_alambda (out->n);
+  out->qintsize = default_qint (out->n);
+}
+
+void
+print_poly (FILE *fp, cado_poly p)
+{
+  int i;
+
+  if (strlen (p->name) != 0)
+    fprintf (fp, "name: %s\n", p->name);
+  fprintf (fp, "n: ");
+  mpz_out_str (fp, 10, p->n);
+  fprintf (fp, "\n");
+  fprintf (fp, "skew: %1.3f\n", p->skew);
+  for (i = p->degree; i >= 0; i--)
+    {
+      fprintf (fp, "c%d: ", i);
+      mpz_out_str (fp, 10, p->f[i]);
+      fprintf (fp, "\n");
+    }
+  fprintf (fp, "Y1: ");
+  mpz_out_str (fp, 10, p->g[1]);
+  fprintf (fp, "\n");
+  fprintf (fp, "Y0: ");
+  mpz_out_str (fp, 10, p->g[0]);
+  fprintf (fp, "\n");
+  fprintf (fp, "type: %s\n", p->type);
+  fprintf (fp, "rlim: %lu\n", p->rlim);
+  fprintf (fp, "alim: %lu\n", p->alim);
+  fprintf (fp, "lpbr: %d\n", p->lpbr);
+  fprintf (fp, "lpba: %d\n", p->lpba);
+  fprintf (fp, "mfbr: %d\n", p->mfbr);
+  fprintf (fp, "mfba: %d\n", p->mfba);
+  fprintf (fp, "rlambda: %1.1f\n", p->rlambda);
+  fprintf (fp, "alambda: %1.1f\n", p->alambda);
+  fprintf (fp, "qintsize: %d\n", p->qintsize);
+}
+
+int
+main (int argc, char *argv[])
+{
+  cado_input in;
+  cado_poly out;
+
+  if (argc != 1)
+    usage ();
+
+  init (in);
+  parse_input (in);
+  init_poly (out, in);
+  clear (in);
+
+  generate_poly (out);
+  print_poly (stdout, out);
+  clear_poly (out);
+
+  return 0;
+}
