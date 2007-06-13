ba6a4fe15f1f98eeae7771e1005d55340ec49c9a
Alexander Kruppa <akruppa@gmail.com>
2007-06-13 11:18:01 +0000

    Split code for reading polynomial file into separate file. Small fix in siever that caused unnecessary refactoring. Cleanup of Makefile.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cado/trunk@181 de4796e2-0b1e-0410-91b4-86bc23549317

--- sieve/sieve.c
+++ sieve/sieve.c
@@ -15,6 +15,7 @@
 #include "mod_ul.c"
 #include "sieve_aux.h"
 #include "basicnt.h"
+#include "polyfile.h"
 
 #define MIN(a,b) ((a)<(b)?(a):(b))
 #define MAX(a,b) ((a)>(b)?(a):(b))
@@ -25,6 +26,8 @@
 #define REFAC_PRP_THRES 500
 #define SIEVE_PERMISSIBLE_ERROR ((unsigned char) 7)
 #define PRP_REPS 1 /* number of tests in mpz_probab_prime_p */
+
+unsigned long sumprimes, nrprimes;
 
 /* Some multiple precision functions we'll need */
 
@@ -331,170 +334,6 @@ sieve (unsigned char *sievearray, factorbase_degn_t *fb,
 
   if (reports_length > 0)
     reports->p = 0; /* Put end marker */
-}
-
-#define TYPE_STRING 0
-#define TYPE_MPZ 1
-#define TYPE_INT 2
-#define TYPE_ULONG 3
-#define TYPE_DOUBLE 4
-#define PARSE_MATCH -1
-#define PARSE_ERROR 0
-#define PARSE_NOMATCH 1
-
-/* Return value: 1: tag does not match, 0: error occurred, -1 tag did match */
-
-static int
-parse_line (void *target, char *line, const char *tag, int *have, 
-	    const int type)
-{
-  char *lineptr = line;
-
-  if (strncmp (lineptr, tag, strlen (tag)) != 0)
-    return PARSE_NOMATCH;
-
-  if (have != NULL && *have != 0)
-    {
-      fprintf (stderr, "parse_line: %s appears twice\n", tag);
-      return PARSE_ERROR;
-    }
-  if (have != NULL)
-    *have = 1;
-  
-  lineptr += strlen (tag);
-  if (type == TYPE_STRING) /* character string of length up to 256 */
-    {
-      strncpy ((char *)target, lineptr, 256);
-      ((char *)target)[255] = '0';
-    }
-  else if (type == TYPE_MPZ)
-    {
-      mpz_init (*(mpz_t *)target);
-      mpz_set_str (*(mpz_t *)target, lineptr, 0);
-    }
-  else if (type == TYPE_INT)
-    *(int *)target = atoi (lineptr);
-  else if (type == TYPE_ULONG)
-    *(unsigned long *)target = strtoul (lineptr, NULL, 10);
-  else if (type == TYPE_DOUBLE)
-    *(double *)target = atof (lineptr);
-  else return PARSE_ERROR;
-  
-  return PARSE_MATCH;
-}
-
-cado_poly *
-read_polynomial (char *filename)
-{
-  FILE *file;
-  const int linelen = 512;
-  char line[linelen];
-  cado_poly *poly;
-  int have_name = 0, have_n = 0, have_Y0 = 0, have_Y1 = 0;
-  int i, ok = PARSE_ERROR;
-
-  file = fopen (filename, "r");
-  if (file == NULL)
-    {
-      fprintf (stderr, "read_polynomial: could not open %s\n", filename);
-      return NULL;
-    }
-
-  poly = (cado_poly *) malloc (sizeof(cado_poly));
-  (*poly)->f = (mpz_t *) malloc (MAXDEGREE * sizeof (mpz_t));
-  (*poly)->g = (mpz_t *) malloc (2 * sizeof (mpz_t));
-
-  (*poly)->name[0] = '\0';
-  (*poly)->degree = -1;
-  (*poly)->type[0] = '\0';
-
-  while (!feof (file))
-    {
-      ok = 1;
-      if (fgets (line, linelen, file) == NULL)
-	break;
-      if (line[0] == '#')
-	continue;
-
-      ok *= parse_line (&((*poly)->name), line, "name: ", &have_name, TYPE_STRING);
-      ok *= parse_line (&((*poly)->n), line, "n: ", &have_n, TYPE_MPZ);
-      ok *= parse_line (&((*poly)->skew), line, "skew: ", NULL, TYPE_DOUBLE);
-      ok *= parse_line (&((*poly)->g[0]), line, "Y0: ", &have_Y0, TYPE_MPZ);
-      ok *= parse_line (&((*poly)->g[1]), line, "Y1: ", &have_Y1, TYPE_MPZ);
-      ok *= parse_line (&((*poly)->type), line, "type: ", NULL, TYPE_STRING);
-      ok *= parse_line (&((*poly)->rlim), line, "rlim: ", NULL, TYPE_ULONG);
-      ok *= parse_line (&((*poly)->alim), line, "alim: ", NULL, TYPE_ULONG);
-      ok *= parse_line (&((*poly)->lpbr), line, "lpbr: ", NULL, TYPE_INT);
-      ok *= parse_line (&((*poly)->lpba), line, "lpba: ", NULL, TYPE_INT);
-      ok *= parse_line (&((*poly)->mfbr), line, "mfbr: ", NULL, TYPE_INT);
-      ok *= parse_line (&((*poly)->mfba), line, "mfba: ", NULL, TYPE_INT);
-      ok *= parse_line (&((*poly)->rlambda), line, "rlambda: ", NULL, TYPE_DOUBLE);
-      ok *= parse_line (&((*poly)->alambda), line, "alambda: ", NULL, TYPE_DOUBLE);
-      ok *= parse_line (&((*poly)->qintsize), line, "qintsize: ", NULL, TYPE_INT);
-
-
-      if (ok == 1 && line[0] == 'c' && isdigit (line[1]) && line[2] == ':' &&
-	       line[3] == ' ')
-	{
-	  int index = line[1] - '0', i;
-	  for (i = (*poly)->degree + 1; i <= index; i++)
-	    mpz_init ((*poly)->f[i]);
-	  if (index > (*poly)->degree)
-	    (*poly)->degree = index;
-	  mpz_set_str ((*poly)->f[index], line + 4, 0);
-	  ok = -1;
-	}
-
-      if (ok == PARSE_NOMATCH)
-	{
-	  fprintf (stderr, 
-		   "read_polynomial: Cannot parse line %s\nIgnoring.\n", 
-		   line);
-	  continue;
-	}
-      
-      if (ok == PARSE_ERROR)
-	break;
-    }
-  
-  if (ok != PARSE_ERROR)
-    {
-      if (have_n == 0)
-	{
-	  fprintf (stderr, "n ");
-	  ok = PARSE_ERROR;
-	}
-      if (have_Y0 == 0)
-	{
-	  fprintf (stderr, "Y0 ");
-	  ok = PARSE_ERROR;
-	}
-      if (have_Y1 == 0)
-	{
-	  fprintf (stderr, "Y1 ");
-	  ok = PARSE_ERROR;
-	}
-      if (ok == PARSE_ERROR)
-	fprintf (stderr, "are missing in polynomial file\n");
-    }
-
-  if (ok == PARSE_ERROR)
-    {
-      for (i = 0; i <= (*poly)->degree; i++)
-	mpz_clear ((*poly)->f[i]);
-      if (have_n)
-	mpz_clear ((*poly)->n);
-      if (have_Y0)
-	mpz_clear ((*poly)->g[0]);
-      if (have_Y1)
-	mpz_clear ((*poly)->g[1]);
-      free ((*poly)->f);
-      free ((*poly)->g);
-      free (poly);
-      poly = NULL;
-    }
-  
-  return poly;
 }
 
 void
@@ -944,9 +783,20 @@ trialdiv_one_side (mpz_t norm, mpz_t *scaled_poly, int degree,
 	    maxp = FBPRIME_MAX;
 	  else
 	    maxp = (int) ceil(maxp_d);
+	  if (fbptr->p > maxp)
+	    break;
 	}
     }
+
+  if (fbptr->p == 0)
+    fprintf (stderr, "Warning, did not reach maxp for (%ld, %lu)\n", a, b);
   
+  if (fbptr->p != 0)
+    {
+      sumprimes += (unsigned long) (fbptr->p);
+      nrprimes++;
+    }
+
   /* We divided out each prime only once so far, because that's what the
      sieving code did and we wanted identical size of the cofactor.
      Now see if any of the primes so far divide in a power greater than one */
@@ -1060,6 +910,8 @@ trialdiv_and_print (cado_poly *poly, const unsigned long b,
   trialdiv_slow (proj_divisor_r, proj_primes_r, &nr_proj_primes_r, 
 		 max_nr_proj_primes);
 
+  sumprimes = nrprimes = 0;
+
   for (i = 0, j = 0; i < reports_a_nr && j < reports_r_nr;)
     {
       if (reports_a[i].a == reports_r[j].a && 
@@ -1098,26 +950,32 @@ trialdiv_and_print (cado_poly *poly, const unsigned long b,
 	  /* Now print the relations */
 	  printf ("%ld,%lu:", a, b);
 	  for (k = 0; k < nr_primes_r; k++)
-	    printf ("%x%s", primes_r[k], k+1==nr_primes_r?":":",");
+	    printf ("%x%s", primes_r[k], k+1==nr_primes_r?"":",");
+	  printf (":");
 	  for (k = 0; k < nr_primes_a; k++)
-	    printf ("%x%s", primes_a[k], k+1==nr_primes_a?"\n":",");
+	    printf ("%x%s", primes_a[k], k+1==nr_primes_a?"":",");
+	  printf ("\n");
 	  fflush (stdout);
 
+	nextreport:
 	  /* Skip over duplicates that might cause relations to be
-	     output repeatedly */
+	     trial divided/output repeatedly */
 	  while (i < reports_a_nr && reports_a[i].a == reports_a[i + 1].a)
 	    i++;
 	  while (j < reports_r_nr && reports_r[j].a == reports_r[j + 1].a)
 	    j++;
 	}
 
-    nextreport:
       /* Assumes values in reports_a are sorted, same for reports_r  */
       if (reports_a[i].a < reports_r[j].a)
 	i++;
       else
 	j++;
     }
+
+  if (verbose)
+    printf ("# Sum and number of maxp: %lu, %lu, avg: %.0f\n",
+	    sumprimes, nrprimes, (double)sumprimes / (double)nrprimes);
   
   for (i = 0; i <= (unsigned)(*poly)->degree; i++)
     mpz_clear (scaled_poly_a[i]);
@@ -1297,7 +1155,7 @@ main (int argc, char **argv)
 #endif
 
   sievearray = (unsigned char *) malloc ((amax - amin + 1) * sizeof (char));
-  reports_a_len = ((amax - amin + 1)) / 10 + 1000;
+  reports_a_len = ((amax - amin + 1)) / 5 + 1000;
   reports_r_len = ((amax - amin + 1)) / 2 + 1000;
 
   reports_a = (sieve_report_t *) malloc (reports_a_len * 
