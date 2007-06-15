765cf37dc47e14e65aa377f87a121cdb93100344
Paul Zimmermann <Paul.Zimmermann@loria.fr>
2007-06-15 15:21:51 +0000

    moved function cputime() from cado.h to libutils
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cado/trunk@204 de4796e2-0b1e-0410-91b4-86bc23549317

---
+++ utils/cputime.c
@@ -0,0 +1,19 @@
+#include <sys/types.h>    /* for cputime */
+#include <sys/resource.h> /* for cputime */
+
+/* Common timing function: return user time since beginning of session
+   in milliseconds. Example:
+
+   int st = cputime ();
+   foo (...);
+   printf ("foo took %dms\", cputime () - st);
+*/
+static int
+cputime ()
+{
+  struct rusage rus;
+
+  getrusage (0, &rus);
+  return rus.ru_utime.tv_sec * 1000 + rus.ru_utime.tv_usec / 1000;
+}
+
