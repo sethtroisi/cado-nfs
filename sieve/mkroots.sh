a6b2e01b2f12d5bd294d2bc0de92da7e3d0440e3
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2007-06-18 12:35:49 +0000

    also read alim in mkroots.sh
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cado/trunk@211 de4796e2-0b1e-0410-91b4-86bc23549317

--- sieve/mkroots.sh
+++ sieve/mkroots.sh
@@ -4,6 +4,8 @@ input="$1"
 
 if [ ! -f "$input" ] ; then echo "$1 : no such file" ; exit 1 ; fi
 
-pol=`< "$input" tr -d c:  | awk ' /^[0-9]/ { print $2,"*x^",$1,"+"; }' | xargs echo | sed -e 's/ *+$//g'`
+pol=`< "$input" tr -d c:  | awk ' /^[0-9]/ { print $2 "*x^" $1,"+"; }' | xargs echo | sed -e 's/ *+$//g'`
 
-echo "rootfind($pol, 200000)" | gp -q -p 200000 rootfind.gp
+alim=`< $input awk '/^alim:/ { print $2; }'`
+
+echo "rootfind($pol, $alim)" | gp -q -p $alim rootfind.gp
