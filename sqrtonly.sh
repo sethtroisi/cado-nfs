94fca017deda10abdede3d7c640430b8649800e6
François Morain <Francois.Morain@lix.polytechnique.fr>
2007-12-19 12:28:33 +0000

    to help in just performing the sqrt phase, once everything else is done. So that now, we can distribute the computations...
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cado/trunk@406 de4796e2-0b1e-0410-91b4-86bc23549317

---
+++ sqrtonly.sh
@@ -0,0 +1,27 @@
+#!/bin/sh -
+#
+# Typical use: ./sqrtonly.sh Examples/c90/c90 Examples/c90/c90.20x40 0 30
+#
+root=$1; name=$2; ndepmin=$3; ndepmax=$4
+poly=$root.poly
+
+sqrt=sqrt/naive
+mag=$name.mag
+ndep=$ndepmin
+while [ $ndep -lt $ndepmax ]
+do
+  suf=`echo $ndep | awk '{printf("%03d\n", $1)}'`
+  echo "# Dependency $suf"
+  rat=$name.dep.rat.$suf
+  alg=$name.dep.alg.$suf
+  args2="$poly $rat $alg $name.algside.$suf"
+  time $sqrt/fmalgsqrt $args2 $mag
+magma << EOF
+load "$sqrt/jmc.mag";
+load "$sqrt/finish.mag";
+load "$mag";
+finish(f, m, N, "$rat", "$alg", "$name.algside.$suf");
+quit;
+EOF
+  ndep=`expr $ndep '+' 1`
+done
