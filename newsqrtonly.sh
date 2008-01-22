eaf4e0da1dde698dc050e2931613211e70802531
Pierrick Gaudry <Pierrick.Gaudry@gmail.com>
2008-01-22 15:42:24 +0000

    forgot to add the new sqrt script...
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cado/trunk@561 de4796e2-0b1e-0410-91b4-86bc23549317

---
+++ newsqrtonly.sh
@@ -0,0 +1,27 @@
+#!/bin/sh -
+#
+# Typical use: ./newsqrtonly.sh Examples/c90/c90 Examples/c90/c90.20x40 0 30
+#
+root=$1; name=$2; ndepmin=$3; ndepmax=$4
+poly=$root.poly
+
+sqrt=sqrt/naive
+mag=$name.mag
+ndep=$ndepmin
+while [ $ndep -lt $ndepmax ]
+do
+  suf=`echo $ndep | awk '{printf("%03d\n", $1)}'`
+  echo "# Dependency $suf"
+  rat=$name.dep.rat.$suf
+  alg=$name.dep.alg.$suf
+  fact=$name.fact.$suf
+  $sqrt/algsqrt $alg $rat $poly > $fact
+  cat $fact
+  res=`grep "Failed" $fact`
+  if [ "X$res" == "X" ]
+  then
+      echo "Stopping, since we have probably finished"
+      exit
+  fi
+  ndep=`expr $ndep '+' 1`
+done
