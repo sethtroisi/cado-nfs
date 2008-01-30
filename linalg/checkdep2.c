0a227a955162faa1a8eab43e45d806a53b55e884
Paul Zimmermann <Paul.Zimmermann@loria.fr>
2008-01-30 20:55:11 +0000

    program to check dependencies (ker_raw) with respect to linalg input (small) [might be redundant with checkdep]
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cado/trunk@644 de4796e2-0b1e-0410-91b4-86bc23549317

---
+++ linalg/checkdep2.c
@@ -0,0 +1,139 @@
+/* program to check raw dependencies (ker_raw) against input to linalg
+   (matrix.small) */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+void
+check (char *matname, char *kername, int depnum, int verbose)
+{
+  FILE *mat, *ker;
+  unsigned long nrows, ncols, i, j, n, depr = 0, k, nonzero, totnonzero = 0;
+  uint64_t *vec, depw;
+
+  mat = fopen (matname, "r");
+  if (mat == NULL)
+    {
+      fprintf (stderr, "Error, cannot open file %s\n", matname);
+      exit (1);
+    }
+
+  ker = fopen (kername, "r");
+  if (ker == NULL)
+    {
+      fprintf (stderr, "Error, cannot open file %s\n", kername);
+      exit (1);
+    }
+
+  i = fscanf (mat, "%lu %lu\n", &nrows, &ncols);
+  if (i != 2)
+    {
+      fprintf (stderr, "Error, cannot read nrows/ncols\n");
+      exit (1);
+    }
+  if (verbose)
+    printf ("Matrix has %lu rows and %lu columns\n", nrows, ncols);
+
+  /* go to depnum dependency (one per line) */
+  if (verbose)
+    printf ("Checking dependency %d\n", depnum);
+  while (depnum > 0)
+    {
+      while (getc (ker) != '\n');
+      depnum --;
+    }
+
+  /* initialize vector with zeroes */
+  n = (ncols - 1) / 64 + 1;
+  vec = (uint64_t*) malloc (n * sizeof (uint64_t));
+  for (i = 0; i < n; i++)
+    vec[i] = 0;
+
+  while (nrows-- > 0)
+    {
+      /* read next bit from dependency */
+      if (depr == 0) /* read next 64-bit word */
+        fscanf (ker, "%lx ", &depw);
+      
+      /* read next row from matrix */
+      fscanf (mat, "%lu", &k); /* number of elements */
+      for (i = 0; i < k; i++)
+        {
+          fscanf (mat, " %lu", &j);
+          if (depw & 1)
+            vec[j >> 6] ^= (uint64_t) 1 << (j & 63);
+        }
+      fscanf (mat, "\n");
+      
+      depr --;
+      depw >>= 1;
+    }
+
+  for (i = 0; i < n; i++)
+    if (vec[i] != 0)
+      {
+        nonzero = 0;
+        for (j = 0; j < 64; j++)
+          {
+            if ((vec[i] >> j) & 1)
+              printf ("column %lu is non zero\n", 64 * i + j);
+            nonzero += (vec[i] >> j) & 1;
+          }
+        printf ("word %lu of vector is non zero (%lu bits)\n", i, nonzero);
+        totnonzero += nonzero;
+      }
+  if (totnonzero != 0)
+    printf ("Total %lu non-zero bits\n", totnonzero);
+
+  free (vec);
+  fclose (mat);
+  fclose (ker);
+}
+
+int
+main (int argc, char *argv[])
+{
+  char *matname = NULL;
+  char *kername = NULL;
+  int verbose = 0;
+  int depnum = 0;
+  
+  while (argc > 1 && argv[1][0] == '-')
+    {
+      if (argc > 2 && strcmp (argv[1], "-mat") == 0)
+        {
+          matname = argv[2];
+          argc -= 2;
+          argv += 2;
+        }
+      else if (argc > 2 && strcmp (argv[1], "-ker") == 0)
+        {
+          kername = argv[2];
+          argc -= 2;
+          argv += 2;
+        }
+      else if (argc > 2 && strcmp (argv[1], "-dep") == 0)
+        {
+          depnum = atoi (argv[2]);
+          argc -= 2;
+          argv += 2;
+        }
+      else if (argc > 1 && strcmp (argv[1], "-v") == 0)
+        {
+          verbose ++;
+          argc -= 1;
+          argv += 1;
+        }
+      else
+        {
+          fprintf (stderr, "Invalid option: %s\n", argv[1]);
+          exit (1);
+        }
+    }
+
+  check (matname, kername, depnum, verbose);
+
+  return 0;
+}
