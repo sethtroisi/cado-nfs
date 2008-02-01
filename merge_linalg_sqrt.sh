e5636514b9ed66d4a31c3f0f1120b797e4be3d53
Paul Zimmermann <Paul.Zimmermann@loria.fr>
2008-02-01 19:59:15 +0000

    forgot to add -forbw in merge: if somebody knows how to remove it from the command line (if one wants to run gauss), please go on!
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cado/trunk@685 de4796e2-0b1e-0410-91b4-86bc23549317

--- merge_linalg_sqrt.sh
+++ merge_linalg_sqrt.sh
@@ -55,7 +55,7 @@ fi
 echo "Performing merges"
 
 nb_merge_max=1000000
-argsa="-prune $prune -merge $nb_merge_max -mat $purged"
+argsa="-forbw -prune $prune -merge $nb_merge_max -mat $purged"
 argsa="$argsa -maxlevel $maxlevel -cwmax $cwmax -rwmax $rwmax $verbose"
 time $linalg/merge $argsa > $name.merge.his # 2> $name.merge.err
 echo "SIZE(merge.his): `ls -s $name.merge.his`"
