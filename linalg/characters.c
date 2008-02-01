d210f3dcc005107f75ad1b2f10765db2e886f1d2
Paul Zimmermann <Paul.Zimmermann@loria.fr>
2008-02-01 10:11:56 +0000

    characters.c: now use roots_mod_long from utils, removed old code (Pierrick, I let you fix the FIXME about my_read_polynomial...) Makefile: now use -O2 by default
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cado/trunk@663 de4796e2-0b1e-0410-91b4-86bc23549317

--- linalg/characters.c
+++ linalg/characters.c
@@ -69,7 +69,7 @@ parse_line (void *target, char *line, const char *tag, int *have,
 }
 
 // FIXME: why not using the one in polyfile?
-cado_poly *
+static cado_poly *
 my_read_polynomial (char *filename)
 {
   FILE *file;
@@ -183,16 +183,13 @@ my_read_polynomial (char *filename)
   return poly;
 }
 
-
-
 // Data structure for one algebraic prime and for a table of those.
 typedef struct {
   unsigned long prime;
   unsigned long root;
 } rootprime_t;
 
-
-int eval_char(long a, unsigned long b, rootprime_t ch) {
+static int eval_char(long a, unsigned long b, rootprime_t ch) {
   unsigned long ua, aux;
   int res;
 #if DEBUG >= 1
@@ -218,312 +215,36 @@ int eval_char(long a, unsigned long b, rootprime_t ch) {
   return res;
 }
 
-/* norm <- |f(a,b)| = |f[d]*a^d + ... + f[0]*b^d| */
-void
-eval_algebraic (mpz_t norm, mpz_t *f, int d, long a, unsigned long b)
+static void
+create_characters (rootprime_t * tabchar, int k, cado_poly pol)
 {
-  mpz_t B; /* powers of b */
-  mpz_init_set_ui (B, 1);
-  mpz_set (norm, f[d]);
-  while (d-- > 0)
-    {
-      mpz_mul_si (norm, norm, a);
-      mpz_mul_ui (B, B, b);
-      mpz_addmul (norm, f[d], B);
-    }
-  mpz_clear (B);
-  mpz_abs (norm, norm);
-}
-
-void
-handleOneKer(int *charval, int k, rootprime_t * tabchar, FILE * purgedfile, FILE * kerfile, int nlimbs, cado_poly pol)
-{  
-  int i, j, jj;
-  int ret;
-  unsigned long w;
-  long a;
-  unsigned long b;
-  char str[1024];
-  mpz_t prd, z;
-
-  mpz_init_set_ui(prd, 1);
-  mpz_init(z);
-
-  rewind(purgedfile);
-  fgets(str, 1024, purgedfile); // skip first line
-
-  for (i = 0; i < k; ++i) 
-    charval[i] = 1;
-
-  for (i = 0; i < nlimbs; ++i) {
-    ret = fscanf(kerfile, "%lx", &w);
-//    printf("%lx ", w);
-    ASSERT (ret == 1);
-    for (j = 0; j < GMP_NUMB_BITS; ++j) {
-      if (fgets(str, 1024, purgedfile)) {
-	if (w & 1UL) {
-	  ret = gmp_sscanf(str, "%ld %lu", &a, &b);
-	  ASSERT (ret == 2);
-	  for (jj = 0; jj < k; ++jj)
-	    charval[jj] *= eval_char(a, b, tabchar[jj]);
-
-	  eval_algebraic(z, pol->f, pol->degree, a, b);
-#if 0
-	  if (mpz_divisible_ui_p(z, 40009)) {
-	    fprintf(stderr, "%ld %lu\n", a, b);
-	    gmp_fprintf(stderr, "norm = %Zd\n", z);
-	  }
-#endif
-	  mpz_mul(prd, prd, z);
-	}
-      }
-      w >>= 1;
-    }
-  }
-
-#if 0
-  {
-    mpz_t q, r;
-    mpz_init(q);
-    mpz_init(r);
-    for(;;) {
-      long p;
-      int val;
-      ret = scanf("%ld", &p);
-      if (ret != 1) 
-	break;
-      val = 0;
-      while (mpz_divisible_ui_p(prd, p)) {
-	val++;
-	mpz_divexact_ui(prd, prd, p);
-      }
-      fprintf(stderr, "%d\n", val);
-    }
-    mpz_clear(q);
-    mpz_clear(r);
-  }
-#endif
-
-  mpz_sqrtrem(prd, z, prd);
-  if (mpz_cmp_ui(z, 0)!=0)
-    fprintf(stderr, "Problem: norm is not a square...\n");
-
-  mpz_clear(prd);
-  mpz_clear(z);
-}
-
-/* allocate a polynomial of degree d, and initialize it */
-mpz_t*
-alloc_poly (int d)
-{
-  mpz_t *f;
-  int i;
-  
-  f = (mpz_t*) malloc ((d + 1) * sizeof (mpz_t));
-  for (i = 0; i <= d; i++)
-    mpz_init (f[i]);
-  return f;
-} 
-
-/* free a polynomial of degree d */
-void
-poly_clear (mpz_t *f, int d)
-{
-  int i;
-  
-  for (i = 0; i <= d; i++)
-    mpz_clear (f[i]); 
-  free (f);
-} 
-
-/* f <- g */
-void
-copy_poly (mpz_t *f, mpz_t *g, int d)
-{
-  int i;
-  
-  for (i = 0; i <= d; i++)
-    mpz_set (f[i], g[i]);
-}   
-
-/* f <- diff(g,x) */
-void
-diff_poly (mpz_t *f, mpz_t *g, int d)
-{
-  int i;
-  
-  for (i = 0; i < d; i++)
-    mpz_mul_ui (f[i], g[i + 1], i + 1);
-}   
-/* Returns the number of roots of f(x) mod p, where f has degree d.
-   Assumes p does not divide disc(f).
-   In case where nb of root is 1, the root is put in r.
-   TODO: share this code with polyselect.c 
-   */
-int
-roots_mod (unsigned long *r, mpz_t *f, int d, const unsigned long p)
-{
-  mpz_t *fp, *g, *h;
-  int i, j, k, df, dg, dh;
-
-  /* the number of roots is the degree of gcd(x^p-x,f) */
-
-  /* we first compute fp = f/lc(f) mod p */
-  while (d >= 0 && mpz_divisible_ui_p (f[d], p))
-    d --;
-  ASSERT (d >= 0); /* f is 0 mod p: should not happen since otherwise p would
-		      divide N, because f(m)=N */
-  fp = alloc_poly (d);
-  mpz_set_ui (fp[d], p);
-  mpz_invert (fp[d], f[d], fp[d]); /* 1/f[d] mod p */
-  for (i = 0; i < d; i++)
-    {
-      mpz_mul (fp[i], f[i], fp[d]);
-      mpz_mod_ui (fp[i], fp[i], p);
-    }
-  mpz_set_ui (fp[d], 1); /* useless? */
-  df = d;
-
-  /* we first compute x^p mod fp; since fp has degree d, all operations can
-     be done with polynomials of degree < 2d */
-  g = alloc_poly (2 * d - 1);
-  h = alloc_poly (2 * d - 1);
-
-  /* initialize g to x */
-  mpz_set_ui (g[1], 1);
-  dg = 1;
-
-  mpz_set_ui (g[2], p);
-  k = mpz_sizeinbase (g[2], 2); /* number of bits of p: bit 0 to k-1 */
-
-  for (k -= 2; k >= 0; k--)
-    {
-      /* square g into h: g has degree dg -> h has degree 2*dg */
-      for (i = 0; i <= dg; i++)
-	for (j = i + 1; j <= dg; j++)
-	  if (i == 0 || j == dg)
-	    mpz_mul (h[i + j], g[i], g[j]);
-	  else
-	    mpz_addmul (h[i + j], g[i], g[j]);
-      for (i = 1; i < 2 * dg; i++)
-	mpz_mul_2exp (h[i], h[i], 1);
-      mpz_mul (h[0], g[0], g[0]);
-      mpz_mul (h[2 * dg], g[dg], g[dg]);
-      for (i = 1; i < dg; i++)
-	mpz_addmul (h[2 * i], g[i], g[i]);
-      dh = 2 * dg;
-
-      /* reduce mod p */
-      ASSERT (dh < 2 * d);
-      for (i = 0; i <= dh; i++)
-	mpz_mod_ui (h[i], h[i], p);
-      
-      /* multiply h by x if bit k of p is set */
-      if (p & (1 << k))
-	{
-	  for (i = dh; i >= 0; i--)
-	    mpz_swap (h[i+1], h[i]);
-	  mpz_set_ui (h[0], 0);
-	  dh ++;
-	  ASSERT (dh < 2 * d);
-	}
-
-      /* reduce mod fp */
-      while (dh >= d)
-	{
-	  /* subtract h[dh]*fp*x^(dh-d) from h */
-	  mpz_mod_ui (h[dh], h[dh], p);
-	  for (i = 0; i < d; i++)
-	    mpz_submul (h[dh - d + i], h[dh], fp[i]);
-	  /* it is not necessary to reduce h[j] for j < dh */
-	  dh --;
-          ASSERT (dh < 2 * d);
-	}
-
-      /* reduce h mod p and copy to g */
-      for (i = 0; i <= dh; i++)
-	mpz_mod_ui (g[i], h[i], p);
-      dg = dh;
-      ASSERT (dg < 2 * d);
-    }
-
-  /* subtract x */
-  mpz_sub_ui (g[1], g[1], 1);
-  mpz_mod_ui (g[1], g[1], p);
-
-  while (dg >= 0 && mpz_cmp_ui (g[dg], 0) == 0)
-    dg --;
-
-  /* take the gcd with fp */
-  while (dg > 0)
-    {
-      while (df >= dg)
-	{
-	  /* divide f by g */
-	  mpz_set_ui (h[0], p);
-	  mpz_invert (h[0], g[dg], h[0]); /* 1/g[dg] mod p */
-	  mpz_mul (h[0], h[0], fp[df]);
-	  mpz_mod_ui (h[0], h[0], p); /* fp[df]/g[dg] mod p */
-	  for (i = 0; i < dg; i++)
-	    {
-	      mpz_submul (fp[df - dg + i], h[0], g[i]);
-	      mpz_mod_ui (fp[df - dg + i], fp[df - dg + i], p);
-	    }
-	  df --;
-	  while (df >= 0 && mpz_cmp_ui (fp[df], 0) == 0)
-	    df --;
-	}
-      /* now d < dg: swap f and g */
-      for (i = 0; i <= dg; i++)
-	mpz_swap (fp[i], g[i]);
-      i = df;
-      df = dg;
-      dg = i;
-    }
-
-  /* if g=0 now, gcd is f, otherwise if g<>0, gcd is 1 */
-  if (mpz_cmp_ui (g[0], 0) != 0)
-    df = 0;
-
-  if (df == 1) {
-    mpz_set_ui (h[0], p);
-    mpz_invert(h[0], fp[1], h[0]);
-    mpz_mul(h[0], h[0], fp[0]);
-    mpz_neg(h[0], h[0]);
-    mpz_mod_ui(h[0], h[0], p);
-    *r = mpz_get_ui(h[0]);
-  }
-
-  poly_clear (g, 2 * d - 1);
-  poly_clear (h, 2 * d - 1);
-  poly_clear (fp, d);
-
-  return df;
-}
-
-void create_characters(rootprime_t * tabchar, int k, cado_poly pol) {
-  unsigned long p, r;
+  unsigned long p;
   int ret;
   int i = 0;
   mpz_t pp;
+  LONG *roots;
 
-  p = 1<<(pol->lpba+1);
+  mpz_init_set_ui (pp, 1 << (pol->lpba + 1));
+  roots = malloc (pol->degree * sizeof (LONG));
   
-  do {
-    mpz_init_set_ui(pp, p);
-    mpz_nextprime(pp, pp);
-    p = mpz_get_ui(pp);
-    ret = roots_mod(&r, pol->f, pol->degree, p);
-    if (ret != 1)
-      continue;
-    tabchar[i].prime = p;
-    tabchar[i].root = r;
-    i++;
-  } while (i < k);
+  do
+    {
+      mpz_nextprime (pp, pp);
+      p = mpz_get_ui (pp);
+      ret = roots_mod_long (roots, pol->f, pol->degree, p);
+      if (ret == 0)
+        continue;
+      tabchar[i].prime = p;
+      tabchar[i].root = roots[0];
+      i++;
+    }
+  while (i < k);
+
+  free (roots);
   mpz_clear(pp);
 }
 
-void
+static void
 readOneKer(mp_limb_t *vec, FILE *file, int nlimbs) {
   unsigned long w;
   int ret, i;
@@ -543,30 +264,8 @@ typedef struct {
   unsigned int limbs_per_col;
 } dense_mat_t;
 
-// str belongs to the [i, j]-th relation-set in the ker-ified matrix.
-void
-treatOneabpair(int **charval, char *str, int n, int k, int i, int j, mp_limb_t **ker, rootprime_t * tabchar)
-{
-    long a;
-    unsigned long b;
-    int ret, ii, jj;
-
-    ret = gmp_sscanf(str, "%ld %lu", &a, &b);
-#if DEBUG >= 1
-    fprintf(stderr, "str=[%s], a=%ld, b=%lu\n", str, a, b);
-#endif
-    ASSERT (ret == 2);
-    for (jj = 0; jj < n; ++jj) { // for each vector in ker...
-	if ((ker[jj][i]>>j) & 1UL) {
-	    for (ii = 0; ii < k; ++ii) { // for each character...
-		charval[jj][ii] *= eval_char(a, b, tabchar[ii]);
-	    }
-	}
-    }
-}
-
 // charmat is small_nrows x k
-void
+static void
 computeAllCharacters(char **charmat, int i, int k, rootprime_t * tabchar, long a, unsigned long b)
 {
     int j;
@@ -579,7 +278,7 @@ computeAllCharacters(char **charmat, int i, int k, rootprime_t * tabchar, long a
 // relfile is the big rough relation files;
 // purgedfile contains crunched rows, with their label referring to relfile;
 // indexfile contains the coding "row[i] uses rows i_0...i_r in purgedfile".
-void
+static void
 buildCharacterMatrix(char **charmat, int k, rootprime_t * tabchar, FILE *purgedfile, FILE *indexfile, FILE *relfile)
 {
     relation_t rel;
@@ -631,7 +330,8 @@ buildCharacterMatrix(char **charmat, int k, rootprime_t * tabchar, FILE *purgedf
     free(charbig);
 }
 
-void
+#if DEBUG >= 1
+static void
 printCompactMatrix(mp_limb_t **A, int nrows, int ncols)
 {
     int i, j;
@@ -657,7 +357,7 @@ printCompactMatrix(mp_limb_t **A, int nrows, int ncols)
     fprintf(stderr, "])");
 }
 
-void
+static void
 printTabMatrix(dense_mat_t *mat, int nrows, int ncols)
 {
     int i, j;
@@ -682,8 +382,9 @@ printTabMatrix(dense_mat_t *mat, int nrows, int ncols)
     }
     fprintf(stderr, "])");
 }
+#endif
 
-void
+static void
 handleKer(dense_mat_t *mat, rootprime_t * tabchar, FILE * purgedfile,
           mp_limb_t ** ker, /*int nlimbs, cado_poly pol, */
           FILE *indexfile, FILE *relfile)
@@ -829,6 +530,7 @@ int main(int argc, char **argv) {
   ker = (mp_limb_t **)malloc(n*sizeof(mp_limb_t *));
   ASSERT (ker != NULL);
   for (j = 0; j < n; ++j) {
+    fprintf (stderr, "Reading dependency %u\n", j);
     ker[j] = (mp_limb_t *) malloc (nlimbs*sizeof(mp_limb_t));
     ASSERT (ker[j] != NULL);
     readOneKer(ker[j], kerfile, nlimbs);
