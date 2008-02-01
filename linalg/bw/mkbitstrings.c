c4d90731cb17ca7bb22c4a0fa20de82ac4dc1dd5
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2008-02-01 16:11:02 +0000

    [bw] fixed mkbitstrings to accomodate new format ; a transpose, again...
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cado/trunk@681 de4796e2-0b1e-0410-91b4-86bc23549317

--- linalg/bw/mkbitstrings.c
+++ linalg/bw/mkbitstrings.c
@@ -1,7 +1,51 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
+#include <ctype.h>
 #include "manu.h"
+
+unsigned int read_hexstring(FILE *f, unsigned long * ptr, unsigned int n)
+{
+    unsigned int i = 0;
+    unsigned int ir = 0;
+    unsigned long x = 0UL;
+    unsigned long v;
+    char c;
+
+    for( ; isspace(c = fgetc(f)) ; );
+    ungetc(c, f);
+
+    for( ; i < n ; ) {
+        char c;
+        c = fgetc(f);
+
+        if (c >= '0' && c <= '9') v = c - '0';
+        else if (c >= 'a' && c <= 'f') v = c - 'a' + 10;
+        else if (c >= 'A' && c <= 'F') v = c - 'A' + 10;
+        else {
+            ungetc(c, f);
+            return i;
+        }
+
+        x |= v << ir;
+        i += 4;
+        ir += 4;
+        if (ir == ULONG_BITS) {
+            ir = 0;
+            *ptr++ = x;
+            x = 0UL;
+        }
+    }
+    if (i == n)
+        return n;
+
+    /* now n < i < n + 4. According to the specification, we check that
+     * the top i-n bits are zero, and return n.
+     */
+    v >>= n+4-i;
+    BUG_ON(v);
+    return n;
+}
 
 unsigned int write_hexstring(FILE * f, const unsigned long * ptr, unsigned int n)
 {
@@ -23,63 +67,54 @@ unsigned int write_hexstring(FILE * f, const unsigned long * ptr, unsigned int n
 
 int main(int argc, char * argv[])
 {
-    argc--, argv++;
-    FILE * ix[argc];
-    int i, j, nbits = 0;
-    unsigned long x;
-
-    for(i = 0 ; i < argc ; i++) {
-        ix[i] = fopen(argv[i], "r");
-        BUG_ON(ix == NULL);
-    }
+    FILE * f;
+    int i, j, k;
+    unsigned long * x;
+    unsigned int size = 0;
+    unsigned int alloc = 0;
     uint64_t w;
 
-#if 0
-    unsigned long foo[BITS_TO_WORDS(argc, ULONG_BITS)];
-    for(;;) {
-        memset(foo,0,sizeof(foo));
-        int fail = 0;
-        for(i = 0 ; i < argc ; i++) {
-            fscanf(ix[i], "%lu", &x);
-            fail += feof(ix[i]) || ferror(ix[i]);
-            foo[i / ULONG_BITS] ^= x << (i % ULONG_BITS);
+
+    /* read all data in memory */
+    alloc = 1024; x = malloc(alloc * sizeof(unsigned long));
+    f = fopen(argv[1], "r");
+    BUG_ON(f == NULL);
+    fprintf(stderr, "reading %s...", argv[1]); fflush(stderr);
+
+    for ( ; !(feof(f) || ferror(f)) ; size++) {
+        if (size >= alloc) {
+            /* fprintf(stderr, "realloc(%d)", alloc); fflush(stderr); */
+            alloc <<= 1;
+            x = realloc(x, alloc * sizeof(unsigned long));
         }
-        if (fail == argc)
+        read_hexstring(f, x + size, 64);
+        if (feof(f) || ferror(f))
             break;
-        BUG_ON(fail);
-        write_hexstring(stdout, foo, argc);
-        fputc('\n', stdout);
     }
-#else
-    /* format is one line per dependency */
-    for (i = 0; i < argc; i++)
-      {
+
+    fprintf(stderr, "read %d rows\n", size);
+
+    fclose(f);
+
+
+    for(i = 0 ; i < 64 ; i++) {
         j = 0;
         w = 0;
-        nbits = 0;
-        while (!(feof(ix[i]) || ferror(ix[i])))
-          {
-            fscanf(ix[i], "%lu", &x);
-            if (feof(ix[i]) || ferror(ix[i]))
-              break;
-            w |= x << j;
+        for(k = 0 ; k < size ; k++) {
+            w |= ((x[k] >> i) & 1UL) << j;
             j ++;
-            nbits ++;
-            if (j == 64)
-              {
-                printf ("%lx ", w);
+            if (j == 64) {
+                printf ("%016lx ", w);
                 j = 0;
                 w = 0;
-              }
-          }
+            }
+        }
         /* print last word */
         if (j > 0)
-          printf ("%lx\n", w);
-      }
-#endif
-    for(i = 0 ; i < argc ; i++) {
-        fclose(ix[i]);
+            printf ("%lx", w);
+        printf("\n");
     }
-    fprintf (stderr, "Printed %d bits per dependency\n", nbits);
+
+    fprintf (stderr, "Printed %d bits per dependency\n", size);
     return 0;
 }
