36e437c9c68c2f7e31ed1b8bf979b2ab5e86baff
Pierrick Gaudry <Pierrick.Gaudry@gmail.com>
2008-02-07 15:12:16 +0000

    - First attempt to do real lattice sieving (inside the q-lattice). No   buckets yet. - Fixed bugs inside small conversion functions
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cado/trunk@730 de4796e2-0b1e-0410-91b4-86bc23549317

--- sieve/las.c
+++ sieve/las.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
+#include <math.h>   // for ceiling, floor in cfrac
 #include "cado.h"
 #include "../utils/mod_ul.h"
 #include "fb.h"
@@ -15,6 +16,7 @@ typedef struct {
     uint64_t q;
     uint64_t rho;
     int32_t a0, b0, a1, b1;
+    int logI; // such that I = 1<<logI
     int bucket_size;    // should be around L1 cache size
     int nb_buckets;
     int bucket_limit;   // maximal number of bucket_reports allowed in one bucket.
@@ -178,10 +180,176 @@ sieve_slow (unsigned char *S, const factorbase_degn_t *fb,
                     S_ptr[i0] -= logp;
             }
         }
-//        fprintf(stderr, "done %lu\n", p);
         fb = fb_next (fb); // cannot do fb++, due to variable size !
     }
 }
+
+
+
+
+/*
+ * Algorithm by Franke and Kleinjung for lattice sieving of largish
+ * primes.
+ */
+
+typedef struct {
+    int32_t alpha, beta, gamma, delta;  // coordinates of the basis
+    uint32_t a, c;       // sieving offsets in x-coordinate
+    uint32_t b0, b1;     // thresholds for the branch inside siever
+} plattice_info_t;
+
+// Proposition 1 of [FrKl05]:
+// Compute a basis <(alpha, beta), (gamma, delta)> of the p-lattice
+// inside the q-lattice, such that
+//    beta, delta > 0
+//    -I < alpha <= 0 <= gamma < I
+//    gamma-alpha >= I
+// TODO: 
+// The algorithm is essentially the continued fraction of r/p. This can
+// probably be made faster by avoiding the ugly floating point divides.
+// Assuming all ak are close to one, iterated subtracts should do the
+// job.
+// For the moment, it seems to take negligible time in the whole process.
+void
+reduce_plattice(plattice_info_t *pli, const fbprime_t p, const fbprime_t r, const sieve_info_t * si)
+{
+    int32_t a0, a1, b0, b1, c0, c1, I, ak;
+    int k = 1;
+    I = si->I;
+    a0 = -p; a1 = 0;
+    b0 = r;  b1 = 1;
+    while ( abs(b0) >= I )  // Since I is a power of 2, this can be made fast
+    {
+        ak = (int32_t) floor( fabs( (double)a0 / (double)b0 ) ); // costly!
+        c0 = a0 + ak*b0;
+        c1 = a1 + ak*b1;
+        a0 = b0; a1 = b1;
+        b0 = c0; b1 = c1;
+        k++;
+    }
+    ak = (int32_t) ceil( fabs((double)(abs(a0)+1-I) / (double)b0) );
+    a0 += ak*b0; 
+    a1 += ak*b1; 
+    if ((k%2) == 1) {
+        pli->alpha = a0;
+        pli->beta = a1;
+        pli->gamma = b0;
+        pli->delta = b1;
+    } else {
+        pli->alpha = b0;
+        pli->beta = b1;
+        pli->gamma = a0;
+        pli->delta = a1;
+    } 
+    assert (pli->beta > 0);
+    assert (pli->delta > 0);
+    assert ((pli->alpha <= 0) && (pli->alpha > -I));
+    assert ((pli->gamma >= 0) && (pli->gamma < I));
+    assert (pli->gamma-pli->alpha >= I);
+    pli->a = pli->beta*I + pli->alpha;
+    pli->c = pli->delta*I + pli->gamma;
+    pli->b0 = -pli->alpha;
+    pli->b1 = I - pli->gamma;
+}
+
+
+// This version of the sieve implements the following:
+//   - naive line-sieving for small p
+//   - Franke-Kleinjung lattice-sieving for large p
+//   - write directly the reports in the sieve-array (no buckets)
+// WARNING: STILL AT WORK!
+static void
+sieve_random_access (unsigned char *S, const factorbase_degn_t *fb,
+        const sieve_info_t *si)
+{
+    fbprime_t p;
+    fbprime_t r, R;
+    unsigned int d;
+    unsigned char logp;
+    unsigned char *S_ptr;
+
+    // Loop over all primes in the factor base.
+    while (fb->p > 0) {
+        unsigned char nr;
+        p = fb->p;
+        logp = fb->plog;
+        unsigned long Is2modp = (unsigned long)(si->I>>1) % p;
+
+        for (nr = 0; nr < fb->nr_roots; ++nr) {
+            R = fb->roots[nr];
+            r = fb_root_in_qlattice(p, R, si);
+            if (r == 0) {
+                special_case_0();
+                continue;
+            } 
+            if (r == p) {
+                special_case_p();
+                continue;
+            } 
+            
+            const uint32_t I = si->I;
+            const uint32_t maskI = I-1;
+
+            // Branch to lattice-sieving for large p
+            if (p > I) {
+                plattice_info_t pli;
+                reduce_plattice(&pli, p, r, si);
+
+                // Start sieving from (0,0) which is I/2 in x-coordinate
+                uint32_t x;
+                x = (I>>1);
+                // TODO: to gain speed, by aligning the start of the
+                // loop, in assembly.
+                // Besides this small trick, gcc does a good job with
+                // this loop: no branching for the if(), and a dozen of
+                // instructions inside the while().
+                asm("## Inner sieving routine starts here!!!\n");
+                while (x < I*si->J) {
+                    uint32_t i;
+                    i = x & maskI;   // x mod I
+                    S[x] -= logp;
+                    if (i >= pli.b1)
+                        x += pli.a;
+                    if (i < pli.b0)
+                        x += pli.c;
+                }
+                asm("## Inner sieving routine stops here!!!\n");
+            }  else { 
+                unsigned long j;
+                for (j = 0; j < si->J; ++j) {
+                    long i0;
+                    // init i0 = -I/2 + ( (rj+I/2) mod p)
+                    {
+                        modulus_t m;
+                        residueul_t x, rr, jj;
+
+                        // TODO:
+                        // Assuming that p < 2^32 and we are on 64bit
+                        // machine, this can be improved a lot! No need to
+                        // use mod_ul lib.
+                        mod_initmod_ul(m, p);
+                        modul_initmod_ul(rr, r);
+                        if (j >= p) 
+                            modul_initmod_ul(jj, j % p);
+                        else
+                            modul_initmod_ul(jj, j);
+                        modul_mul(x, rr, jj, m);            // here there is a modulo p
+                        modul_add_ul(x, x, Is2modp, m);
+                        i0 = (long)modul_get_ul(x, m) - (long)(I>>1) ;
+                    }
+
+                    S_ptr = S + (j*I + (I>>1));
+                    // sieve one j-line
+                    for (; i0 < (I>>1); i0 += p)
+                        S_ptr[i0] -= logp;
+                }
+            }
+        }
+        fb = fb_next (fb); // cannot do fb++, due to variable size !
+    }
+}
+
+
 
 // Conversions between different representations for sieve locations:
 //   x          is the index in the sieving array. x in [0,I*J[
@@ -192,8 +360,8 @@ sieve_slow (unsigned char *S, const factorbase_degn_t *fb,
 void 
 xToIJ(int *i, int *j, int x, sieve_info_t * si)
 {
-    *i = (x % (si.I)) - (si.I >> 1);
-    *j = x / si.I;
+    *i = (x % (si->I)) - (si->I >> 1);
+    *j = x / si->I;
 }
 
 void
@@ -213,8 +381,8 @@ void
 xToAB(int64_t *a, uint64_t *b, int x, sieve_info_t * si)
 {
     int i, j;
-    i = (x % (si.I)) - (si.I >> 1);
-    j = x / si.I;
+    i = (x % (si->I)) - (si->I >> 1);
+    j = x / si->I;
     *a = i*si->a0 + j*si->a1;
     *b = i*si->b0 + j*si->b1;
 }
@@ -231,6 +399,7 @@ int main(int argc, char ** argv) {
 
 
     si.I = 1<<13;
+    si.logI = 13;
     si.J = 1<<12;
     si.q = 16777291;
     si.rho = 4078255;
@@ -251,7 +420,8 @@ int main(int argc, char ** argv) {
     fb = fb_read (argv[1], log_scale, 1);
     ASSERT_ALWAYS(fb != NULL);
 
-    sieve_slow(S, fb, &si);
+    //sieve_slow(S, fb, &si);
+    sieve_random_access(S, fb, &si);
 
     unsigned char min=255;
     int kmin = 0;
@@ -262,7 +432,7 @@ int main(int argc, char ** argv) {
             min = S[k];
         }
     
-    xToIJ(&i, &j, kmin, si);
+    xToIJ(&i, &j, kmin, &si);
     printf("Min of %d is obtained for (i,j) = (%d,%d)\n", min, i, j);
 
     int stat[256];
@@ -273,8 +443,6 @@ int main(int argc, char ** argv) {
     for (i = 0; i < 256; ++i)
         printf("[%d]%d ", i, stat[i]);
     printf("\n");
-
-
 
 }
 
