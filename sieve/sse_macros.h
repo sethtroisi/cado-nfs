873d5e0b3fe545030ac9c7c42b0f731c7fc6d90d
Pierrick Gaudry <Pierrick.Gaudry@gmail.com>
2008-03-13 11:06:05 +0000

    Fixed the problem with gcc-4.3 (sure?). Have to put a maybe suboptimal asm constraint.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cado/trunk@984 de4796e2-0b1e-0410-91b4-86bc23549317

--- sieve/sse_macros.h
+++ sieve/sse_macros.h
@@ -19,6 +19,12 @@ typedef uint64_t v2di __attribute__ ((vector_size (16)));
 
 static inline v2di cado_psrlq128(v2di x, v2di sh)
 {
-    __asm__("psrlq %2,%0" : "=x,x"(x) : "0,0"(x), "x,m"(sh));
+    // In theory, the "x,m" constraint is appropriate, but for some
+    // reason, gcc-4.3 does not like it (internal compiler error!).
+    // Let's stick to "x,x" constraint, which might be slightly worse
+    // in cases where the shift value changes frequently (this is not the
+    // case in our application).
+    // __asm__("psrlq %2,%0" : "=x,x"(x) : "0,0"(x), "x,m"(sh));
+    __asm__("psrlq %2,%0" : "=x"(x) : "0"(x), "x"(sh));
     return x;
 }
