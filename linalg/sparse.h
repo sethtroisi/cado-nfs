f43d682f4f1195c7a4bd2c52f04f9a397edf32d6
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2008-07-09 08:14:53 +0000

    [linalg] pending change, someday. I wanna know why unsigned types are frowned upon.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cado/trunk@1253 de4796e2-0b1e-0410-91b4-86bc23549317

--- linalg/sparse.h
+++ linalg/sparse.h
@@ -1,11 +1,21 @@
+#ifndef CADO_SPARSE_H_
+#define CADO_SPARSE_H_
 /* Data type to store column values: a 32-bit integer should be enough for
    most applications!
 */
 #include <stdint.h>
-#define INT int32_t // please, do not use an unsigned type!
+
+/* TODO: rename this to sparse_coeff_t when Francois is done with the mpi
+ * merge stuff.  */
+
+// please, do not use an unsigned type!
+// XXX [ET] Why ?
+#define INT int32_t
 
 extern void fprintRow(FILE *file, INT *row);
 extern INT * copyRow(INT *row);
 extern void removeWeight(INT **rows, int *wt, int i);
 extern void addWeight(INT **rows, int *wt, int i);
 extern void addRows(INT **rows, int i1, int i2, int len0);
+
+#endif  /* CADO_SPARSE_H_ */
