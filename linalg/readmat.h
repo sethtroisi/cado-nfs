ec6229d9d810a9e7e84712d67dd130ce2e8a4a78
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2008-11-24 16:27:01 +0000

    added some doc for readmat.h
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cado/trunk@1611 de4796e2-0b1e-0410-91b4-86bc23549317

--- linalg/readmat.h
+++ linalg/readmat.h
@@ -29,7 +29,25 @@ extern void read_matrix_header(FILE *file, sparse_mat_t mat);
 
 /* Reads only one row of the matrix. Reallocates the sparse_mat_t
  * structure if needed. The data is put at the location pointed to by
- * dst. The position of the next matrix row is returned. */
+ * dst. A pointer to the end of the written area is returned.
+ *
+ * BEWARE: dst must be a pointer within mat->data. However, besides that,
+ * the caller is free to place the data at an arbitrary place within that
+ * buffer. An example use could be:
+ *
+ * dst = mat->data;     // beginning of buffer
+ * dst0 = dst;          // save pointer.
+ * dst = read_matrix_row(f,m,dst,flag);       // read first row
+ * // note that the buffer is auto-extended to fit the matrix row.
+ * // dst now points after the first row
+ * // do something with [dst0..dst[
+ * dst0 = dst;
+ * dst = read_matrix_row(f,m,dst,flag);       // read second row
+ * // now say the first two rows are completely unneeded.
+ * // It is ok to trash the corresponding data.
+ * dst = mat->data
+ *
+ */
 extern unsigned int * read_matrix_row(FILE * file, sparse_mat_t mat, unsigned int * dst, int compact);
 
 extern void sparse_mat_init(sparse_mat_t dst);
