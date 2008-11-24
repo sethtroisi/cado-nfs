031b6e9c320aaa30a1cce146b0f2f5bc71349159
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2008-11-24 16:33:24 +0000

    two new files
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cado/trunk@1613 de4796e2-0b1e-0410-91b4-86bc23549317

---
+++ utils/random_generation.c
@@ -0,0 +1,51 @@
+#define _BSD_SOURCE     /* For random() */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <gmp.h>
+
+#include "manu.h"
+#include "random_generation.h"
+
+#ifdef  USE_GMP_RANDOM
+static gmp_randstate_t random_state;
+mp_limb_t myrand()
+{
+    return gmp_urandomb_ui(random_state, GMP_LIMB_BITS);
+}
+void myseed(unsigned long int x)
+{
+    gmp_randseed_ui(random_state, x);
+}
+#else
+mp_limb_t myrand()
+{
+    return random();
+}
+void myseed(unsigned long int x)
+{
+    srand(x);
+}
+#endif
+
+void myrand_area(void * p, size_t s)
+{
+    /* XXX This does not work with seeding ! */
+    FILE * f = fopen("/dev/urandom", "r");
+    BUG_ON(f == NULL);
+    size_t r = fread(p, 1, s, f);
+    BUG_ON(r != s);
+    fclose(f);
+}
+
+void setup_seeding(unsigned int s)
+{
+#ifdef  USE_GMP_RANDOM
+    gmp_randinit_mt(random_state);
+#endif
+    if (s == 0) {
+        myseed(time(NULL));
+    } else {
+        myseed(s);
+    }
+}
