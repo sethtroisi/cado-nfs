45e207b90694e91eabdf31da23ca573ca67f265d
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2008-12-06 16:35:28 +0000

    [utils] added a target for testing the root finding code.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cado/trunk@1626 de4796e2-0b1e-0410-91b4-86bc23549317

---
+++ utils/test-rootfinder-script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# This script eats a testbase on stdin.  Normally it gives nothing on
+# stdout, and gives progress report on stderr. If it prints anything,
+# that means bug.
+
+# testbases are built for examble by the magma script, which may be
+# modified.
+
+n=0
+nok=0
+while read in x ; do
+    y=$(./test-rootfinder `echo $x`)
+    read out z
+    if [ "$y" != "$z" ] ; then
+        (echo in $x ; echo exp $z ; echo got $y)
+    else
+        let nok+=1
+    fi
+    let n+=1
+    if [ `expr $n % 1000` = 0 ] ; then
+        echo -e "# $nok/$n\r" >&2
+    fi
+done
+
+# That's our return code.
+[ $nok = $n ]
