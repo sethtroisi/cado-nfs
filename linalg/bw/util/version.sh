aeca5986d4460ff87cda8a8c68a403d562f0d9ff
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2008-12-08 20:38:14 +0000

    [bw] added some shell protection.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cado/trunk@1656 de4796e2-0b1e-0410-91b4-86bc23549317

--- linalg/bw/util/version.sh
+++ linalg/bw/util/version.sh
@@ -3,14 +3,14 @@
 # This displays as much information as might be relevant, either within
 # an svn checkout or from my local git-svn managed branch.
 
-cd `dirname $0`
+cd "`dirname $0`"
 if [ -d .svn ] ; then
     svnversion
     svn status -q
 else
-    commit=`git-show --pretty=format:%h 2>/dev/null | head -1`
-    if [ $commit != "" ] ; then
-        ver=`git svn log | head -n2 | tail -n1 | cut -d\  -f1`
+    commit="`git-show --pretty=format:%h 2>/dev/null | head -1`"
+    if [ "$commit" != "" ] ; then
+        ver="`git svn log | head -n2 | tail -n1 | cut -d\  -f1`"
         echo -n "svn $ver -- git $commit"
         # git-diff --name-status is svn status -q
         if [ "`git-diff --name-status`" != "" ] ; then
