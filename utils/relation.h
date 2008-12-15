e577e3a0c7a5a1e4efb5139c6fe3d84b0259c25a
Paul Zimmermann <Paul.Zimmermann@loria.fr>
2008-12-15 22:12:52 +0000

    [utils/relation.c] moved fread_bug function from duplicates.c to relation.c, 	and rewrote using fgets(). Now duplicates is twice as fast.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cado/trunk@1697 de4796e2-0b1e-0410-91b4-86bc23549317

--- utils/relation.h
+++ utils/relation.h
@@ -1,6 +1,8 @@
 #ifndef RELATION_H_
 #define RELATION_H_
 
+#define STR_LEN_MAX 1024 /* maximal number of characters of a relation line */
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -9,6 +11,7 @@ extern "C" {
 extern void copy_rel(relation_t *Rel, relation_t rel);
 extern void clear_relation(relation_t *rel);
 extern int read_relation(relation_t *rel, const char *str);
+extern int fread_buf (char str[STR_LEN_MAX], FILE *file);
 extern int fread_relation(FILE *file, relation_t *rel);
 extern unsigned long findroot(long a, unsigned long b, unsigned long p);
 extern void computeroots(relation_t * rel);
