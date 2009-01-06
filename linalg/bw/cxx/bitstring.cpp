sethtroisi/cado-nfs

commit ca997ed2da878a93b270dbc0d136042631fa85dc

[bw] ugly fix to work around warning w/ gcc 4.1.x
git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cado/trunk@1797 de4796e2-0b1e-0410-91b4-86bc23549317

linalg/bw/cxx/bitstring.cpp
@@ -103,6 +103,44 @@ unsigned int read_hexstring(std::istream& is, unsigned long * ptr, unsigned int 
     return n;
 }
 
+unsigned int read_hexstring_u64(std::istream& is, uint64_t * ptr, unsigned int n)
+{
+    unsigned int i = 0;
+    unsigned int ir = 0;
+    uint64_t x = 0UL;
+    uint64_t v;
+    is >> ws;
+    for( ; i < n ; ) {
+        char c;
+        c = is.peek();
+        if (c >= '0' && c <= '9') v = c - '0';
+        else if (c >= 'a' && c <= 'f') v = c - 'a' + 10;
+        else if (c >= 'A' && c <= 'F') v = c - 'A' + 10;
+        else {
+            return i;
+        }
+        is.ignore(1);
+
+        x |= v << ir;
+        i += 4;
+        ir += 4;
+        if (ir == ULONG_BITS) {
+            ir = 0;
+            *ptr++ = x;
+            x = 0UL;
+        }
+    }
+    if (i == n)
+        return n;
+
+    /* now n < i < n + 4. According to the specification, we check that
+     * the top i-n bits are zero, and return n.
+     */
+    v >>= n+4-i;
+    BUG_ON(v);
+    return n;
+}
+
 unsigned int write_hexstring(std::ostream& o, const unsigned long * ptr, unsigned int n)
 {
     const char hxits[] = "0123456789abcdef";
@@ -110,6 +148,22 @@ unsigned int write_hexstring(std::ostream& o, const unsigned long * ptr, unsigne
     for( ; i < n ; ) {
         unsigned long v = *ptr++;
         for(unsigned int ir = 0 ; ir < ULONG_BITS && i < n ; ) {
+            o << hxits[v&0x0f];
+            if (!o) return i;
+            v >>= 4;
+            ir += 4;
+            i += 4;
+        }
+    }
+    return n;
+}
+unsigned int write_hexstring_u64(std::ostream& o, const uint64_t * ptr, unsigned int n)
+{
+    const char hxits[] = "0123456789abcdef";
+    unsigned int i = 0;
+    for( ; i < n ; ) {
+        uint64_t v = *ptr++;
+        for(unsigned int ir = 0 ; ir < 64 && i < n ; ) {
             o << hxits[v&0x0f];
             if (!o) return i;
             v >>= 4;

linalg/bw/cxx/bitstring.hpp
@@ -28,6 +28,8 @@
  */
 unsigned int read_hexstring(std::istream&, unsigned long *, unsigned int);
 unsigned int write_hexstring(std::ostream&, const unsigned long *, unsigned int);
+unsigned int read_hexstring_u64(std::istream&, uint64_t *, unsigned int);
+unsigned int write_hexstring_u64(std::ostream&, const uint64_t *, unsigned int);
 unsigned int read_hexstring(FILE * f, unsigned long *, unsigned int);
 unsigned int write_hexstring(FILE * f, const unsigned long *, unsigned int);
 

linalg/bw/cxx/gather.cpp
@@ -95,7 +95,7 @@ void add_file(const std::string& fn)
 		traits::assign(v[i], foo, 0);
 #endif
                 uint64_t x;
-                read_hexstring(f, (unsigned long*) &x, 64);
+                read_hexstring_u64(f, &x, 64);
                 v[i].p ^= x;
 	}
 	f >> ws;
@@ -150,7 +150,7 @@ void shipout(scalar_t * v, string const & s, bool check=true)
 			v[i] -= globals::modulus;
 		traits::print(wf,v[i]) << "\n";
         */
-                write_hexstring(wf, (const unsigned long *) &(v[i].p), 64);
+                write_hexstring_u64(wf, &(v[i].p), 64);
                 wf << "\n";
 	}
 	wf.close();

linalg/purge.c
@@ -625,12 +625,13 @@ renumber (int *nprimes, hashtable_t *H, char *sos)
           if (H->hashcount[i] == 1 && count ++ < 10)
             {
               if (GET_HASH_R(H,i) == minus2)
-                fprintf (stderr, "Warning: singleton rational prime %llu\n",
-                         (unsigned long long) GET_HASH_P(H,i));
+                fprintf (stderr, "Warning: singleton rational prime %"
+                        PRIu64 "\n",
+                         GET_HASH_P(H,i));
               else
-                fprintf (stderr, "Warning: singleton algebraic ideal (%llu,%llu)\n",
-                         (unsigned long long) GET_HASH_P(H,i),
-                         (unsigned long long) GET_HASH_R(H,i));
+                fprintf (stderr, "Warning: singleton algebraic ideal (%"
+                        PRIu64",%"PRIu64")\n",
+                         GET_HASH_P(H,i), GET_HASH_R(H,i));
             }
           H->hashcount[i] = nb++;
           if (fsos != NULL)

commit a52f5101c726b8ed592e8baaf2ba9ce9d64b6fe3

[bw] fixed bug introduced in r1797.
git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cado/trunk@1801 de4796e2-0b1e-0410-91b4-86bc23549317

linalg/bw/cxx/bitstring.cpp
@@ -124,7 +124,7 @@ unsigned int read_hexstring_u64(std::istream& is, uint64_t * ptr, unsigned int n
         x |= v << ir;
         i += 4;
         ir += 4;
-        if (ir == ULONG_BITS) {
+        if (ir == 64) {
             ir = 0;
             *ptr++ = x;
             x = 0UL;