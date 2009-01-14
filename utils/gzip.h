47e093689c7a838b8bc85344bbaea7982b5820d1
Alexander Kruppa <akruppa@gmail.com>
2009-01-14 15:12:43 +0000

    Added the usual boilerplate
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cado/trunk@1825 de4796e2-0b1e-0410-91b4-86bc23549317

--- utils/gzip.h
+++ utils/gzip.h
@@ -1,5 +1,16 @@
+#ifndef GZIP_H_
+#define GZIP_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 extern int is_gzip(const char * s);
 extern FILE *gzip_open(const char * s, const char * t);
 extern void gzip_close(FILE *ofile, const char * s);
 
+#ifdef __cplusplus
+}
+#endif
 
+#endif	/* GZIP_H_ */
