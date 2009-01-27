144f059f5d700469d30ae88adf4bd1c833e020f2
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2009-01-27 14:15:42 +0000

    [bwc] make check works w/ mpich2 & openmpi.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cado/trunk@1856 de4796e2-0b1e-0410-91b4-86bc23549317

--- linalg/bwc/checks.sh
+++ linalg/bwc/checks.sh
@@ -4,6 +4,19 @@
 
 # Note that the typical failure situation yields a job which never
 # finishes.
+
+if [ -f Makefile.local ] ; then
+    source Makefile.local
+    if [ -n "$MPI" ] ; then
+        export PATH="$MPI:$PATH"
+    fi
+fi
+
+# This is for mpich2 only.
+if [ -z "$HYDRA_HOST_FILE" ] ; then
+    export HYDRA_USE_LOCALHOST=1
+fi
+
 ntests=0
 while read cmd ; do
     let ntests+=1
