cba48053015f03109cc217f3e4c7b55422146fdb
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2009-02-13 10:28:51 +0000

    [main] changed main version string.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cado/trunk@1895 de4796e2-0b1e-0410-91b4-86bc23549317

---
+++ version.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# This displays as much information as might be relevant, either within
+# an svn checkout or from my local git-svn managed branch.
+
+cd "`dirname $0`"
+if [ -d .svn ] ; then
+    svnversion
+    svn status -q
+else
+    commit="`git-show --pretty=format:%h 2>/dev/null | head -1`"
+    if [ "$commit" != "" ] ; then
+        # ver="`git svn log | head -n2 | tail -n1 | cut -d\  -f1`"
+        # much faster
+        perl='while (<>) { /trunk@(\d+)/ && print "$1\n"; }'
+        ver=`git-cat-file commit refs/remotes/trunk | perl -e "$perl"`
+        echo -n "svn $ver -- git $commit"
+        # git-diff --name-status is svn status -q
+        if [ "`git-diff --name-status`" != "" ] ; then
+            echo " +mods"
+        else
+            echo
+        fi
+    else
+        echo "exported"
+    fi
+fi
