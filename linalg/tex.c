cf02adff3b092edf93e23fdc71d4f191fc86b646
Paul Zimmermann <Paul.Zimmermann@loria.fr>
2009-02-16 16:40:50 +0000

    new file tex.c containing all TeX routines
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cado/trunk@1900 de4796e2-0b1e-0410-91b4-86bc23549317

---
+++ linalg/tex.c
@@ -0,0 +1,101 @@
+/* routines for TeX output, extracted from sparse_mat.c */
+
+void
+dclistTex(FILE *file, dclist dcl)
+{
+    fprintf(stderr, "\\begin{array}{r}\n");
+    while(dcl != NULL){
+      fprintf(file, "%ld\\\\\n", (long int) dcl->j);
+	dcl = dcl->next;
+    }
+    fprintf(stderr, "\\end{array}\n");
+}
+
+void
+matrix2tex(sparse_mat_t *mat)
+{
+    int i, j, k, *tab;
+
+    tab = (int *)malloc(mat->ncols * sizeof(int));
+    fprintf(stderr, "$$\\begin{array}{l|");
+    for(j = 0; j < mat->ncols; j++)
+	fprintf(stderr, "c");
+    fprintf(stderr, "}\n");
+    for(i = 0; i < mat->nrows; i++){
+	memset(tab, 0, mat->ncols * sizeof(int));
+	for(k = 0; k < lengthRow(mat, i); k++)
+	    tab[cell(mat, i, k)] = 1;
+	fprintf(stderr, "R_{%d}", i);
+	for(j = 0; j < mat->ncols; j++)
+	    fprintf(stderr, "& %d", tab[j]);
+	fprintf(stderr, "\\\\\n");
+    }
+    fprintf(stderr, "\\hline\n");
+    fprintf(stderr, "\\text{weight}");
+    for(j = 0; j < mat->ncols; j++)
+	fprintf(stderr, "& %d", mat->wt[GETJ(mat, j)]);
+    fprintf(stderr, "\\\\\n");
+    fprintf(stderr, "\\end{array}$$\n");
+    free(tab);
+}
+
+void
+Sparse2Tex(sparse_mat_t *mat)
+{
+    int j;
+    int32_t k;
+
+    fprintf(stderr, "\\end{verbatim}\n");
+    matrix2tex(mat);
+#ifndef USE_MARKOWITZ
+    texSWAR(mat);
+#endif
+    fprintf(stderr, "$$\\begin{array}{|c|");
+    for(j = 0; j < mat->ncols; j++)
+	fprintf(stderr, "r|");
+    fprintf(stderr, "}\\hline\n");
+    fprintf(stderr, "j");
+    for(j = 0; j < mat->ncols; j++)
+	fprintf(stderr, "& %d", j);
+    fprintf(stderr, "\\\\\\hline\n");
+    fprintf(stderr, "W[j]");
+    for(j = 0; j < mat->ncols; j++)
+	fprintf(stderr, "& %d", mat->wt[GETJ(mat, j)]);
+    fprintf(stderr, "\\\\\\hline\n\\end{array}$$\n");
+#ifndef USE_COMPACT_R
+    fprintf(stderr, "\\begin{verbatim}\n");
+    for(j = 0; j < mat->ncols; j++){
+	if(mat->R[GETJ(mat, j)] == NULL)
+	    continue;
+	fprintf(stderr, "  R[%d]=", j);
+	for(k = 1; k <= mat->R[GETJ(mat, j)][0]; k++)
+          fprintf(stderr, " %ld", (long int) mat->R[GETJ(mat, j)][k]);
+	fprintf(stderr, "\n");
+    }
+#endif
+}
+
+void
+texSWAR(sparse_mat_t *mat)
+{
+    int w;
+
+    fprintf(stderr, "$$\\begin{array}{|");
+    for(w = 0; w <= mat->cwmax; w++)
+	fprintf(stderr, "r|");
+    fprintf(stderr, "|r|}\\hline\n");
+    for(w = 0; w <= mat->cwmax+1; w++){
+	fprintf(stderr, "S[%d]", w);
+	if(w < mat->cwmax+1)
+	    fprintf(stderr, "&");
+    }
+    fprintf(stderr, "\\\\\\hline\n");
+    for(w = 0; w <= mat->cwmax+1; w++){
+	dclistTex(stderr, mat->S[w]->next);
+	if(w < mat->cwmax+1)
+            fprintf(stderr, "&");
+    }
+    fprintf(stderr, "\\\\\\hline\n");
+    fprintf(stderr, "\\end{array}$$\n");
+}
+
