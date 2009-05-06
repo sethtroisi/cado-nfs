c465a922907e77ad7cce73bf95afe9f040bf7065
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2009-05-06 22:42:09 +0000

    accomodate git-only checkouts
    do not try to print an svn rev number if it can't be obtained. I use this
    for pushing sources to g5k.
    
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cado/trunk@2103 de4796e2-0b1e-0410-91b4-86bc23549317

--- scripts/version.sh
+++ scripts/version.sh
@@ -13,8 +13,12 @@ else
         # ver="`git svn log | head -n2 | tail -n1 | cut -d\  -f1`"
         # much faster
         perl='while (<>) { /trunk@(\d+)/ && print "$1\n"; }'
-        ver=`git show refs/remotes/trunk | perl -e "$perl"`
-        echo -n "svn $ver -- git $commit"
+        ver=`git show refs/remotes/trunk 2>/dev/null | perl -e "$perl"`
+        if [ "$ver" ] ; then
+            echo -n "svn $ver -- git $commit"
+        else
+            echo -n "git $commit"
+        fi
         # git diff --name-status is svn status -q
         if [ "`git diff --name-status`" != "" ] ; then
             echo " +mods"
