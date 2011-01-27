ae774a71d4d6a40be1fc0b2e8411bc1008d8da0c
Lionel Muller <Lionel.Muller@loria.fr>
2011-01-27 14:05:00 +0000

    bench for polyselect2
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cado-nfs/trunk@701 3eaf19af-ecc0-40f6-b43f-672aa0c71c71

---
+++ scripts/cadopoly2.pl
@@ -0,0 +1,144 @@
+#!/usr/bin/perl -w
+
+# Usage: cadopoly2.pl $paramfile1 $paramfile2 ...
+#        read paramfiles and run polyselect2
+#        the paramfiles must contain: different name!, n, machines, params polyselect2 ...
+#
+# TODO: generate input
+
+use Cwd qw(abs_path);
+use File::Basename;
+use lib abs_path(dirname(dirname($0)));
+use cadofct;
+use POSIX qw(ceil);
+use strict;
+use warnings;
+
+# input
+print "$0 @ARGV\n";
+my @files;
+while (@ARGV) {
+  push @files, shift @ARGV;
+}
+@files = sort @files;
+
+# recover
+my @done = <*.polysel_done>;
+my @recover = <*.polysel_jobs>;
+
+# polyselect
+my %link_name_params;
+my $old_prefix;
+my $file;
+while (@files) {
+  $file = shift @files;
+  read_param(\%param, { strict => 1 }, "$file");
+  $link_name_params{"$param{'name'}"} = $file;
+  next if (@done);
+  if (@recover) {
+    next if ($recover[0] ne basename("$param{'prefix'}.polysel_jobs"));
+    shift @recover;
+  }
+  rename ( "$old_prefix.polysel_jobs", "$param{'prefix'}.polysel_jobs" )
+    if ($old_prefix);
+  read_machines();
+  open FILE, "> $param{'prefix'}.n"
+    or die "Cannot open `$param{'prefix'}.n' for writing: $!.\n";
+  print FILE "n: $param{'n'}\n";
+  close FILE;
+  if (@files) {
+    do_polysel_bench();
+  } else {
+    do_polysel_bench(1);
+    unlink "$param{'prefix'}.polysel_jobs";
+    open FILE, "> $param{'prefix'}.polysel_done"
+      or die "Cannot open `$param{'prefix'}.polysel_done' for writing: $!.\n";
+    close FILE;
+  }
+  $old_prefix = $param{'prefix'};
+}
+
+# info kjout
+banner "Info kjout";
+my @kjout_files;
+my $time;
+my $phase;
+my %h = ( Total  => 0 );
+foreach my $name (sort keys %link_name_params) {
+  my (@Emax, $Emin, $Emoy);
+  my $best;
+  my (@min, @max, @total);
+  my $total;
+
+  opendir DIR, "."
+    or die "Cannot open directory `.': $!\n";
+  @kjout_files = grep /^$name\.kjout\.[\de.]+-[\de.]+$/, readdir DIR;
+  closedir DIR;
+  my $size = scalar(@kjout_files);
+
+  foreach my $f (@kjout_files) {
+    open FILE, "< $f"
+      or die "Cannot open `$f' for reading: $!.\n";
+    my $murphy;
+    while (<FILE>) {
+      if ( /#\s+(\S+)\s+phase took\s+([\d.]+)s/ ) {
+        $time = $2; 
+        $total += $time;
+        $phase = $1;
+        $total[$h{$phase}] += $time;
+        $min[$h{$phase}] = $time if (!defined $min[$h{$phase}] || $time < $min[$h{$phase}]);
+        $max[$h{$phase}] = $time if (!defined $max[$h{$phase}] || $time > $max[$h{$phase}]);
+      }
+      $murphy = $_ if /Murphy/;
+    }
+    close FILE;
+		
+    next unless $murphy;
+    $murphy =~ /\)=(.+)$/;
+    $Emoy += $1;
+    if (!defined $Emax[2] || $1 > $Emax[2]) {
+      if (!defined $Emax[1] || $1 > $Emax[1]) {
+        $Emax[2] = $Emax[1];
+        if (!defined $Emax[0] || $1 > $Emax[0]) {
+          $Emax[1] = $Emax[0];
+          $Emax[0] = $1;
+          $best = $f;
+        } else {
+          $Emax[1] = $1;
+        }
+      } else {
+        $Emax[2] = $1;
+      }
+    }
+    $Emin = $1 if (!defined $Emin || $1 < $Emin);
+  }
+		
+  die "No polynomial was found for configuration $name!\n"
+    unless defined $Emax[0];
+
+  $Emoy = $Emoy / $size;
+  my $ampl = $Emax[0]-$Emin;
+  $file = $link_name_params{"$name"};
+  read_param(\%param, { strict => 1 }, "$file");
+  my $size_expect = ceil (($param{'kjadmax'} -  $param{'kjadmin'} ) /
+                          $param{'kjadrange'});
+  die "kjout missing for configuration $name! ($size on $size_expect)\n"
+    if ( $size != $size_expect );
+
+  # print infos
+  info "Params: P $param{'kjP'} maxnorm $param{'kjmaxnorm'} kmax $param{'kjkmax'} ".
+       "degree $param{'degree'}\n".
+       "        adrange $param{'kjadrange'} admax $param{'kjadmax'} ".
+       "incr $param{'kjincr'}\n";
+  $tab_level++;
+  info "The best polynomial for $name is from `".basename($best)."'\n".
+       "Emax = $Emax[0] - Emax2 = $Emax[1] - Emax3 = $Emax[2]\n".
+       "Emoy = \033[01;31m$Emoy\033[01;00m - ".
+       "Emin = $Emin - ". "Ampl = $ampl\n".
+       "Time min: $min[0]"."s\n".
+       "Time max: $max[0]"."s\n".
+       "Time total: $total"."s,  \033[01;31m".format_dhms($total)."\033[01;00m\n";
+  print "\n";
+  $tab_level--;
+}
+			
