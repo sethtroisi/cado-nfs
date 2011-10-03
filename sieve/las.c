fc986f8ff64171f06a2519a9f575baf951315e5e
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2011-10-03 08:54:08 +0200

    created las-config.h

--- sieve/las.c
+++ sieve/las.c
@@ -13,111 +13,20 @@
 #include "trialdiv.h"
 #include <pthread.h>
 #include "mpz_poly.h"
-
-#ifdef HAVE_SSE2
-#define SSE_NORM_INIT
-#endif
-
-#ifdef SSE_NORM_INIT
-#include <emmintrin.h>
-#endif
-
-#ifndef HAVE_LOG2
-static double
-log2 (double x)
-{
-  return log (x) / log (2.0);
-}
-#endif
-
-/* As its name says, this is a ugly hack that initializes all lognorms to the
-   maximal value (255) on the rational side. But it seems to work well, and to
-   miss only about 7% to 8% relations wrt a more accurate estimation. */
-//#define UGLY_HACK
-
-/* number of bits used to estimate the norms */
-#define NORM_BITS 10
-
-/* define PROFILE to keep certain function from being inlined, in order to
-   make them show up on profiler output */
-//#define PROFILE
-
-/* Trick to discard lognorms that will probably lead to non L-smooth
-   cofactors. Disabled for now since it requires accurate sieving
-   (in particular by prime powers and bad primes). */
-// #define COFACTOR_TRICK
-
-/* This triggers code which fills bucket in several passes, one for each
- * congruence class mod 2 (three such, the trivial one leading to
- * spurious reports). It's currently only part of the story, and at the
- * moment it is almost neutral in terms of efficiency (small slowdown
- * because of access pattern).  But it's the way to go if one wants to
- * support I=16. There are many other places where changes must be made.
- * This particular flag affects only the treatment of the ``bucket
- * sieved'' primes, not the pattern-sieved, or small-sieved.
- */
-#define MOD2_CLASSES_BS 0       /* define to 0 or 1 */
-
-/* default sieve region side is 2^DEFAULT_I */
-#define DEFAULT_I 12
-
-/* default bucket region: 2^16 = 64K == close to L1 size, but this is the
-   (current) largest possible value, otherwise bucket.h must be changed,
-   since it stores positions on 16 bits */
-#ifndef LOG_BUCKET_REGION
-#define LOG_BUCKET_REGION 16
-#endif
-
-#if LOG_BUCKET_REGION > 16
-#error "Too large LOG_BUCKET_REGION, please adapt bucket.h first"
-#endif
-
-/* This flag is necessary to support I=16. Otherwise it's a useless
- * burden
- */
-#define SUPPORT_I16
-
-/* Define SKIP_GCD3 to skip updates where 3 divides gcd(i,j) in the
-   bucket sieving phase. Slightly slower than not skipping them
-   in single-thread mode, but might be useful for multi-threading,
-   or when memory is tight */
-// #define SKIP_GCD3
-
-/* These parameters control the size of the buckets.
- * The number of updates that a bucket can accumulate is estimated as
- *   (loglog(factor base bound) - loglog(bucket sieving threshold))
- *     * BUCKET_LIMIT_FACTOR * I * J + BUCKET_LIMIT_ADD 
- * We don't store updates where 2 divides gcd(i,j) which reduces the number 
- * of updates to about (1-1/4)=3/4, so 0.8 should be safe.
- * If we don't store updates where 3 divides gcd(i,j) either, their number
- * is reduced to about (1-1/4)*(1-1/9)=2/3, then 0.7 should be ok
- */
-
-#ifndef BUCKET_LIMIT_FACTOR
-#ifdef SKIP_GCD3
-#define BUCKET_LIMIT_FACTOR 0.7
-#else
-#define BUCKET_LIMIT_FACTOR 0.8
-#endif
-#endif
-
-#ifndef BUCKET_LIMIT_ADD
-#define BUCKET_LIMIT_ADD 0
-#endif
+#include "las-config.h"
 
 #define LOG_SCALE 1.4426950408889634 /* 1/log(2) to 17 digits, rounded to
                                         nearest. This is enough to uniquely
                                         identify the corresponding IEEE 754
                                         double precision number */
 
-/* Guard for the logarithms of norms, so that the value does not wrap around
-   zero due to roundoff errors. */
-#define GUARD 4
-
-/* GUARD+LOG_MAX should be as near as possible from 256, to get more accuracy
-   in the norm computations, but not too much, otherwise a norm might be
-   rounded to zero. */
-#define LOG_MAX (255.9 - (double) GUARD)
+#ifndef HAVE_LOG2
+static double
+MAYBE_UNUSED log2 (double x)
+{
+  return log (x) / log (2.0);
+}
+#endif
 
 /* define TRACE_K, and exactly one of the trace_* values to something
  * non-zero, in order to get tracing information on a particular
@@ -155,20 +64,10 @@ static inline int trace_on_spot_ij(int i, unsigned int j) {
     return i == trace_ij.i && j == trace_ij.j;
 }
 
-
 /* Define CHECK_UNDERFLOW to check for underflow when subtracting
    the rounded log(p) from sieve array locations */
 //#define CHECK_UNDERFLOW
 
-/* Some functions should not be inlined when we profile or it's hard or
-   impossible to tell them apart from the rest in the profiler output */
-#ifdef PROFILE
-#define NOPROFILE_INLINE
-#define NOPROFILE_STATIC
-#else
-#define NOPROFILE_INLINE static inline
-#define NOPROFILE_STATIC static
-#endif
 
 /* uintmax_t is guaranteed to be larger or equal to uint64_t */
 #define strtouint64(nptr,endptr,base) (uint64_t) strtoumax(nptr,endptr,base)
