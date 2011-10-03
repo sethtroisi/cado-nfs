2c764e46dfa572168aaf0995b4f535fb60a3954e
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2011-10-03 09:00:34 +0200

    created las-qlattice.[ch]

--- sieve/las.c
+++ sieve/las.c
@@ -21,6 +21,7 @@
 #include "las-norms.h"
 #include "las-unsieve.h"
 #include "las-arith.h"
+#include "las-qlattice.h"
 
 #define LOG_SCALE 1.4426950408889634 /* 1/log(2) to 17 digits, rounded to
                                         nearest. This is enough to uniquely
@@ -270,7 +271,6 @@ sieve_info_clear (sieve_info_ptr si)
   cado_poly_clear(si->cpoly);
 }
 
-
 // Compute the root r describing the lattice inside the q-lattice
 // corresponding to the factor base prime (p,R).
 // Formula: r = - (a1-R*b1)/(a0-R*b0) mod p
@@ -285,77 +285,6 @@ sieve_info_clear (sieve_info_ptr si)
    So projective roots are stored as their reciprocal, and have p added
    to signal the fact that it's a reciprocal value.
 */
-
-NOPROFILE_INLINE fbprime_t
-fb_root_in_qlattice (const fbprime_t p, const fbprime_t R,
-        const uint32_t invp, sieve_info_srcptr si)
-{
-  int64_t aux1, aux2;
-  uint64_t u, v;
-  fbprime_t add;
-
-    /* Handle powers of 2 separately, REDC doesn't like them */
-    if (UNLIKELY(p % 2 == 0))
-      {
-	fbprime_t u, v;
-	ASSERT(p == (p & -p)); /* Test that p is power of 2 */
-	if (R < p) /* Root in a,b-plane is non-projective */
-	  {
-	    u = R * si->b1 - si->a1;
-	    v = si->a0 - R * si->b0;
-	  }
-	else /* Root in a,b-plane is projective */
-	  {
-	    u = si->b1 - (R - p) * si->a1;
-	    v = (R - p) * si->a0 - si->b0;
-	  }
-
-	if (v % 2 != 0)
-	  {
-	    /* root in i,j-plane is non-projective */
-	    v = invmod_po2 (v);
-	    return (u * v) & (p - 1);
-	  }
-	else
-	  {
-	    /* root in i,j-plane is projective */
-	    u = invmod_po2 (u);
-	    return p + ((u * v) & (p - 1));
-	  }
-      }
-
-    // Use Signed Redc for the computation:
-    // Numerator and denominator will get divided by 2^32, but this does
-    // not matter, since we take their quotient.
-
-    if (LIKELY(R < p)) /* Root in a,b-plane is affine */
-      {
-	aux1 = ((int64_t)R)*((int64_t)si->b1) - ((int64_t)si->a1);
-	aux2 = ((int64_t)si->a0) - ((int64_t)R)*((int64_t)si->b0);
-      }
-    else /* Root in a,b-plane is projective */
-      {
-	aux1 = ((int64_t)si->b1) - ((int64_t)(R - p))*((int64_t)si->a1);
-	aux2 = ((int64_t)(R - p))*((int64_t)si->a0) - ((int64_t)si->b0);
-      }
-    u = redc_32(aux1, p, invp); /* 0 <= u < p */
-    v = redc_32(aux2, p, invp); /* 0 <= den < p */
-
-    add = 0;
-    if (UNLIKELY(!invmod_REDC(&v, p)))
-      {
-	/* root in i,j-plane is projective */
-	if (UNLIKELY(!invmod_REDC(&u, p)))
-          {
-            fprintf (stderr, "Error, root in (i,j)-plane is projective\n");
-            exit (EXIT_FAILURE); /* Should never happen! */
-          }
-	add = p;
-      }
-    u *= v;
-    return (fbprime_t) redc_u32 (u, p, invp) + add;
-}
-
 
 
 /*
@@ -2456,96 +2385,6 @@ void test_divisible_x (const fbprime_t p, const unsigned long x,
                 p, x, n, side, i, j, v);
   
   mpz_clear (v);
-}
-
-
-/*********************** norm computation ************************************/
-
-/* check that the double x fits into an int32_t */
-#define fits_int32_t(x) \
-  ((double) INT32_MIN <= (x)) && ((x) <= (double) INT32_MAX)
-
-/* return non-zero when the reduced lattice has entries that do not
-   fit into int32_t, otherwise return 0 */
-static int
-SkewGauss (sieve_info_ptr si, double skewness)
-{
-  double a[2], b[2], q, maxab0, maxab1;
-
-  a[0] = (double) si->q;
-  ASSERT_ALWAYS(a[0] < 9007199254740992.0); /* si->q should be less than 2^53
-                                               so that a[0] is exact */
-  b[0] = 0.0;
-  a[1] = (double) si->rho;
-  b[1] = skewness;
-  ASSERT(b[1] != 0);
-  while (1)
-    {
-      /* reduce vector (a[0], b[0]) with respect to (a[1], b[1]) */
-      q = (a[0] * a[1] + b[0] * b[1]) / (a[1] * a[1] + b[1] * b[1]);
-      q = rint (q);
-      if (q == 0.0)
-        break;
-      a[0] -= q * a[1];
-      b[0] -= q * b[1];
-
-      /* reduce vector (a[1], b[1]) with respect to (a[0], b[0]) */
-      q = (a[0] * a[1] + b[0] * b[1]) / (a[0] * a[0] + b[0] * b[0]);
-      q = rint (q);
-      if (q == 0.0)
-        break;
-      a[1] -= q * a[0];
-      b[1] -= q * b[0];
-    }
-  if (!(fits_int32_t(a[0]) && fits_int32_t(b[0] / skewness) &&
-	fits_int32_t(a[1]) && fits_int32_t(b[1] / skewness)))
-    return 1;
-  /* now b[0], b[1] should be of the form i*skewness, but this might not be
-     exact due to rounding errors, thus we round them to the nearest integer */
-  maxab0 = fabs (a[0]) > fabs (b[0]) ? fabs (a[0]) : fabs (b[0]);
-  maxab1 = fabs (a[1]) > fabs (b[1]) ? fabs (a[1]) : fabs (b[1]);
-  if (maxab0 <= maxab1)
-    {
-      si->a0 = (int32_t) a[0];
-      si->b0 = (int32_t) rint (b[0] / skewness);
-      si->a1 = (int32_t) a[1];
-      si->b1 = (int32_t) rint (b[1] / skewness);
-    }
-  else /* swap (a0,b0) and (a1,b1) */
-    {
-      si->a1 = (int32_t) a[0];
-      si->b1 = (int32_t) rint (b[0] / skewness);
-      si->a0 = (int32_t) a[1];
-      si->b0 = (int32_t) rint (b[1] / skewness);
-      maxab1 = maxab0;
-    }
-
-  /* make sure J does not exceed I/2 */
-  if (maxab1 >= si->B)
-    si->J = (uint32_t) (si->B * skewness / maxab1 * (double) (si->I >> 1));
-  else
-    si->J = si->I >> 1;
-
-  /* Make sure the bucket region size divides the sieve region size, 
-     partly covered bucket regions may lead to problems when 
-     reconstructing p from half-empty buckets. */
-  /* Compute number of i-lines per bucket region, must be integer */
-  ASSERT_ALWAYS(LOG_BUCKET_REGION >= si->logI);
-  uint32_t i = 1U << (LOG_BUCKET_REGION - si->logI);
-  i *= si->nb_threads;  /* ensures nb of bucket regions divisible by nb_threads */
-  si->J = ((si->J - 1U) / i + 1U) * i; /* Round up to multiple of i */
-
-  return 0;
-}
-
-/* return max(|a0|,|a1|)/min(|a0|,|a1|) */
-double
-skewness (sieve_info_ptr si)
-{
-  double a0 = fabs ((double) si->a0);
-  double a1 = fabs ((double) si->a1);
-
-  return (a0 > a1) ? a0 / a1 : a1 / a0;
 }
 
 /************************ cofactorization ********************************/
