504c570e96eaf85f55adc611c467441f483226ed
Jérémie Detrey <Jeremie.Detrey@loria.fr>
2012-03-01 17:16:58 +0100

    Input/output functions for ffspol_t.

--- ffs/ffspol.c
+++ ffs/ffspol.c
@@ -1,14 +1,16 @@
 #include <stdlib.h>
 #include <stdarg.h>
+#include <string.h>
 
 #include "ffspol.h"
 
 
 
-/* Initialization/destruction.
+/* Miscellaneous functions for internal use only.
  *****************************************************************************/
 
 // Reallocate memory with space for n coefficients.
+static
 void __ffspol_realloc(ffspol_ptr r, unsigned n)
 {
   for (unsigned i = n; i < r->alloc; ++i)
@@ -26,6 +28,18 @@ static inline
 void __ffspol_realloc_lazy(ffspol_ptr r, unsigned n)
 { if (n > r->alloc) __ffspol_realloc(r, n); }
 
+
+// Update the degree: assume that the given degree is an upper bound, and 
+// check the nullity of the coefficients.
+// /!\ This function works by side-effects.
+static inline
+void __ffspol_update_degree(ffspol_ptr r)
+{ for (; r->deg >= 0 && fppol_is_zero(r->coeffs[r->deg]); --r->deg); }
+
+
+
+/* Initialization/destruction.
+ *****************************************************************************/
 
 // Initialize polynomial.
 void ffspol_init(ffspol_ptr r)
@@ -70,4 +84,74 @@ void ffspol_clears(ffspol_ptr r, ...)
   for (va_start(ap, r); r != NULL; r = va_arg(ap, ffspol_ptr))
     ffspol_clear(r);
   va_end(ap);
+}
+
+
+/* Input / output.
+ *****************************************************************************/
+
+// Conversion to string.
+char *ffspol_get_str(char *str, ffspol_srcptr p)
+{
+  if (str == NULL) {
+    str = (char *)malloc((ffspol_strlen(p)+1) * sizeof(char));
+    ASSERT_ALWAYS(str != NULL);
+  }
+  strcpy(str, "0");
+  char *ptr = str;
+  for (int i = 0; i <= p->deg; ++i) {
+    if (i) *ptr++ = ',';
+    fppol_get_str(ptr,  p->coeffs[i]);
+    ptr += fppol_strlen(p->coeffs[i]);
+  }
+  return str;
+}
+
+
+// Conversion from string.
+// Return 1 if successful.
+int ffspol_set_str(ffspol_ptr r, const char *str)
+{
+  unsigned n = 0, lmax = 0;
+  for (unsigned i = 0, j = 0; i <= strlen(str); ++i) {
+    if (str[i] && str[i] != ',') continue;
+    ++n;
+    if (lmax < i-j) lmax = i-j;
+    j = i+1;
+  }
+  __ffspol_realloc_lazy(r, n);
+  char *buf = (char *)malloc((lmax+1) * sizeof(char));
+  ASSERT_ALWAYS(buf != NULL);
+  r->deg = -1;
+  for (unsigned i = 0, j = 0; i <= strlen(str); ++i) {
+    if (str[i] && str[i] != ',') continue;
+    strncpy(buf, str+j, i-j); buf[i-j] = '\0';
+    if (!fppol_set_str(r->coeffs[++r->deg], buf)) return 0;
+    j = i+1;
+  }
+  __ffspol_update_degree(r);
+  return 1;
+}
+
+
+// Output to stream.
+void ffspol_out(FILE *f, ffspol_srcptr p)
+{
+  char *buf = ffspol_get_str(NULL, p);
+  fprintf(f == NULL ? stdout : f, "%s", buf);
+  free(buf);
+}
+
+
+// Input from stream.
+// Return 1 if successful.
+int ffspol_inp(ffspol_ptr r, FILE *f)
+{
+  if (f == NULL) f = stdin;
+  int c = ',';
+  for (r->deg = -1; c == ','; c = getc(f))
+    if (!fppol_inp(r->coeffs[++r->deg], f)) return 0;
+  ungetc(c, f);
+  __ffspol_update_degree(r);
+  return 1;
 }
--- ffs/ffspol.h
+++ ffs/ffspol.h
@@ -45,4 +45,33 @@ void ffspol_clear(ffspol_ptr r);
 // Free a NULL-terminated list of polynomials.
 void ffspol_clears(ffspol_ptr r, ...);
 
+
+
+/* Input / output.
+ *****************************************************************************/
+
+// Length of string representation, not counting the null-terminator.
+static inline
+size_t ffspol_strlen(ffspol_srcptr p)
+{
+  unsigned l = p->deg < 0 ? 1 : p->deg;
+  for (int i = 0; i <= p->deg; ++i)
+    l += fppol_strlen(p->coeffs[i]);
+  return l;
+}
+
+// Conversion to string.
+char *ffspol_get_str(char *str, ffspol_srcptr p);
+
+// Conversion from string.
+// Return 1 if successful.
+int ffspol_set_str(ffspol_ptr r, const char *str);
+
+// Output to stream.
+void ffspol_out(FILE *f, ffspol_srcptr p);
+
+// Input from stream.
+// Return 1 if successful.
+int ffspol_inp(ffspol_ptr r, FILE *f);
+
 #endif   /* __FFSPOL_H__ */
