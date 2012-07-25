e8cd56195a03472866c94a3295f4f98f832ff422
Alain Filbois <alain.filbois@inria.fr>
2012-07-25 18:03:35 +0200

    [BUG] Coherence of messages in cmake.

--- config/sync_fetch.cmake
+++ config/sync_fetch.cmake
@@ -1,5 +1,5 @@
-# Test the existence of __sync_fetch functions
-message(STATUS " Test the existence of __sync_fetch functions")
+#Testing the existence of __sync_fetch functions
+message(STATUS "Testing the existence of __sync_fetch functions")
 try_compile(HAVE_SYNC_FETCH
     ${CADO_NFS_BINARY_DIR}/config
     ${CADO_NFS_SOURCE_DIR}/config/sync_fetch.c)
