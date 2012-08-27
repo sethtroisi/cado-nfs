876e90a55544f14ff619cd304d554e08c239b57c
Jérémie Detrey <Jeremie.Detrey@loria.fr>
2012-08-27 14:07:41 +0200

    Prevent segfault when no command-line parameters are given.

--- ffs/sieve.c
+++ ffs/sieve.c
@@ -302,6 +302,8 @@ int main(int argc, char **argv)
         fprintf(stderr, "Unhandled parameter %s\n", argv[0]);
         usage(argv0, NULL);
     }
+    // Update parameter list at least once to register argc/argv pointers.
+    param_list_update_cmdline(pl, &argc, &argv);
     param_list_print_command_line(stdout, pl);
 
     param_list_parse_int(pl, "gf", &gf);
