b2891fb8b9850769abc3ec4ea4928bdfe5ce7f86
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2012-12-03 10:02:45 +0100

    [config/sse41.c] disable SSE 4.1 on 32-bit (emulated) computers

--- config/sse41.c
+++ config/sse41.c
@@ -5,6 +5,9 @@ int main() {
     volatile __v2di x = { (uint64_t) 42, (uint64_t) 17 };
     volatile __v2di y = { (uint64_t) 41, (uint64_t) 17 };
     x = _mm_cmpeq_epi64(x, y);
+    /* the following test is for emulated 32-bit on physical 64-bit */
+    if (sizeof(unsigned long) != 8)
+      return 1;
     return 0;
 }
 
