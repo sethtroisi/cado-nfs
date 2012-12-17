0fca4b408b51a98e1085d143f4db81ddc2d56bd5
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2012-12-17 11:27:40 +0100

    [cadofct.pm] removed trailing blank

--- cadofct.pm
+++ cadofct.pm
@@ -277,7 +277,7 @@ for (my $i = 0; $i < @default_param; $i += 2) {
 }
 
 
-# Merge second hash into first hash. Value of a key in first hash remains 
+# Merge second hash into first hash. Value of a key in first hash remains
 # unchanged in case of collision
 sub merge_hash {
   die unless ref $_[0] eq 'HASH' && ref $_[1] eq 'HASH';
