a17375391a8cfc5fd62b6a409d291a24be34a3be
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2012-12-19 18:08:27 +0100

    [roots_mod.c] new r-th root code for any r (untested, but unused so far...)

--- utils/roots_mod.c
+++ utils/roots_mod.c
@@ -40,6 +40,8 @@ invert_mod (uint64_t a, uint64_t b)
   return ret;
 }
 
+/************************** square roots *************************************/
+
 /* Roots of x^d = a (mod p) for d even and a 64-bit word */
 static int
 roots2 (residue_t *rr, residue_t aa, int d, modulus_t pp)
@@ -142,6 +144,8 @@ roots2 (residue_t *rr, residue_t aa, int d, modulus_t pp)
   return 2*k;
 }
 
+/************************** cubic roots **************************************/
+
 static void 
 one_cubic_root_2mod3 (residue_t rr, residue_t ddelta, modulus_t pp)
 {
@@ -236,53 +240,6 @@ one_cubic_root_1mod3 (residue_t rr, residue_t zeta, residue_t ddelta, modulus_t 
   mod_clear (d, pp);
 }
 
-/* Return a r-th root of delta (mod p), assuming one exists,
-   using the algorithm from Table 4 in reference [1]. */
-static uint64_t
-one_rth_root (uint64_t r, uint64_t delta, uint64_t p)
-{
-  uint64_t rho, i, j, s, t, alpha, a, c, h, b, d;
-
-  /* when p-1 is not divisible by r, there is exactly one root */
-  rho = (p - 1) % r;
-  if (rho != 0)
-    {
-      for (i = 1; (i * (p - 1) + 1) % r != 0; i++);
-      return uint64_pow_mod (delta, (i * (p - 1) + 1) / r, p);
-    }
-
-  /* now p-1 is divisible by r */
-  
-  for (s = (p - 1) / r, t = 1; (s % r) == 0; s /= r, t++);
-  for (rho = 2; rho < p; rho++)
-    {
-      c = uint64_pow_mod (rho, s, p);
-      for (a = c, i = 0; i < t - 1; i++)
-        a = uint64_pow_mod (a, r, p);
-      if (a != 1)
-        break;
-    }
-  alpha = invert_mod (r, s);
-  b = uint64_pow_mod (delta, r * alpha - 1, p);
-  h = 1;
-  for (i = 1; i < t; i++)
-    {
-      d = b;
-      for (j = 0; j < t - 1- i; j++)
-        d = uint64_pow_mod (d, r, p);
-      if (d == 1)
-        c = uint64_pow_mod (c, r, p);
-      else
-        {
-          for (j = 0; d != 1; d = (d * a) % p, j++);
-          h = (h * uint64_pow_mod (c, j, p)) % p;
-          c = uint64_pow_mod (c, r, p);
-          b = (b * uint64_pow_mod (c, r, p)) % p;
-        }
-    }
-  return (h * uint64_pow_mod (delta, alpha, p)) % p;
-}
-
 /* return 1 iff a is a cube mod p */
 static int
 is_cube (residue_t aa, modulus_t pp)
@@ -347,6 +304,150 @@ roots3 (residue_t *rr, residue_t aa, int d, modulus_t pp)
     }
 }
 
+/************************** r-th roots ***************************************/
+
+/* Put in rop a r-th root of delta (mod p), assuming one exists,
+   using the algorithm from Table 4 in reference [1]. */
+static void
+one_rth_root (residue_t rop, uint64_t r, residue_t delta, modulus_t pp)
+{
+  residue_t a, b, c, d, h;
+  const uint64_t p = mod_getmod_ul (pp);
+  uint64_t i, j, s, t, alpha;
+
+  /* when p-1 is not divisible by r, there is exactly one root */
+  if (((p - 1) % r) != 0)
+    {
+      for (i = 1; (i * (p - 1) + 1) % r != 0; i++);
+      mod_pow_ul (rop, delta, (i * (p - 1) + 1) / r, pp);
+      return;
+    }
+
+  /* now p-1 is divisible by r */
+
+  mod_init (a, pp);
+  mod_init (b, pp);
+  mod_init (c, pp);
+  mod_init (d, pp);
+  mod_init (h, pp);
+  for (s = (p - 1) / r, t = 1; (s % r) == 0; s /= r, t++);
+  for (i = 2; i < p; i++)
+    {
+      mod_set_ul (c, i, pp);
+      mod_pow_ul (c, c, s, pp);
+      mod_set (a, c, pp);
+      for (i = 0; i < t - 1; i++)
+        mod_pow_ul (a, a, r, pp);
+      if (mod_is1 (a, pp) != 1)
+        break;
+    }
+  alpha = invert_mod (r, s);
+  mod_pow_ul (b, delta, r * alpha - 1, pp);
+  mod_set1 (h, pp);
+  for (i = 1; i < t; i++)
+    {
+      mod_set (d, b, pp);
+      for (j = 0; j < t - 1- i; j++)
+        mod_pow_ul (d, d, r, pp);
+      if (mod_is1 (d, pp))
+        mod_pow_ul (c, c, r, pp);
+      else
+        {
+          for (j = 0; mod_is1 (d, pp) == 0; mod_mul (d, d, a, pp), j++);
+          mod_pow_ul (d, c, j, pp);
+          mod_mul (h, h, d, pp);
+          mod_pow_ul (c, c, r, pp);
+          mod_mul (b, b, c, pp);
+        }
+    }
+  mod_pow_ul (rop, delta, alpha, pp);
+  mod_mul (rop, rop, h, pp);
+  mod_clear (a, pp);
+  mod_clear (b, pp);
+  mod_clear (c, pp);
+  mod_clear (d, pp);
+  mod_clear (h, pp);
+}
+
+static int
+is_rth_power (residue_t a, uint64_t r, modulus_t pp)
+{
+  const uint64_t p = mod_getmod_ul (pp);
+  residue_t t;
+  int ret = 1;
+
+  if ((p % r) == 1)
+    {
+      mod_init (t, pp);
+      mod_pow_ul (t, a, (p - 1) / r, pp);
+      ret = mod_is1 (t, pp);
+      mod_clear (t, pp);
+    }
+  return ret;
+}
+
+/* Roots of x^d = a (mod p), assuming d is not divisible by 2 nor 3 */
+static int
+roots (residue_t *rr, residue_t a, int d, modulus_t pp)
+{
+  uint64_t r, n, i, j, k;
+  const uint64_t p = mod_getmod_ul (pp);
+  residue_t z, *rr0;
+
+  /* first find the smallest prime r dividing d (r can be d) */
+  for (r = 5; d % r; r += 2);
+
+  if (is_rth_power (a, r, pp) == 0)
+    return 0;
+
+  /* find the roots of x^(d/r) = a (mod p) */
+  n = mod_roots (rr0 = rr + d - d / r, a, d / r, pp);
+
+  /* For the roots of x^r = a (mod p):
+     either p = 1 (mod r), then
+     (a) if a^((p-1)/r) <> 1, there is no solution;
+     (b) if a^((p-1)/r) == 1, there are r solutions. If we have one of them,
+         the others are x*zeta^i for 1 <= i < r, with zeta^((p-1)/r) <> 1;
+     or p <> 1 (mod r), then there is only one solution, which is given by
+     a^(e/r) where e = i*(p-1)+1 is divisible by r. */
+
+  if ((p % r) != 1)
+    {
+      uint64_t e;
+
+      for (e = p; e % r; e += p - 1);
+      for (i = 0; i < n; i++)
+        mod_pow_ul (rr[i], rr0[i], e / r, pp);
+      return n;
+    }
+
+  /* now p = 1 (mod r) */
+  k = 0;
+  /* get a primitive r-th root of unity z */
+  mod_init (z, pp);
+  for (i = 2; i < p; i++)
+    {
+      mod_set_ul (z, i, pp);
+      mod_pow_ul (rr[0], z, (p - 1) / r, pp);
+      if (mod_is1 (rr[0], pp) == 0)
+        break;
+    }
+  for (i = k = 0; i < n; i++)
+    {
+      /* check rr0[i] is a r-th power */
+      mod_pow_ul (rr[k], rr0[i], (p - 1) / r, pp);
+      if (mod_is1 (rr[k], pp) == 0)
+        continue;
+      /* get one r-th root */
+      one_rth_root (rr[k++], r, rr0[i], pp);
+      for (j = 1; j < r; j++, k++)
+        mod_mul (rr[k], rr[k-1], z, pp);
+    }
+  mod_clear (z, pp);
+  return k;
+}
+
+/*****************************************************************************/
 
 static int 
 mod_roots (residue_t *rr, residue_t aa, int d, modulus_t pp)
@@ -409,7 +510,7 @@ roots_mod_uint64 (uint64_t *r, uint64_t a, int d, uint64_t p)
     {
       modulus_t pp;
       residue_t aa, rr[10];
-      
+
       mod_initmod_ul (pp, p);
       mod_init (aa, pp);
       mod_set_ul (aa, a, pp);
