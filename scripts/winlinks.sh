cfabc32ef88d836562fe5a60301690d9547fc458
Alexander Kruppa <Alexander.Kruppa@loria.fr>
2013-01-11 14:13:41 +0100

    Script to fix broken symlinks that Git produces under Windows. The script simply copyies the linked-to file and tells Git not to consider the file modified

---
+++ scripts/winlinks.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/env bash
+
+# This script looks for symbolic links stored in the Git repository,
+# tests for each symlink whether it is actually a file containing the 
+# name of the linked-to file (as Git produces on Windows when checking 
+# out a symlink) and, if so, replaces the symlink by a copy of the file
+
+git ls-files -s | awk '/120000/{print $4}' > symlinked.files
+for F in $(< symlinked.files)
+do
+  TARGET="`cat "$F" | head -n 1`"
+  if ! test -f "$TARGET"
+  then
+    echo "$F does not seem to be a borked symlink file"
+  else
+    rm "$F"
+    cp "$TARGET" "$F"
+    git update-index --assume-unchanged "$F"
+  fi
+done
+rm symlinked.files
