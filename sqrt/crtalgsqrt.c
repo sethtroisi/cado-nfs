5d9ce219433a00c5b8b0fdc1874d2b92ad8a70f9
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2013-01-16 14:51:01 +0100

    [crtalgsqrt.c] revert commit ed0cd61 (defined with -std=gnu99 under MinGW)

--- sqrt/crtalgsqrt.c
+++ sqrt/crtalgsqrt.c
@@ -69,22 +69,6 @@
 #include "select_mpi.h"
 #include "gmp-hacks.h"          // TODO: REMOVE !
 
-#ifndef M_LN2
-#define M_LN2 0.69314718055994530942 /* log(2) */
-#endif
-
-#ifndef M_SQRT2
-#define M_SQRT2 1.41421356237309504880 /* sqrt(2) */
-#endif
-
-#ifndef M_SQRT1_2
-#define M_SQRT1_2 0.70710678118654752440 /* 1/sqrt(2) */
-#endif
-
-#ifndef M_PI
-#define M_PI 3.14159265358979323846 /* pi */
-#endif
-
 /* {{{ time */
 double program_starttime;
 #define WCT     (wct_seconds() - program_starttime)
