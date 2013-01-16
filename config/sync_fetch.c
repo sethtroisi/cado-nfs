10b727314311e364d6d4bf3f9f4c107666630f73
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2013-01-16 17:11:41 +0100

    [sync_fetch.c] fixed configure test

--- config/sync_fetch.c
+++ config/sync_fetch.c
@@ -1,7 +1,9 @@
 #include <stdlib.h>
 int main () {
-  int o;
+  int o, *p;
   __sync_fetch_and_add(&o, 1);
-  __sync_sub_and_fetch(&o, 1);
+  /* according to http://trac.wxwidgets.org/ticket/4542, one should use the
+     return value for a complete configure test */
+  p = __sync_sub_and_fetch(&o, 1);
   exit (0);
 }
