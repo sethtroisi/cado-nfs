9dff407d383397bb64167322260b4322d0428c24
Alex Kruppa <akruppa@gmail.com>
2013-03-04 21:56:28 +0100

    More help output, allow multiple --program parameters

--- scripts/verify.py
+++ scripts/verify.py
@@ -38,14 +38,27 @@ def do_check(acc, wu, programs):
     return result
 
 if __name__ == "__main__":
+    description = "Run a verification tool on files of received work units"
+    epilog = "For each WU processed, run PROGRAM on those files of the WU whose filenames match REGEX. " + \
+             "The client side filename (as specified in a RESULT line of a WU) is used for the REGEX match. " + \
+             "Multiple programs can be specified; each program is run for any file(s) that match the respective REGEX. " + \
+             "If the exit status of the PROGRAMs is zero for all tested files of a WU, the WU is considered verified ok. " + \
+             "If the exit status is non-zero for any tested file, the WU is considered verified with error. " + \
+             "The database is updated accordingly only if --update is given; otherwise only screen output happens."
+
     wudb_name = "wudb"
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-file', action = 'store')
-    parser.add_argument('-program', nargs = 2, required = True, 
-                        action = 'store')
-    parser.add_argument('-wudb', action = 'store', default = wudb_name)
-    parser.add_argument('-received', action = 'store_true')
-    parser.add_argument('-update', action = 'store_true')
+    parser = argparse.ArgumentParser(description=description, epilog = epilog)
+    parser.add_argument('-f', '--file', action = 'store', 
+                        help = "Does nothing")
+    parser.add_argument('-p', '--program', nargs = 2, required = True, 
+                        action = 'append', metavar=('PROGRAM', 'REGEX'),
+                        help = "run PROGRAM on files whose names match REGEX")
+    parser.add_argument('--wudb', action = 'store', default = wudb_name,
+                        metavar = "FILE", help = "use FILE as Workunit DB")
+    parser.add_argument('-r', '--received', action = 'store_true',
+                        help = 'Process all workunits currently marked as received without error')
+    parser.add_argument('--update', action = 'store_true', 
+                        help = 'Update status of processed WUs to verified with/without error according to test result')
     args = parser.parse_args()
 
     if "wudb" in args:
@@ -61,11 +74,10 @@ if __name__ == "__main__":
 
     if args.received:
         # Test all workunits whose status is RECEIVED_OK
-        programs = [(args.program[0], args.program[1])]
         received = acc.query(eq={"status": wudb.WuStatus.RECEIVED_OK})
         print ("Found " + str(len(received)) + " workunits")
         for wu in received:
-            result = do_check(acc, wu, programs)
+            result = do_check(acc, wu, list(args.program))
             if result is None:
                 print ("Warning: none of the supplied programs matched for workunit " 
                        + wu["wuid"])
