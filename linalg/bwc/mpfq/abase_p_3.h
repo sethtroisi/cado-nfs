a5308c1c798d42528299c46f9a690cd0097a4556
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2013-03-15 17:50:16 +0100

    regenerate mpfq code

--- linalg/bwc/mpfq/abase_p_3.h
+++ linalg/bwc/mpfq/abase_p_3.h
@@ -31,131 +31,61 @@
 /* Active handler: Mpfq::gfp::field */
 /* Active handler: Mpfq::gfp::elt */
 /* Active handler: Mpfq::defaults::mpi_flat */
-/* Options used: w=64 fieldtype=prime n=3 nn=7 vtag=p_3 vbase_stuff={
-                 'vc:includes' => [
-                                    '<stdarg.h>'
-                                  ],
-                 'member_templates_restrict' => {
-                                                  'p_1' => [
-                                                             {
-                                                               'cpp_ifdef' => 'COMPILE_MPFQ_PRIME_FIELDS',
-                                                               'tag' => 'p_1'
-                                                             }
-                                                           ],
-                                                  'p_4' => [
-                                                             {
-                                                               'cpp_ifdef' => 'COMPILE_MPFQ_PRIME_FIELDS',
-                                                               'tag' => 'p_4'
-                                                             }
-                                                           ],
-                                                  'u64k2' => [
-                                                               'u64k1',
-                                                               'u64k2'
-                                                             ],
-                                                  'p_3' => [
-                                                             {
-                                                               'cpp_ifdef' => 'COMPILE_MPFQ_PRIME_FIELDS',
-                                                               'tag' => 'p_3'
-                                                             }
-                                                           ],
-                                                  'u64k1' => $vbase_stuff->{'member_templates_restrict'}{'u64k2'}
-                                                },
-                 'families' => [
-                                 $vbase_stuff->{'member_templates_restrict'}{'p_4'},
-                                 $vbase_stuff->{'member_templates_restrict'}{'p_1'},
-                                 $vbase_stuff->{'member_templates_restrict'}{'p_3'},
-                                 $vbase_stuff->{'member_templates_restrict'}{'u64k2'}
-                               ],
-                 'choose_byfeatures' => sub { "DUMMY" }
-               };
- tag=p_3 type=plain opthw= virtual_base={
-                  'filebase' => 'abase_vbase',
-                  'substitutions' => [
-                                       [
-                                         qr/(?^:abase_p_3_elt \*)/,
-                                         'void *'
-                                       ],
-                                       [
-                                         qr/(?^:abase_p_3_src_elt\b)/,
-                                         'const void *'
-                                       ],
-                                       [
-                                         qr/(?^:abase_p_3_elt\b)/,
-                                         'void *'
-                                       ],
-                                       [
-                                         qr/(?^:abase_p_3_dst_elt\b)/,
-                                         'void *'
-                                       ],
-                                       [
-                                         qr/(?^:abase_p_3_elt_ur \*)/,
-                                         'void *'
-                                       ],
-                                       [
-                                         qr/(?^:abase_p_3_src_elt_ur\b)/,
-                                         'const void *'
-                                       ],
-                                       [
-                                         qr/(?^:abase_p_3_elt_ur\b)/,
-                                         'void *'
-                                       ],
-                                       [
-                                         qr/(?^:abase_p_3_dst_elt_ur\b)/,
-                                         'void *'
-                                       ],
-                                       [
-                                         qr/(?^:abase_p_3_vec \*)/,
-                                         'void *'
-                                       ],
-                                       [
-                                         qr/(?^:abase_p_3_src_vec\b)/,
-                                         'const void *'
-                                       ],
-                                       [
-                                         qr/(?^:abase_p_3_vec\b)/,
-                                         'void *'
-                                       ],
-                                       [
-                                         qr/(?^:abase_p_3_dst_vec\b)/,
-                                         'void *'
-                                       ],
-                                       [
-                                         qr/(?^:abase_p_3_vec_ur \*)/,
-                                         'void *'
-                                       ],
-                                       [
-                                         qr/(?^:abase_p_3_src_vec_ur\b)/,
-                                         'const void *'
-                                       ],
-                                       [
-                                         qr/(?^:abase_p_3_vec_ur\b)/,
-                                         'void *'
-                                       ],
-                                       [
-                                         qr/(?^:abase_p_3_dst_vec_ur\b)/,
-                                         'void *'
-                                       ],
-                                       [
-                                         qr/(?^:abase_p_3_poly \*)/,
-                                         'void *'
-                                       ],
-                                       [
-                                         qr/(?^:abase_p_3_src_poly\b)/,
-                                         'const void *'
-                                       ],
-                                       [
-                                         qr/(?^:abase_p_3_poly\b)/,
-                                         'void *'
-                                       ],
-                                       [
-                                         qr/(?^:abase_p_3_dst_poly\b)/,
-                                         'void *'
-                                       ]
-                                     ],
-                  'name' => 'abase_vbase',
-                  'global_prefix' => 'abase_'
-                };
- family=[HASH(0x130a7b8)] */
+/* Options used:{
+   w=64,
+   fieldtype=prime,
+   n=3,
+   nn=7,
+   vtag=p_3,
+   vbase_stuff={
+    vc:includes=[ <stdarg.h>, ],
+    member_templates_restrict={
+     p_1=[ { cpp_ifdef=COMPILE_MPFQ_PRIME_FIELDS, tag=p_1, }, ],
+     p_4=[ { cpp_ifdef=COMPILE_MPFQ_PRIME_FIELDS, tag=p_4, }, ],
+     u64k2=[ u64k1, u64k2, ],
+     p_3=[ { cpp_ifdef=COMPILE_MPFQ_PRIME_FIELDS, tag=p_3, }, ],
+     u64k1=[ u64k1, u64k2, ],
+     },
+    families=[
+     [ { cpp_ifdef=COMPILE_MPFQ_PRIME_FIELDS, tag=p_4, }, ],
+     [ { cpp_ifdef=COMPILE_MPFQ_PRIME_FIELDS, tag=p_1, }, ],
+     [ { cpp_ifdef=COMPILE_MPFQ_PRIME_FIELDS, tag=p_3, }, ],
+     [ u64k1, u64k2, ],
+     ],
+    choose_byfeatures=<code>,
+    },
+   tag=p_3,
+   type=plain,
+   opthw=,
+   virtual_base={
+    filebase=abase_vbase,
+    substitutions=[
+     [ (?^:abase_p_3_elt \*), void *, ],
+     [ (?^:abase_p_3_src_elt\b), const void *, ],
+     [ (?^:abase_p_3_elt\b), void *, ],
+     [ (?^:abase_p_3_dst_elt\b), void *, ],
+     [ (?^:abase_p_3_elt_ur \*), void *, ],
+     [ (?^:abase_p_3_src_elt_ur\b), const void *, ],
+     [ (?^:abase_p_3_elt_ur\b), void *, ],
+     [ (?^:abase_p_3_dst_elt_ur\b), void *, ],
+     [ (?^:abase_p_3_vec \*), void *, ],
+     [ (?^:abase_p_3_src_vec\b), const void *, ],
+     [ (?^:abase_p_3_vec\b), void *, ],
+     [ (?^:abase_p_3_dst_vec\b), void *, ],
+     [ (?^:abase_p_3_vec_ur \*), void *, ],
+     [ (?^:abase_p_3_src_vec_ur\b), const void *, ],
+     [ (?^:abase_p_3_vec_ur\b), void *, ],
+     [ (?^:abase_p_3_dst_vec_ur\b), void *, ],
+     [ (?^:abase_p_3_poly \*), void *, ],
+     [ (?^:abase_p_3_src_poly\b), const void *, ],
+     [ (?^:abase_p_3_poly\b), void *, ],
+     [ (?^:abase_p_3_dst_poly\b), void *, ],
+     ],
+    name=abase_vbase,
+    global_prefix=abase_,
+    },
+   family=[ { cpp_ifdef=COMPILE_MPFQ_PRIME_FIELDS, tag=p_3, }, ],
+   } */
 
 typedef mpfq_p_field abase_p_3_field;
 typedef mpfq_p_dst_field abase_p_3_dst_field;
