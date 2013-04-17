d4c2d714675daae96758084bcbc7e1426bc51e1f
Alexander Kruppa <Alexander.Kruppa@loria.fr>
2013-04-17 18:07:31 +0200

    Poly selection, factorbase and freerels can run now

---
+++ scripts/cadofactor/patterns.py
@@ -0,0 +1,40 @@
+import abc
+
+class Observable(object):
+    """ Implements the subject part of the Observer pattern.
+    
+    An Observer can register with the subject, the subject then can notify 
+    the Observer of state changes by calling the Observer's updateObserver()
+    method.
+    """
+    def __init__(self, *args, **kwargs):
+        self.__observers = []
+        super().__init__(*args, **kwargs)
+    
+    def subscribeObserver(self, observer):
+        if not observer in self.__observers:
+            self.__observers.append(observer)
+    
+    def unsubscribeObserver(self, observer):
+        try:
+            self.__observers.remove(observer)
+        except ValueError:
+            pass
+    
+    def notifyObservers(self, message):
+        for observer in self.__observers:
+            observer.updateObserver(message)
+
+class Observer(object):
+    """ Implements the client part of the Observer pattern. 
+    
+    This is an abstract base class; the subclass must implement 
+    updateObserver. 
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @abc.abstractmethod
+    def updateObserver(self, message):
+        # Subclasses should implement this
+        return
