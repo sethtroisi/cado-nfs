619c3dbabb87d380ad17639b6fbdee89cc1c5b70
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2013-05-15 18:42:56 +0200

    Some improvements to las-debug.
    Some las-debug functions were external calls, while trivial inlines are
    preferred.
    
    Also, we now check the divisibility of the norms of the traced (a,b) pair
    even while sieving, including bucket sieving. The unsieved norm is
    displayed.

--- sieve/las-debug.c
+++ sieve/las-debug.c
@@ -18,6 +18,24 @@
 #include <execinfo.h>   /* For backtrace. Since glibc 2.1 */
 #endif
 
+/* The trivial calls for when TRACE_K is *not* defined are inlines in
+ * las-debug.h */
+
+#ifndef TRACE_K
+#if defined(TRACE_AB) || defined(TRACE_IJ) || defined(TRACE_Nx)
+#warning "TRACE_(AB|IJ|Nx) values are ignored unless TRACE_K is defined"
+#endif
+#endif
+
+
+/* recall that TRACE_K requires TRACK_CODE_PATH ; so we may safely use
+ * all where_am_I types here */
+
+#ifdef TRACE_K
+
+#if defined(TRACE_AB) + defined(TRACE_IJ) + defined(TRACE_Nx) != 1
+#error "when TRACE_K is defined, exactly one of TRACE_(AB|IJ|Nx) must be defined"
+#endif
 
 #ifdef  TRACE_Nx
 struct trace_Nx_t trace_Nx = TRACE_Nx;
@@ -37,23 +55,14 @@ struct trace_ij_t trace_ij = TRACE_IJ;
 struct trace_ij_t trace_ij = { 0, UINT_MAX, };
 #endif
 
-#ifdef TRACE_K
-#if defined(TRACE_AB) + defined(TRACE_IJ) + defined(TRACE_Nx) != 1
-#error "when TRACE_K is defined, exactly one of TRACE_(AB|IJ|Nx) must be defined"
-#endif
-#else
-#if defined(TRACE_AB) || defined(TRACE_IJ) || defined(TRACE_Nx)
-#warning "TRACE_(AB|IJ|Nx) values are ignored unless TRACE_K is defined"
-#endif
-#endif
+/* two norms of the traced (a,b) pair */
+mpz_t traced_norms[2];
 
 /* This fills all the trace_* structures from the main one. Which
  * structure is the main structure depends on which among the TRACE_*
  * flags has been defined */
-void trace_update_conditions(sieve_info_srcptr si MAYBE_UNUSED)
+void trace_per_sq_init(sieve_info_srcptr si MAYBE_UNUSED)
 {
-#ifdef TRACE_K
-
 #if     defined(TRACE_AB)
     /* can possibly fall outside the q-lattice. We have to check for it */
     if (ABToIJ(&trace_ij.i, &trace_ij.j, trace_ab.a, trace_ab.b, si)) {
@@ -84,101 +93,57 @@ void trace_update_conditions(sieve_info_srcptr si MAYBE_UNUSED)
         fprintf(stderr, "# Tracing relation (a,b)=(%"PRId64",%"PRIu64") (i,j)=(%d,%u), (N,x)=(%u,%u)\n",
                 trace_ab.a, trace_ab.b, trace_ij.i, trace_ij.j, trace_Nx.N, trace_Nx.x);
     }
-#else
-    return;
-#endif
+
+    for(int side = 0 ; side < 2 ; side++) {
+        mpz_init(traced_norms[side]);
+        mp_poly_homogeneous_eval_siui(traced_norms[side], si->sides[side]->fij,
+                si->cpoly->pols[side]->degree, trace_ij.i, trace_ij.j);
+    }
 }
 
-#if defined(TRACK_CODE_PATH) && (defined(TRACE_AB) || defined(TRACE_IJ) || defined(TRACE_Nx))
+void trace_per_sq_clear(sieve_info_srcptr si MAYBE_UNUSED)
+{
+    for(int side = 0 ; side < 2 ; side++)
+        mpz_init(traced_norms[side]);
+}
+
 int test_divisible(where_am_I_ptr w)
 {
-  /* we only check divisibility for the given (N,x) value */
-  if (!trace_on_spot_Nx(w->N, w->x))
-    return 1;
+    /* we only check divisibility for the given (N,x) value */
+    if (!trace_on_spot_Nx(w->N, w->x))
+        return 1;
 
+    /* Note that when we are reaching here through apply_one_bucket, we
+     * do not know the prime number. */
     fbprime_t p = w->p;
     if (p==0) return 1;
+
     const unsigned int logI = w->si->conf->logI;
     const unsigned int I = 1U << logI;
 
     const unsigned long X = w->x + (w->N << LOG_BUCKET_REGION);
     long i = (long) (X & (I-1)) - (long) (I/2);
     unsigned long j = X >> logI;
-    static int init = 0;
-    static mpz_t v[2];
-    static int64_t a0 = 0, b0 = 0, a1 = 0, b1 = 0;
     fbprime_t q;
 
     q = fb_is_power (p);
     if (q == 0)
-      q = p;
+        q = p;
 
-    if (init == 0)
-      {
-        /* those won't be cleared, but this is not really a problem in debug
-           mode (TRACE_K defined) */
-        mpz_init (v[0]);
-        mpz_init (v[1]);
-        init = 1;
-      }
-
-    if (w->si->a0 != a0 || w->si->b0 != b0 || w->si->a1 != a1 ||
-        w->si->b1 != b1)
-      {
-        mp_poly_homogeneous_eval_siui(v[0], w->si->sides[0]->fij,
-                                      w->si->cpoly->pols[0]->degree, i, j);
-        mp_poly_homogeneous_eval_siui(v[1], w->si->sides[1]->fij,
-                                      w->si->cpoly->pols[1]->degree, i, j);
-        a0 = w->si->a0;
-        b0 = w->si->b0;
-        a1 = w->si->a1;
-        b1 = w->si->b1;
-      }
-
-    int rc = mpz_divisible_ui_p (v[w->side], (unsigned long) q);
+    int rc = mpz_divisible_ui_p (traced_norms[w->side], (unsigned long) q);
 
     if (rc)
-      mpz_divexact_ui (v[w->side], v[w->side], (unsigned long) q);
+        mpz_divexact_ui (traced_norms[w->side], traced_norms[w->side], (unsigned long) q);
     else
-      gmp_fprintf(stderr, "# FAILED test_divisible(p=" FBPRIME_FORMAT
-                  ", N=%d, x=%lu, %.3s): i = %ld, j = %lu, norm = %Zd\n",
-                  w->p, w->N, w->x, sidenames[w->side], i, j, v);
+        gmp_fprintf(stderr, "# FAILED test_divisible(p=" FBPRIME_FORMAT
+                ", N=%d, x=%lu, %.3s): i = %ld, j = %lu, norm = %Zd\n",
+                w->p, w->N, w->x, sidenames[w->side], i, j, traced_norms[w->side]);
 
     return rc;
 }
-#endif
 
 /* {{{ helper: sieve_decrease */
-/* Decrease the sieve array entry *S by logp, with underflow checking
-   and tracing if desired. Variables x, bucket_nr, p, and si
-   are used only for trace test and output */
 
-#ifdef CHECK_UNDERFLOW
-void sieve_decrease_underflow_trap(unsigned char *S, const unsigned char logp, where_am_I_ptr w)
-{
-    int i;
-    unsigned int j;
-    int64_t a;
-    uint64_t b;
-    static unsigned char maxdiff = 0;
-
-    NxToIJ(&i, &j, w->N, w->x, w->si);
-    IJToAB(&a, &b, i, j, w->si);
-    if (logp - *S > maxdiff)
-      {
-        maxdiff = logp - *S;
-        fprintf(stderr, "# Error, underflow at (N,x)=(%u, %u), "
-                "(i,j)=(%d, %u), (a,b)=(%ld, %lu), S[x] = %hhu, log("
-                FBPRIME_FORMAT ") = %hhu\n",
-                w->N, w->x, i, j, a, b, *S, w->p, logp);
-      }
-    /* arrange so that the unconditional decrease which comes next
-     * has the effect of taking the result to zero */
-    *S = logp;
-}
-#endif
-
-#ifdef TRACE_K
 /* Do this so that the _real_ caller is always 2 floors up */
 void sieve_decrease_logging_backend(unsigned char *S, const unsigned char logp, where_am_I_ptr w)
 {
@@ -213,13 +178,15 @@ void sieve_decrease_logging_backend(unsigned char *S, const unsigned char logp, 
     free(freeme);
 #endif
 }
+
 void sieve_decrease_logging(unsigned char *S, const unsigned char logp, where_am_I_ptr w)
 {
     sieve_decrease_logging_backend(S, logp, w);
 }
-#endif
 
-#ifdef TRACE_K
+/* Decrease the sieve array entry *S by logp, with underflow checking
+ * and tracing if desired. w is used only for trace test and output */
+
 void sieve_decrease(unsigned char *S, const unsigned char logp, where_am_I_ptr w)
 {
     sieve_decrease_logging_backend(S, logp, w);
@@ -228,6 +195,36 @@ void sieve_decrease(unsigned char *S, const unsigned char logp, where_am_I_ptr w
 #endif
     *S -= logp;
 }
+
+#endif  /* TRACE_K */
+
+/* This function is useful both with and without TRACE_K, as the flag
+ * controlling it is CHECK_UNDERFLOW
+ */
+#ifdef CHECK_UNDERFLOW
+void sieve_decrease_underflow_trap(unsigned char *S, const unsigned char logp, where_am_I_ptr w)
+{
+    int i;
+    unsigned int j;
+    int64_t a;
+    uint64_t b;
+    static unsigned char maxdiff = 0;
+
+    NxToIJ(&i, &j, w->N, w->x, w->si);
+    IJToAB(&a, &b, i, j, w->si);
+    if (logp - *S > maxdiff)
+      {
+        maxdiff = logp - *S;
+        fprintf(stderr, "# Error, underflow at (N,x)=(%u, %u), "
+                "(i,j)=(%d, %u), (a,b)=(%ld, %lu), S[x] = %hhu, log("
+                FBPRIME_FORMAT ") = %hhu\n",
+                w->N, w->x, i, j, a, b, *S, w->p, logp);
+      }
+    /* arrange so that the unconditional decrease which comes next
+     * has the effect of taking the result to zero */
+    *S = logp;
+}
 #endif
+
 /* }}} */
 
