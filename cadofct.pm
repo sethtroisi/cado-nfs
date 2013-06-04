04e901d87def83dd88effde4eb1ac18c2370e7d5
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2013-06-04 10:58:52 +0200

    -renumberfile -> -renumber

--- cadofct.pm
+++ cadofct.pm
@@ -2188,7 +2188,7 @@ sub do_freerels {
     my $cmd = "$param{'bindir'}/sieve/freerel ".
               "-poly $param{'prefix'}.poly ".
               "-fb $param{'prefix'}.roots ".
-              "-renumberfile $param{'prefix'}.renumber ".
+              "-renumber $param{'prefix'}.renumber ".
               "> $param{'prefix'}.freerels ";
 
     cmd($cmd, { cmdlog => 1, kill => 1,
