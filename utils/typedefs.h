a64cb6128479c60e3869d8bb913a5e6500b1dd8e
Cyril Bouvier <cyril.bouvier@loria.fr>
2013-06-05 15:50:06 +0200

    Alex told me that macros.h was not the good place to put these typedefs

---
+++ utils/typedefs.h
@@ -0,0 +1,29 @@
+#ifndef	CADO_TYPEDEFS_H_
+#define	CADO_TYPEDEFS_H_
+
+/* data type to store the (p,r) values */
+#ifndef p_r_values_size
+#define p_r_values_size 32
+#endif
+
+/* data type to store the renumber table */
+#ifndef index_size
+#define index_size 32
+#endif
+
+#if p_r_values_size == 32
+#define p_r_values_t uint32_t
+#else
+#define p_r_values_t uint64_t
+#endif
+
+#if index_size == 32
+#define index_t uint32_t
+#else
+#define index_t uint64_t
+#endif 
+
+#define weight_t uint8_t
+#define exponent_t uint8_t
+
+#endif	/* CADO_TYPEDEFS_H_ */
