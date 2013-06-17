926f057ecdaf386074bcb5a04a9fee53e0341326
Cyril Bouvier <cyril.bouvier@loria.fr>
2013-06-17 17:14:33 +0200

    freerels for FFS now output the renumbering table

--- ffs/freerels.c
+++ ffs/freerels.c
@@ -13,6 +13,9 @@
 #include "fq.h"
 #include "fqpol.h"
 
+#include "renumber.h"
+
+#define MAX_FFS_DEG 20
 
 int sq_is_split(sq_t * roots, sq_srcptr q, ffspol_srcptr F) {
     fq_info_t Fq;
@@ -28,6 +31,30 @@ int sq_is_split(sq_t * roots, sq_srcptr q, ffspol_srcptr F) {
     return ret;
 }
 
+
+int sq_roots(unsigned long *roots, sq_srcptr q, ffspol_srcptr F)
+{
+  fq_info_t Fq;
+  fq_info_init(Fq, q);
+  fq_t r[MAX_FFS_DEG];
+
+  fqpol_t f;
+  fqpol_init(f);
+  fqpol_set_ffspol(f, F, Fq);
+
+  int nb = fqpol_roots(r, f, Fq);
+  fqpol_clear(f);
+  fq_info_clear(Fq);
+
+  for (int i = 0; i < nb; i++)
+    roots[i] = fppol64_get_ui_sparse (r[i]);
+  if (f->deg != F->deg) // There is a projective root
+  {
+    roots[nb] = fppol64_get_ui_sparse (q);
+    nb++;
+  }
+  return nb;
+}
 
 int sq_is_irreducible(sq_srcptr p) {
     fppol_t P;
@@ -58,12 +85,15 @@ void usage(const char *argv0, const char * missing)
 int main(int argc, char **argv)
 {
     ffspol_t ffspol[2]; 
-    int lpb[2] = {0, 0};  
+    int lpb[2] = {0, 0}, add_full_col = 0;
     char *argv0 = argv[0];
     int gf = 0;
+    const char * renumberfilename = NULL;
+    const char * badidealsfilename = NULL;
     
     param_list pl;
     param_list_init(pl);
+    param_list_configure_knob(pl, "-addfullcol", &add_full_col);
     argv++, argc--;
     for (; argc;) {
         if (param_list_update_cmdline(pl, &argc, &argv)) {
@@ -112,38 +142,90 @@ int main(int argc, char **argv)
     if (lpb[0] == 0) usage(argv0, "lpb0");
     if (lpb[1] == 0) usage(argv0, "lpb1");
 
-    param_list_clear(pl);
+    //read filename
+    renumberfilename = param_list_lookup_string(pl, "renumber");
+    if (renumberfilename == NULL)
+      usage (argv0, "renumber");
+    badidealsfilename = param_list_lookup_string(pl, "badideals");
+
+
+    cado_poly dummy_poly;
+    renumber_t tab;
+    cado_poly_init(dummy_poly);
+
+    dummy_poly->pols[0]->degree = ffspol[0]->deg; 
+    dummy_poly->pols[0]->lpb = __FP_BITS + __FP_BITS * lpb[0]; 
+    dummy_poly->pols[1]->degree = ffspol[1]->deg; 
+    dummy_poly->pols[1]->lpb = __FP_BITS + __FP_BITS * lpb[1]; 
+
+    if (dummy_poly->pols[1]->degree == 1)
+    {
+      dummy_poly->rat  = dummy_poly->pols[1];
+      dummy_poly->alg  = dummy_poly->pols[0];
+    }
+    else if (dummy_poly->pols[0]->degree == 1)
+    {
+      dummy_poly->rat  = dummy_poly->pols[0];
+      dummy_poly->alg  = dummy_poly->pols[1];
+    }
+
+    renumber_init(tab, dummy_poly);
+    renumber_init_write (tab, renumberfilename, badidealsfilename, add_full_col);
 
     uint64_t nrel = 0;
     sq_t p;
     sq_set_ti(p, 1);   // start with the polynomial t, always irreducible
-#define MAX_FFS_DEG 20
-    fq_t roots0[MAX_FFS_DEG], roots1[MAX_FFS_DEG];
-    while (sq_deg(p) <= MIN(lpb[0], lpb[1])) {
-        if (sq_is_split(roots0, p, ffspol[0])
-                && sq_is_split(roots1, p, ffspol[1])) {
-            sq_out(stdout, p);
-            printf(",0:");
-            for (int i = 0; i < ffspol[0]->deg - 1; ++i) {
-                sq_out(stdout, roots0[i]);
-                printf(",");
-            }
-            sq_out(stdout, roots0[ffspol[0]->deg-1]);
-            printf(":");
-            for (int i = 0; i < ffspol[1]->deg - 1; ++i) {
-                sq_out(stdout, roots1[i]);
-                printf(",");
-            }
-            sq_out(stdout, roots1[ffspol[1]->deg-1]);
-            printf("\n");
-            nrel++;
-        }
-        do {
-            sq_monic_set_next(p, p, 64);
-        } while (!sq_is_irreducible(p));
+    unsigned long *roots[2];
+    int max_deg = MAX(lpb[0], lpb[1]); 
+    int min_deg = MIN(lpb[0], lpb[1]); 
+    int k[2], d[2];
+    index_t old_table_size = tab->size;
+
+    d[0] = ffspol[0]->deg;
+    d[1] = ffspol[1]->deg;
+    roots[0] = (unsigned long*) malloc (d[0] * sizeof (unsigned long));
+    roots[1] = (unsigned long*) malloc (d[1] * sizeof (unsigned long));
+
+    fprintf (stderr, "Generating freerels up to degree %d\n", min_deg);
+    fprintf (stderr, "Generating renumber up to degree %d\n", max_deg);
+
+    while (sq_deg(p) <= max_deg) 
+    {
+      /* first compute the roots */
+      for (int i = 0; i < 2; i++)
+      {
+        if (sq_deg(p) < lpb[i])
+          k[i] = sq_roots(roots[i], p, ffspol[i]);
+        else
+          k[i] = 0;
+      }
+
+      unsigned long p_int= fppol64_get_ui_sparse (p);
+      renumber_write_p (tab, p_int, roots, k);
+
+      if (sq_deg(p) <= max_deg && k[0] == d[0] && k[1] == d[1])
+      { 
+        //print the free rels
+        index_t l;
+        printf ("%lx,0:%lx", p_int, (unsigned long) old_table_size);
+        for (l = old_table_size + 1; l < tab->size; l++)
+          printf (",%lx", (unsigned long) l);
+        printf ("\n");
+        nrel++;
+      }
+      old_table_size = tab->size;
+
+      do 
+      {
+        sq_monic_set_next(p, p, 64);
+      } while (!sq_is_irreducible(p));
     } 
 
     fprintf(stdin, "# Computed %" PRIu64 " free relations\n", nrel);
+    fprintf(stderr, "# Computed %" PRIu64 " free relations\n", nrel);
+    param_list_clear(pl);
+    cado_poly_clear(dummy_poly);
+    renumber_close_write(tab);
     ffspol_clear(ffspol[0]);
     ffspol_clear(ffspol[1]);
 
