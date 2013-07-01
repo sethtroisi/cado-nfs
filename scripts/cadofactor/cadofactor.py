b14dd340834fa10c10029f43df8bc2340fde1865
Alexander Kruppa <Alexander.Kruppa@loria.fr>
2013-07-01 17:35:53 +0200

    Allow setting logging level for log file

--- scripts/cadofactor/cadofactor.py
+++ scripts/cadofactor/cadofactor.py
@@ -10,12 +10,14 @@ if __name__ == '__main__':
     # Parse command line arguments
     parser = argparse.ArgumentParser(description='Integer Factorisation with the Number Field Sieve')
     parser.add_argument("--screenlog", help="Screen logging level, e.g., INFO/COMMAND/DEBUG", default="INFO", metavar="LEVEL")
+    parser.add_argument("--filelog", help="Log file logging level, e.g., INFO/COMMAND/DEBUG", default="DEBUG", metavar="LEVEL")
     parser.add_argument("--old", help="Use old parameter file format", action='store_true')
     parser.add_argument("parameters", help="A file with the parameters to use")
     parser.add_argument("options", metavar='OPTION', help="An option as in parameter file (format: key=value)", nargs="*")
     args = parser.parse_args()
     paramfile = args.parameters
     screenlvlname = args.screenlog
+    filelvlname = args.filelog
     
     parameters = cadoparams.Parameters()
     parameters.readfile(open(paramfile), old_format = args.old)
@@ -38,8 +40,9 @@ if __name__ == '__main__':
     logger.addHandler(cadologger.CmdFileHandler(cmdfilename))
     
     # Add a logger to write debugging information to a log file
+    filelvl = getattr(cadologger, filelvlname.upper())
     logfilename = tasksparams["workdir"] + os.sep + tasksparams["name"] + ".log"
-    logger.addHandler(cadologger.FileHandler(filename = logfilename, lvl = logging.DEBUG))
+    logger.addHandler(cadologger.FileHandler(filename = logfilename, lvl = filelvl))
     
     wudb_file = tasksparams["workdir"] + os.sep + tasksparams["name"] + ".db"
     factorjob = cadotask.CompleteFactorization(db=wudb_file, parameters = parameters, path_prefix = [])
