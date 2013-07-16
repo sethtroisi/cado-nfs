sethtroisi/cado-nfs

commit 0ed3ba42a2b064bbddee9716d9bb2f93f590202a

[las] Fix bug 15897: (re-)sieve correctly (i,j)=(1,0).

sieve/las-smallsieve.c
@@ -318,8 +318,8 @@ int * small_sieve_start(small_sieve_data_t *ssd, unsigned int j0, sieve_info_src
             ASSERT(j1 >= j0);
             ASSERT(j1 % ssp->g == 0);
             /* Now we'd like to avoid row number 0 (so j1 == 0).  */
-            /* Note that we avoid it entirely -- we could fathom sieving
-             * (1,0), but it's probably not really worth it */
+            /* The position (1,0) is updated with a special code in the */
+            /* sieving code below */
             if (j1 == 0) {
                 j1 += ssp->g;
             }
@@ -702,6 +702,18 @@ void sieve_small_bucket_region(unsigned char *S, int N,
                    with g|j, beginning with the line starting at S[ssdpos] */
                 unsigned long logps;
                 unsigned int i0 = ssdpos[i];
+                // The following is for the case where p divides the norm
+                // at the position (i,j) = (1,0).
+                if (UNLIKELY(N == 0 && i0 == ssp->g * I)) {
+#ifdef TRACE_K
+                    if (trace_on_spot_Nx(w->N, 1+(I>>1))) {
+                        WHERE_AM_I_UPDATE(w, x, trace_Nx.x);
+                        unsigned int v = logp;
+                        sieve_increase_logging(S + w->x, v, w);
+                    }
+#endif
+                    S[1+(I>>1)] += logp;
+                }
                 ASSERT (ssp->U == 0);
                 ASSERT (i0 % I == 0);
                 ASSERT (I % (4 * sizeof (unsigned long)) == 0);
@@ -921,6 +933,15 @@ resieve_small_bucket_region (bucket_primes_t *BP, int N, unsigned char *S,
 
             /* Test every p-th line, starting at S[ssdpos] */
             unsigned int i0 = ssdpos[i];
+            // This block is for the case where p divides at (1,0).
+            if (UNLIKELY(N == 0 && i0 == ssp->g * I)) {
+                bucket_prime_t prime;
+                prime.p = g;
+                prime.x = 1+(I>>1);
+                ASSERT(prime.p >= si->td_thresh);
+                push_bucket_prime (BP, prime);
+            }
+
             unsigned int ii;
             ASSERT (i0 % I == 0); /* make sure ssdpos points at start
                                      of line */

sieve/las.c
@@ -507,13 +507,8 @@ sieve_info_init_from_siever_config(las_info_ptr las, sieve_info_ptr si, siever_c
     /* overrides default only if parameter is given */
     param_list_parse_int(pl, "bkthresh", &(si->bucket_thresh));
 
-    si->td_thresh = si->I;	/* default value */
+    si->td_thresh = 1024;	/* default value */
     param_list_parse_uint(pl, "tdthresh", &(si->td_thresh));
-    if (si->td_thresh < si->I)
-      {
-        fprintf (stderr, "Using tdthresh < I not allowed due to bug #15897\n");
-        exit (1);
-      }
 
     /* Initialize the number of buckets */
 

commit ad7cfdc381ef31ec44679df7f98c0eb4433fed58

solved (partially) time regression from commit 0ed3ba4

sieve/las-smallsieve.c
@@ -704,7 +704,7 @@ void sieve_small_bucket_region(unsigned char *S, int N,
                 unsigned int i0 = ssdpos[i];
                 // The following is for the case where p divides the norm
                 // at the position (i,j) = (1,0).
-                if (UNLIKELY(N == 0 && i0 == ssp->g * I)) {
+                if (UNLIKELY(N == 0 && i0 == ssp->g << si->conf->logI)) {
 #ifdef TRACE_K
                     if (trace_on_spot_Nx(w->N, 1+(I>>1))) {
                         WHERE_AM_I_UPDATE(w, x, trace_Nx.x);
@@ -934,7 +934,7 @@ resieve_small_bucket_region (bucket_primes_t *BP, int N, unsigned char *S,
             /* Test every p-th line, starting at S[ssdpos] */
             unsigned int i0 = ssdpos[i];
             // This block is for the case where p divides at (1,0).
-            if (UNLIKELY(N == 0 && i0 == ssp->g * I)) {
+            if (UNLIKELY(N == 0 && i0 == ssp->g << si->conf->logI)) {
                 bucket_prime_t prime;
                 prime.p = g;
                 prime.x = 1+(I>>1);