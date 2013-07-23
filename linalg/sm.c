312389c80ee01524acefcba4d8e8ad97cf3aa4b9
Laurent Imbert <Laurent.Imbert@lirmm.fr>
2013-07-23 15:00:46 +0200

    indentation (this is a trivial commit)

--- linalg/sm.c
+++ linalg/sm.c
@@ -14,10 +14,11 @@ Input:
   
 Output
 
-* A matrix of (small_nrows) rows and (nmaps)=deg(f) cols (mpz_t).
-  For each relation (rel) the (nmaps) Shirokauer maps are computed as the second least-significant digit
-  of the ell-adic representation of the polynomial equal to (rel^eps - 1) / ell.
-  Note: In the very unlikely case where the second lsd is zero, the program stops!
+* A matrix of (small_nrows) rows and (nmaps)=deg(f) cols (mpz_t).  For each
+  relation (rel) the (nmaps) Shirokauer maps are computed as the second
+  least-significant digit of the ell-adic representation of the polynomial equal
+  to (rel^eps - 1) / ell.  Note: In the very unlikely case where the second lsd
+  is zero, the program stops!
 */
 
 #include "cado.h"
