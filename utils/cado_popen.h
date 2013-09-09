d4e88c1a349287b21bf2ae15e3c56d86d925a0eb
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2013-09-09 22:18:48 +0200

    be more radical

--- utils/cado_popen.h
+++ utils/cado_popen.h
@@ -11,15 +11,20 @@
 extern "C" {
 #endif
 
-
+#ifndef MinGW
 FILE * cado_popen(const char * command, const char * mode);
 #ifdef HAVE_GETRUSAGE
 void cado_pclose2(FILE * stream, struct rusage * r);
 #else
 void cado_pclose2(FILE * stream, void * r);
 #endif
-
 static inline void cado_pclose(FILE * stream) { cado_pclose2(stream, NULL); }
+#else
+
+static inline FILE * cado_popen(const char * command, const char * mode) { return popen(command, mode); }
+static inline FILE * cado_pclose(FILE * stream) { pclose(stream); }
+/* we don't even provide cado_pclose2 for mingw */
+#endif
 
 #ifdef __cplusplus
 }
