0464ff109d193b1fefcdc2144c6519cec9450a94
Cyril Bouvier <cyril.bouvier@loria.fr>
2013-11-13 18:04:27 +0100

    A little bit of cleaning in default parameters in filter binaries

--- filter/filter_utils.h
+++ filter/filter_utils.h
@@ -20,6 +20,9 @@ typedef struct {
   double W; //weight of the active part of the matrix
 } info_mat_t;
 
+#define STR(s) XSTR(s)
+#define XSTR(s) #s
+
 #define CA_DUP1 314159265358979323UL
 #define CB_DUP1 271828182845904523UL
 
@@ -29,6 +32,25 @@ typedef struct {
 /* Maximum level for a merge. Such a large value is only useful when not using
  * BW. */
 #define MERGE_LEVEL_MAX 256
+
+#define DEFAULT_FILTER_EXCESS 160
+
+#define DEFAULT_PURGE_NPASS 50
+#define DEFAULT_PURGE_REQUIRED_EXCESS 0.1
+#define DEFAULT_PURGE_NPT 4
+
+#define DEFAULT_MERGE_MAXLEVEL 10
+#define DEFAULT_MERGE_FORBW 0
+#define DEFAULT_MERGE_RATIO 1.1
+#define DEFAULT_MERGE_COVERNMAX 100.0
+#define DEFAULT_MERGE_MKZTYPE 1 /* pure Markowitz */
+#define DEFAULT_MERGE_WMSTMAX 7 /* relevant only if mkztype == 2 */
+#ifndef FOR_DL
+#define DEFAULT_MERGE_SKIP 32
+#else
+#define DEFAULT_MERGE_SKIP 0
+#endif
+
 
 #include <time.h>
 #include <pthread.h>
