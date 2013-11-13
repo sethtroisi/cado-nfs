213dbd8f9af467800569383d46250d46f708d593
Alexander Kruppa <Alexander.Kruppa@loria.fr>
2013-11-13 11:10:41 +0100

    Q&D script to plot parameters as function of input number size among parameter files

--- scripts/plot_params.sh
+++ scripts/plot_params.sh
@@ -1,8 +1,23 @@
 # Run inside the parameter directory
 
+PARAMDIR=.
+if [ -n "$1" ]
+then
+  PARAMDIR="$1"
+fi
+
+if ! test -d "$PARAMDIR" || ! test -f "$PARAMDIR/params.c91"
+then
+  echo Please specify the parameter directory
+  exit 1
+fi
+
 for S in degree qrange I polsel_P polsel_maxnorm polsel_admax polsel_adrange rlim alim lpbr lpba
 do
-  grep "^$S=" params.c[1-9][0-9] params.c[1-9][0-9][0-9] | sed "s/params.c//; s/:$S=/ /" | cut -d " " -f 1,2 >| /tmp/$S.data
-  echo "set terminal png; set log y 2; set output \"$S.png\"; plot \"/tmp/$S.data\" " | gnuplot
+  OUTPUTFILE="$S.png"
+  echo "Creating $OUTPUTFILE"
+  grep "^$S=" "$PARAMDIR"/params.c[1-9][0-9] "$PARAMDIR"/params.c1[0-9][0-9] | sed "s/^.*params.c//; s/:$S=/ /" | cut -d " " -f 1,2 >| /tmp/$S.data
+  echo "set terminal png; set log y 2; set output \"$OUTPUTFILE\"; plot \"/tmp/$S.data\" " | gnuplot
 done
+echo "Creating montage.png"
 montage -geometry 640x480 *.png montage.png
