20db6eb478639ab9070ddc842b5d76d8463117ae
Alexander Kruppa <Alexander.Kruppa@loria.fr>
2013-11-22 16:36:09 +0100

    Use SSE for pattern, and pattern for unsieving 7. Still slow

--- sieve/las-unsieve.c
+++ sieve/las-unsieve.c
@@ -1,16 +1,16 @@
 #include "cado.h"
 #include <stdlib.h>
+#include <string.h>
 
 #include "las-unsieve.h"
 
 /* Set every stride-th byte, starting at index 0, to 255 in an array of
-   stride unsigned longs */
+   stride unsieve_pattern_t's */
 static void
-minisieve(unsigned long *array, size_t stride)
+minisieve(unsieve_pattern_t *array, size_t stride)
 {
-    for (size_t i = 0; i < stride; i++)
-      array[i] = 0;
-    for (size_t i = 0; i < stride * sizeof(unsigned long); i += stride)
+    memset (array, 0, stride * sizeof(unsieve_pattern_t));
+    for (size_t i = 0; i < stride * sizeof(unsieve_pattern_t); i += stride)
       ((unsigned char *) array)[i] = 255;
 }
 
@@ -46,6 +46,8 @@ void sieve_info_init_unsieve_data(sieve_info_ptr si)
     minisieve(si->us->pattern3, 3);
     /* Create pattern for sieving 5 */
     minisieve(si->us->pattern5, 5);
+    /* Create pattern for sieving 7 */
+    minisieve(si->us->pattern7, 7);
 }
 
 void sieve_info_clear_unsieve_data(sieve_info_ptr si)
@@ -77,17 +79,20 @@ static inline void
 unsieve_3(unsigned char *line_start, const unsigned int start_idx,
           sieve_info_srcptr si)
 {
-  const unsigned int I_ul  = si->I / sizeof (unsigned long);
+  const unsigned int I_upt  = si->I / sizeof (unsieve_pattern_t);
   unsigned int i, pattern_idx;
-  unsigned long p0, p1, p2;
-  unsigned long * restrict ul_line_start = (unsigned long *) line_start;
+  unsieve_pattern_t p0, p1, p2;
+  unsieve_pattern_t * restrict ul_line_start = (unsieve_pattern_t *) line_start;
 
-  if (sizeof(unsigned long) == 4)
+  if (sizeof(unsieve_pattern_t) == 4)
     /* -4^(-1) == 2 (mod 3) */
     pattern_idx = (2 * start_idx) % 3;
-  else if (sizeof(unsigned long) == 8)
+  else if (sizeof(unsieve_pattern_t) == 8)
     /* -8^(-1) == 1 (mod 3) */
     pattern_idx = start_idx;
+  else if (sizeof(unsieve_pattern_t) == 16)
+    /* -16^(-1) == 2 (mod 3) */
+    pattern_idx = (2 * start_idx) % 3;
   else
     abort();
   
@@ -98,16 +103,16 @@ unsieve_3(unsigned char *line_start, const unsigned int start_idx,
   ASSERT_ALWAYS(((unsigned char *)&p0)[start_idx] == 255);
   
   /* Apply pattern to array */
-  for (i = 0U; i < I_ul - 2U; i += 3U)
+  for (i = 0U; i < I_upt - 2U; i += 3U)
     {
-      ul_line_start[i] |= p0;
-      ul_line_start[i + 1] |= p1;
-      ul_line_start[i + 2] |= p2;
+      UNSIEVE_OR(ul_line_start[i], p0);
+      UNSIEVE_OR(ul_line_start[i + 1], p1);
+      UNSIEVE_OR(ul_line_start[i + 2], p2);
     }
-  if (i < I_ul)
-    ul_line_start[i] |= p0;
-  if (i + 1 < I_ul)
-    ul_line_start[i + 1] |= p1;
+  if (i < I_upt)
+    UNSIEVE_OR(ul_line_start[i], p0);
+  if (i + 1 < I_upt)
+    UNSIEVE_OR(ul_line_start[i + 1], p1);
 }
 
 
@@ -115,19 +120,22 @@ static inline void
 unsieve_5(unsigned char *line_start, const unsigned int start_idx,
           sieve_info_srcptr si)
 {
-  const unsigned int I_ul  = si->I / sizeof (unsigned long);
+  const unsigned int I_upt  = si->I / sizeof (unsieve_pattern_t);
   unsigned int i;
-  unsigned long p0, p1, p2, p3, p4;
-  unsigned long * restrict ul_line_start = (unsigned long *) line_start;
+  unsieve_pattern_t p0, p1, p2, p3, p4;
+  unsieve_pattern_t * restrict ul_line_start = (unsieve_pattern_t *) line_start;
   size_t pattern_idx;
 
-  if (sizeof(unsigned long) == 4)
+  if (sizeof(unsieve_pattern_t) == 4) {
     /* -4^(-1) == 1 (mod 5) */
     pattern_idx = start_idx;
-  else if (sizeof(unsigned long) == 8)
+  } else if (sizeof(unsieve_pattern_t) == 8) {
     /* -8^(-1) == 3 (mod 5) */
     pattern_idx = (3 * start_idx) % 5;
-  else
+  } else if (sizeof(unsieve_pattern_t) == 16) {
+    /* -16^(-1) == -1 (mod 5) */
+    pattern_idx = (5 - start_idx) % 5;
+  } else
     abort();
   
   p0 = si->us->pattern5[pattern_idx];
@@ -139,22 +147,79 @@ unsieve_5(unsigned char *line_start, const unsigned int start_idx,
   ASSERT_ALWAYS(((unsigned char *)&p0)[start_idx] == 255);
   
   /* Apply pattern to array */
-  for (i = 0U; i < I_ul - 4U; i += 5U)
+  for (i = 0U; i < I_upt - 4U; i += 5U)
     {
-      ul_line_start[i] |= p0;
-      ul_line_start[i + 1] |= p1;
-      ul_line_start[i + 2] |= p2;
-      ul_line_start[i + 3] |= p3;
-      ul_line_start[i + 4] |= p4;
+      UNSIEVE_OR(ul_line_start[i], p0);
+      UNSIEVE_OR(ul_line_start[i + 1], p1);
+      UNSIEVE_OR(ul_line_start[i + 2], p2);
+      UNSIEVE_OR(ul_line_start[i + 3], p3);
+      UNSIEVE_OR(ul_line_start[i + 4], p4);
     }
-  if (i < I_ul)
-    ul_line_start[i] |= p0;
-  if (i + 1 < I_ul)
-    ul_line_start[i + 1] |= p1;
-  if (i + 2 < I_ul)
-    ul_line_start[i + 2] |= p2;
-  if (i + 3 < I_ul)
-    ul_line_start[i + 3] |= p3;
+  if (i < I_upt)
+    UNSIEVE_OR(ul_line_start[i], p0);
+  if (i + 1 < I_upt)
+    UNSIEVE_OR(ul_line_start[i + 1], p1);
+  if (i + 2 < I_upt)
+    UNSIEVE_OR(ul_line_start[i + 2], p2);
+  if (i + 3 < I_upt)
+    UNSIEVE_OR(ul_line_start[i + 3], p3);
+}
+
+static inline void
+unsieve_7(unsigned char *line_start, const unsigned int start_idx,
+          sieve_info_srcptr si)
+{
+  const unsigned int I_upt  = si->I / sizeof (unsieve_pattern_t);
+  unsigned int i;
+  unsieve_pattern_t p0, p1, p2, p3, p4, p5, p6;
+  unsieve_pattern_t * restrict ul_line_start = (unsieve_pattern_t *) line_start;
+  size_t pattern_idx;
+
+  if (sizeof(unsieve_pattern_t) == 4) {
+    /* -4^(-1) == 5 (mod 7) */
+    pattern_idx = (5 * start_idx) % 7;
+  } else if (sizeof(unsieve_pattern_t) == 8) {
+    /* -8^(-1) == -1 (mod 7) */
+    pattern_idx = (7 - start_idx) % 7;
+  } else if (sizeof(unsieve_pattern_t) == 16) {
+    /* -16^(-1) == 3 (mod 7) */
+    pattern_idx = (3 * start_idx) % 7;
+  } else
+    abort();
+  
+  p0 = si->us->pattern7[pattern_idx];
+  p1 = si->us->pattern7[(pattern_idx + 1) % 7];
+  p2 = si->us->pattern7[(pattern_idx + 2) % 7];
+  p3 = si->us->pattern7[(pattern_idx + 3) % 7];
+  p4 = si->us->pattern7[(pattern_idx + 4) % 7];
+  p5 = si->us->pattern7[(pattern_idx + 5) % 7];
+  p6 = si->us->pattern7[(pattern_idx + 6) % 7];
+
+  ASSERT_ALWAYS(((unsigned char *)&p0)[start_idx] == 255);
+  
+  /* Apply pattern to array */
+  for (i = 0U; i < I_upt - 6U; i += 7U)
+    {
+      UNSIEVE_OR(ul_line_start[i], p0);
+      UNSIEVE_OR(ul_line_start[i + 1], p1);
+      UNSIEVE_OR(ul_line_start[i + 2], p2);
+      UNSIEVE_OR(ul_line_start[i + 3], p3);
+      UNSIEVE_OR(ul_line_start[i + 4], p4);
+      UNSIEVE_OR(ul_line_start[i + 5], p5);
+      UNSIEVE_OR(ul_line_start[i + 6], p6);
+    }
+  if (i < I_upt)
+    UNSIEVE_OR(ul_line_start[i], p0);
+  if (i + 1 < I_upt)
+    UNSIEVE_OR(ul_line_start[i + 1], p1);
+  if (i + 2 < I_upt)
+    UNSIEVE_OR(ul_line_start[i + 2], p2);
+  if (i + 3 < I_upt)
+    UNSIEVE_OR(ul_line_start[i + 3], p3);
+  if (i + 4 < I_upt)
+    UNSIEVE_OR(ul_line_start[i + 4], p4);
+  if (i + 5 < I_upt)
+    UNSIEVE_OR(ul_line_start[i + 5], p5);
 }
 
 /* Set locations where gcd(i,j) != 1 to 255 */
@@ -177,11 +242,19 @@ unsieve_not_coprime (unsigned char *S, const int N, sieve_info_srcptr si)
           p = si->us->entries[c].lpf; /* set p to largest prime factor of c */
           start_idx = si->us->entries[c].start;
           c = si->us->entries[c].cof;
-          if (p <= 5)
+          if (p <= 7)
             break;
           unsieve_one_prime (line_start, p, y, start_idx, si);
         }
       
+      if (p == 7U)
+        {
+          unsieve_7(line_start, start_idx, si);
+          p = si->us->entries[c].lpf;
+          start_idx = si->us->entries[c].start;
+          c = si->us->entries[c].cof;
+        }
+
       if (p == 5U)
         {
           unsieve_5(line_start, start_idx, si);
--- sieve/las-unsieve.h
+++ sieve/las-unsieve.h
@@ -5,13 +5,23 @@ typedef struct {
     unsigned int lpf, cof, start;
 } unsieve_entry_t;
 
+#ifdef HAVE_SSE2
+#include <xmmintrin.h>
+typedef __m128 unsieve_pattern_t;
+#define UNSIEVE_OR(a,b) do{(a) = _mm_or_ps((a),(b));}while(0)
+#else
+typedef unsigned long unsieve_pattern_t;
+#define UNSIEVE_OR(a,b) do{(a) |= (b);}while(0)
+#endif
+
 struct unsieve_aux_data_s {
     /* entry[i].lpf is largest prime factor of i, for i < I,
        cof is the cofactor i/lpf^k s.t. lfp^k || i,
        start is (I/2) % lpf */
     unsieve_entry_t *entries;
-    unsigned long pattern3[3];
-    unsigned long pattern5[5];
+    unsieve_pattern_t pattern3[3];
+    unsieve_pattern_t pattern5[5];
+    unsieve_pattern_t pattern7[7];
 };
 typedef struct unsieve_aux_data_s unsieve_aux_data[1];
 typedef struct unsieve_aux_data_s * unsieve_aux_data_ptr;
