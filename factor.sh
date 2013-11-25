sethtroisi/cado-nfs

commit d3d6a82c8c5692a6b49398ef4c785413f72d1c04

Don't pass server.address if hostnames != localhost to make cadofactor listen on *

factor.sh
@@ -260,9 +260,9 @@ cp $file $t/param
 
 # Sets the machine description file
 if [ -z "$CADO_USEHOST" ] && [ "$hostnames" = localhost ]; then
-   host=localhost
+   server_address="server.address=localhost"
 else
-   host=`hostname --short`
+   server_address=""
 fi
 
 mkdir $t/tmp
@@ -276,7 +276,7 @@ if $python; then
   "${TIMEOUT[@]}" $PYTHON $cadofactor "$t/param" N=$n tasks.execpath="$bindir" \
   threads=$cores tasks.workdir="$t" slaves.hostnames="$hostnames" \
   slaves.nrclients=$slaves \
-  slaves.scriptpath="$scriptpath" server.address=$host \
+  slaves.scriptpath="$scriptpath" "$server_address" \
   slaves.basepath="$t/client/" "$@"
 else
   cat > $t/mach_desc <<EOF

commit 49cd3292bd577a7c217a7da002eaa04dbb76fe22

Fix bug introduced in d3d6a82c, pointed out by C. Clavier

factor.sh
@@ -261,8 +261,10 @@ cp $file $t/param
 # Sets the machine description file
 if [ -z "$CADO_USEHOST" ] && echo "$hostnames" | grep -E "^(localhost, *)*localhost$" > /dev/null
 then
-   server_address="server.address=localhost"
+   host=localhost # For the Perl script
+   server_address="server.address=localhost" # For the Python script
 else
+   host=`hostname --short`
    server_address=""
 fi
 