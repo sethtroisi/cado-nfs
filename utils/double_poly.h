2d852fe02e2e89db3ce0d275b41587b538dd76a5
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2013-12-13 10:51:11 +0100

    refactoring of polynomials with double coefficients

--- utils/double_poly.h
+++ utils/double_poly.h
@@ -22,8 +22,9 @@ typedef const double_poly_struct_t * double_poly_srcptr;
 void double_poly_init (double_poly_ptr, unsigned int);
 void double_poly_clear (double_poly_ptr);
 double double_poly_eval (double_poly_srcptr, const double);
-double fpoly_dichotomy (double_poly_srcptr, double, double, double, unsigned int);
-void   fpoly_print (FILE *, const double *f, const unsigned int deg, char *name);
+double double_poly_dichotomy (double_poly_srcptr, double, double, double,
+                              unsigned int);
+void double_poly_print (FILE *, double_poly_srcptr, char *name);
 
 
 #ifdef __cplusplus
