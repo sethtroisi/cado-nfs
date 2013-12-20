c669982e4d248dbc49fe4e8f715c82156103a8fc
Cyril Bouvier <cyril.bouvier@loria.fr>
2013-12-20 14:33:42 +0100

    Remove specific code for Francois algorithm (will be in another branch)

--- filter/reconstructlog.c
+++ filter/reconstructlog.c
@@ -147,9 +147,6 @@ sm_data_free (sm_data_t *d)
 static inline void
 add_sm_contribution (mpz_ptr l, sm_data_t *sm, int64_t a, uint64_t b)
 {
-#ifdef FOR_ALGO_FM
-  mpz_add_log_mod_si (l, sm->smlog[0], a, sm->q);
-#else
   mpz_poly_t SMres;
   mpz_poly_init(SMres, sm->F->deg);
   SMres->deg = 0;
@@ -159,7 +156,6 @@ add_sm_contribution (mpz_ptr l, sm_data_t *sm, int64_t a, uint64_t b)
   for (i = 0; i < sm->nbsm && i <= (unsigned int) SMres->deg; i++)
     mpz_add_log_mod_mpz (l, sm->smlog[i], SMres->coeff[i], sm->q);
   mpz_poly_free(SMres);
-#endif
 }
 #endif /* ifndef FOR_FFS */
 
@@ -267,11 +263,7 @@ static uint64_t
 write_log (const char *filename, mpz_t *log, mpz_t q, renumber_t tab,
            cado_poly poly, uint64_t known_log)
 {
-#ifdef FOR_ALGO_FM
-  uint64_t i, missing = 1;
-#else
   uint64_t i, missing = 0;
-#endif
   double tt = seconds();
   FILE *f = NULL;
 
@@ -282,13 +274,7 @@ write_log (const char *filename, mpz_t *log, mpz_t q, renumber_t tab,
 
   for (i = 0; i < tab->size; i++)
   {
-#ifdef FOR_ALGO_FM
-    if (i==0)
-      gmp_fprintf (f, "0 0 0 rat %Zd\n", log[tab->size]);
-    else if (mpz_sgn(log[i]) < 0) // we do not know the log if this ideal
-#else
 	  if (mpz_sgn(log[i]) < 0) // we do not know the log if this ideal
-#endif
       missing++;
     else if (tab->table[i] == RENUMBER_SPECIAL_VALUE)
     {
