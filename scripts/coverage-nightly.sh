b33f5400958ea7a653eac1633b04173dd7873141
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2014-01-13 15:00:28 +0100

    added new script

---
+++ scripts/coverage-nightly.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+export DIR=`mktemp -d /tmp/cado-cov.XXXXXXXXXXXX`
+export CADO_DEBUG=1
+export COV=1
+F=`mktemp /tmp/cado-cov.local.XXXXXXXXX.sh`
+cat > $F <<EOF
+CFLAGS="-O0 -g -fprofile-arcs -ftest-coverage"
+CXXFLAGS="-O0 -g -fprofile-arcs -ftest-coverage"
+EOF
+export LOCALFILE=$F
+$HOME/NFS/cado/scripts/nightly-test
+geninfo --no-checksum --ignore-errors gcov,source -q --output-filename $DIR/cado-nfs.info  ./ --no-external
+rm -rf ~/.webdir/cado/ || :
+genhtml   -o ~/.webdir/cado/ $DIR/cado-nfs.info
+rm -rf $DIR $F
