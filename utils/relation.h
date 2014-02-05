ffe6b67c07f352789f696824fbf76e535b447df5
Cyril Bouvier <cyril.bouvier@loria.fr>
2014-02-05 14:29:14 +0100

    Remove outdated relation_stream_* functions (replaced by filter_io.cpp)

--- utils/relation.h
+++ utils/relation.h
@@ -32,38 +32,6 @@ typedef struct {
   uint8_t nb_ap_alloc;	/* allocated space for ap */
 } relation_t;
 
-struct relation_stream_s {
-    FILE * source;
-    relation_t rel;     // This object is owned by the relation_stream
-                        // object. Callers may manipulate the relation_t
-                        // member, but not clear it.
-
-    // parameters. may be set by the caller after calling
-    // relation_stream_init.
-    
-    int parse_only_ab;  // used when only the (a,b) pair information
-                        // is interesting, e.g. for duplicates removal.
-                        
-    // note that if parse_only_ab is unset, the relation returned by
-    // relation_stream_get is sorted.
-    
-    // various stats stuff. May be used by the caller.
-    size_t pos;
-    uint64_t nrels;
-    uint64_t lnum;
-    // only valid after relation_stream_trigger_disp_progress
-    // of relation_stream_disp_progress_now_p
-    double dt, mb_s, rels_s;
-
-    // temporaries, + various stuff for internal use.
-    int pipe;   /* whether source was popen()ed */
-    double t0, t1;
-};
-
-typedef struct relation_stream_s relation_stream[1];
-typedef struct relation_stream_s * relation_stream_ptr;
-typedef const struct relation_stream_s * relation_stream_srcptr;
-
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -71,8 +39,6 @@ extern "C" {
 // Relation I/O
 extern void relation_init(relation_t *rel);
 extern void relation_clear(relation_t *rel);
-// extern int read_relation(relation_t *rel, const char *str);
-// extern int fread_relation(FILE *file, relation_t *rel);
 extern unsigned long findroot(long a, unsigned long b, unsigned long p);
 extern void computeroots(relation_t * rel);
 extern char * u64toa16 (char *p, uint64_t m);
@@ -83,7 +49,6 @@ extern char * d64toa16 (char *p, int64_t m);
 
 extern void fprint_relation(FILE *file, relation_t * rel, const char *);
 extern void fprint_relation_raw (FILE *file, relation_t * rel);
-extern void reduce_exponents_mod2 (relation_t *rel);
 
 /* FIXME: The following interface still strongly relies on the fact that
  * the rational side is [0] and the algebraic side is [1] */
@@ -94,30 +59,11 @@ extern void relation_add_prime (relation_t *rel, int side, unsigned long p);
 extern void relation_copy (relation_t *s, relation_t * r);
 
 
-/* reads over relations in a file, just discarding them */
-extern void skip_relations_in_file(FILE * file, int n) __attribute__((deprecated));
-
-/*
-extern int read_relation_quick_and_dirty(int64_t * pa, uint64_t * pb, char * line, FILE * f);
-extern int read_relation_from_stream(relation_t * rel, char * line, FILE * f);
-*/
-
 /* Not clear whether we're willing to insist on _not_ exposing these
  * three functions or not */
 extern void relation_provision_for_primes(relation_t * rel, int nr, int na);
 extern void relation_compress_rat_primes(relation_t * rel);
 extern void relation_compress_alg_primes(relation_t * rel);
-
-extern void relation_stream_init(relation_stream_ptr rs);
-extern void relation_stream_closefile(relation_stream_ptr rs);
-extern void relation_stream_clear(relation_stream_ptr rs);
-extern void relation_stream_openfile(relation_stream_ptr rs, const char * name);
-extern void relation_stream_bind(relation_stream_ptr rs, FILE * f);
-extern void relation_stream_unbind(relation_stream_ptr rs);
-extern int relation_stream_disp_progress_now_p(relation_stream_ptr rs);
-extern int relation_stream_get(relation_stream_ptr rs, char * line, size_t lsize, int forced_read, unsigned int ab_base, int allow_comment);
-extern int relation_stream_get_skip(relation_stream_ptr rs);
-extern void relation_stream_trigger_disp_progress(relation_stream_ptr rs);
 
 #ifdef __cplusplus
 }
