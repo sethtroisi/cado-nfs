c0a65c5240e98905e3c77909553c7e7200b2531f
Alexander Kruppa <akruppa@gmail.com>
2014-02-06 11:04:07 +0100

    Add prototype

--- tests/test_iter.h
+++ tests/test_iter.h
@@ -15,6 +15,7 @@ struct test_iter_s {
 typedef struct test_iter_s test_iter_t[1];
 
 void test_iter_init (test_iter_t, unsigned long, test_iter_next_func_ptr);
+void test_iter_next (void *, test_iter_t);
 int test_iter_isdone (test_iter_t);
 void test_iter_int64_next(void *, test_iter_t);
 void test_iter_uint64_next(void *, test_iter_t);
