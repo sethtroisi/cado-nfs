16e9e87c6352ec980057f58e84e93d5cd999316e
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2014-02-06 23:22:46 +0100

    added test scripts for polynomial selection

---
+++ tests/polyselect/polysel.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash                                                             
+BINDIR="$1"
+DEGREE="$2"
+P="$3"
+ADMAX="$4"
+NQ="$5"
+POLYSELECT="${BINDIR}/polyselect/polyselect2l"
+make polyselect2l
+"$POLYSELECT" -N 90377629292003121684002147101760858109247336549001090677693 -degree "$DEGREE" -P "$P" -admax "$ADMAX" -nq "$NQ"
