d3ca8db1bbf478b46da27922725e664a4ddd94b9
Cyril Bouvier <cyril.bouvier@loria.fr>
2014-02-20 17:53:02 +0100

    twoquadratics.c: can now generate skewed polynomials

--- polyselect/twoquadratics.c
+++ polyselect/twoquadratics.c
@@ -15,16 +15,15 @@ typedef struct {
 
 typedef mpz_vector_struct_t mpz_vector_t[1];
 typedef mpz_vector_struct_t * mpz_vector_ptr;
-typedef const mpz_vector_struct_t * mpz_vector_srcptr;
 
 /* Management of the structure, set and print coefficients. */
-void mpz_vector_init(mpz_vector_t v, unsigned int l)
+void mpz_vector_init(mpz_vector_t v, unsigned int d)
 {
-  v->dim = l;
-  ASSERT_ALWAYS (l > 0);
-  v->c = (mpz_t *) malloc (l * sizeof(mpz_t));
+  ASSERT_ALWAYS (d > 0);
+  v->dim = d;
+  v->c = (mpz_t *) malloc (d * sizeof(mpz_t));
   ASSERT_ALWAYS (v->c != NULL);
-  for (unsigned int i = 0; i < l; i++)
+  for (unsigned int i = 0; i < d; i++)
     mpz_init (v->c[i]);
 }
 
@@ -69,6 +68,12 @@ void mpz_vector_setcoordinate_ui (mpz_vector_t v, unsigned int i,
   mpz_set_ui (v->c[i], z);
 }
 
+int
+mpz_vector_is_coordinate_zero (mpz_vector_t v, unsigned int i)
+{
+  ASSERT_ALWAYS (i < v->dim);
+  return (mpz_sgn (v->c[i]) == 0);
+}
 
 void mpz_vector_dot_product (mpz_t res, mpz_vector_t a, mpz_vector_t b)
 {
@@ -83,6 +88,35 @@ void mpz_vector_norm (mpz_t res, mpz_vector_t a)
   mpz_vector_dot_product (res, a, a);
 }
 
+void mpz_vector_skew_dot_product (mpz_t res, mpz_vector_t a, mpz_vector_t b,
+                                  mpz_t skewness)
+{
+  ASSERT_ALWAYS (a->dim == b->dim);
+  mpz_t tmp, s, s2;
+  mpz_init (tmp);
+  mpz_init (s2);
+  mpz_init_set_ui (s, 1);
+  mpz_mul (s2, skewness, skewness);
+
+  mpz_mul(res, a->c[0], b->c[0]);
+  for (unsigned int i = 1; i < a->dim; i++)
+  {
+    mpz_mul(tmp, a->c[i], b->c[i]);
+    mpz_mul(s, s, s2);
+    mpz_addmul(res, tmp, s);
+  }
+
+  mpz_clear(tmp);
+  mpz_clear(s2);
+  mpz_clear(s);
+}
+
+void mpz_vector_skew_norm (mpz_t res, mpz_vector_t a, mpz_t skewness)
+{
+  mpz_vector_skew_dot_product (res, a, a, skewness);
+}
+
+
 /* compute r <- r - q*v */
 void
 mpz_vector_submul (mpz_vector_t r, mpz_t q, mpz_vector_t v)
@@ -91,6 +125,103 @@ mpz_vector_submul (mpz_vector_t r, mpz_t q, mpz_vector_t v)
   for (unsigned int i = 0; i < r->dim; i++)
     mpz_submul(r->c[i], q, v->c[i]);
 }
+
+
+
+typedef struct {
+  cado_poly poly;
+  mpz_t m;
+  mpz_t p;  // common root is m/p mod N
+  mpz_t skew;
+  double note;
+} cado_poly_extended_s;
+
+typedef cado_poly_extended_s cado_poly_extended[1];
+typedef cado_poly_extended_s *cado_poly_extended_ptr;
+
+void
+cado_poly_extended_init (cado_poly_extended poly)
+{
+  cado_poly_init (poly->poly);
+  mpz_init (poly->m);
+  mpz_init (poly->p);
+  mpz_init (poly->skew);
+  poly->note = 0.0;
+}
+
+void
+cado_poly_extended_clear (cado_poly_extended poly)
+{
+  cado_poly_clear (poly->poly);
+  mpz_clear (poly->m);
+  mpz_clear (poly->p);
+  mpz_clear (poly->skew);
+  poly->note = 0.0;
+}
+
+void
+cado_poly_extended_set (cado_poly_extended poly, mpz_poly_t f, mpz_poly_t g,
+                        mpz_t N, mpz_t m, mpz_t p, mpz_t skew, double note)
+{
+  mpz_poly_set (poly->poly->pols[0], f->coeff, f->deg);
+  mpz_poly_set (poly->poly->pols[1], g->coeff, g->deg);
+  mpz_set (poly->poly->n, N);
+  poly->poly->skew = mpz_get_d (skew);
+  mpz_invert (poly->poly->m, p, N);
+  mpz_mul (poly->poly->m, poly->poly->m, m);
+  mpz_mod (poly->poly->m, poly->poly->m, N);
+
+  mpz_set (poly->m, m);
+  mpz_set (poly->p, p);
+  mpz_set (poly->skew, skew);
+  poly->note = note;
+}
+
+void
+cado_poly_extended_set_note (cado_poly_extended poly, double note)
+{
+  poly->note = note;
+}
+
+double
+cado_poly_extended_get_note (cado_poly_extended poly)
+{
+  return poly->note;
+}
+
+void
+cado_poly_extended_print (FILE *out, cado_poly_extended poly, char *pre)
+{
+  mpz_t tmp;
+  mpz_init (tmp);
+  double s = poly->poly->skew;
+
+  mpz_poly_ptr f0 = poly->poly->pols[0];
+  mpz_poly_ptr f1 = poly->poly->pols[1];
+
+  gmp_fprintf (out, "%sN = %Zd\n", pre, poly->poly->n);
+
+  fprintf(out, "%sf0 = ", pre);
+  mpz_poly_fprintf (out, f0);
+  fprintf(out, "%sf1 = ", pre);
+  mpz_poly_fprintf (out, f1);
+
+  fprintf (out, "%snote = %e\n", pre, poly->note);
+  fprintf (out, "%salpha_f0 = %.2f\n", pre, get_alpha (f0, ALPHA_BOUND));
+  fprintf (out, "%salpha_f1 = %.2f\n", pre, get_alpha (f1, ALPHA_BOUND));
+  gmp_fprintf (out, "%sskewness = %Zd\n", pre, poly->skew);
+  fprintf (out, "%sL2_skew_norm_f0 = %.2f\n", pre, L2_lognorm (f0, s));
+  fprintf (out, "%sL2_skew_norm_f1 = %.2f\n", pre, L2_lognorm (f1, s));
+  gmp_fprintf (out, "%sp = %Zd\n", pre, poly->p);
+  gmp_fprintf (out, "%sm = %Zd\n", pre, poly->m);
+  mpz_mod (tmp, poly->m, poly->p);
+  gmp_fprintf (out, "%sr = %Zd\n", pre, tmp);
+  gmp_fprintf (out, "%scommon_root = %Zd\n", pre, poly->poly->m);
+
+  mpz_clear (tmp);
+}
+
+
 
 /* return 0 if gcd(a, b) != 1, return non-zero if a and b are coprime */
 int
@@ -104,17 +235,34 @@ mpz_coprime_p (mpz_t a, mpz_t b)
   return (ret == 0) ? 1 : 0;
 }
 
-/* Set res to the smaller integer greater or equal to b, such that
-   res = r mod M
+/* Set m to the nearest integer to m0, such that
+   m = r mod P, i.e. find the integer in [m-p/2,m+p/2] which is congruent to
+   r modulo P.
  */
 void
-compute_m (mpz_t res, mpz_t b, mpz_t r, mpz_t M)
+compute_m (mpz_t m, mpz_t m0, mpz_t r, mpz_t P)
 {
-  mpz_mod (res, b, M);   // res = b mod M
-  mpz_sub (res, r, res); // res = r - (b mod M)
-  if (mpz_sgn (res) < 0) // add M if res < 0
-    mpz_add (res, res, M);
-  mpz_add (res, b, res); // res = b + r - (b mod M) (and maybe + M)
+  mpz_t t;
+  mpz_init (t);
+  mpz_tdiv_q_2exp (m, P, 1); // m = floor(p/2)
+  mpz_add (m, m ,m0);        // m = m0 + floor(p/2)
+  mpz_sub (t, m ,r);         // t = m0 + floor(p/2) - r
+  mpz_mod (t, t, P);         // t = (m0 + floor(p/2) -r) mod P
+  mpz_sub (m, m, t);         // m = m0 + floor(p/2) - t
+                             // By definition, (m0 + floor(p/2) - t) = r mod P
+  mpz_clear (t);
+}
+
+/* Compute maximun skewness, which in this case is (1/6)^(1/4) * m^(1/2).
+   We approximate (1/6)^(1/4) by 3124081/4889451 (the difference is less than
+   10^-14).
+ */
+void
+compute_default_max_skew (mpz_t skew, mpz_t m)
+{
+  mpz_sqrt (skew, m);
+  mpz_mul_ui (skew, skew, 3124081);
+  mpz_tdiv_q_ui (skew, skew, 4889451);
 }
 
 /* Returns the square root of N modulo P, using Tonelli-Shanks' algorithm.
@@ -160,7 +308,7 @@ mpz_msqrt (mpz_t r, mpz_t N, mpz_t P)
     mpz_sub_ui (Q, Q, 1);
     mpz_tdiv_q_2exp (Q, Q, 1);
     mpz_powm (R, n, Q, P); // R = n ^ ((Q-1)/2) mod P
-    mpz_mul (t, R, R);      
+    mpz_mul (t, R, R);
     mpz_mod (t, t, P);       // t = n^(Q-1) mod P
     mpz_mul (R, R, n);
     mpz_mod (R, R, P);       // R = n ^ ((Q+1)/2) mod P
@@ -192,7 +340,7 @@ mpz_msqrt (mpz_t r, mpz_t N, mpz_t P)
       mpz_mod(t, t, P);
       M = i;
     }
-        
+
   mpz_set (r, R);
 
   mpz_clear(Q);
@@ -209,10 +357,10 @@ mpz_msqrt (mpz_t r, mpz_t N, mpz_t P)
 
 /* Lagrange Algorithm.
    Reduce lattice of rank 2.
-   The obtained basis is always the two shortest vector. 
+   The obtained basis is always the two shortest vector.
    See, Nguyen And Vallee, "The LLL algorithm: Survey and applications" p.41
    */
-void LagrangeAlgo (mpz_vector_t a, mpz_vector_t b)
+void LagrangeAlgo (mpz_vector_t a, mpz_vector_t b, mpz_t skewness)
 {
   mpz_t norm_a, norm_b, q, r, tmp;
   mpz_init (norm_a);
@@ -221,8 +369,19 @@ void LagrangeAlgo (mpz_vector_t a, mpz_vector_t b)
   mpz_init (r);
   mpz_init (tmp);
 
-  mpz_vector_norm (norm_a, a);
-  mpz_vector_norm (norm_b, b);
+  int skew = (mpz_cmp_ui (skewness, 1) > 0);
+
+  if (skew)
+  {
+    mpz_vector_skew_norm (norm_a, a, skewness);
+    mpz_vector_skew_norm (norm_b, b, skewness);
+  }
+  else
+  {
+    mpz_vector_norm (norm_a, a);
+    mpz_vector_norm (norm_b, b);
+  }
+
   if (mpz_cmp (norm_a, norm_b) < 0)
   {
     mpz_vector_swap (a, b);
@@ -230,12 +389,18 @@ void LagrangeAlgo (mpz_vector_t a, mpz_vector_t b)
   }
   while (mpz_cmp (norm_a, norm_b) > 0)
   {
-    mpz_vector_dot_product (tmp, a, b);
+    if (skew)
+      mpz_vector_skew_dot_product (tmp, a, b, skewness);
+    else
+      mpz_vector_dot_product (tmp, a, b);
     mpz_ndiv_q (q, tmp, norm_b);
-    mpz_vector_submul (a, q, b); 
+    mpz_vector_submul (a, q, b);
     mpz_vector_swap (a, b);
     mpz_set (norm_a, norm_b);
-    mpz_vector_norm (norm_b, b);
+    if (skew)
+      mpz_vector_skew_norm (norm_b, b, skewness);
+    else
+      mpz_vector_norm (norm_b, b);
   }
 
   mpz_clear (norm_a);
@@ -255,20 +420,24 @@ void LagrangeAlgo (mpz_vector_t a, mpz_vector_t b)
    We compute a reduced basis of the lattice spanned by a and b with the
    Lagrange algorithm.
  */
-void MontgomeryTwoQuadratics (mpz_poly_t f, mpz_poly_t g, mpz_t N,
-                              mpz_t P, mpz_t m)
+void MontgomeryTwoQuadratics (mpz_poly_t f, mpz_poly_t g, mpz_t skew, mpz_t N,
+                              mpz_t P, mpz_t m, mpz_t max_skewness)
 {
   ASSERT_ALWAYS (mpz_coprime_p(P, N));
 
-  mpz_vector_t a, b;
+  mpz_vector_t a, b, reduced_a, reduced_b;
   mpz_vector_init (a, 3);
   mpz_vector_init (b, 3);
+  mpz_vector_init (reduced_a, 3);
+  mpz_vector_init (reduced_b, 3);
 
-  mpz_t tmp, c2, t;
+  mpz_t tmp, c2, t, max_skew, min_skew;
   mpz_init(tmp);
   mpz_init(c2);
   mpz_init(t);
-  
+  mpz_init(max_skew);
+  mpz_init(min_skew);
+
   // compute c2
   mpz_mul (tmp, m, m);
   mpz_sub (tmp, tmp, N);
@@ -296,18 +465,57 @@ void MontgomeryTwoQuadratics (mpz_poly_t f, mpz_poly_t g, mpz_t N,
   mpz_vector_setcoordinate (b, 1, tmp); // b[1] = -t
   mpz_vector_setcoordinate_ui (b, 2, 1); // b[2] = 1
 
-  // Perform Lagrange algorithm a and b
-  LagrangeAlgo (a, b);
+  /* Find the maximun value of skew for which LagrangeAlgo returns 2 polynomials
+   * of degree 2. By taking skew <= default_max_skew, we already know that the
+   * first polynomial (the one corresponding to a) will have degree 2, so we
+   * just check that b[2] != 0.
+   */
+  mpz_set_ui (min_skew, 1);
+  compute_default_max_skew (max_skew, m);
+  // Overwrite the maximum default value if max_skewness argument is greater
+  // than 0 and lesser than default value
+  if (mpz_cmp_ui(max_skewness, 0) > 0 && mpz_cmp(max_skewness, max_skew) < 0)
+    mpz_set (max_skew, max_skewness);
+
+  mpz_set (skew, max_skew);
+
+  do
+  {
+    mpz_vector_set (reduced_a, a);
+    mpz_vector_set (reduced_b, b);
+
+    // Perform Lagrange algorithm a and b
+    LagrangeAlgo (reduced_a, reduced_b, skew);
+
+    if (mpz_vector_is_coordinate_zero (reduced_b, 2))
+      mpz_set (max_skew, skew);
+    else
+    {
+      mpz_sub (tmp, max_skew, skew);
+      if (mpz_cmp_ui (tmp, 1) == 0)
+      {
+        mpz_set (min_skew, skew);
+        mpz_set (max_skew, skew);
+      }
+      else
+        mpz_set (min_skew, skew);
+    }
+    mpz_add (skew, min_skew, max_skew);
+    mpz_tdiv_q_2exp (skew, skew, 1);
+  } while (mpz_cmp(min_skew, max_skew) < 0); // min_skew < max_skew
+
+  mpz_vector_get_mpz_poly(f, reduced_a);
+  mpz_vector_get_mpz_poly(g, reduced_b);
 
   mpz_clear(tmp);
   mpz_clear(c2);
   mpz_clear(t);
-
-  mpz_vector_get_mpz_poly(f, a);
-  mpz_vector_get_mpz_poly(g, b);
-
+  mpz_clear(max_skew);
+  mpz_clear(min_skew);
   mpz_vector_clear (a);
   mpz_vector_clear (b);
+  mpz_vector_clear (reduced_a);
+  mpz_vector_clear (reduced_b);
 }
 
 
@@ -329,7 +537,11 @@ static void declare_usage(param_list pl)
 {
   param_list_decl_usage(pl, "N", "input number (default c59)");
   param_list_decl_usage(pl, "minP", "Use P > minP (default 2)");
-  param_list_decl_usage(pl, "maxP", "Use P <= maxP (default 500)");
+  param_list_decl_usage(pl, "maxP", "Use P <= maxP (default nextprime(minP))");
+  param_list_decl_usage(pl, "skewness", "maximun skewness possible "
+                                        "(default (1/6)^(1/4) * m^(1/2))");
+  param_list_decl_usage(pl, "v", "verbose output (print all polynomials)");
+  param_list_decl_usage(pl, "q", "quiet output (print only best polynomials)");
 }
 
 static void usage (const char *argv, param_list pl)
@@ -342,17 +554,22 @@ int
 main (int argc, char *argv[])
 {
   char *argv0 = argv[0];
+  int verbose = 0, quiet = 0;
 
-  mpz_t N, minP, maxP;
+  mpz_t N, minP, maxP, max_skewness;
   mpz_init (N);
   mpz_init (minP);
   mpz_init (maxP);
+  mpz_init (max_skewness);
 
   param_list pl;
-  
+
   /* read params */
   param_list_init(pl);
   declare_usage(pl);
+
+  param_list_configure_switch (pl, "-v", &verbose);
+  param_list_configure_switch (pl, "-q", &quiet);
 
   if (argc == 1)
     usage (argv[0], pl);
@@ -365,110 +582,129 @@ main (int argc, char *argv[])
     usage (argv0, pl);
   }
 
+  if (!param_list_parse_mpz(pl, "skewness", max_skewness))
+    mpz_set_ui (max_skewness, 0);
+  else if (mpz_cmp_ui (max_skewness, 1) < 0)
+  {
+    gmp_fprintf(stderr, "Error, skewness (%Zd) should be greater or equal "
+                        "to 1\n", max_skewness);
+    abort();
+  }
+
   if (!param_list_parse_mpz(pl, "minP", minP))
     mpz_set_ui (minP, 2);
   if (!param_list_parse_mpz(pl, "maxP", maxP))
-    mpz_set_ui (maxP, 500);
+    mpz_nextprime(maxP, minP);
   if (!param_list_parse_mpz(pl, "N", N))
     mpz_set_str (N,
         "71641520761751435455133616475667090434063332228247871795429", 10);
+
+  if (mpz_cmp_ui (minP, 0) < 0)
+  {
+    gmp_fprintf(stderr, "Error, minP (%Zd) should be greater or equal to 0\n",
+                        minP);
+    abort();
+  }
+  if (mpz_cmp (maxP, minP) < 0)
+  {
+    gmp_fprintf(stderr, "Error, maxP (%Zd) should be greater or equal to minP "
+                        "(%Zd)\n", maxP, minP);
+    abort();
+  }
+  if (mpz_cmp_ui (N, 3) < 0)
+  {
+    gmp_fprintf(stderr, "Error, N (%Zd) should be greater or equal to 3\n", N);
+    abort();
+  }
+  if (mpz_divisible_ui_p (N, 2))
+  {
+    gmp_fprintf(stderr, "Error, N (%Zd) should not be divisible by 2\n", N);
+    abort();
+  }
+
+  if (quiet)
+    verbose = -1;
 
   if (param_list_warn_unused(pl))
     usage (argv0, pl);
   param_list_print_command_line (stdout, pl);
 
-  gmp_printf("N = %Zd\n", N);
+  gmp_printf("### N = %Zd\n", N);
 
 /* Given an integer N and bounds on P, tests all pairs of polynomials
    with P such that minP < P <= maxP and print the pair having the best
    rating, along with its skewness, norm, alpha(f), alpha(g), and the
    prime number P which gave it
  */
-  double best_note = INFINITY, cur_note;
-  mpz_t P, best_P, sqrtN, r, m, best_m;
-  mpz_poly_t f, best_f, g, best_g;
-  
+  cado_poly_extended best_poly, poly;
+  mpz_t P, sqrtN, r, m, skew_used;
+  mpz_poly_t f, g;
+
+  cado_poly_extended_init (best_poly);
+  cado_poly_extended_init (poly);
+  cado_poly_extended_set_note (best_poly, DBL_MAX);
   mpz_init(P);
-  mpz_init(best_P);
   mpz_init(sqrtN);
   mpz_init(r);
   mpz_init(m);
-  mpz_init(best_m);
+  mpz_init(skew_used);
   mpz_poly_init (f, 2);
-  mpz_poly_init (best_f, 2);
   mpz_poly_init (g, 2);
-  mpz_poly_init (best_g, 2);
 
   // sqrtN = floor(sqrt(N))
-  mpz_sqrt(sqrtN, N);  
+  mpz_sqrt(sqrtN, N);
 
   mpz_nextprime (P, minP);
   while(mpz_cmp (P, maxP) <= 0)
   {
     if (mpz_kronecker(N, P) == 1)
     {
-
       for (unsigned int k = 0; k < 2; k++)
       {
         if (k == 0) // For the first time compute r = sqrt of N modulo P
         {
           int ret = mpz_msqrt (r, N, P);
-          ASSERT_ALWAYS (ret != 0); 
+          ASSERT_ALWAYS (ret != 0);
         }
         else // The second root mod P is P-r
           mpz_sub(r, P, r);
 
+        if (verbose >= 0)
+          gmp_printf("### P = %Zd: compute polynomials for r = %Zd\n", P, r);
         // m is the first integer >= sqrtN such that m = r (mod P)
         compute_m (m, sqrtN, r, P);
-        MontgomeryTwoQuadratics (f, g, N, P, m);
-        cur_note = notation (f, g);
-        if(cur_note < best_note)
+        MontgomeryTwoQuadratics (f, g, skew_used, N, P, m, max_skewness);
+        double note = notation (f, g);
+        if(note < cado_poly_extended_get_note(best_poly))
+          cado_poly_extended_set (best_poly, f, g, N, m, P, skew_used, note);
+        if (verbose >= 1)
         {
-          mpz_poly_copy (best_f, f);
-          mpz_poly_copy (best_g, g);
-          best_note = cur_note;
-          mpz_set (best_P, P);
-          mpz_set (best_m, m);
+          cado_poly_extended_set (poly, f, g, N, m, P, skew_used, note);
+          cado_poly_extended_print (stdout, poly, "# ");
         }
       }
     }
+    else if (verbose >= 0)
+      gmp_printf("### P = %Zd: N is not a square modulo P, skipping it.\n", P);
     mpz_nextprime(P, P);
   }
 
-  printf("# Best polynomials found:\nf = ");
-  mpz_poly_fprintf (stdout, best_f);
-  printf("g = ");
-  mpz_poly_fprintf (stdout, best_g);
+  printf("### Best polynomials found:\n");
+  cado_poly_extended_print (stdout, best_poly, "");
 
-  printf ("note = %e\n", best_note);
-  printf ("alpha_f = %.2f\n", get_alpha (best_f, ALPHA_BOUND));
-  printf ("alpha_g = %.2f\n", get_alpha (best_g, ALPHA_BOUND));
-  double skewness = L2_skewness (best_f, SKEWNESS_DEFAULT_PREC);
-  printf ("Skewness = %f\n", skewness);
-  printf ("L2_skew_norm_f = %.2f\n", L2_lognorm (best_f, skewness));
-  printf ("L2_skew_norm_g = %.2f\n", L2_lognorm (best_g, skewness));
-  gmp_printf ("P = %Zd\n", best_P);
-  gmp_printf ("m = %Zd\n", best_m);
-  mpz_mod (r, best_m, best_P);
-  gmp_printf ("# r = m (mod P)\nr = %Zd\n", r);
-  mpz_invert (r, best_P, N);
-  mpz_mul (r, r, best_m);
-  mpz_mod (r, r, N);
-  gmp_printf ("# common_root = m / P (mod  N)\ncommon_root = %Zd\n", r);
-
-  mpz_clear(best_P);
   mpz_clear(P);
   mpz_clear(sqrtN);
   mpz_clear(r);
   mpz_clear(m);
-  mpz_clear(best_m);
 
   mpz_poly_clear (f);
-  mpz_poly_clear (best_f);
   mpz_poly_clear (g);
-  mpz_poly_clear (best_g);
 
+  cado_poly_extended_clear (poly);
+  cado_poly_extended_clear (best_poly);
   mpz_clear (N);
+  mpz_clear (skew_used);
+  mpz_clear (max_skewness);
   mpz_clear (minP);
   mpz_clear (maxP);
   param_list_clear(pl);
