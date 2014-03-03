3e0fe498bb484b7355457a6e624ffe0a2c4fa964
Alexander Kruppa <akruppa@gmail.com>
2014-03-03 17:38:34 +0100

    Remove leftover debugging output

--- scripts/cadofactor/wuclient2.py
+++ scripts/cadofactor/wuclient2.py
@@ -651,7 +651,6 @@ class WorkunitProcessor(object):
                 # index is 0-based, add 1 to make FILE1, FILE2, etc. 1-based
                 files["%s%d" % (key, index + 1)] = \
                         os.path.join(dirs[key], filename)
-        print(files)
         for (counter, command) in enumerate(self.workunit.get("COMMAND", [])):
             paths = {"DLDIR":self.settings["DLDIR"], 
                      "WORKDIR":self.settings["WORKDIR"],
