3001ff431e55872ed71fe95c25e21720b22cfeb4
Alexander Kruppa <Alexander.Kruppa@loria.fr>
2014-05-26 17:30:53 +0200

    Allow like operator in wudb queries

--- scripts/cadofactor/wudb.py
+++ scripts/cadofactor/wudb.py
@@ -139,7 +139,7 @@ class MyCursor(sqlite3.Cursor):
         
     # This is used in where queries; it converts from named arguments such as 
     # "eq" to a binary operator such as "="
-    name_to_operator = {"lt": "<", "le": "<=", "eq": "=", "ge": ">=", "gt" : ">", "ne": "!="}
+    name_to_operator = {"lt": "<", "le": "<=", "eq": "=", "ge": ">=", "gt" : ">", "ne": "!=", "like": "like"}
     
     def __init__(self, conn):
         # Enable foreign key support
