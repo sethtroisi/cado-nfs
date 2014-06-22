9bfc95e4d10f77baf3cadc2a57c8be5ee52f4c92
Shi Bai <shih.bai@gmail.com>
2014-06-23 00:49:38 +1000

    [polysel] fix sopt

--- polyselect/sopt.c
+++ polyselect/sopt.c
@@ -36,175 +36,177 @@ int fake = 0; // optimize anyway regardless of the common root condition
 /* Care: poly_print is named in utils.h */
 static void
 polyprint (mpz_t *f, mpz_t *g, int deg, mpz_t N) {
-	 int i;
-	 gmp_printf ("\nn: %Zd\n", N);
-	 for (i = deg; i >= 0; i --)
-	 {
-		  gmp_printf ("c%d: %Zd\n", i, f[i]);
-	 }
-	 for (i = 1; i >= 0; i --)
-	 {
-		  gmp_printf ("Y%d: %Zd\n", i, g[i]);
-	 }
+  int i;
+  gmp_printf ("\nn: %Zd\n", N);
+  for (i = deg; i >= 0; i --)
+  {
+    gmp_printf ("c%d: %Zd\n", i, f[i]);
+  }
+  for (i = 1; i >= 0; i --)
+  {
+    gmp_printf ("Y%d: %Zd\n", i, g[i]);
+  }
 }
 
 /* optimize all raw polynomial in the file. "skip" denotes position of raw poly */
 static int
 opt_file (FILE *file, int deg, mpz_t N) {
-	 int i, nroots;
-	 unsigned flag = 0UL, skip = 0UL, count = 0;
-	 char str[MAX_LINE_LENGTH];
-	 mpz_t g[2];
-	 mpz_t *f;
-         mpz_poly_t F;
-	 mpz_t t, M;
-	 double skew = 0.0, alpha = 0.0, logmu = 0.0;
-	 double ave_logmu = 0.0, ave_alpha = 0.0;
-	 mpz_init (t);
-	 mpz_init (M);
-	 mpz_init (g[1]);
-	 mpz_init (g[0]);
+  int i, nroots;
+  unsigned flag = 0UL, skip = 0UL, count = 0;
+  char str[MAX_LINE_LENGTH];
+  mpz_t g[2];
+  mpz_t *f;
+  mpz_poly_t F;
+  mpz_t t, M;
+  double skew = 0.0, alpha = 0.0, logmu = 0.0;
+  double ave_logmu = 0.0, ave_alpha = 0.0;
+  mpz_init (t);
+  mpz_init (M);
+  mpz_init (g[1]);
+  mpz_init (g[0]);
 
-         mpz_poly_init (F, deg);
-         F->deg = deg;
-         f = F->coeff;
-	 while (1) {
-		  if (fgets(str, MAX_LINE_LENGTH, file) == NULL)
-			   break; /* wrong line or EOF */
+  mpz_poly_init (F, deg);
+  F->deg = deg;
+  f = F->coeff;
+  while (1) {
+    if (fgets(str, MAX_LINE_LENGTH, file) == NULL)
+      break; /* wrong line or EOF */
 
-		  if ( str[0] == 'Y' ) {
-			   if ( str[1] == '1' ) {
-					gmp_sscanf (str, "Y1: %Zd\n", g[1]);
-					(flag) ^= ( 1<<(8) );
-			   }
-			   else if ( str[1] == '0' ) {
-					gmp_sscanf (str, "Y0: %Zd\n", g[0]);
-					(flag) ^= ( 1<<(7) );
-			   }
-			   else {
-					fprintf (stderr, "Error in parsing line %s", str);
-					exit (1);
-			   }
-		  }
-		  // TODO: add a readline-like function later.
-		  else if ( str[0] == 'c') {
-			   if ( str[1] == '0' ) {
-					gmp_sscanf (str, "c0: %Zd\n", f[0]);
-					(flag) ^= ( 1<<(0) );
-			   }
-			   else if ( str[1] == '1' ) {
-					gmp_sscanf (str, "c1: %Zd\n", f[1]);
-					(flag) ^= ( 1<<(1) );
-			   }
-			   else if ( str[1] == '2' ) {
-					gmp_sscanf (str, "c2: %Zd\n", f[2]);
-					(flag) ^= ( 1<<(2) );
-			   }
-			   else if ( str[1] == '3' ) {
-					gmp_sscanf (str, "c3: %Zd\n", f[3]);
-					(flag) ^= ( 1<<(3) );
-			   }
-			   else if ( str[1] == '4' ) {
-					gmp_sscanf (str, "c4: %Zd\n", f[4]);
-					(flag) ^= ( 1<<(4) );
-			   }
-			   else if ( str[1] == '5' ) {
-					gmp_sscanf (str, "c5: %Zd\n", f[5]);
-					(flag) ^= ( 1<<(5) );
-			   }
-			   else if ( str[1] == '6' ) {
-					gmp_sscanf (str, "c6: %Zd\n", f[6]);
-					(flag) ^= ( 1<<(6) );
-			   }
-			   else
-			   {
-					fprintf (stderr, "Error in parsing line %s", str);
-					exit (1);
-			   }
-		  }
-		  else
-			   continue;
+    if ( str[0] == 'Y' ) {
+      if ( str[1] == '1' ) {
+        gmp_sscanf (str, "Y1: %Zd\n", g[1]);
+        (flag) ^= ( 1<<(8) );
+      }
+      else if ( str[1] == '0' ) {
+        gmp_sscanf (str, "Y0: %Zd\n", g[0]);
+        (flag) ^= ( 1<<(7) );
+      }
+      else {
+        fprintf (stderr, "Error in parsing line %s", str);
+        exit (1);
+      }
+    }
+    // TODO: add a readline-like function later.
+    else if ( str[0] == 'c') {
+      if ( str[1] == '0' ) {
+        gmp_sscanf (str, "c0: %Zd\n", f[0]);
+        (flag) ^= ( 1<<(0) );
+      }
+      else if ( str[1] == '1' ) {
+        gmp_sscanf (str, "c1: %Zd\n", f[1]);
+        (flag) ^= ( 1<<(1) );
+      }
+      else if ( str[1] == '2' ) {
+        gmp_sscanf (str, "c2: %Zd\n", f[2]);
+        (flag) ^= ( 1<<(2) );
+      }
+      else if ( str[1] == '3' ) {
+        gmp_sscanf (str, "c3: %Zd\n", f[3]);
+        (flag) ^= ( 1<<(3) );
+      }
+      else if ( str[1] == '4' ) {
+        gmp_sscanf (str, "c4: %Zd\n", f[4]);
+        (flag) ^= ( 1<<(4) );
+      }
+      else if ( str[1] == '5' ) {
+        gmp_sscanf (str, "c5: %Zd\n", f[5]);
+        (flag) ^= ( 1<<(5) );
+      }
+      else if ( str[1] == '6' ) {
+        gmp_sscanf (str, "c6: %Zd\n", f[6]);
+        (flag) ^= ( 1<<(6) );
+      }
+      else
+      {
+        fprintf (stderr, "Error in parsing line %s", str);
+        exit (1);
+      }
+    }
+    else
+      continue;
 
-		  // consider raw polynomial only
-		  if ( (flag == 511UL || flag == 447UL) &&  (skip == 0) ) {
+    // consider raw polynomial only
+    if ( (flag == 511UL || flag == 447UL) &&  (skip == 0) ) {
 
-			   /* M = -Y0/Y1 mod N */
-			   mpz_invert (M, g[1], N);
-			   mpz_neg (M, M);
-			   mpz_mul (M, M, g[0]);
-			   mpz_mod (M, M, N);
-			   /* check M ? a root of the algebraic polynomial mod N */
-			   mpz_init_set (t, f[deg]);
-			   for (i = deg - 1; i >= 0; i --) {
-					mpz_mul (t, t, M);
-					mpz_add (t, t, f[i]);
-					mpz_mod (t, t, N);
-			   }
+      /* M = -Y0/Y1 mod N */
+      mpz_invert (M, g[1], N);
+      mpz_neg (M, M);
+      mpz_mul (M, M, g[0]);
+      mpz_mod (M, M, N);
+      /* check M ? a root of the algebraic polynomial mod N */
+      mpz_init_set (t, f[deg]);
+      for (i = deg - 1; i >= 0; i --) {
+        mpz_mul (t, t, M);
+        mpz_add (t, t, f[i]);
+        mpz_mod (t, t, N);
+      }
          
-         if (!fake) {
-           if (mpz_cmp_ui (t, 0) != 0) {
-             fprintf (stderr, "This polynomials have no common root mod N\n");
-             polyprint (f, g, deg, N);
-             exit (1);
-           }
-         }
+      if (!fake) {
+        if (mpz_cmp_ui (t, 0) != 0) {
+          fprintf (stderr, "This polynomials have no common root mod N\n");
+          polyprint (f, g, deg, N);
+          exit (1);
+        }
+      }
 
-			   // need to output raw, since we may re-optimize this using updated optimize.c
-                           nroots = numberOfRealRoots (f, deg, 0, 0, NULL);
-			   skew = L2_skewness (F, SKEWNESS_DEFAULT_PREC);
-			   logmu = L2_lognorm (F, skew);
-			   alpha = get_alpha (F, ALPHA_BOUND);
-			   fprintf (stderr, "\n# Raw polynomial (#%6d):", count + 1);
-			   polyprint (f, g, deg, N);
-			   printf ("# lognorm %1.2f, alpha %1.2f, E %1.2f, %u rroots, skew: %f\n",
-					   logmu, alpha, logmu + alpha, nroots, skew);
-			   // optimize
-			   optimize (F, g, 0, 1); // no verbose
-			   // optimized polynomial
-			   nroots = numberOfRealRoots (f, deg, 0, 0, NULL);
-			   skew = L2_skewness (F, SKEWNESS_DEFAULT_PREC);
-			   logmu = L2_lognorm (F, skew);
-			   alpha = get_alpha (F, ALPHA_BOUND);
-			   fprintf (stderr, "# Optimized polynomial (#%10d): ", count + 1);
-			   polyprint (f, g, deg, N);
-			   printf ("# lognorm %1.2f, alpha %1.2f, E %1.2f, %u rroots, skew: %.2f\n",
-					   logmu, alpha, logmu + alpha, nroots, skew);
+      // need to output raw, since we may re-optimize this using updated optimize.c
+      nroots = numberOfRealRoots (f, deg, 0, 0, NULL);
+      skew = L2_skewness (F, SKEWNESS_DEFAULT_PREC);
+      logmu = L2_lognorm (F, skew);
+      alpha = get_alpha (F, ALPHA_BOUND);
+      fprintf (stderr, "\n# Raw polynomial (#%6d):", count + 1);
+      polyprint (f, g, deg, N);
+      printf ("# lognorm %1.2f, alpha %1.2f, E %1.2f, %u rroots, skew: %f\n",
+              logmu, alpha, logmu + alpha, nroots, skew);
+      // optimize
+      //optimize_aux (F, g, 0, 0); // no verbose
+      optimize (F, g, 0, 1); // no verbose
 
-			   ave_logmu += logmu;
-			   ave_alpha += alpha;
-			   // ignore next polynomial, which is optimized by some old method.
-			   count += 1;
-			   flag = 0UL;
-			   skip = 1UL;
-		  }
-		  /* skip optimized (may be by some old opt method) polynomials,
+      // optimized polynomial
+      nroots = numberOfRealRoots (f, deg, 0, 0, NULL);
+      skew = L2_skewness (F, SKEWNESS_DEFAULT_PREC);
+      logmu = L2_lognorm (F, skew);
+      alpha = get_alpha (F, ALPHA_BOUND);
+      fprintf (stderr, "# Optimized polynomial (#%10d): ", count + 1);
+      polyprint (f, g, deg, N);
+      printf ("# lognorm %1.2f, alpha %1.2f, E %1.2f, %u rroots, skew: %.2f\n",
+              logmu, alpha, logmu + alpha, nroots, skew);
+
+      ave_logmu += logmu;
+      ave_alpha += alpha;
+      // ignore next polynomial, which is optimized by some old method.
+      count += 1;
+      flag = 0UL;
+      skip = 0UL;
+    }
+    /* skip optimized (may be by some old opt method) polynomials,
 			 note, we assume "raw" and "optimized" polys appears
 			 in pair and sequential. So it is very fragile if the input file
 			 is broken for some reason (for instance, order of poly is not sequencial
 			 due to some threads problem.) */
-		  if ( (flag == 511UL || flag == 447UL) && (skip == 1) ) {
-			   flag = 0UL;
-			   skip = 0;
-		  }
-	 }
+    if ( (flag == 511UL || flag == 447UL) && (skip == 1) ) {
+      flag = 0UL;
+      skip = 0;
+    }
+  }
 
-	 fprintf(stderr, "\n# total num. of polys: %u\n", count);
-	 fprintf(stderr, "# ave. l2norm: %3.3f\n", ave_logmu / count);
-	 fprintf(stderr, "# ave. alpha: %3.3f\n", ave_alpha / count);
+  fprintf(stderr, "\n# total num. of polys: %u\n", count);
+  fprintf(stderr, "# ave. l2norm: %3.3f\n", ave_logmu / count);
+  fprintf(stderr, "# ave. alpha: %3.3f\n", ave_alpha / count);
 
-	 mpz_clear (g[0]);
-	 mpz_clear (g[1]);
-	 mpz_clear (t);
-	 mpz_clear (M);
-         mpz_poly_clear (F);
-	 return 0;
+  mpz_clear (g[0]);
+  mpz_clear (g[1]);
+  mpz_clear (t);
+  mpz_clear (M);
+  mpz_poly_clear (F);
+  return 0;
 }
 
 /* print usage and exit */
 static void
 usage (void) {
-	 fprintf (stderr, "Usage: polyopt [--fake] -f POLYFILE -d DEGREE -n NUMBER\n");
-	 exit(1);
+  fprintf (stderr, "Usage: polyopt [--fake] -f POLYFILE -d DEGREE -n NUMBER\n");
+  exit(1);
 }
 
 /* PRIME */
@@ -236,8 +238,8 @@ main (int argc, char **argv)
   /* parse n */
   int have_n = param_list_parse_mpz(pl, "n", N);
   if (!have_n) {
-      fprintf(stderr, "No N defined ; sorry.\n");
-      exit(1);
+    fprintf(stderr, "No N defined ; sorry.\n");
+    exit(1);
   }
   if (mpz_cmp_ui (N, 0) <= 0) usage();
 
