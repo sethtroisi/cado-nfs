bc7109777f5a498bcd9229c04c5980b55073f55f
Alain Filbois <alain.filbois@inria.fr>
2014-06-26 20:00:39 +0200

    [utils/antebuffer.c]: new version with shared variables protected by mutex.

--- utils/antebuffer.c
+++ utils/antebuffer.c
@@ -26,9 +26,6 @@
 #include "portability.h"
 #include "timing.h"
 
-static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-static pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
-
 #ifdef HAVE_MINGW
 int _CRT_fmode = _O_BINARY; /* Binary open for stdin/out/err */
 #endif
@@ -49,48 +46,63 @@ int _CRT_fmode = _O_BINARY; /* Binary open for stdin/out/err */
 #endif
 
 /* These variables are used by pthread and main */
-static volatile uint64_t ab_cptp = 0, ab_cptc = 0;   /* Main counters */
-static volatile int ab_end = 0;                      /* 1 if end */
-static char *ab_buf;                                 /* Main buffer */
-static const struct timespec waiting = { 0, 1<<13 }; /* About 5 to 20 microseconds */
-static int ab_in;                                    /* fd for loading */
-static unsigned int ab_size, ab_sizeio;              /* size for buffer & in */
+static volatile uintptr_t ab_cptp = 0, ab_cptc = 0;        /* Main counters */
+static volatile int ab_end = 0;                            /* 1 if end */
+static char *ab_buf;                                       /* Main buffer */
+static const struct timespec waiting = { 0, 1<<13 };       /* About 8 to 20 microseconds */
+static int ab_in;                                          /* fd for loading */
+static size_t ab_size, ab_sizeio;                          /* size for buffer & in */
 
-static
-void ab_cons () {
-  size_t cpy_ab_cptc = 0;
-  int w;
-  unsigned int c, t = 0;
+/* The 3 shared variables must be considered with non atomical access.
+   So, dedicated mutex protection for all.
+   NB: spinlocks are not mandatory in POSIX and don't exist in macos.
+   But it's so uncommon a mutex is blocking here than the mutex version
+   has the same speed than the spin locks version, and both are faster
+   than the gcc __builtin_fetch_and_add version.
+*/
+static pthread_mutex_t mutex_ab_cptp = PTHREAD_MUTEX_INITIALIZER;
+static pthread_mutex_t mutex_ab_cptc = PTHREAD_MUTEX_INITIALIZER;
+static pthread_mutex_t mutex_ab_end = PTHREAD_MUTEX_INITIALIZER;
+
+static void ab_cons () {
+  uintptr_t cpy_ab_cptc = 0;
+  size_t c, t = 0;
+  ssize_t w;
   
   pthread_setcanceltype (PTHREAD_CANCEL_DEFERRED, NULL);
   for ( ; ; ) {
-    pthread_mutex_lock (&mutex);
-    while (!(c = (int) (ab_cptp - cpy_ab_cptc))) {
-      if (UNLIKELY(ab_end)) {
-	pthread_mutex_unlock (&mutex);
+    for ( ; ; ) {
+      uintptr_t mut_ab_cptp;
+      int mut_ab_end;
+      pthread_mutex_lock (&mutex_ab_cptp); mut_ab_cptp = ab_cptp; pthread_mutex_unlock (&mutex_ab_cptp);
+      c = (mut_ab_cptp - cpy_ab_cptc);
+      if (LIKELY (c)) break;
+      pthread_mutex_lock (&mutex_ab_end); mut_ab_end = ab_end; pthread_mutex_unlock (&mutex_ab_end);
+      if (UNLIKELY (mut_ab_end)) {
+	pthread_mutex_lock (&mutex_ab_cptp); mut_ab_cptp = ab_cptp; pthread_mutex_unlock (&mutex_ab_cptp);
+	c = (int) (mut_ab_cptp - cpy_ab_cptc);
+	if (LIKELY (c)) break;
 	pthread_exit(NULL);
       }
-      pthread_cond_wait (&cond, &mutex);
+      nanosleep (&waiting, NULL);
     }
-    pthread_mutex_unlock (&mutex);
-
     if (c > ab_sizeio) c = ab_sizeio;
     if (c > ab_size - t) c = ab_size - t;
-    while ((w = (int) write(1, &(ab_buf[t]), (size_t) c)) <= 0) nanosleep(&waiting, NULL);
+    while ((w = write(1, &(ab_buf[t]), c)) <= 0) nanosleep (&waiting, NULL);
     cpy_ab_cptc += w;
     t = (t + w) & (ab_size - 1);
-    pthread_mutex_lock (&mutex);
-    ab_cptc = cpy_ab_cptc; pthread_cond_signal(&cond);
-    pthread_mutex_unlock (&mutex);
+    pthread_mutex_lock (&mutex_ab_cptc); ab_cptc = cpy_ab_cptc; pthread_mutex_unlock (&mutex_ab_cptc);
   }
 }
 
 int main(int argc, char **argv) {
   pthread_t ab_tc;
   pthread_attr_t ab_attr;
-  size_t cpy_ab_cptp = 0;
-  int r;
-  unsigned int t, c, p;
+  uintptr_t cpy_ab_cptp = 0;
+  size_t c, t = 0;
+  ssize_t r;
+  unsigned int p;
+  char *real_malloc;
 
 #ifdef HAVE_MINGW
   _fmode = _O_BINARY;     /* Binary open for all others files */
@@ -104,14 +116,15 @@ int main(int argc, char **argv) {
 	     "16<=SIZE<=31, best for 20 to 24.\n", argv[0], argv[0], argv[0]);
     exit (1);
   }
-  if ((sscanf(argv[1], "%u", &ab_size)) != 1) goto error;
+  if ((sscanf(argv[1], "%zu", &ab_size)) != 1) goto error;
   if (ab_size < 16 || ab_size > 31) goto error;
-  ab_size = 1UL << ab_size;
+  ab_size = (uintptr_t) 1 << ab_size;
   ab_sizeio = ab_size >> 4;
-  if (!(ab_buf = malloc(ab_size))) {
+  if (!(real_malloc = malloc (ab_size + 0xFFF))) {
     fprintf (stderr, "%s: malloc error: %s\n", argv[0], strerror(errno));
     exit(1);
   }
+  ab_buf = (char *) (((uintptr_t) real_malloc + 0xFFF) & ~ (uintptr_t) 0xFFF);
   pthread_attr_init(&ab_attr);
   pthread_attr_setstacksize(&ab_attr, 1<<12);
   pthread_attr_setdetachstate(&ab_attr, PTHREAD_CREATE_JOINABLE);
@@ -130,28 +143,26 @@ int main(int argc, char **argv) {
     p = 0;
     ab_in = 0;
   }
-  t = 0;
   for ( ; ; ) {
-    pthread_mutex_lock (&mutex);
-    while (!(c = (int) ((ab_cptc + ab_size) - cpy_ab_cptp))) pthread_cond_wait (&cond, &mutex);
-    pthread_mutex_unlock (&mutex);
-    
+    for ( ; ; ) {
+      uintptr_t mut_ab_cptc;
+      pthread_mutex_lock (&mutex_ab_cptc); mut_ab_cptc = ab_cptc; pthread_mutex_unlock (&mutex_ab_cptc);
+      c = ((mut_ab_cptc + ab_size) - cpy_ab_cptp);
+      if (LIKELY (c)) break;
+      nanosleep (&waiting, NULL);
+    }
     if (c > ab_sizeio)   c = ab_sizeio;
     if (c > ab_size - t) c = ab_size - t;
-    r = read(ab_in, &(ab_buf[t]), (size_t) c);
+    r = read(ab_in, &(ab_buf[t]), c);
     if (r > 0) {
       cpy_ab_cptp += r;
       t = (t + r) & (ab_size - 1);
-      pthread_mutex_lock (&mutex);
-      ab_cptp = cpy_ab_cptp; pthread_cond_signal(&cond);
-      pthread_mutex_unlock (&mutex);
+      pthread_mutex_lock (&mutex_ab_cptp); ab_cptp = cpy_ab_cptp; pthread_mutex_unlock (&mutex_ab_cptp);
     }
     else 
       if (!r) {
 	if (!p || p == (unsigned int) argc) {
-	  pthread_mutex_lock (&mutex);
-	  ab_end = 1; pthread_cond_signal(&cond);
-	  pthread_mutex_unlock (&mutex);
+	  pthread_mutex_lock (&mutex_ab_end); ab_end = 1; pthread_mutex_unlock (&mutex_ab_end);
 	  break;
 	}
 	close(ab_in);
@@ -167,13 +178,13 @@ int main(int argc, char **argv) {
   }
   if (p) close(ab_in);
   pthread_join(ab_tc, NULL);
-  pthread_mutex_destroy (&mutex);
-  pthread_cond_destroy (&cond);
-  free (ab_buf);
-  ab_buf = NULL;
+  pthread_mutex_destroy (&mutex_ab_end);
+  pthread_mutex_destroy (&mutex_ab_cptc);
+  pthread_mutex_destroy (&mutex_ab_cptp);
+  free (real_malloc);
   double tt[2];
   seconds_user_sys(tt);
-  fprintf(stderr, "antebuffer exits after having spent %.2fs+%2.fs on cpu\n",
+  fprintf(stderr, "antebuffer exits after having spent %.2fs+%.2fs on cpu\n",
           tt[0], tt[1]);
   exit (0);
 }
