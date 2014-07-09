b32920cff0e3d2da4f881dff05184a34ab0ea07a
Alain Filbois <alain.filbois@inria.fr>
2014-07-09 17:11:36 +0200

    [test_init_norms_bucket_region.c]: uses now tests_common.c + SMART_ERROR from 3 to 5.

--- tests/sieve/test_init_norms_bucket_region.c
+++ tests/sieve/test_init_norms_bucket_region.c
@@ -2,7 +2,7 @@
 #define MIN_LOGI 10
 #define MAX_LOGI 16      /* MIN_LOGI <= logi <= MAX_LOGI && MAX_LOGI <= LOG_BUCKET_REGION */
 #define MAX_DEGREE 10    /* All degree between 0 and MAX_DEGREE included are tested */
-#define MAX_SMART_ERR 4  /* The error in smart algo must be <= MAX_SMART_ERR */
+#define MAX_SMART_ERR 5  /* The error in smart algo must be <= MAX_SMART_ERR */
 #define MAX_EXACT_ERR 2  /* The error in "exact" algo must be <= MAX_EXACT_ERR */
 
 #include <stdlib.h>
@@ -11,17 +11,14 @@
 #include <stdio.h>
 #include <stdint.h>
 
+#include "cado_config.h"
+#include "utils/utils.h"
+#include "portability.h"
+#include "tests/tests_common.h"
 #include "sieve/las-config.h"
 #include "sieve/las-types.h"
 #include "sieve/las-debug.h"
 #include "sieve/las-norms.c"
-#include "utils/utils.h"
-#include "portability.h"
-
-/* RAND_MAX is at least 15 bits, so 5 calls to rand() are need to have a random 64 bits */
-static int64_t rand_int64_t () {
-  return ((int64_t) rand () ^ ((int64_t) rand () << 15) ^ ((int64_t) rand () << 30) ^ ((int64_t) rand () << 45) ^ ((int64_t) rand () << 60));
-}
 
 static inline void
 my_poly_scale (double *u, const double *t, unsigned int d, double h)
@@ -30,7 +27,8 @@ my_poly_scale (double *u, const double *t, unsigned int d, double h)
   for (double hpow = h; d--; hpow *= h) u[d] = t[d] * hpow;
 }
 
-int main() {
+int
+main(int argc, const char *argv[]) {
   size_t l, logI, k, d;
   uint32_t J, beginJ, endJ;
   double scale, log2max, h, g;
@@ -39,14 +37,17 @@ int main() {
     *S3 = malloc_pagealigned((1U<<LOG_BUCKET_REGION) + MEMSET_MIN), *cS3;
   double_poly_t poly;
   unsigned long long count, smart_err[(MAX_SMART_ERR<<1)+1], exact_err[(MAX_EXACT_ERR<<1)+1];
+  unsigned long iter = NB_TESTS;
   
+  tests_common_cmdline (&argc, &argv, PARSE_SEED | PARSE_ITER | PARSE_VERBOSE);
+  tests_common_get_iter (&iter);
+
   memset(smart_err, 0, sizeof(smart_err));
   memset(exact_err, 0, sizeof(exact_err));
   ASSERT_ALWAYS (MIN_LOGI <= MAX_LOGI);
   ASSERT_ALWAYS (MAX_LOGI <= LOG_BUCKET_REGION);
   ASSERT_ALWAYS (S1);
   ASSERT_ALWAYS (S2);
-  srand (getpid());
   for (logI = MIN_LOGI; logI <= MAX_LOGI; logI++) {
     const uint32_t I = 1U << logI;
     const double didiv2 = (double) (I >> 1);
@@ -56,11 +57,11 @@ int main() {
       struct root_s roots[4 * poly->deg + 1];
       unsigned int nroots;
       poly->coeff = coeff;
-      for (k = NB_TESTS; k--;) {
-	J = rand () ^ (rand() << 15);   /* 0 <= J < 2^30 at least */
-	J &= (I >> 1) - 1;              /* Original J; 0 <= J < I/2 */
-	for (l = poly->deg + 1; l--;) { /* (-2^63)^2 <= coeff <= (2^63-1)^2 */
-	  poly->coeff[l] = (double) rand_int64_t () * (double) rand_int64_t ();
+      for (k = iter; k--;) {
+	J = ((uint32_t) random_uint64()) << 2;    /* 0 <= J < 2^30 at least */
+	J &= (I >> 1) - 1;                        /* Original J; 0 <= J < I/2 */
+	for (l = poly->deg + 1; l--;) {           /* (-2^63)^2 <= coeff <= (2^63-1)^2 */
+	  poly->coeff[l] = (double) random_int64 () * (double) random_int64 ();
 	  if (UNLIKELY(poly->coeff[l] == 0.)) ++l; /* coef must be != 0. */
 	}
 
@@ -108,29 +109,51 @@ int main() {
 	  int test_exact_algo, test_smart_algo;
 
 	  test_exact_algo = S3[l] - S2[l];
-	  ASSERT_ALWAYS (abs(test_exact_algo) <= MAX_EXACT_ERR);
+	  if (UNLIKELY(abs(test_exact_algo) > MAX_EXACT_ERR)) {
+	    fprintf (stderr, 
+		     "code BUG(): the condition abs(test_exact_algo) <= MAX_EXACT_ERR failed!\n"
+		     "test_exact_algo = %d, maximal error accepted in exact algorithm = %u\n",
+		     test_exact_algo, MAX_EXACT_ERR);
+	    exit (EXIT_FAILURE);
+	  }
 	  exact_err[test_exact_algo + MAX_EXACT_ERR]++;
 
 	  test_smart_algo = S3[l] - S1[l];
-	  ASSERT_ALWAYS (abs(test_smart_algo) <= MAX_SMART_ERR);
+	  if (UNLIKELY(abs(test_smart_algo) > MAX_SMART_ERR)) {
+	    fprintf (stderr, 
+		     "code BUG(): the condition abs(test_smart_algo) <= MAX_SMART_ERR failed!\n"
+		     "test_smart_algo=%d, maximal error accepted in smart algorithm = %u\n",
+		     test_smart_algo, MAX_SMART_ERR);
+	    exit (EXIT_FAILURE);
+	  }
 	  smart_err[test_smart_algo + MAX_SMART_ERR]++;
 	}
       }
     }
   }
   free(S1); free(S2); free (S3);
-  count = (MAX_LOGI - MIN_LOGI + 1) * (MAX_DEGREE + 1) * (unsigned long long) (NB_TESTS << LOG_BUCKET_REGION);
-  fprintf (stderr, "The tests are done with %llu values. All the values are between %u and %u included.\n", count, GUARD, UCHAR_MAX - 1);
+  
+  if (tests_common_get_verbose ()) {
+    count = (MAX_LOGI - MIN_LOGI + 1) * (MAX_DEGREE + 1) * (unsigned long long) (iter << LOG_BUCKET_REGION);
+    fprintf (stderr, "This test is done with %llu values. All the values are between %u and %u included.\n", count, GUARD, UCHAR_MAX - 1);
 #ifdef HAVE_GCC_STYLE_AMD64_INLINE_ASM
-  fprintf (stderr, "Computations done with SSE (HAVE_GCC_STYLE_AMD64_INLINE_ASM is defined).\n");
+    fprintf (stderr, "Computations done with SSE (HAVE_GCC_STYLE_AMD64_INLINE_ASM is defined).\n");
 #else
-  fprintf (stderr, "Computations done with doubles (HAVE_GCC_STYLE_AMD64_INLINE_ASM is not defined).\n");
+    fprintf (stderr, "Computations done with doubles (HAVE_GCC_STYLE_AMD64_INLINE_ASM is not defined).\n");
 #endif
-  double lc = 100. / count;
+    double lc = 100. / count;
 
-  fprintf (stderr, "\nDifferences between the exact algorithm and\nthe exact algorithm + fast log2 :\nNo difference: %6.3f%%\n", exact_err[MAX_EXACT_ERR] * lc);
-  for (ssize_t y = 1; y < MAX_EXACT_ERR; y++) if (exact_err[MAX_EXACT_ERR + y] || exact_err[MAX_EXACT_ERR - y]) fprintf (stderr, "Difference %2zu: Negative (bad): %6.3f%% (%10llu); Positive (acceptable): %6.3f%% (%10llu)\n", y, exact_err[MAX_EXACT_ERR - y] * lc, exact_err[MAX_EXACT_ERR - y], exact_err[MAX_EXACT_ERR + y] * lc, exact_err[MAX_EXACT_ERR + y]);
+    fprintf (stderr, "\nDifferences between the exact algorithm and\nthe exact algorithm + fast log2 :\nNo difference: %6.3f%%\n", exact_err[MAX_EXACT_ERR] * lc);
+    for (ssize_t y = 1; y <= MAX_EXACT_ERR; y++)
+      if (exact_err[MAX_EXACT_ERR + y] || exact_err[MAX_EXACT_ERR - y])
+	fprintf (stderr, "Difference %2zu: Negative (bad): %6.3f%% (%10llu); Positive (acceptable): %6.3f%% (%10llu)\n", y, exact_err[MAX_EXACT_ERR - y] * lc, exact_err[MAX_EXACT_ERR - y], exact_err[MAX_EXACT_ERR + y] * lc, exact_err[MAX_EXACT_ERR + y]);
+    
+    fprintf (stderr, "\nDifferences between the exact algorithm and\nthe smart algorithm + roots neighborhood correction + fast log2 :\nNo difference: %6.3f%%\n", smart_err[MAX_SMART_ERR] * lc);
+    for (ssize_t y = 1; y <= MAX_SMART_ERR; y++)
+      if (smart_err[MAX_SMART_ERR + y] || smart_err[MAX_SMART_ERR - y])
+	fprintf (stderr, "Difference %2zu: Negative (bad): %6.3f%% (%10llu); Positive (acceptable): %6.3f%% (%10llu)\n", y, smart_err[MAX_SMART_ERR - y] * lc, smart_err[MAX_SMART_ERR - y], smart_err[MAX_SMART_ERR + y] * lc, smart_err[MAX_SMART_ERR + y]);
+  }
 
-  fprintf (stderr, "\nDifferences between the exact algorithm and\nthe smart algorithm + roots neighborhood correction + fast log2 :\nNo difference: %6.3f%%\n", smart_err[MAX_SMART_ERR] * lc);
-  for (ssize_t y = 1; y < MAX_SMART_ERR; y++) if (smart_err[MAX_SMART_ERR + y] || smart_err[MAX_SMART_ERR - y]) fprintf (stderr, "Difference %2zu: Negative (bad): %6.3f%% (%10llu); Positive (acceptable): %6.3f%% (%10llu)\n", y, smart_err[MAX_SMART_ERR - y] * lc, smart_err[MAX_SMART_ERR - y], smart_err[MAX_SMART_ERR + y] * lc, smart_err[MAX_SMART_ERR + y]);
+  tests_common_clear();
+  exit (EXIT_SUCCESS);
 }
