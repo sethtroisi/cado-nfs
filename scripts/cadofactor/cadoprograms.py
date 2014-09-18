199fd547823abb5cab37835efdf4993c87289320
Alexander Kruppa <Alexander.Kruppa@loria.fr>
2014-09-18 15:44:28 +0200

    Add precmd parameter to BWC class

--- scripts/cadofactor/cadoprograms.py
+++ scripts/cadofactor/cadoprograms.py
@@ -938,6 +938,7 @@ class BWC(Program):
                  interleaving: ParameterEq()=None,
                  shuffled_product: ParameterEq()=None,
                  bwc_bindir: ParameterEq()=None,
+                 precmd: ParameterEq()=None,
                  **kwargs):
         if os.name == "nt":
             kwargs.setdefault("runprefix", "perl.exe")
