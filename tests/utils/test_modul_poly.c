sethtroisi/cado-nfs

commit b494620fbbe77e32733ead2eeb6bd857b857f406

improved modul_poly_roots_naive(), added comment

utils/modul_poly.c
@@ -493,7 +493,10 @@ modul_poly_is_squarefree (modul_poly_t f, modulusul_t p)
 }
 
 /* Return the number n of roots of f mod p using a naive algorithm.
-   If r is not NULL, put the roots in r[0], ..., r[n-1]. */
+   If r is not NULL, put the roots in r[0], ..., r[n-1].
+   TODO: rewrite using a table of differences, with cost O(d*p) additions
+   after an initialization of O(d^2), instead of O(d*p) multiplications.
+*/
 static int
 modul_poly_roots_naive (residueul_t *r, modul_poly_t f, modulusul_t p)
 {
@@ -508,6 +511,12 @@ modul_poly_roots_naive (residueul_t *r, modul_poly_t f, modulusul_t p)
 	if (r != NULL)
 	  modul_set(r[n], x, p);
 	n ++;
+	/* Let d = f->degree: if we have already d roots, then we have
+	   them all; if we have already d-1 roots, then f has d-1 linear
+	   factors, thus the last one is linear too, then if we only need
+	   the number of roots we can conclude that it is d */
+	if (n == f->degree || (r == NULL && n == f->degree - 1))
+	  return f->degree;
       }
   }
   return n;

commit ef7a0dc131138508a969bb1f0fa3d9ff1ddf7126

fixed bug introduced in b494620 (too aggressive optimization)

utils/modul_poly.c
@@ -492,7 +492,8 @@ modul_poly_is_squarefree (modul_poly_t f, modulusul_t p)
   return squarefree;
 }
 
-/* Return the number n of roots of f mod p using a naive algorithm.
+/* Return the number n of roots of f mod p (without multiplicities)
+   using a naive algorithm.
    If r is not NULL, put the roots in r[0], ..., r[n-1].
    TODO: rewrite using a table of differences, with cost O(d*p) additions
    after an initialization of O(d^2), instead of O(d*p) multiplications.
@@ -503,7 +504,7 @@ modul_poly_roots_naive (residueul_t *r, modul_poly_t f, modulusul_t p)
   int n = 0;
   residueul_t x;
   modul_set0(x, p);
-  for ( ; !modul_finished(x, p) ; modul_next(x, p) ) {
+  for ( ; !modul_finished(x, p); modul_next(x, p) ) {
       residueul_t v;
     modul_poly_eval (v, f, x, p);
     if (modul_is0(v, p))
@@ -512,11 +513,11 @@ modul_poly_roots_naive (residueul_t *r, modul_poly_t f, modulusul_t p)
 	  modul_set(r[n], x, p);
 	n ++;
 	/* Let d = f->degree: if we have already d roots, then we have
-	   them all; if we have already d-1 roots, then f has d-1 linear
-	   factors, thus the last one is linear too, then if we only need
-	   the number of roots we can conclude that it is d */
-	if (n == f->degree || (r == NULL && n == f->degree - 1))
-	  return f->degree;
+	   them all. Warning: if we have already d-1 roots, one of them
+           might have order 2, thus the return value is either d-1 or d,
+           but we can't conclude at that point. */
+	if (n == f->degree)
+          break;
       }
   }
   return n;

tests/utils/test_modul_poly.c
@@ -119,6 +119,18 @@ test_modul_poly_roots_ulong (unsigned long iter)
   ASSERT_ALWAYS(n == 1 && r[0] == 0);
   modul_clearmod (p);
 
+  modul_initmod_ul (p, 13);
+  mpz_set_ui (f[0], 4);
+  mpz_set_ui (f[1], 10);
+  mpz_set_ui (f[2], 12);
+  mpz_set_ui (f[3], 0);
+  mpz_set_ui (f[4], 9);
+  mpz_set_ui (f[5], 3);
+  mpz_set_ui (f[6], 1);
+  F->deg = 6;
+  n = modul_poly_roots (NULL, F, p);
+  ASSERT_ALWAYS (n == 5);
+
   while (iter--)
     {
       d = 1 + lrand48 () % (MAXDEGREE - 1);