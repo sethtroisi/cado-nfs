d4c6e9c7e2934bd947373fa8a2496d9002feedff
Pierrick Gaudry <pierrick.gaudry@loria.fr>
2014-10-20 18:06:49 +0200

    Exporting variable.

--- tests/test_full_p59.sh
+++ tests/test_full_p59.sh
@@ -2,5 +2,5 @@
 
 CADO_NFS_SOURCE_DIR=$1
 
-t=`mktemp -d /tmp/cado-check.XXXXXXX`
+export t=`mktemp -d /tmp/cado-check.XXXXXXX`
 ${CADO_NFS_SOURCE_DIR}/factor.sh 43341748620473677010074177283795146221310971425909898235183 -dlp -t 2 && rm -rf $t
