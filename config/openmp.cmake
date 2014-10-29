2aa05eddbe00b0a21ca00393474e9b94ecc858c2
Pierrick Gaudry <pierrick.gaudry@loria.fr>
2014-10-29 18:14:31 +0100

    Disable OPENMP for the moment

--- config/openmp.cmake
+++ config/openmp.cmake
@@ -1,8 +1,11 @@
 
 find_package(OpenMP)
-if (OPENMP_FOUND)
-    set (HAVE_OPENMP 1)
-    # Don't set unconditionally !
-    # set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
-    # set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
-endif()
+
+set (HAVE_OPENMP 0)
+
+#if (OPENMP_FOUND)
+#    set (HAVE_OPENMP 1)
+#    # Don't set unconditionally !
+#    # set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
+#    # set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
+#endif()
