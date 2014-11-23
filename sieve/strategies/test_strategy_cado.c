3cf8accb19fc64691272bb28cf1f8f5abc4e5930
Thomas Richard <thomas.richard@inria.fr>
2014-11-23 03:59:41 +0100

    fixed a mistake in test_strategy_cado.c (nb_curves).

--- sieve/strategies/test_strategy_cado.c
+++ sieve/strategies/test_strategy_cado.c
@@ -15,7 +15,6 @@
 #include "tab_fm.h"
 #include "tab_decomp.h"
 
-
 /*
 This binary allows to test our procedure choosing optimal
 strategies. In fact, using the strategy of CADO, we can use it to
@@ -24,70 +23,64 @@ with certains parameters. Then, comparing the "theorical" value with the
 real value, we could detect if a problem exists in our procedure.
  */
 
-
-
-
-tabular_fm_t*
-generate_methods_cado (const int lpb)
+tabular_fm_t *generate_methods_cado(const int lpb)
 {
-    int n =  nb_curves (lpb);
-    tabular_fm_t* res = tabular_fm_create ();
-    fm_t* fm = fm_create ();
+    int n = nb_curves(lpb);
+    tabular_fm_t *res = tabular_fm_create();
+    fm_t *fm = fm_create();
     unsigned long method[4];
 
     /* run one P-1 curve with B1=315 and B2=2205 */
-    method[0] = PM1_METHOD;//method
-    method[1] = 0;//curve
-    method[2] = 315;//B1
-    method[3] = 2205;//B2
-    fm_set_method (fm, method, 4);
-    tabular_fm_add_fm (res, fm);
+    method[0] = PM1_METHOD;	//method
+    method[1] = 0;		//curve
+    method[2] = 315;		//B1
+    method[3] = 2205;		//B2
+    fm_set_method(fm, method, 4);
+    tabular_fm_add_fm(res, fm);
 
     /* run one P+1 curve with B1=525 and B2=3255 */
-    method[0] = PP1_27_METHOD;//method
-    method[1] = 0;//curve
-    method[2] = 525;//B1
-    method[3] = 3255;//B2
-    fm_set_method (fm, method, 4);
-    tabular_fm_add_fm (res, fm);
+    method[0] = PP1_27_METHOD;	//method
+    method[1] = 0;		//curve
+    method[2] = 525;		//B1
+    method[3] = 3255;		//B2
+    fm_set_method(fm, method, 4);
+    tabular_fm_add_fm(res, fm);
 
     /* run one ECM curve with Montgomery parametrization, B1=105, B2=3255 */
-    method[0] = EC_METHOD;//method
-    method[1] = MONTY12;//curve
-    method[2] = 105;//B1
-    method[3] = 3255;//B2
-    fm_set_method (fm, method, 4);
-    tabular_fm_add_fm (res, fm);
+    method[0] = EC_METHOD;	//method
+    method[1] = MONTY12;	//curve
+    method[2] = 105;		//B1
+    method[3] = 3255;		//B2
+    fm_set_method(fm, method, 4);
+    tabular_fm_add_fm(res, fm);
 
-    if (n > 0)
-	{
-	    method[0] = EC_METHOD;//method
-	    method[1] = BRENT12;//curve
-	    method[2] = 315;//B1
-	    method[3] = 5355;//B2
-	    fm_set_method (fm, method, 4);
-	    tabular_fm_add_fm (res, fm);
-	}
-    
+    if (n > 0) {
+	method[0] = EC_METHOD;	//method
+	method[1] = BRENT12;	//curve
+	method[2] = 315;	//B1
+	method[3] = 5355;	//B2
+	fm_set_method(fm, method, 4);
+	tabular_fm_add_fm(res, fm);
+    }
+
     /* heuristic strategy where B1 is increased by sqrt(B1) at each curve */
     double B1 = 105.0;
-    for (int i = 4; i < n + 3; i++)
-	{
-	    double B2;
-	    unsigned int k;
-	    B1 += sqrt (B1);
-	    B2 = 17.0 * B1;
-	    /* we round B2 to (2k+1)*105, thus k is the integer nearest to
-	       B2/210-0.5 */
-	    k = B2 / 210.0;
-	    method[0] = EC_METHOD;//method
-	    method[1] = MONTY12;//curve
-	    method[2] = B1;
-	    method[3] = (2 * k + 1) * 105;//B2
-	    fm_set_method (fm, method, 4);
-	    tabular_fm_add_fm (res, fm);
-	}
-    fm_free (fm);
+    for (int i = 4; i < n + 3; i++) {
+	double B2;
+	unsigned int k;
+	B1 += sqrt(B1);
+	B2 = 17.0 * B1;
+	/* we round B2 to (2k+1)*105, thus k is the integer nearest to
+	   B2/210-0.5 */
+	k = B2 / 210.0;
+	method[0] = EC_METHOD;	//method
+	method[1] = MONTY12;	//curve
+	method[2] = B1;
+	method[3] = (2 * k + 1) * 105;	//B2
+	fm_set_method(fm, method, 4);
+	tabular_fm_add_fm(res, fm);
+    }
+    fm_free(fm);
     return res;
 }
 
@@ -97,55 +90,54 @@ This function generate the strategy of cado and compute the
 cofactor of 'r' bits with the bound fbb and lpb.
   This strategy is the concatenation of all methods in 'methods'.
 */
-tabular_strategy_t*
-generate_strategy_cado (tabular_fm_t* methods, tabular_decomp_t* tab_dec,
-			int fbb, int lpb, int r)
+tabular_strategy_t *generate_strategy_cado(tabular_fm_t * methods,
+					   tabular_decomp_t * tab_dec, int fbb,
+					   int lpb, int r)
 {
-    tabular_strategy_t* tab_strat = tabular_strategy_create ();
-    strategy_t* strat = strategy_create ();
+    tabular_strategy_t *tab_strat = tabular_strategy_create();
+    strategy_t *strat = strategy_create();
 
     int lim = 2 * fbb - 1;
     if (r < lim) {
-	fm_t* zero = fm_create ();
-	unsigned long method[4] = {PM1_METHOD,0,0,0};
-	fm_set_method (zero, method, 4);
-	strategy_add_fm (strat, zero);
-	strategy_set_time (strat, 0.0);
+	fm_t *zero = fm_create();
+	unsigned long method[4] = { PM1_METHOD, 0, 0, 0 };
+	fm_set_method(zero, method, 4);
+	strategy_add_fm(strat, zero);
+	strategy_set_time(strat, 0.0);
 
 	if (r != 1 && (r < fbb || r > lpb))
 	    strategy_set_proba(strat, 0.0);
-	else	// r==1 or fbb<= r0 <= lpb
+	else			// r==1 or fbb<= r0 <= lpb
 	    strategy_set_proba(strat, 1.0);
-	fm_free (zero);
+	fm_free(zero);
     } else {
-	int len = 3+nb_curves (fbb);
-	ASSERT (len <= methods->index);
-	
+	int len = 3 + nb_curves(lpb);
+	ASSERT(len <= methods->index);
+
 	for (int i = 0; i < len; i++)
-	    strategy_add_fm (strat, methods->tab[i]);
-	
+	    strategy_add_fm(strat, methods->tab[i]);
+
 	//eval
 	double p = compute_proba_strategy(tab_dec, strat, fbb, lpb);
-	
+
 	double t = compute_time_strategy(tab_dec, strat, r);
-	
+
 	strategy_set_proba(strat, p);
 	strategy_set_time(strat, t);
     }
-    
-    tabular_strategy_add_strategy (tab_strat, strat);
-    strategy_free (strat);
-    
+
+    tabular_strategy_add_strategy(tab_strat, strat);
+    strategy_free(strat);
+
     return tab_strat;
 }
-
 
 /*
   generate the matrix with the strategy of CADO.
 */
 
 tabular_strategy_t ***generate_matrix_cado(const char *name_directory_decomp,
-					   tabular_fm_t* methods,
+					   tabular_fm_t * methods,
 					   int fbb0, int lpb0, int mfb0,
 					   int fbb1, int lpb1, int mfb1)
 {
@@ -172,7 +164,7 @@ tabular_strategy_t ***generate_matrix_cado(const char *name_directory_decomp,
     fm_set_method(zero, method_zero, 4);
 
     tabular_strategy_t **data_rat = malloc(sizeof(*data_rat) * (mfb0 + 1));
-    ASSERT (data_rat);
+    ASSERT(data_rat);
 
     int lim1 = 2 * fbb0 - 1;
     for (int r0 = 0; r0 <= mfb0; r0++) {
@@ -191,8 +183,8 @@ tabular_strategy_t ***generate_matrix_cado(const char *name_directory_decomp,
 	    }
 	    fclose(file);
 	}
-	data_rat[r0] = generate_strategy_cado (methods, tab_decomp,
-					       fbb0, lpb0, r0);
+	data_rat[r0] = generate_strategy_cado(methods, tab_decomp,
+					      fbb0, lpb0, r0);
 	tabular_decomp_free(tab_decomp);
     }
 
@@ -219,7 +211,7 @@ tabular_strategy_t ***generate_matrix_cado(const char *name_directory_decomp,
 	}
 
 	tabular_strategy_t *strat_r1 =
-	    generate_strategy_cado (methods, tab_decomp, fbb1, lpb1, r1);
+	    generate_strategy_cado(methods, tab_decomp, fbb1, lpb1, r1);
 
 	tabular_decomp_free(tab_decomp);
 
@@ -239,6 +231,7 @@ tabular_strategy_t ***generate_matrix_cado(const char *name_directory_decomp,
     fm_free(zero);
     return matrix;
 }
+
 /************************************************************************/
 /*                            USAGE                                     */
 /************************************************************************/
@@ -249,7 +242,7 @@ static void declare_usage(param_list pl)
 			    "This binary allows to test the strategy of cado,"
 			    "and especially compute the theorical number of "
 			    "relations found per second by this strategy.\n");
-    
+
     param_list_decl_usage(pl, "fbb0",
 			  "set rationnal factor base bound to 2^fbb0\n");
     param_list_decl_usage(pl, "fbb1",
@@ -259,33 +252,33 @@ static void declare_usage(param_list pl)
     param_list_decl_usage(pl, "lpb1",
 			  "set algebraic large prime bound to 2^lpb1");
     param_list_decl_usage(pl, "mfb0", "set the first cofactor bound to 2^mfb0");
-    param_list_decl_usage(pl, "mfb1", "set the second cofactor bound to 2^mfb1");
+    param_list_decl_usage(pl, "mfb1",
+			  "set the second cofactor bound to 2^mfb1");
     param_list_decl_usage(pl, "decomp",
-	  "to locate the file or the directory , according to\n"
-	  "\t \t if you need one or several files,\n"
-	  "\t \t which contain(s) the file(s) of cofactors decompositions.");
+			  "to locate the file or the directory , according to\n"
+			  "\t \t if you need one or several files,\n"
+			  "\t \t which contain(s) the file(s) of cofactors decompositions.");
     param_list_decl_usage(pl, "dist",
-	  "the pathname of our file which contains the distribution\n"
-	  "\t\t of our pairs of cofactors.");
+			  "the pathname of our file which contains the distribution\n"
+			  "\t\t of our pairs of cofactors.");
     param_list_decl_usage(pl, "t",
-	  "specify the time (seconds) to optain cofactors in the file\n"
-	  "\t\t given by the option 'dist'.");
+			  "specify the time (seconds) to optain cofactors in the file\n"
+			  "\t\t given by the option 'dist'.");
     param_list_decl_usage(pl, "out",
-          "the output file which contain our strategies\n");
+			  "the output file which contain our strategies\n");
 }
-
 
 /************************************************************************/
 /*     MAIN                                                             */
 /************************************************************************/
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 
     param_list pl;
     param_list_init(pl);
     declare_usage(pl);
-    
+
     if (argc <= 1) {
 	param_list_print_usage(pl, argv[0], stderr);
 	exit(EXIT_FAILURE);
@@ -318,7 +311,7 @@ int main (int argc, char *argv[])
     int mfb0 = -1;
     int mfb1 = -1;
     double C0 = -1;
-    
+
     param_list_parse_int(pl, "lpb0", &lpb0);
     param_list_parse_int(pl, "lpb1", &lpb1);
     param_list_parse_int(pl, "fbb1", &fbb1);
@@ -329,21 +322,20 @@ int main (int argc, char *argv[])
 
     if (fbb0 == -1 || lpb0 == -1 || mfb0 == -1 ||
 	fbb0 == -1 || lpb0 == -1 || mfb0 == -1 || C0 == -1) {
-	fputs("ALL parameters are mandatory!\n", stderr);	
+	fputs("ALL parameters are mandatory!\n", stderr);
 	param_list_clear(pl);
 	exit(EXIT_FAILURE);
     }
-    
-    
-    int fbb = (fbb0<fbb1)?fbb0:fbb1;
-    int lpb = (lpb0>lpb1)?lpb0:lpb1;
+
+    int fbb = (fbb0 < fbb1) ? fbb0 : fbb1;
+    int lpb = (lpb0 > lpb1) ? lpb0 : lpb1;
     //convert the time in micro-s. because all previous binaries
     //compute their times in micro-s.
-    C0 *= 1000000;    //s-->micro-s 
+    C0 *= 1000000;		//s-->micro-s 
 
     //option: tab decomp
     const char *name_directory_decomp;
-    //	"/localdisk/trichard/results/decomp_cofactor/decomp_tmp";
+    //  "/localdisk/trichard/results/decomp_cofactor/decomp_tmp";
     if ((name_directory_decomp =
 	 param_list_lookup_string(pl, "decomp")) == NULL) {
 	fputs("Parser error: Please re-run with the option "
@@ -351,7 +343,6 @@ int main (int argc, char *argv[])
 	param_list_clear(pl);
 	exit(EXIT_FAILURE);
     }
-    
     //option: distribution cofactors 
     const char *name_file_cofactor;
     //"/localdisk/trichard/cado768/cofactors";
@@ -363,7 +354,6 @@ int main (int argc, char *argv[])
 	exit(EXIT_FAILURE);
     }
 
-
     FILE *file_C = fopen(name_file_cofactor, "r");
     unsigned long **distrib_C = extract_matrix_C(file_C, mfb0 + 1, mfb1 + 1);
     if (distrib_C == NULL) {
@@ -373,18 +363,17 @@ int main (int argc, char *argv[])
     }
     fclose(file_C);
 
-   
     gmp_randstate_t state;
     gmp_randinit_default(state);
 
     //select our methods
-    tabular_fm_t* methods = generate_methods_cado (lpb);
+    tabular_fm_t *methods = generate_methods_cado(lpb);
     //benchmark
     bench_proba(state, methods, fbb);
     bench_time(state, methods);
     //generate our strategies
     //remark: for each pair (r0, r1), the tabular contain only one strategy!!
-    tabular_strategy_t*** matrix =
+    tabular_strategy_t ***matrix =
 	generate_matrix_cado(name_directory_decomp, methods,
 			     fbb0, lpb0, mfb0,
 			     fbb1, lpb1, mfb1);
@@ -392,18 +381,18 @@ int main (int argc, char *argv[])
     //eval our strategy!
     double Y = 0, T = C0;
     for (int r0 = 0; r0 <= mfb0; r0++) {
-    	for (int r1 = 0; r1 <= mfb1; r1++) {
-    	    Y += distrib_C[r0][r1] * matrix[r0][r1]->tab[0]->proba;
-    	    T += distrib_C[r0][r1] * matrix[r0][r1]->tab[0]->time;
-    	}
+	for (int r1 = 0; r1 <= mfb1; r1++) {
+	    Y += distrib_C[r0][r1] * matrix[r0][r1]->tab[0]->proba;
+	    T += distrib_C[r0][r1] * matrix[r0][r1]->tab[0]->time;
+	}
     }
     //print the result!
     printf(" Y = %lf relations, T = %lf s., yt = %1.10lf rel/s\n", Y,
-    	   T / 1000000, Y / T * 1000000);
+	   T / 1000000, Y / T * 1000000);
 
     const char *pathname_output;
     pathname_output = param_list_lookup_string(pl, "out");
-    
+
     if (pathname_output != NULL) {
 	FILE *file_output = fopen(pathname_output, "w");
 	for (int r0 = 0; r0 <= mfb0; r0++)
@@ -419,15 +408,15 @@ int main (int argc, char *argv[])
     }
 //free
 
-for (int r0 = 0; r0 <= mfb0; r0++) {
-    for (int r1 = 0; r1 <= mfb1; r1++) 
-	tabular_strategy_free(matrix[r0][r1]);
-    free (distrib_C[r0]);
-    free(matrix[r0]);
- }
-free(distrib_C);
-free(matrix);
-gmp_randclear(state);
-param_list_clear(pl);
-return EXIT_SUCCESS;
+    for (int r0 = 0; r0 <= mfb0; r0++) {
+	for (int r1 = 0; r1 <= mfb1; r1++)
+	    tabular_strategy_free(matrix[r0][r1]);
+	free(distrib_C[r0]);
+	free(matrix[r0]);
+    }
+    free(distrib_C);
+    free(matrix);
+    gmp_randclear(state);
+    param_list_clear(pl);
+    return EXIT_SUCCESS;
 }
