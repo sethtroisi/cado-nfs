c0fe878c859aba7078b8959f674d5f9fa7b0ce6e
Alexander Kruppa <Alexander.Kruppa@loria.fr>
2014-11-28 13:57:39 +0100

    Move declarations to fb.h

--- sieve/fb.cpp
+++ sieve/fb.cpp
@@ -6,15 +6,13 @@
 #include <cstdarg>
 #include <cctype>
 #include <gmp.h>
-#include "fb_new.h"
+#include "fb.h"
 #include "mod_ul.h"
 #include "verbose.h"
 #include "getprime.h"
 #include "gmp_aux.h"
 #include "gzip.h"
 
-#define FB_MAX_PARTS 4
-#define MAXDEGREE 8
 
 static unsigned int fb_log_2 (fbprime_t);
 
@@ -78,33 +76,6 @@ fb_is_power (fbprime_t q, unsigned long *final_k)
     }
   return 0;
 }
-
-/* General entries are anything that needs auxiliary information:
-   Prime powers, projective roots, ramified primes where exp != oldexp + 1,
-   etc. They could, of course, also store the simple cases, but for those we
-   use the simple struct to conserve memory and to decide algorithms (batch
-   inversion, etc.) statically. */
-class fb_general_entry {
-  void read_roots (const char *, unsigned long);
-public:
-  fbprime_t q, p; /* q = p^k */
-  redc_invp_t invq; /* invq = -1/q (mod 2^32), or (mod 2^64), depending on
-		       the size of redc_invp_t */
-  fbroot_t roots[MAXDEGREE];
-  unsigned char exp[MAXDEGREE], oldexp[MAXDEGREE];
-  bool projective[MAXDEGREE];
-  
-  /* exp and oldexp are maximal such that:
-     If not projective and a == br (mod p^k), then p^exp | F(a,b)
-     -"-               a == br (mod p^(k-1)), then p^oldexp | F(a,b)
-     If projective and ar == b  -"- */
-  
-  unsigned char k, nr_roots;
-  void parse_line (const char *line, unsigned long linenr);
-  void merge (const fb_general_entry &);
-  void fprint(FILE *out);
-};
-
 
 /* Read roots from a factor base file line and store them in roots.
    line must point at the first character of the first root on the line.
@@ -266,17 +237,6 @@ fb_general_entry::merge (const fb_general_entry &other)
   }
 }
 
-/* "Simple" factor base entries. We imply q=p, k=1, oldexp=0, exp=1,
-   and projective=false for all roots. */
-template <int Nr_roots>
-class fb_entry_x_roots_s {
-public:
-  fbprime_t p;
-  fbroot_t roots[Nr_roots];
-  fb_entry_x_roots_s(const fb_general_entry &);
-  void fprint(FILE *);
-};
-
 template <int Nr_roots>
 fb_entry_x_roots_s<Nr_roots>::fb_entry_x_roots_s(const fb_general_entry &fb_cur)
 {
@@ -300,21 +260,6 @@ fb_entry_x_roots_s<Nr_roots>::fprint(FILE *out)
 
 
 template <int Nr_roots>
-class fb_vector: public std::vector<fb_entry_x_roots_s<Nr_roots> > {
-  public:
-  using std::vector<fb_entry_x_roots_s<Nr_roots> >::push_back; /* wtf C++ */
-
-  /* Overloaded push_back() method that accepts a fb_general_entry, which is
-     converted to this vector's fb_entry_x_roots_s type before adding it */
-  void push_back(const fb_general_entry &fb_cur)
-  {
-    fb_entry_x_roots_s<Nr_roots> f = fb_cur;
-    push_back(f);
-  }
-  void fprint(FILE *);
-};
-
-template <int Nr_roots>
 void
 fb_vector<Nr_roots>::fprint(FILE *out)
 {
@@ -322,24 +267,9 @@ fb_vector<Nr_roots>::fprint(FILE *out)
     (*this)[i].fprint(out);
 }
 
-typedef std::vector<fb_general_entry> fb_general_vector;
-
 /* http://stackoverflow.com/questions/24130093/gdb-could-not-find-operator */
 template class std::vector<fb_general_entry>;
 
-
-/* The fb_slices class has nr_slices vectors; when appending elements with
-   append(), it appends to these vectors in a round-robin fashion. */
-template <int Nr_roots>
-class fb_slices {
-  fb_vector<Nr_roots> *vectors;
-  size_t nr_slices, next_slice;
-public:
-  fb_slices(size_t nr_slices);
-  void append(const fb_general_entry &);
-  fb_entry_x_roots_s<Nr_roots> *get_slice(size_t slice);
-  void fprint(FILE *);
-};
 
 template <int Nr_roots>
 fb_slices<Nr_roots>::fb_slices(const size_t nr_slices)
@@ -375,35 +305,6 @@ fb_slices<Nr_roots>::fprint(FILE *out)
     vectors[slice].fprint(out);
   }
 }
-
-/* A "part" is the set of all factor base primes that get sieved over a given
-   bucket region size.
-   E.g., when we have only 1 level of bucket sorting, then the factor base has
-   2 parts: the line-sieved primes, and the bucket-sieved primes. */
-class fb_part {
-  /* How do we identify the number of slices in each array?
-     Should we have
-     size_t nr_entries[9];
-     here, or let each array end with a "magic value", such as a slice with
-     0 entries, or a NULL pointer? */
-
-  /* These vectors are filled when we read or generate the factor base.
-     The slices point into the vectors' storage. */
-  fb_slices<0> *fb0_slices; /* From 0 to MAXDEGREE */
-  fb_slices<1> *fb1_slices;
-  fb_slices<2> *fb2_slices;
-  fb_slices<3> *fb3_slices;
-  fb_slices<4> *fb4_slices;
-  fb_slices<5> *fb5_slices;
-  fb_slices<6> *fb6_slices;
-  fb_slices<7> *fb7_slices;
-  fb_slices<8> *fb8_slices;
-  fb_general_vector general_vector;
-  public:
-  fb_part(size_t nr_slices);
-  void append(const fb_general_entry &);
-  void fprint(FILE *);
-};
 
 fb_part::fb_part(const size_t nr_slices) {
   fb0_slices = new fb_slices<0>(nr_slices);
@@ -484,24 +385,6 @@ fb_part::fprint(FILE *out)
     general_vector[i].fprint(out);
   }
 }
-
-/* Splits the factor base for a polynomial into disjoint parts which are
-   sieved over different sieve region sizes.
-   For example, 
-   parts[0] will contain very small primes that are line-sieved,
-   parts[1] contains bucket-sieved primes with 1 level of bucket sorting
-   (i.e., hits get sorted into bucket regions of size 2^16)
-*/
-class fb_factorbase {
-  fb_part *parts[FB_MAX_PARTS];
-  void append(const fb_general_entry &);
-  fbprime_t thresholds[FB_MAX_PARTS];
-  public:
-  fb_factorbase(const fbprime_t *thresholds, const size_t *nr_slices);
-  void read(const char * const filename);
-  void make_linear (const mpz_t *poly, fbprime_t powbound, bool do_projective);
-  void fprint(FILE *);
-};
 
 fb_factorbase::fb_factorbase(const fbprime_t *thresholds,
 			     const size_t *nr_slices)
--- sieve/fb.h
+++ sieve/fb.h
@@ -17,6 +17,7 @@
 #endif
 #include <gmp.h>
 #include "las-config.h"
+#include "cado_poly.h" // for MAXDEGREE
 
 /* Data types */
 
@@ -26,12 +27,14 @@ typedef unsigned int fbprime_t; /* 32 bits should be enough for everyone */
 #define FBPRIME_BITS 32
 typedef fbprime_t fbroot_t;
 #define FBROOT_FORMAT "u"
-typedef unsigned long largeprime_t; /* On IA32 they'll only get 32 bit 
+typedef unsigned long largeprime_t; /* On IA32 they'll only get 32 bit
                                        large primes */
 #define LARGEPRIME_FORMAT "lu"
 
+#define FB_MAX_PARTS 4
+
 /* If SUPPORT_LARGE_Q is defined, 64-bit redc is used in the function that
-   converts roots to the p-lattice, and the redc code needs a 64-bit 
+   converts roots to the p-lattice, and the redc code needs a 64-bit
    precomputed inverse. If SUPPORT_LARGE_Q is not defined, we store only a
    32-bit inverse to conserve memory. */
 #if defined(SUPPORT_LARGE_Q)
@@ -45,17 +48,130 @@ typedef uint32_t redc_invp_t;
                                         identify the corresponding IEEE 754
                                         double precision number */
 
-/* The following format takes 16+4k bytes per prime with k roots. Since
- * the expected number of roots for primes with at least one root is
- * 1.58 (for generic Galois group), we are slightly above 14 bytes per
- * root.
- */
+
+/* General entries are anything that needs auxiliary information:
+   Prime powers, projective roots, ramified primes where exp != oldexp + 1,
+   etc. They could, of course, also store the simple cases, but for those we
+   use the simple struct to conserve memory and to decide algorithms (batch
+   inversion, etc.) statically. */
+class fb_general_entry {
+  void read_roots (const char *, unsigned long);
+public:
+  fbprime_t q, p; /* q = p^k */
+  redc_invp_t invq; /* invq = -1/q (mod 2^32), or (mod 2^64), depending on
+		       the size of redc_invp_t */
+  fbroot_t roots[MAXDEGREE];
+  unsigned char exp[MAXDEGREE], oldexp[MAXDEGREE];
+  bool projective[MAXDEGREE];
+
+  /* exp and oldexp are maximal such that:
+     If not projective and a == br (mod p^k), then p^exp | F(a,b)
+     -"-               a == br (mod p^(k-1)), then p^oldexp | F(a,b)
+     If projective and ar == b  -"- */
+
+  unsigned char k, nr_roots;
+  void parse_line (const char *line, unsigned long linenr);
+  void merge (const fb_general_entry &);
+  void fprint(FILE *out);
+};
+typedef std::vector<fb_general_entry> fb_general_vector;
+
+
+
+/* "Simple" factor base entries. We imply q=p, k=1, oldexp=0, exp=1,
+   and projective=false for all roots. */
+template <int Nr_roots>
+class fb_entry_x_roots_s {
+public:
+  fbprime_t p;
+  fbroot_t roots[Nr_roots];
+  fb_entry_x_roots_s(const fb_general_entry &);
+  void fprint(FILE *);
+};
+
+
+template <int Nr_roots>
+class fb_vector: public std::vector<fb_entry_x_roots_s<Nr_roots> > {
+  public:
+  using std::vector<fb_entry_x_roots_s<Nr_roots> >::push_back; /* wtf C++ */
+
+  /* Overloaded push_back() method that accepts a fb_general_entry, which is
+     converted to this vector's fb_entry_x_roots_s type before adding it */
+  void push_back(const fb_general_entry &fb_cur)
+  {
+    fb_entry_x_roots_s<Nr_roots> f = fb_cur;
+    push_back(f);
+  }
+  void fprint(FILE *);
+};
+
+
+/* The fb_slices class has nr_slices vectors; when appending elements with
+   append(), it appends to these vectors in a round-robin fashion. */
+template <int Nr_roots>
+class fb_slices {
+  fb_vector<Nr_roots> *vectors;
+  size_t nr_slices, next_slice;
+public:
+  fb_slices(size_t nr_slices);
+  void append(const fb_general_entry &);
+  fb_entry_x_roots_s<Nr_roots> *get_slice(size_t slice);
+  void fprint(FILE *);
+};
+
+
+/* A "part" is the set of all factor base primes that get sieved over a given
+   bucket region size.
+   E.g., when we have only 1 level of bucket sorting, then the factor base has
+   2 parts: the line-sieved primes, and the bucket-sieved primes. */
+class fb_part {
+  /* How do we identify the number of slices in each array?
+     Should we have
+     size_t nr_entries[9];
+     here, or let each array end with a "magic value", such as a slice with
+     0 entries, or a NULL pointer? */
+
+  /* These vectors are filled when we read or generate the factor base.
+     The slices point into the vectors' storage. */
+  fb_slices<0> *fb0_slices; /* From 0 to MAXDEGREE */
+  fb_slices<1> *fb1_slices;
+  fb_slices<2> *fb2_slices;
+  fb_slices<3> *fb3_slices;
+  fb_slices<4> *fb4_slices;
+  fb_slices<5> *fb5_slices;
+  fb_slices<6> *fb6_slices;
+  fb_slices<7> *fb7_slices;
+  fb_slices<8> *fb8_slices;
+  fb_general_vector general_vector;
+  public:
+  fb_part(size_t nr_slices);
+  void append(const fb_general_entry &);
+  void fprint(FILE *);
+};
+
+
+/* Splits the factor base for a polynomial into disjoint parts which are
+   sieved over different sieve region sizes.
+   For example,
+   parts[0] will contain very small primes that are line-sieved,
+   parts[1] contains bucket-sieved primes with 1 level of bucket sorting
+   (i.e., hits get sorted into bucket regions of size 2^16)
+*/
+class fb_factorbase {
+  fb_part *parts[FB_MAX_PARTS];
+  void append(const fb_general_entry &);
+  fbprime_t thresholds[FB_MAX_PARTS];
+  public:
+  fb_factorbase(const fbprime_t *thresholds, const size_t *nr_slices);
+  void read(const char * const filename);
+  void make_linear (const mpz_t *poly, fbprime_t powbound, bool do_projective);
+  void fprint(FILE *);
+};
+
 
 unsigned char	fb_log (double, double, double);
 fbprime_t       fb_pow (fbprime_t, unsigned long);
 fbprime_t       fb_is_power (fbprime_t, unsigned long *);
 unsigned char   fb_make_steps(fbprime_t *, fbprime_t, double);
 
-/* Some inlined functions which need to be fast */
-  
 #endif
