3ee140882d5fa7e09860e46f43c38e8e12dc4fa0
Thomas Richard <thomas.richard@inria.fr>
2014-12-12 14:07:53 +0100

    Update script script_str_r.sh and script_str.sh

--- sieve/strategies/script_str_r.sh
+++ sieve/strategies/script_str_r.sh
@@ -7,18 +7,18 @@
 #the sieving region
 lim0=$1 # say 2^27
 lpb0=$2 # say 37
-mfb0=$3 # say 110
-ncurves=$4 # say 10
+r0=$3  #say 0
+mfb0=$4 # say 110
+ncurves=$5 # say 10
 
 #directory which store our decomposition files!
-decomp=$5 # say '/localdisk/trichard/results/decomp_cofactor/decomp_tmp/'
+decomp=$6 # say '/localdisk/trichard/results/decomp_cofactor/decomp_tmp/'
 #file which contains our factoring methods (PM1, PP1, EC)!
-methods=$6 # say 'All_methods'
+methods=$7 # say 'All_methods'
 #directory where the output file will be stored, because this binary
 #generates a file for each r.
-out=$7 # say 'res_precompt_st/'
+out=$8 # say 'res_precompt_st/'
 
-let "r0=0"
 while [ $r0 -le $mfb0 ]; do
 pathname_decomp=$decomp"/decomp_"$lim0"_"$r0
 ./gst -gst_r -lim0 $lim0 -lpb0 $lpb0 -r0 $r0 -ncurves $ncurves -in $methods -decomp $pathname_decomp -out $out &
