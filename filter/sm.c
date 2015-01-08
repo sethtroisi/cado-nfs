bf7cb042d5e460fad25ceed7d454882b427b660c
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2015-01-08 12:31:30 +0100

    ah ah !

--- filter/sm.c
+++ filter/sm.c
@@ -304,7 +304,14 @@ void sm (const char * outname, sm_relset_ptr rels, uint64_t sr,
   for (int side = 0; side < 2; side++) {
       nsm_total += nsm[side];
   }
+  /*
   gmp_fprintf(out, "%" PRIu64 " %d %Zd\n", sr, nsm_total, ell);
+  */
+  /* mingw's gmp chokes on the %I64u format string which is used by
+   * windows as a real value for PRIu64...
+   */
+  fprintf(out, "%" PRIu64 " %d", sr, nsm_total);
+  gmp_fprintf(out, " %Zd\n", ell);
 
   uint64_t i;
   stats_init (stats, stdout, &i, nbits(sr)-5, "Computed", "SMs", "", "SMs");
