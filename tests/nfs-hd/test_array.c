151fdb78ad4a552db43922ecea0a069db1581cad
Laurent Grémy <laurent.gremy@inria.fr>
2015-01-08 09:57:34 +0100

    Test for array in nfs-hd.

--- tests/nfs-hd/test_array.c
+++ tests/nfs-hd/test_array.c
@@ -1,8 +1,12 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <inttypes.h>
 #include "sieving_interval.h"
 #include "array.h"
 #include "cado.h"
 #include "utils.h"
 #include "macros.h"
+#include "int64_vector.h"
 
 int main()
 {
@@ -17,44 +21,35 @@ int main()
   uint64_t nb;
   sieving_interval_number_element(&nb, H);
 
-  mpz_vector_t vector;
-  mpz_vector_init(vector, t);
+  int64_vector_t vector;
+  int64_vector_init(vector, t);
+
+  mpz_vector_t v;
+  mpz_vector_init(v, t);
 
   for (unsigned int i = 0; i < t - 1; i++) {
-    mpz_vector_setcoordinate_si(vector, i, -H->h[i]);
+    int64_vector_setcoordinate(vector, i, -(int64_t)H->h[i]);
   }
 
   uint64_t index;
   uint64_t index1;
 
-  mpz_vector_t vector1;
-  mpz_vector_init(vector1, t);
-
-  mpz_poly_t poly;
-  mpz_poly_init(poly, 0);
-
-  mpz_poly_t poly1;
-  mpz_poly_init(poly1, 0);
-
-  array_mpz_vector_index(&index, vector, H, nb);
+  array_int64_vector_index(&index, vector, H, nb);
   ASSERT_ALWAYS(index == 0);
   for (unsigned int i = 1; i < nb; i++) {
-    mpz_vector_add_one(vector, H);
-    array_mpz_vector_index(&index, vector, H, nb);
+    int64_vector_add_one(vector, H);
+    array_int64_vector_index(&index, vector, H, nb);
     ASSERT_ALWAYS(index == i);
-    array_index_mpz_vector(vector1, index, H, nb);
-    array_index_mpz_poly(poly, index, H, nb);
-    ASSERT_ALWAYS(mpz_vector_cmp(vector, vector1) == 0);
-    mpz_vector_get_mpz_poly(poly1, vector1);
-    ASSERT_ALWAYS(mpz_poly_cmp(poly1, poly) == 0);
-    array_mpz_poly_index(&index1, poly1, H, nb);
-    ASSERT_ALWAYS(index1 == index);
+    array_index_mpz_vector(v, index, H, nb);
+    for (unsigned int j = 0; j < t; j++) {
+      ASSERT_ALWAYS(mpz_cmp_si(v->c[j], vector->c[j] == 0));
+    }
+    array_mpz_vector_index(&index1, v, H, nb);
+    ASSERT_ALWAYS(index == index1);
   }
 
-  mpz_vector_clear(vector1);
-  mpz_vector_clear(vector);
-  mpz_poly_clear(poly);
-  mpz_poly_clear(poly1);
+  mpz_vector_clear(v);
+  int64_vector_clear(vector);
   sieving_interval_clear(H);
 
   return 0;
