ddb5f51721e9a2cff6f6f2b75145a1186b7858f3
Alexander Kruppa <akruppa@gmail.com>
2015-02-16 18:34:22 +0100

    Drop "partially projective" roots

--- sieve/fb.cpp
+++ sieve/fb.cpp
@@ -357,8 +357,12 @@ fb_slice<FB_ENTRY_TYPE>::make_lattice_bases(qlattice_basis_srcptr basis, const i
     for (unsigned char i_root = 0; i_root < transformed.nr_roots; i_root++) {
       const fbroot_t r = transformed.get_r(i_root);
       const bool proj = transformed.get_proj(i_root);
-      plattice_sieve_entry pli = plattice_sieve_entry(transformed.p, r, proj, logI, (prime_hint_t)i_entry);
-      result->push_back(pli);
+      /* If proj and r > 0, then r == 1/p (mod p^2), so all hits would be in
+         locations with p | gcd(i,j). */
+      if (LIKELY(!proj || r == 0)) {
+        plattice_sieve_entry pli = plattice_sieve_entry(transformed.p, r, proj, logI, (prime_hint_t)i_entry);
+        result->push_back(pli);
+      }
     }
   }
   return result;
