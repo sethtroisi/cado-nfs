2322bc006e700535453b913372c55ec2f37a4baf
Alexander Kruppa <akruppa@gmail.com>
2015-02-24 20:54:33 +0100

    Add malloc attribute to malloc_aligned(), and warn_unused_result to realloc_aligned()

--- utils/memory.h
+++ utils/memory.h
@@ -2,6 +2,7 @@
 #define CADO_UTILS_MEMORY_H_
 
 #include <stdlib.h>
+#include "macros.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -13,9 +14,9 @@ extern void physical_free(const void *, size_t);
 
 void *malloc_hugepages(size_t);
 extern long pagesize (void);
-extern void * malloc_aligned(size_t size, size_t alignment);
-void * realloc_aligned(void * p, const size_t old_size, const size_t new_size,
-                       const size_t alignment);
+ATTRIBUTE((malloc)) extern void * malloc_aligned(size_t size, size_t alignment);
+ATTRIBUTE((warn_unused_result)) void * realloc_aligned(void * p, 
+        const size_t old_size, const size_t new_size, const size_t alignment);
 extern void free_aligned(const void * ptr);
 
 extern void * malloc_pagealigned(size_t sz);
