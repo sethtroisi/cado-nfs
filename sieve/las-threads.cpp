sethtroisi/cado-nfs

commit fa0efface3b5a13333f4e7c578a2e3dcf15691c5

Move some thread-related functions to their own file

sieve/las-threads.cpp
@@ -1,0 +1,166 @@
+#include "cado.h"
+#include <pthread.h>
+#include "memory.h"
+#include "las-threads.h"
+#include "las-types.h"
+#include "las-config.h"
+
+thread_side_data_s::thread_side_data_s()
+{
+  /* Allocate memory for each side's bucket region */
+  bucket_region = (unsigned char *) contiguous_malloc(BUCKET_REGION + MEMSET_MIN);
+}
+
+thread_side_data_s::~thread_side_data_s()
+{
+  contiguous_free(bucket_region);
+  bucket_region = NULL;
+}
+
+thread_data_s::thread_data_s() : checksum_post_sieve{0, 0}, is_initialized(false)
+{
+  /* Allocate memory for the intermediate sum (only one for both sides) */
+  SS = (unsigned char *) contiguous_malloc(BUCKET_REGION);
+}
+
+thread_data_s::~thread_data_s()
+{
+  ASSERT_ALWAYS(is_initialized);
+  ASSERT_ALWAYS(SS != NULL);
+  contiguous_free(SS);
+  SS = NULL;
+  las_report_clear(rep);
+}
+
+void thread_data_s::init(const int _id, las_info_srcptr _las)
+{
+  id = _id;
+  las = _las;
+  las_report_init(rep);
+  is_initialized = true;
+}
+
+void thread_data_s::pickup_si(sieve_info_ptr _si)
+{
+  si = _si;
+  for(int s = 0 ; s < 2 ; s++) {
+    sides[s]->set_fb(si->sides[s]->fb->get_part(1));
+  }
+}
+
+
+thread_data_ptr thread_data_alloc(las_info_ptr las, int n)
+{
+    thread_data_ptr thrs = new thread_data_s[n];
+    ASSERT_ALWAYS(thrs != NULL);
+
+    for(int i = 0 ; i < n ; i++)
+        thrs[i].init(i, las);
+    return thrs;
+}
+
+void thread_data_free(thread_data_ptr thrs)
+{
+    delete[] thrs;
+}
+
+void thread_pickup_si(thread_data_ptr thrs, sieve_info_ptr si, int n)
+{
+    for (int i = 0; i < n ; ++i) {
+        thrs[i].pickup_si(si);
+    }
+}
+
+void thread_do(thread_data_ptr thrs, void * (*f) (thread_data_ptr), int n)/*{{{*/
+{
+    if (n == 1) {
+        /* Then don't bother with pthread calls */
+        (*f)(&thrs[0]);
+        return;
+    }
+    pthread_t * th = (pthread_t *) malloc(n * sizeof(pthread_t)); 
+    ASSERT_ALWAYS(th);
+
+#if 0
+    /* As a debug measure, it's possible to activate this branch instead
+     * of the latter. In effect, this causes las to run in a
+     * non-multithreaded way, albeit strictly following the code path of
+     * the multithreaded case.
+     */
+    for (int i = 0; i < n ; ++i) {
+        (*f)(&thrs[i]);
+    }
+#else
+    for (int i = 0; i < n ; ++i) {
+        int ret = pthread_create(&(th[i]), NULL, 
+		(void * (*)(void *)) f,
+                (void *)(&thrs[i]));
+        ASSERT_ALWAYS(ret == 0);
+    }
+    for (int i = 0; i < n ; ++i) {
+        int ret = pthread_join(th[i], NULL);
+        ASSERT_ALWAYS(ret == 0);
+    }
+#endif
+
+    free(th);
+}
+
+/* {{{ thread_buckets_alloc
+ * TODO: Allow allocating larger buckets if bucket_fill_ratio ever grows
+ * above the initial guess. This can easily be made a permanent choice.
+ *
+ * Note also that we could consider having bucket_fill_ratio global.
+ */
+void thread_buckets_alloc(thread_data_ptr thrs, unsigned int n)/*{{{*/
+{
+  for (unsigned int i = 0; i < n ; ++i) {
+    for(unsigned int side = 0 ; side < 2 ; side++) {
+      thread_side_data_ptr ts = thrs[i].sides[side];
+      sieve_info_srcptr si = thrs[i].si;
+      /* We used to re-allocate whenever the number of buckets changed. Now we
+         always allocate memory for the max. number of buckets, so that we
+         never have to re-allocate */
+      uint32_t nb_buckets;
+      /* If shell environment variable LAS_REALLOC_BUCKETS is *not* set,
+         always allocate memory for the max. number of buckets, so that we
+         never have to re-allocate. If it is set, allocate just enough for
+         for the current number of buckets, which will re-allocate memory
+         if number of buckets changes. */
+      if (getenv("LAS_REALLOC_BUCKETS") == NULL) {
+        nb_buckets = si->nb_buckets_max;
+      } else {
+        nb_buckets = si->nb_buckets;
+      }
+      init_buckets(&(ts->BA), &(ts->kBA), &(ts->mBA),
+                   si->sides[side]->max_bucket_fill_ratio, nb_buckets);
+    }
+  }
+}/*}}}*/
+
+void thread_buckets_free(thread_data_ptr thrs, unsigned int n)/*{{{*/
+{
+  for (unsigned int i = 0; i < n ; ++i) {
+    thread_side_data_ptr ts;
+    for(unsigned int side = 0 ; side < 2 ; side++) {
+      // fprintf ("# Freeing buckets, thread->id=%d, side=%d\n", thrs[i].id, side);
+      ts = thrs[i].sides[side];
+      /* if there is no special-q in the interval, the arrays are not malloced */
+      if (ts->BA.bucket_write != NULL)
+        clear_buckets(&(ts->BA), &(ts->kBA), &(ts->mBA));
+    }
+  }
+}/*}}}*/
+
+double thread_buckets_max_full(thread_data_ptr thrs, int n)/*{{{*/
+{
+    double mf0 = 0;
+    for (int i = 0; i < n ; ++i) {
+        double mf = buckets_max_full (thrs[i].sides[RATIONAL_SIDE]->BA);
+        if (mf > mf0) mf0 = mf;
+        mf = buckets_max_full (thrs[i].sides[ALGEBRAIC_SIDE]->BA);
+        if (mf > mf0) mf0 = mf;
+    }
+    return mf0;
+}/*}}}*/
+

sieve/CMakeLists.txt
@@ -15,7 +15,7 @@ set (las_sources las.cpp fb.cpp
   las-coordinates.cpp las-debug.cpp las-report-stats.cpp las-duplicate.cpp
   las-config.cpp bucket.cpp las-smallsieve.cpp las-unsieve.cpp las-arith.cpp
   las-qlattice.cpp las-descent-helpers.cpp las-cofactor.cpp
-  las-fill-in-buckets.cpp)
+  las-fill-in-buckets.cpp las-threads.cpp)
 # set (las_sources las.cpp las-norms.cpp fb.cpp
 #     las-coordinates.cpp las-debug.cpp las-report-stats.cpp las-duplicate.cpp
 #     las-config.cpp bucket.cpp las-smallsieve.cpp las-unsieve.cpp las-arith.cpp

sieve/fb.h
@@ -22,19 +22,7 @@
 #include "fb-types.h"
 #include "las-qlattice.h"
 #include "las-plattice.h"
-
-/* Base class with private copy-constructor and assignment operator.
-   Classes which are not copy-constructible can inherit this with:
-   private NonCopyable */
-class NonCopyable {
- protected:
-   NonCopyable() {}
-   ~NonCopyable() {}
- private:
-   NonCopyable(const NonCopyable&);
-   NonCopyable& operator=(const NonCopyable&);
-};
-
+#include "las-base.hpp"
 
 /* Forward declaration so fb_general_entry can use it in constructors */
 template <int Nr_roots>

sieve/las-base.hpp
@@ -1,0 +1,19 @@
+#ifndef LAS_BASE_HPP_
+#define LAS_BASE_HPP_
+
+/* C++ base classes that are not application-specific. */
+
+
+/* Base class with private copy-constructor and assignment operator.
+   Classes which are not copy-constructible can inherit this with:
+   private NonCopyable */
+class NonCopyable {
+ protected:
+   NonCopyable() {}
+   ~NonCopyable() {}
+ private:
+   NonCopyable(const NonCopyable&);
+   NonCopyable& operator=(const NonCopyable&);
+};
+
+#endif

sieve/las-fill-in-buckets.cpp
@@ -921,21 +921,27 @@ fill_in_m_buckets(thread_data_ptr th, int side, where_am_I_ptr w MAYBE_UNUSED)
 #endif
 
 
+void
+fill_in_buckets_one_slice(thread_data_ptr th, const int side, const fb_slice_interface *slice)
+{
+    where_am_I w;
+    WHERE_AM_I_UPDATE(w, si, th->si);
+    const fb_transformed_vector *transformed_vector = slice->make_lattice_bases(th->si->qbasis, th->si->conf->logI);
+    fill_in_buckets(th, side, transformed_vector, slice, w);
+    delete transformed_vector;
+}
+
 static void
 fill_in_buckets_one_side(thread_data_ptr th, const int side)
 {
-    fb_part *fb = th->sides[side]->fb;
-    where_am_I w;
-    WHERE_AM_I_UPDATE(w, si, th->si);
+    const fb_part *fb = th->sides[side]->fb;
     if (th->sides[side]->BA.n_bucket < THRESHOLD_K_BUCKETS) {
       /* Process all slices in this factor base part */
       const fb_slice_interface *slice;
       for (slice_index_t slice_index = fb->get_first_slice_index();
            (slice = fb->get_slice(slice_index)) != NULL;
            slice_index++) {
-          const fb_transformed_vector *transformed_vector = slice->make_lattice_bases(th->si->qbasis, th->si->conf->logI);
-          fill_in_buckets(th, side, transformed_vector, slice, w);
-          delete transformed_vector;
+          fill_in_buckets_one_slice(th, side, slice);
       }
     }
 #ifdef HAVE_K_BUCKETS

sieve/las-fill-in-buckets.h
@@ -2,6 +2,7 @@
 #define LAS_FILL_IN_BUCKETS_H_
 
 #include "las-types.h"
+#include "las-threads.h"
 
 #ifdef __cplusplus
 extern "C" {

sieve/las-forwardtypes.h
@@ -1,0 +1,16 @@
+#ifndef LAS_FORWARDTYPES_H_
+#define LAS_FORWARDTYPES_H_
+
+/* These must be forward-declared, because tvarious header files use them */
+
+struct sieve_info_s;
+typedef struct sieve_info_s * sieve_info_ptr;
+typedef const struct sieve_info_s * sieve_info_srcptr;
+struct las_info_s;
+typedef struct las_info_s * las_info_ptr;
+typedef const struct las_info_s * las_info_srcptr;
+struct where_am_I_s;
+typedef struct where_am_I_s * where_am_I_ptr;
+typedef const struct where_am_I_s * where_am_I_srcptr;
+
+#endif

sieve/las-threads.h
@@ -1,0 +1,57 @@
+#ifndef LAS_THREADS_H_
+#define LAS_THREADS_H_
+
+#include "las-forwardtypes.h"
+#include "bucket.h"
+#include "fb.h"
+#include "las-report-stats.h"
+#include "las-base.hpp"
+
+/* {{{ thread-related defines */
+/* All of this exists _for each thread_ */
+struct thread_side_data_s : private NonCopyable {
+  m_bucket_array_t mBA; /* Not used if not fill_in_m_buckets (3 passes sort) */
+  k_bucket_array_t kBA; /* Ditto for fill_in_k_buckets (2 passes sort) */
+  bucket_array_t BA;    /* Always used */
+  const fb_part *fb;
+
+  /* For small sieve */
+  int * ssdpos;
+  int * rsdpos;
+
+  unsigned char *bucket_region;
+
+  thread_side_data_s();
+  ~thread_side_data_s();
+  void set_fb(const fb_part *_fb) {fb = _fb;}
+};
+typedef struct thread_side_data_s thread_side_data[1];
+typedef struct thread_side_data_s * thread_side_data_ptr;
+typedef const struct thread_side_data_s * thread_side_data_srcptr;
+
+struct thread_data_s : private NonCopyable {
+  int id;
+  thread_side_data sides[2];
+  las_info_srcptr las;
+  sieve_info_ptr si;
+  las_report rep;
+  unsigned int checksum_post_sieve[2];
+  unsigned char *SS;
+  bool is_initialized;
+  thread_data_s();
+  ~thread_data_s();
+  void init(int id, las_info_srcptr las);
+  void pickup_si(sieve_info_ptr si);
+};
+typedef struct thread_data_s *thread_data_ptr;
+typedef const struct thread_data_s *thread_data_srcptr;
+
+void thread_do(thread_data_ptr, void * (*) (thread_data_ptr), int);
+thread_data_ptr thread_data_alloc(las_info_ptr las, int n);
+void thread_data_free(thread_data_ptr thrs);
+void thread_pickup_si(thread_data_ptr thrs, sieve_info_ptr si, int n);
+void thread_buckets_alloc(thread_data_ptr thrs, unsigned int n);
+void thread_buckets_free(thread_data_ptr thrs, unsigned int n);
+double thread_buckets_max_full(thread_data_ptr thrs, int n);
+
+#endif

commit 65adf72feea2340d2d9b0f4f32368c1fcbd7fcab

Fixed bugs & C++11-ism introduced by fa0effa.
(yet fa0effa was not a morning commit...)

For the C++11-ism: it's called brace-initialization.

files.dist
@@ -1189,3 +1189,7 @@ linalg/bwc/lingen_qcode_do.cpp
 linalg/bwc/lingen_qcode.h
 utils/version_info.h
 utils/version_info.c.in
+sieve/las-base.hpp
+sieve/las-forwardtypes.h
+sieve/las-threads.cpp
+sieve/las-threads.h

sieve/las-threads.cpp
@@ -17,8 +17,10 @@ thread_side_data_s::~thread_side_data_s()
   bucket_region = NULL;
 }
 
-thread_data_s::thread_data_s() : checksum_post_sieve{0, 0}, is_initialized(false)
+thread_data_s::thread_data_s() : is_initialized(false)
 {
+  checksum_post_sieve[0] = 0;
+  checksum_post_sieve[1] = 0;
   /* Allocate memory for the intermediate sum (only one for both sides) */
   SS = (unsigned char *) contiguous_malloc(BUCKET_REGION);
 }