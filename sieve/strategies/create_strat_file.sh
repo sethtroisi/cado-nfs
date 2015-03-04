e97aa62e7d76f79b033281b628c21f9bb6712650
Pierrick Gaudry <pierrick.gaudry@loria.fr>
2015-03-04 10:40:54 +0100

    few changes to scripts to generate strat file

--- sieve/strategies/create_strat_file.sh
+++ sieve/strategies/create_strat_file.sh
@@ -25,6 +25,7 @@ fbb=`echo "l($lim)/l(2)" | bc -l | cut -d "." -f 1`
 GFM=$cadodir/sieve/strategies/gfm
 BENCHFM=$cadodir/sieve/strategies/benchfm
 GST=$cadodir/sieve/strategies/gst
+FINALST=$cadodir/sieve/strategies/finalst
 MAKEFB=$cadodir/sieve/makefb
 LAS=$cadodir/sieve/las
 
@@ -32,6 +33,7 @@ for m in PM1 ECM-M12 ECM-M16 ECM-B12 PP1-27 PP1-65; do
     rm -f data_${m}_*
 done
 
+echo "### First selection of methods"
 x=0
 while read m b1min b1max b1step cmin cmax cstep; do
     outfile=`mktemp -p . data_${m}_XXXXXXX`
@@ -45,7 +47,15 @@ while read m b1min b1max b1step cmin cmax cstep; do
     else
         $cmd &
     fi
-done << EOF
+# done << EOF
+# PM1      50   150   50  10  30   10 
+# ECM-M12  50   150   50  10  30   10 
+# ECM-M16  50   150   50  10  30   10 
+# ECM-B12  50   150   50  10  30   10 
+# PP1-27   50   150   50  10  30   10 
+# PP1-65   50   150   50  10  30   10 
+# EOF
+done <<EOF
 PM1      50   300   20  10  50   5 
 PM1      300  600   20  10  50   5 
 PM1      50   300   20  50  200  10 
@@ -104,8 +114,13 @@ $GFM -fch -fch_in data_ECM_B12 -fch_out data_ECM_B12_ch
 #     rm -f data_${m}_*
 # done
 
+echo "######## Second selection of methods, full bench"
+echo "######## This might take some time, be patient..."
 for XXX in PP1_27 PM1 PP1_65 ECM_M12 ECM_M16 ECM_B12; do 
-    $BENCHFM -in data_${XXX}_ch -p -t -lb $fbb -out data_${XXX}_pt
+    echo "# $XXX"
+    cmd="$BENCHFM -in data_${XXX}_ch -p -t -lb $fbb -out data_${XXX}_pt"
+    echo $cmd
+    $cmd
 done
 
 cat data_PP1_65_pt data_PP1_27_pt > data_PP1_pt
@@ -117,17 +132,23 @@ for XXX in PP1 PM1 ECM_M16 ECM_RC; do
 done
 cat PP1 PM1 ECM_M16 ECM_RC > All_methods
 
+echo "######## Generate decompositions"
 # Generate decompositions
 mkdir decomp
 xmin=`echo "l($lim0*$lim0)/l(2)" | bc -l | cut -d "." -f 1`
 for x in `seq $xmin $mfb0`; do
-    $GST -gdc -lim0 $lim0 -mfb0 $x -out decomp/decomp_${lim0}_${x}
+    cmd="$GST -gdc -lim0 $lim0 -mfb0 $x -out decomp/decomp_${lim0}_${x}"
+    echo $cmd
+    $cmd
 done
 xmin=`echo "l($lim1*$lim1)/l(2)" | bc -l | cut -d "." -f 1`
 for x in `seq $xmin $mfb1`; do
-    $GST -gdc -lim0 $lim1 -mfb0 $x -out decomp/decomp_${lim1}_${x}
+    cmd="$GST -gdc -lim0 $lim1 -mfb0 $x -out decomp/decomp_${lim1}_${x}"
+    echo $cmd
+    $cmd
 done
 
+echo "######## Strategies"
 # precompute strategies
 mkdir res_precompt_st
 for r0 in `seq 0 $mfb0`; do
@@ -145,6 +166,7 @@ for r0 in `seq 0 $mfb0`; do
     done
 done
 
+echo "######## Running las for getting a sample of caofactors"
 #collect a sample of the distribution of the pairs of cofactors.
 $MAKEFB -poly $poly -alim $lim0 -maxbits $I -out XXXX.roots0 -side 0
 $MAKEFB -poly $poly -alim $lim1 -maxbits $I -out XXXX.roots1 -side 1
@@ -155,7 +177,8 @@ $LAS -poly $poly -I $I -fb0 XXXX.roots0 -fb1 XXXX.roots1 -lim0 $lim0 -lim1 $lim1
 line=`grep "Total cpu time" XXXX.out`
 t0=`echo $line | cut -d " " -f 5 | cut -d s -f 1`
 t1=`echo $line | cut -d " " -f 19 | cut -d ")" -f 1`
-t=$(($t0-$t1))
+t=`echo $t0-$t1 | bc -l`
 
+echo "######## Final strategy"
 # emulator
 $FINALST -st res_matrix -dist cofactors.stats -t $t -mfb0 $mfb0 -mfb1 $mfb1 final_st
