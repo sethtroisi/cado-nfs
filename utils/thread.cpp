1bcc22a837d5f225303fcfb5500dbc0baa6531d9
Alexander Kruppa <Alexander.Kruppa@loria.fr>
2015-03-17 20:27:03 +0100

    More steps towards thread pool

--- utils/thread.cpp
+++ utils/thread.cpp
@@ -1,35 +1,31 @@
-static void *thread_wait_for_task(void * pool)
+#include "cado.h"
+#include "thread.h"
+
+worker_thread::worker_thread(thread_pool &_pool) : pool(_pool)
 {
-  /* See if there is a task in my preferred queue. If not, see if there is a task in another queue */
-  /* If there is a task, remove it from the list, remove the thread from the list of waiting threads, and work on the task */
-  /* If there is no task, wait for the condition variable */
+  int rc = pthread_create(&thread, NULL, pool.thread_work_on_tasks, this);
+  ASSERT_ALWAYS(rc == 0);
 }
 
-thread_pool::thread_pool(const size_t _nr_queues)
-    : mutex(PTHREAD_MUTEX_INITIALIZER), cond(PTHREAD_COND_INITIALIZER), nr_queues(_nr_queues)
-{
-  queues = new task_queue_t[nr_queues];
-  busy_threads = new thread_queue_t[nr_queues];
-  waiting_threads = new thread_queue_t[nr_queues];
+worker_thread::~worker_thread() {
+  int rc = pthread_join(thread, NULL);
+  ASSERT_ALWAYS(rc == 0);
 }
 
-thread_pool::~thread_pool()
+void *
+thread_pool::thread_work_on_tasks(void *arg)
 {
-  /* Wait until all the tasks queue is empty and all threads are idle */
-  /* Then join all threads, delete the queues */
-}
-
-void thread_pool::add_threads(const size_t nr_threads, const size_t preferred_queue)
-{
-  pthread_t *thread = new pthread_t;
-  pthread_attr_t attr;
-  int rc;
-  rc = pthread_attr_init (&attr);
-  rc = pthread_create(thread, attr, thread_wait_for_task, this);
-  rc = pthread_attr_destroy(&attr);
-  
-}
-
-void thread_pool::add_task(size_t which_queue, task_function_t, void *)
-{
+  worker_thread *I = (worker_thread *) arg;
+  thread_pool &pool = I->pool;
+  while (1) {
+    thread_task *task = pool.get_task();
+    if (task->please_die)
+      break;
+    task_function_t func = task->func;
+    task_parameters *params = task->parameters;
+    task_result *result = func(params);
+    pool.add_result(result);
+    delete task;
+  }
+  return NULL;
 }
--- utils/thread.h
+++ utils/thread.h
@@ -1,41 +1,139 @@
 #include <pthread.h>
 #include <queue>
-#include <list>
+#include <vector>
 #include <stdarg.h>
 
-typedef void *(*task_function_t)(void *);
+#include "macros.h"
 
-class thread_task {
-  task_function_t func;
-  void *parameters;
+class NonCopyable {
+ protected:
+   NonCopyable() {}
+   ~NonCopyable() {}
+ private:
+   NonCopyable(const NonCopyable&);
+   NonCopyable& operator=(const NonCopyable&);
 };
 
-class thread {
-  pthread_t thread;
-  size_t preferred_queue;
-  thread(size_t _preferred_queue) : preferred_queue(_preferred_queue){
+/* Base for classes that hold parameters for worker functions */
+class task_parameters;
+
+/* Base for classes that hold results produced by worker functions */
+class task_result;
+
+class monitor {
+  pthread_mutex_t mutex;
+public:
+  monitor() : mutex(PTHREAD_MUTEX_INITIALIZER) {}
+  ~monitor(){ASSERT_ALWAYS(pthread_mutex_destroy(&mutex) == 0);}
+  void enter() {ASSERT_ALWAYS(pthread_mutex_lock(&mutex) == 0);}
+  void leave() {ASSERT_ALWAYS(pthread_mutex_unlock(&mutex) == 0);}
+  void signal(pthread_cond_t &cond) {ASSERT_ALWAYS(pthread_cond_signal(&cond) == 0);}
+  void wait(pthread_cond_t &cond) {ASSERT_ALWAYS(pthread_cond_wait(&cond, &mutex) == 0);}
+};
+
+typedef task_result *(*task_function_t)(const task_parameters *);
+
+class thread_task {
+public:
+  const task_function_t func;
+  const int id;
+  task_parameters * const parameters;
+  task_result *result;
+  const bool please_die;
+
+  thread_task(task_function_t _func, int _id, task_parameters *_parameters) :
+    func(_func), id(_id), parameters(_parameters), result(NULL), please_die(false) {};
+  thread_task(bool _kill)
+    : func(NULL), id(0), parameters(NULL), result(NULL), please_die(true) {
+    ASSERT_ALWAYS(_kill);
   }
 };
 
-class thread_pool {
-  typedef std::queue<thread_task *> task_queue_t;
-  typedef std::list<thread *> threads_t;
+class thread_pool;
 
-  task_queue_t *task_queues;
-  threads_t busy_threads, waiting_threads;
-  size_t nr_queues;
-  size_t nr_threads;
-  pthread_mutex_t mutex;
-  pthread_cond_t cond;
+class worker_thread : private NonCopyable {
+  friend class thread_pool;
+  thread_pool &pool;
+  pthread_t thread;
+public:
+  worker_thread(thread_pool &_pool);
+  ~worker_thread();
+};
 
-  void enter() {pthread_mutex_lock(mutex);}
-  void leave() {pthread_mutex_unlock(mutex);}
+class thread_pool : private monitor, private NonCopyable {
+  friend class worker_thread;
+  typedef worker_thread *worker_thread_ptr;
+  worker_thread_ptr *threads;
+  std::queue<thread_task *> tasks;
+  std::queue<task_result *> results;
 
-  void thread_assign_task();
+  const size_t nr_threads;
+
+  pthread_cond_t tasks_not_empty_cond, results_not_empty_cond;
+
+  bool kill_threads; /* If true, hands out kill tasks once queue is empty */
+
+  static void * thread_work_on_tasks(void *pool);
 
 public:
-  thread_pool(const size_t _nr_queues);
-  ~thread_pool();
-  void add_threads(const size_t nr_threads, size_t preferred_queue);
-  void add_task(size_t which_queue, task_function_t, void *);
+  thread_pool(size_t _nr_threads) : nr_threads(_nr_threads), kill_threads(false) {
+    threads = new worker_thread_ptr[nr_threads];
+    for (size_t i = 0; i < nr_threads; i++)
+      threads[i] = new worker_thread(*this);
+  };
+  ~thread_pool() {
+    enter();
+    kill_threads = true;
+    leave();
+    for (size_t i = 0; i < nr_threads; i++)
+      delete threads[i];
+    delete threads;
+    ASSERT_ALWAYS(tasks.empty());
+    ASSERT_ALWAYS(results.empty());
+    pthread_cond_destroy(&tasks_not_empty_cond);
+    pthread_cond_destroy(&results_not_empty_cond);
+  }
+
+  void add_task(task_function_t func, task_parameters *params, int id) {
+    enter();
+    tasks.push(new thread_task(func, id, params));
+    signal(tasks_not_empty_cond);
+    leave();
+  }
+  
+  thread_task *get_task() {
+    enter();
+    thread_task *task;
+    if (kill_threads && tasks.empty()) {
+      task = new thread_task(true);
+    } else {
+      while (tasks.empty()) {
+        /* No work -> tell this thread to wait until work becomes available.
+           The while() protects against spurious wake-ups that can fire even
+           if the queue is still empty */
+        wait(tasks_not_empty_cond);
+      }
+      task = tasks.front();
+      tasks.pop();
+    }
+    leave();
+    return task;
+  }
+  
+  void add_result(task_result *const result) {
+    enter();
+    results.push(result);
+    signal(results_not_empty_cond);
+    leave();
+  }
+
+  task_result *get_result() {
+    enter();
+    while (results.empty())
+      wait(results_not_empty_cond);
+    task_result *result = results.front();
+    results.pop();
+    leave();
+    return result;
+  }
 };
