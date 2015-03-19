8fc5aed44f5c4c20c8b4e7565b331595a79114b9
Pierrick Gaudry <pierrick.gaudry@loria.fr>
2015-03-19 16:19:10 +0100

    Remove workaround bug 18751 in tests

--- tests/sieve/F9.todotest.sh
+++ tests/sieve/F9.todotest.sh
@@ -7,8 +7,6 @@ TODO="$4"
 
 cmd="${LAS} -poly ${SRCDIR}/params/F9.poly -lim0 2300000 -lim1 1200000 -lpb0 26 -lpb1 26 -mfb0 52 -mfb1 52 -I 12 -fb1 ${FB} -todo ${TODO}"
 echo $cmd
-## There is a bug on 32-bit archs so that we find only 418 relations
-## instead of 420. This is referenced in the tracker as Bug #18751
-$cmd | grep -e "Total 420 reports" -e "Total 418 reports" || exit 1
+$cmd | grep -e "Total 420 reports" || exit 1
 
 exit 0
