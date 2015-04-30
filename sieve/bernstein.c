5fbeda8275c3bbcbe76ac6ee491d921f1cdd87e6
Christophe Clavier <christophe.clavier@unilim.fr>
2015-04-30 09:45:42 +0200

    Smoothness test ; in course of writing

--- sieve/bernstein.c
+++ sieve/bernstein.c
@@ -13,6 +13,10 @@
 #include "gmp.h"
 #include "utils.h"
 
+#define STATUS_SMOOTH  0
+#define STATUS_UNKNOWN 1
+#define STATUS_USELESS 2
+
 unsigned long
 tree_height (unsigned long n)
 {
@@ -27,18 +31,20 @@ tree_height (unsigned long n)
 }
 
 /* Input:
-   R[0], ..., R[n-1] are cofactors (will be destroyed)
+   R[0], ..., R[n-1] are cofactors
    P is the product of primes
    Output:
-   for all cofactors that have only factors in P, the new value of R[j] is 0
+   Each R[j] has been divided by its P-smooth part
 */
 void
 smoothness_test (mpz_t *R, unsigned long n, mpz_t P)
 {
   unsigned long h = tree_height (n), i, j, w[64];
   mpz_t **T;
+  mpz_t w1, w2;
 
-  ASSERT_ALWAYS(h >= 2);
+  mpz_init(w1);
+  mpz_init(w2);
 
   T = (mpz_t**) malloc ((h + 1) * sizeof (mpz_t*));
   T[0] = R;
@@ -79,19 +85,21 @@ smoothness_test (mpz_t *R, unsigned long n, mpz_t P)
   /* special last loop for i=1 */
   for (j = 0; j < n / 2; j++)
     {
-      mpz_mod (T[2][0], T[1][j], T[0][2*j]);
-      if (mpz_cmp_ui (T[2][0], 0) != 0) /* put non-smooth residues to 0 */
-        mpz_set_ui (T[0][2*j], 0);
-      mpz_mod (T[2][0], T[1][j], T[0][2*j+1]);
-      if (mpz_cmp_ui (T[2][0], 0) != 0) /* put non-smooth residues to 0 */
-        mpz_set_ui (T[0][2*j+1], 0);
+      mpz_mod (w1, T[1][j], T[0][2*j]);
+      mpz_gcd(w2, w1, T[0][2*j]);
+      mpz_divexact(T[0][2*j], T[0][2*j], w2);
+      mpz_mod (w1, T[1][j], T[0][2*j+1]);
+      mpz_gcd(w2, w1, T[0][2*j+1]);
+      mpz_divexact(T[0][2*j+1], T[0][2*j+1], w2);
     }
   if (n & 1)
     {
-      if (mpz_cmp_ui (T[1][w[1]-1], 0) != 0) /* put non-smooth residues to 0 */
-        mpz_set_ui (T[0][n-1], 0);
+      mpz_gcd(w2, T[1][w[1]-1], T[0][n-1]);
+      mpz_divexact(T[0][n-1], T[0][n-1], w2);
     }
 
+  mpz_clear(w1);
+  mpz_clear(w2);
   for (i = 1; i <= h; i++)
     {
       for (j = 0; j < w[i]; j++)
@@ -121,19 +129,88 @@ prime_product (mpz_t P, unsigned long lim, unsigned long pmax)
           mpz_set_ui (L[n++], p);
         }
     }
-  /* FIXME: equilibrate the product */
+  getprime(0);
+
+    /* FIXME: equilibrate the product */
   while (n > 1)
     {
       for (i = 0; i+1 < n; i+=2)
         mpz_mul (L[i/2], L[i], L[i+1]);
       if (n & 1)
-        mpz_swap (L[n/2], L[n-1]);
+        mpz_set(L[n/2], L[n-1]); 
+//        mpz_swap (L[n/2], L[n-1]);
       n = (n + 1) / 2;
     }
-  mpz_swap (P, L[0]);
+//  mpz_swap (P, L[0]);
+  mpz_set (P, L[0]);
+  
   for (i = 0; i < alloc; i++)
     mpz_clear (L[i]);
   free (L);
+}
+
+void update_status(mpz_t *R, mpz_t *A, unsigned char *b_status_r, unsigned char *b_status_a, unsigned long int n, unsigned int b_side_R,
+                   unsigned long int rlim_high, unsigned long int lpbr, unsigned long int alim_high, unsigned long int lpba,
+                   unsigned long int *nb_smooth_r, unsigned long int *nb_useless_r, unsigned long int *nb_smooth_a, unsigned long int *nb_useless_a)
+{
+  unsigned long int i;
+  
+  if (b_side_R)
+  {
+    for (i = 0; i < n; i++)
+    {
+      if (b_status_r[i] == STATUS_UNKNOWN)
+      {
+        if (mpz_cmp_ui (R[i], 1) == 0)
+          b_status_r[i] = STATUS_SMOOTH;
+        else if ( (mpz_cmp_ui(R[i], (1UL << lpbr)) <= 0) && (mpz_probab_prime_p(R[i], 6) != 0) )
+        {
+          b_status_r[i] = STATUS_SMOOTH;
+          mpz_set_ui(R[i], 1);
+        }
+        else if ( (mpz_cmp_ui(R[i], rlim_high * (1UL << lpbr)) <= 0) && (mpz_probab_prime_p(R[i], 6) == 0) )
+        {
+          b_status_r[i] = STATUS_SMOOTH;
+          mpz_set_ui(R[i], 1);
+        }
+        else if ( (mpz_cmp_ui(R[i], (1UL << lpbr)) > 0) && (mpz_probab_prime_p(R[i], 6) != 0) )
+        {
+          b_status_r[i] = STATUS_NON_SMOOTH;
+          mpz_set_ui(R[i], 1);
+          b_status_a[i] = STATUS_NON_SMOOTH;
+          mpz_set_ui(A[i], 1);
+        }
+      }
+    }
+  }
+  else
+  {
+    for (i = 0; i < n; i++)
+    {
+      if (b_status_a[i] == STATUS_UNKNOWN)
+      {
+        if (mpz_cmp_ui (A[i], 1) == 0)
+          b_status_a[i] = STATUS_SMOOTH;
+        else if ( (mpz_cmp_ui(A[i], (1UL << lpba)) <= 0) && (mpz_probab_prime_p(A[i], 6) != 0) )
+        {
+          b_status_a[i] = STATUS_SMOOTH;
+          mpz_set_ui(A[i], 1);
+        }
+        else if ( (mpz_cmp_ui(A[i], alim_high * (1UL << lpba)) <= 0) && (mpz_probab_prime_p(A[i], 6) == 0) )
+        {
+          b_status_a[i] = STATUS_SMOOTH;
+          mpz_set_ui(A[i], 1);
+        }
+        else if ( (mpz_cmp_ui(A[i], (1UL << lpba)) > 0) && (mpz_probab_prime_p(A[i], 6) != 0) )
+        {
+          b_status_a[i] = STATUS_NON_SMOOTH;
+          mpz_set_ui(A[i], 1);
+          b_status_r[i] = STATUS_NON_SMOOTH;
+          mpz_set_ui(R[i], 1);
+        }
+      }
+    }
+  }
 }
 
 int
@@ -145,7 +222,30 @@ main ()
   unsigned long i;
   size_t ret;
   double s;
+  unsigned int n0_pass;
+  unsigned long int lpbr = 33;
+  unsigned long int lpba = 33;
+  unsigned long int rlim_low;
+  unsigned long int alim_low;
+  unsigned long int rlim_high;
+  unsigned long int alim_high;
+  unsigned long int rlim_step = 250000000;
+  unsigned long int alim_step = 250000000;
+  unsigned char *b_status_r;
+  unsigned char *b_status_a;
+  unsigned long int nb_smooth_r;
+  unsigned long int nb_useless_r;
+  unsigned long int nb_smooth_a;
+  unsigned long int nb_useless_a;
 
+
+  b_status_r = (unsigned char *) malloc(n * sizeof(unsigned char));
+  b_status_a = (unsigned char *) malloc(n * sizeof(unsigned char));
+  for (i = 0; i < n; i++)
+  {
+    b_status_r[i] = STATUS_UNKNOWN;
+    b_status_a[i] = STATUS_UNKNOWN;
+  }
   cofac_r = fopen ("/tmp/cofac_r", "r");
   cofac_a = fopen ("/tmp/cofac_a", "r");
   R = malloc (n * sizeof (mpz_t));
@@ -162,39 +262,76 @@ main ()
   fprintf (stderr, "Read %lu cofactors\n", n);
   mpz_init (P);
 
-  /* rational side */
-  s = seconds ();
-  prime_product (P, 250000000, 280000000); /* UPDATE */
-  fprintf (stderr, "Computed P of %lu bits took %.0f seconds\n",
-           mpz_sizeinbase (P, 2), seconds () - s);
-  s = seconds ();
-  smoothness_test (R, n, P);
-  printf ("smoothness_test took %.0f seconds\n", seconds () - s);
 
-  for (i = 0; i < n; i++)
-    if (mpz_cmp_ui (R[i], 0) == 0)
-      mpz_set_ui (A[i], 1);
+  /**** Initial pass ***/
+  
+  printf("\n");
+  printf("Initial pass\n");
 
-  /* algebraic side */
-  s = seconds ();
-  prime_product (P, 500000000, 530000000); /* UPDATE */
-  fprintf (stderr, "Computed P of %lu bits took %.0f seconds\n",
-           mpz_sizeinbase (P, 2), seconds () - s);
-  s = seconds ();
-  smoothness_test (A, n, P);
-  printf ("smoothness_test took %.0f seconds\n", seconds () - s);
+  rlim_high = 250000000;
+  alim_high = 500000000;
+
+  nb_  
+  update_status(R, A, b_status_r, b_status_a, n, 1, rlim_high, lpbr, alim_high, lpba);
+  update_status(R, A, b_status_r, b_status_a, n, 0, rlim_high, lpbr, alim_high, lpba);
+
+  n0_pass = 0;
+  while (n0_pass < 20)
+  {
+    n0_pass++;
+ 
+    printf("\n");
+    printf("Pass %u\n", n0_pass);
+    
+    /* rational side */
+
+    rlim_low = rlim_high;
+    rlim_high += rlim_step;
+
+    printf("rlim : %lu:%lu\n", rlim_low, rlim_high);  
+
+    s = seconds ();
+    prime_product (P, rlim_low, rlim_high); /* UPDATE */
+    fprintf (stderr, "Computed P of %lu bits took %.0f seconds\n",
+             mpz_sizeinbase (P, 2), seconds () - s);
+    s = seconds ();
+    smoothness_test (R, n, P);
+    printf ("smoothness_test took %.0f seconds\n", seconds () - s);
+
+    update_status(R, A, b_status_r, b_status_a, n, 1, rlim_high, lpbr, alim_high, lpba);
+
+    /* algebraic side */
+
+    alim_low = alim_high;
+    alim_high += alim_step;
+
+    printf("alim : %lu:%lu\n", alim_low, alim_high);  
+
+    s = seconds ();
+    prime_product (P, alim_low, alim_high); /* UPDATE */
+    fprintf (stderr, "Computed P of %lu bits took %.0f seconds\n",
+             mpz_sizeinbase (P, 2), seconds () - s);
+    s = seconds ();
+    smoothness_test (A, n, P);
+    printf ("smoothness_test took %.0f seconds\n", seconds () - s);
+
+    update_status(R, A, b_status_r, b_status_a, n, 0, rlim_high, lpbr, alim_high, lpba);
+  }
 
   mpz_clear (P);
 
   for (i = 0; i < n; i++)
     {
-      if (mpz_cmp_ui (R[i], 0) != 0 && mpz_cmp_ui (A[i], 0) != 0)
-        gmp_printf ("%Zd %Zd\n", R[i], A[i]);
+//      gmp_fprintf (stderr, "%Zd %Zd\n", R[i], A[i]);
       mpz_clear (R[i]);
       mpz_clear (A[i]);
     }
+    
+  free (b_status_r);
+  free (b_status_a);
   free (R);
   free (A);
   fclose (cofac_r);
   fclose (cofac_a);
 }
+
