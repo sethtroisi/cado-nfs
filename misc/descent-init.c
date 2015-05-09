67ae771bc10cf03d97aeda8134f93662fd246b84
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2015-05-09 10:29:20 +0200

    new version with a pool for promising candidates

--- misc/descent-init.c
+++ misc/descent-init.c
@@ -35,6 +35,185 @@
 #include "gmp.h"
 #include "ecm.h"
 
+typedef struct {
+  unsigned long e; /* exponent of z */
+  mpz_t u0, v0;    /* z^e = u0/v0 mod p */
+  mpz_t u, v;      /* unfactored parts of u, v */
+  int uprime, vprime;
+  double B1;       /* current B1 value */
+  unsigned long l; /* potential size */
+} cand_t;
+typedef cand_t cand[1];
+
+unsigned long
+bsize (mpz_t u, int uprime)
+{
+  if (uprime)
+    return mpz_sizeinbase (u, 2);
+  else /* if u has n bits, we have 2^(n-1) <= u < 2^n: if u = p*q with p <= q,
+          then q >= 2^((n-1)/2) and q has at least floor(((n-1)/2)+1 bits */
+    return (mpz_sizeinbase (u, 2) + 1) / 2;
+}
+
+void
+cand_init_set (cand c, unsigned long e, mpz_t u0, mpz_t v0, mpz_t u, mpz_t v,
+               int uprime, int vprime, double B1, unsigned long l)
+{
+  c->e = e;
+  mpz_init_set (c->u0, u0);
+  mpz_init_set (c->v0, v0);
+  mpz_init_set (c->u, u);
+  mpz_init_set (c->v, v);
+  c->uprime = uprime;
+  c->vprime = vprime;
+  c->B1 = B1;
+  c->l = l;
+}
+
+void
+cand_clear (cand c)
+{
+  mpz_clear (c->u0);
+  mpz_clear (c->v0);
+  mpz_clear (c->u);
+  mpz_clear (c->v);
+}
+
+void
+cand_scan (cand c, ecm_params params)
+{
+  mpz_t f;
+  unsigned long lu, lv;
+
+  mpz_init (f);
+  if (c->uprime == 0)
+    {
+      mpz_set_d (params->sigma, c->B1);
+      mpz_set_ui (f, 1);
+      ecm_factor (f, c->u, c->B1, params);
+      params->B1done = ECM_DEFAULT_B1_DONE; /* issue in 6.4.x */
+      if (mpz_cmp_ui (f, 1) > 0)
+        {
+          mpz_divexact (c->u, c->u, f);
+          c->uprime = mpz_probab_prime_p (c->u, 1);
+          lu = bsize (c->u, c->uprime);
+          lv = bsize (c->v, c->vprime);
+          c->l = (lu > lv) ? lu : lv;
+        }
+    }
+  if (c->vprime == 0)
+    {
+      mpz_set_d (params->sigma, c->B1);
+      mpz_set_ui (f, 1);
+      ecm_factor (f, c->v, c->B1, params);
+      params->B1done = ECM_DEFAULT_B1_DONE; /* issue in 6.4.x */
+      if (mpz_cmp_ui (f, 1) > 0)
+        {
+          mpz_divexact (c->v, c->v, f);
+          c->vprime = mpz_probab_prime_p (c->v, 1);
+          lu = bsize (c->u, c->uprime);
+          lv = bsize (c->v, c->vprime);
+          c->l = (lu > lv) ? lu : lv;
+        }
+    }
+  c->B1 = c->B1 + sqrt (c->B1);
+  mpz_clear (f);
+}
+
+void
+cand_print (cand c)
+{
+  printf ("e=%lu\n", c->e);
+  gmp_printf ("u0=%Zd\n", c->u0);
+  gmp_printf ("v0=%Zd\n", c->v0);
+  gmp_printf ("u=%Zd (%lu bits)\n", c->u, mpz_sizeinbase (c->u, 2));
+  gmp_printf ("v=%Zd (%lu bits)\n", c->v, mpz_sizeinbase (c->v, 2));
+  fflush (stdout);
+}
+
+void
+cand_set (cand c, cand d)
+{
+  c->e = d->e;
+  mpz_swap (c->u0, d->u0);
+  mpz_swap (c->v0, d->v0);
+  mpz_swap (c->u, d->u);
+  mpz_swap (c->v, d->v);
+  c->uprime = d->uprime;
+  c->vprime = d->vprime;
+  c->B1 = d->B1;
+  c->l = d->l;
+}
+
+/*****************************************************************************/
+
+typedef struct {
+  cand *list;
+  unsigned long n;
+} pool_t;
+typedef pool_t pool[1];
+
+void
+pool_init (pool p)
+{
+  p->list = NULL;
+  p->n = 0;
+}
+
+void
+pool_clear (pool p)
+{
+  unsigned long i;
+
+  for (i = 0; i < p->n; i++)
+    cand_clear (p->list[i]);
+  free (p->list);
+}
+
+void
+pool_add (pool p, unsigned long e, mpz_t u0, mpz_t v0, mpz_t u, mpz_t v,
+          int uprime, int vprime, double B1, unsigned long l)
+{
+  p->list = realloc (p->list, (p->n + 1) * sizeof (cand));
+  cand_init_set (p->list[p->n], e, u0, v0, u, v, uprime, vprime, B1, l);
+  p->n += 1;
+}
+
+/* perform an ECM curve for each candidate in the pool */
+unsigned long
+pool_scan (pool p, ecm_params params, unsigned long L)
+{
+  unsigned long i, j;
+
+  for (i = 0; i < p->n; i++)
+    cand_scan (p->list[i], params);
+  for (i = 0; i < p->n; i++)
+    if (p->list[i]->uprime && p->list[i]->vprime)
+      {
+        if (p->list[i]->l < L) /* found better solution */
+          {
+            cand_print (p->list[i]);
+            L = p->list[i]->l;
+          }
+      }
+  for (i = j = 0; i < p->n; i++)
+    if ((p->list[i]->uprime && p->list[i]->vprime) || p->list[i]->l >= L)
+      {
+      }
+    else
+      cand_set (p->list[j++], p->list[i]);
+  if (j < p->n)
+    {
+      p->n = j;
+      p->list = realloc (p->list, p->n * sizeof (cand));
+      printf ("Remains %lu candidates in pool\n", p->n);
+      fflush (stdout);
+    }
+  return L;
+}
+
+/*****************************************************************************/
+
 int
 cputime ()
 {
@@ -83,16 +262,31 @@ int
 main (int argc, char *argv[])
 {
   mpz_t p, z, ze, t, u, v, f, u0, v0;
-  unsigned long e, L, l, lu, lv, i, seed = 0;
+  unsigned long e, L, l, lu, lv, i, seed = 0, target = 0;
   double B1, S, Smax = 1000.0;
   int uprime, vprime;
   ecm_params params;
+  pool P;
 
-  if (argc > 2 && strcmp (argv[1], "-seed") == 0)
+  while (argc > 2 && argv[1][0] == '-')
     {
-      seed = strtoul (argv[2], NULL, 10);
-      argc -= 2;
-      argv += 2;
+      if (strcmp (argv[1], "-seed") == 0)
+        {
+          seed = strtoul (argv[2], NULL, 10);
+          argc -= 2;
+          argv += 2;
+        }
+      else if (strcmp (argv[1], "-target") == 0)
+        {
+          target = strtoul (argv[2], NULL, 10);
+          argc -= 2;
+          argv += 2;
+        }
+      else
+        {
+          fprintf (stderr, "Unknown option %s\n", argv[1]);
+          exit (1);
+        }
     }
 
   assert (argc == 3);
@@ -103,6 +297,7 @@ main (int argc, char *argv[])
       nb_test[i] = 0;
     }
 
+  pool_init (P);
   mpz_init_set_str (p, argv[1], 10);
   mpz_init_set_str (z, argv[2], 10);
   gmp_printf ("p=%Zd\n", p);
@@ -119,7 +314,7 @@ main (int argc, char *argv[])
   mpz_init (v0);
   mpz_init (f);
   L = mpz_sizeinbase (p, 2);
-  while (1)
+  while (L > target)
     {
       e = lrand48 ();
       mpz_powm_ui (u, z, e, p);
@@ -155,10 +350,10 @@ main (int argc, char *argv[])
       S = 0.0;
       unsigned int i = 0;
       uprime = mpz_probab_prime_p (u, 1);
-      if (uprime && mpz_sizeinbase (u, 2) > L)
+      if (uprime && mpz_sizeinbase (u, 2) >= L)
         continue;
       vprime = mpz_probab_prime_p (v, 1);
-      if (vprime && mpz_sizeinbase (v, 2) > L)
+      if (vprime && mpz_sizeinbase (v, 2) >= L)
         continue;
       ecm_init (params);
       while (S < Smax && (uprime == 0 || vprime == 0))
@@ -181,7 +376,7 @@ main (int argc, char *argv[])
                 {
                   mpz_divexact (u, u, f);
                   uprime = mpz_probab_prime_p (u, 1);
-                  if (uprime && mpz_sizeinbase (u, 2) > L)
+                  if (uprime && mpz_sizeinbase (u, 2) >= L)
                     break;
                 }
             }
@@ -200,7 +395,7 @@ main (int argc, char *argv[])
                 {
                   mpz_divexact (v, v, f);
                   vprime = mpz_probab_prime_p (v, 1);
-                  if (vprime && mpz_sizeinbase (v, 2) > L)
+                  if (vprime && mpz_sizeinbase (v, 2) >= L)
                     break;
                 }
             }
@@ -210,72 +405,13 @@ main (int argc, char *argv[])
           B1 += sqrt (B1);
           S += B1;
         }
-      lu = (uprime) ? mpz_sizeinbase (u, 2) : mpz_sizeinbase (u, 2) / 2;
-      lv = (vprime) ? mpz_sizeinbase (v, 2) : mpz_sizeinbase (v, 2) / 2;
+      lu = bsize (u, uprime);
+      lv = bsize (v, vprime);
       l = (lu > lv) ? lu : lv;
       if (l < L) /* potential better relation */
         {
-          int s = cputime ();
-          printf ("focus on u=%c%lu and v=%c%lu\n",
-                  (uprime) ? 'p' : 'c', mpz_sizeinbase (u, 2),
-                  (vprime) ? 'p' : 'c', mpz_sizeinbase (v, 2));
-          while (S < 10 * Smax && (uprime == 0 || vprime == 0))
-            {
-              if (uprime == 0)
-                {
-                  mpz_set_d (params->sigma, B1);
-                  mpz_set_ui (f, 1);
-                  ecm_factor (f, u, B1, params);
-                  params->B1done = ECM_DEFAULT_B1_DONE; /* issue in 6.4.x */
-                  if (mpz_cmp_ui (f, 1) > 0)
-                    {
-                      gmp_printf ("found factor of u with B1=%.0f: %Zd\n",
-                                  B1, f);
-                      mpz_divexact (u, u, f);
-                      uprime = mpz_probab_prime_p (u, 1);
-                      if (uprime && mpz_sizeinbase (u, 2) > L)
-                        break;
-                    }
-                }
-              if (vprime == 0)
-                {
-                  mpz_set_d (params->sigma, B1);
-                  mpz_set_ui (f, 1);
-                  ecm_factor (f, v, B1, params);
-                  params->B1done = ECM_DEFAULT_B1_DONE; /* issue in 6.4.x */
-                  if (mpz_cmp_ui (f, 1) > 0)
-                    {
-                      gmp_printf ("found factor of v with B1=%.0f: %Zd\n",
-                                  B1, f);
-                      mpz_divexact (v, v, f);
-                      vprime = mpz_probab_prime_p (v, 1);
-                      if (vprime && mpz_sizeinbase (v, 2) > L)
-                        break;
-                    }
-                }
-              B1 += sqrt (B1);
-              S += B1;
-            }
-          lu = (uprime) ? mpz_sizeinbase (u, 2) : mpz_sizeinbase (u, 2) / 2;
-          lv = (vprime) ? mpz_sizeinbase (v, 2) : mpz_sizeinbase (v, 2) / 2;
-          l = (lu > lv) ? lu : lv;
-          printf ("focus took %dms: u=%c%lu and v=%c%lu\n",
-                  cputime () - s,
-                  (uprime) ? 'p' : 'c', mpz_sizeinbase (u, 2),
-                  (vprime) ? 'p' : 'c', mpz_sizeinbase (v, 2));
-          if (l < L)
-            {
-              L = l;
-              printf ("e=%lu L=%lu aver_gain[%d]=%f gain_u=%f gain_v=%f S=%f Smax=%f\n",
-                      e, l, i, aver_gain[i], gain_u[i], gain_v[i], S, Smax);
-              gmp_printf ("u0=%Zd\n", u0);
-              gmp_printf ("v0=%Zd\n", v0);
-              gmp_printf ("u=%Zd (%c%lu)\n", u, (uprime) ? 'p' : 'c',
-                          mpz_sizeinbase (u, 2));
-              gmp_printf ("v=%Zd (%c%lu)\n", v, (vprime) ? 'p' : 'c',
-                          mpz_sizeinbase (v, 2));
-              fflush (stdout);
-            }
+          pool_add (P, e, u0, v0, u, v, uprime, vprime, B1, l);
+          L = pool_scan (P, params, L);
         }
       Smax += sqrt (Smax);
     }
@@ -289,4 +425,5 @@ main (int argc, char *argv[])
   mpz_clear (v0);
   mpz_clear (f);
   ecm_clear (params);
+  pool_clear (P);
 }
