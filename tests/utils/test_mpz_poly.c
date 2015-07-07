b984c734333852b147f0700dd0d62ba4febbf7ef
Laurent Grémy <laurent.gremy@inria.fr>
2015-07-07 21:21:19 +0200

    Compute the resultant of two polynomials.

--- tests/utils/test_mpz_poly.c
+++ tests/utils/test_mpz_poly.c
@@ -41,6 +41,18 @@ static void mpz_poly_setcoeffs_ui_var(mpz_poly_t f, int d, ...)
     va_end(ap);
 }
 
+static void mpz_poly_setcoeffs_si_var(mpz_poly_t f, int d, ...)
+{
+    va_list ap;
+    va_start(ap, d);
+    mpz_poly_realloc(f, d + 1);
+    for(int i = 0 ; i <= d ; i++) {
+        mpz_set_si(f->coeff[i], va_arg(ap, int));
+    }
+    mpz_poly_cleandeg(f, d);
+    va_end(ap);
+}
+
 void
 test_polymodF_mul ()
 {
@@ -875,6 +887,101 @@ void test_mpz_poly_trivialities()
     mpz_clear(p);
 }
 
+void test_mpz_poly_resultant()
+{
+  mpz_poly_t f, g;
+  mpz_poly_init(f, 10);
+  mpz_poly_init(g, 10);
+  mpz_t res;
+  mpz_init(res);
+  mpz_t val;
+  mpz_init(val);
+
+  /*f=6+7*x^1+0*x^2+9*x^3+13*x^4+13*x^5+1*x^6+4*x^7+8*x^8+4*x^9+6*x^10*/
+  /*g=1+10*x^1+7*x^2+2*x^3+9*x^4+5*x^5+0*x^6+10*x^7+7*x^8+5*x^9+4*x^10*/
+  mpz_poly_setcoeffs_ui_var(f, 10, 6, 7, 0, 9, 13, 13, 1, 4, 8, 4, 6);
+  mpz_poly_setcoeffs_ui_var(g, 10, 1, 10, 7, 2, 9, 5, 0, 10, 7, 5, 4);
+  mpz_poly_resultant(res, f, g);
+  mpz_set_str(val, "3787840596130306882", 10);
+  ASSERT_ALWAYS(mpz_cmp(res, val) == 0);
+
+  /*f=12+11*x^1+6*x^2+9*x^3+11*x^4+13*x^5+2*x^6+14*x^7+14*x^8+1*x^9+1*x^10*/
+  /*g=0+10*x^1+13*x^2+5*x^3+4*x^4+1*x^5+1*x^6+9*x^7+6*x^8+5*x^9+13*x^10*/
+  mpz_poly_setcoeffs_ui_var(f, 10, 12, 11, 6, 9, 11, 13, 2, 14, 14, 1, 1);
+  mpz_poly_setcoeffs_ui_var(g, 10, 0, 10, 13, 5, 4, 1, 1, 9, 6, 5, 13);
+  mpz_poly_resultant(res, f, g);
+  mpz_set_str(val, "52543088043796652195928", 10);
+  ASSERT_ALWAYS(mpz_cmp(res, val) == 0);
+
+  /*f=0+6*x^1+6*x^2+7*x^3+9*x^4+1*x^5+7*x^6+6*x^7+1*x^8+9*x^9+5*x^10*/
+  /*g=5+7*x^1+11*x^2+0*x^3+13*x^4+9*x^5+5*x^6+0*x^7+4*x^8+1*x^9+5*x^10*/
+  mpz_poly_setcoeffs_ui_var(f, 10, 0, 6, 6, 7, 9, 1, 7, 6, 1, 9, 5);
+  mpz_poly_setcoeffs_ui_var(g, 10, 5, 7, 11, 0, 13, 9, 5, 0, 4, 1, 5);
+  mpz_poly_resultant(res, f, g);
+  mpz_set_str(val, "137271514893118787175", 10);
+  ASSERT_ALWAYS(mpz_cmp(res, val) == 0);
+
+  /*f=-10-11*x^1+13*x^2+6*x^3-13*x^4+3*x^5+5*x^6-13*x^7+11*x^8-6*x^9-11*x^10*/
+  /*g=-8-14*x^1-9*x^2+2*x^3-4*x^4+0*x^5-7*x^6-10*x^7-3*x^8-3*x^9-11*x^10*/
+  mpz_poly_setcoeffs_si_var(f, 10, -10, -11, 13, 6, -13, 3, 5, -13, 11, -6,
+      -11);
+  mpz_poly_setcoeffs_si_var
+    (g, 10, -8, -14, -9, 2, -4, 0, -7, -10, -3, -3, -11);
+  mpz_poly_resultant(res, f, g);
+  mpz_set_str(val, "408310242047874808370080", 10);
+  ASSERT_ALWAYS(mpz_cmp(res, val) == 0);
+
+  /*f=-3-10*x^1-12*x^2+1*x^3-3*x^4+5*x^5+0*x^6+0*x^7+10*x^8-12*x^9+14*x^10*/
+  /*g=13-11*x^1-8*x^2-13*x^3-14*x^4-9*x^5+10*x^6-5*x^7-3*x^8-11*x^9+12*x^10*/
+  mpz_poly_setcoeffs_si_var(f, 10, -3, -10, -12, 1, -3, 5, 0, 0, 10, -12, 14);
+  mpz_poly_setcoeffs_si_var(g, 10, 13, -11, -8, -13, -14, -9, 10, -5, -3, -11,
+      12);
+  mpz_poly_resultant(res, f, g);
+  mpz_set_str(val, "-36491329842163368368577782", 10);
+  ASSERT_ALWAYS(mpz_cmp(res, val) == 0);
+
+  /*f=-3-15*x^1-9*x^2+3*x^3-13*x^4-12*x^5-1*x^6-1*x^7-12*x^8-14*x^9+4*x^10*/
+  /*g=-6-13*x^1+9*x^2+7*x^3-5*x^4-5*x^5+11*x^6+2*x^7-5*x^8-4*x^9*/
+  mpz_poly_setcoeffs_si_var(f, 10, -3, -15, -9, 3, -13, -12, -1, -1, -12, -14,
+      4);
+  mpz_poly_cleandeg(g, 9);
+  mpz_poly_setcoeffs_si_var(g, 9, -6, -13, 9, 7, -5, -5, 11, 2, -5, -4);
+  mpz_poly_resultant(res, f, g);
+  mpz_set_str(val, "3719519175576976543932", 10);
+  ASSERT_ALWAYS(mpz_cmp(res, val) == 0);
+
+  /*f=0*/
+  /*g=-6-13*x^1+9*x^2+7*x^3-5*x^4-5*x^5+11*x^6+2*x^7-5*x^8-4*x^9*/
+  mpz_poly_cleandeg(f, -1);
+  mpz_poly_resultant(res, f, g);
+  ASSERT_ALWAYS(mpz_cmp_ui(res, 0) == 0);
+
+  /*f=-3-15*x^1-9*x^2+3*x^3-13*x^4-12*x^5-1*x^6-1*x^7-12*x^8-14*x^9+4*x^10*/
+  /*g=-6-13*x^1+9*x^2+7*x^3-5*x^4-5*x^5+11*x^6+2*x^7*/
+  mpz_poly_setcoeffs_si_var(f, 10, -3, -15, -9, 3, -13, -12, -1, -1, -12, -14,
+      4);
+  mpz_poly_cleandeg(g, 7);
+  mpz_poly_setcoeffs_si_var(g, 7, -6, -13, 9, 7, -5, -5, 11, 2);
+  mpz_poly_resultant(res, f, g);
+  mpz_set_str(val, "-26778351555137831424", 10);
+  ASSERT_ALWAYS(mpz_cmp(res, val) == 0);
+
+  /*f=-3-15*x^1-9*x^2+3*x^3-12*x^4-12*x^5-3*x^6-3*x^7-12*x^8-15*x^9+6*x^10*/
+  /*g=-6-13*x^1+9*x^2+7*x^3-5*x^4-5*x^5+11*x^6+2*x^7*/
+  mpz_poly_setcoeffs_si_var(f, 10, -3, -15, -9, 3, -12, -12, -3, -3, -12, -15,
+      6);
+  mpz_poly_cleandeg(g, 7);
+  mpz_poly_setcoeffs_si_var(g, 7, -6, -13, 9, 7, -5, -5, 11, 2);
+  mpz_poly_resultant(res, f, g);
+  mpz_set_str(val, "-61519394185549843500", 10);
+  ASSERT_ALWAYS(mpz_cmp(res, val) == 0);
+
+  mpz_clear(res);
+  mpz_clear(val);
+  mpz_poly_clear(f);
+  mpz_poly_clear(g);
+}
+
 int
 main (int argc, const char *argv[])
 {
@@ -894,6 +1001,7 @@ main (int argc, const char *argv[])
   test_mpz_poly_is_root(iter);
   test_mpz_poly_factor(iter / 5);
   test_mpz_poly_trivialities ();
+  test_mpz_poly_resultant();
   tests_common_clear ();
   exit (EXIT_SUCCESS);
 }
