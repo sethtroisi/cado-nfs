4b56c03d697295e8f71fe0ffa264847825519867
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2015-08-25 09:12:23 +0200

    use more cpus for expensive checks

--- tests/test_gfp2.sh
+++ tests/test_gfp2.sh
@@ -2,5 +2,6 @@
 
 CADO_NFS_SOURCE_DIR=$1
 
+NCPUS=$("`dirname $0`/ncpus.sh)
 export t=`mktemp -d /tmp/cado-check.XXXXXXX`
-${CADO_NFS_SOURCE_DIR}/factor.sh 100000000000000000039 -dlp -gfpext 2 -t 2 -ell 164354743277891 && rm -rf $t
+${CADO_NFS_SOURCE_DIR}/factor.sh 100000000000000000039 -dlp -gfpext 2 -t $NCPUS -ell 164354743277891 && rm -rf $t
--- tests/test_gfp3.sh
+++ tests/test_gfp3.sh
@@ -3,6 +3,8 @@
 CADO_NFS_SOURCE_DIR=$1
 CADO_NFS_BINARY_DIR=$2
 
+NCPUS=$("`dirname $0`/ncpus.sh)
+
 t=`mktemp -d /tmp/cado-check.XXXXXXX`
 
 POLYFILE=$t/p3dd15-f4g3-GJL-1.poly
@@ -51,8 +53,8 @@ dlp = true
 N = 100000000010189
 gorder = 10000000002037900000103825911
 
-slaves.nrclients = 2
-tasks.threads = 2
+slaves.nrclients = $NCPUS
+tasks.threads = $NCPUS
 tasks.execpath = $CADO_NFS_BINARY_DIR
 slaves.scriptpath = $CADO_NFS_SOURCE_DIR/scripts/cadofactor
 tasks.workdir = $WDIR
