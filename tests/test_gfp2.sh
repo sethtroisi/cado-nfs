809675abdf2397a44dd0d761ea131a551c765041
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2015-08-25 11:48:00 +0200

    get rid of script files when they are not useful

--- tests/test_gfp2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-CADO_NFS_SOURCE_DIR=$1
-
-# this test is quick enough, -t 2 is sufficient.
-export t=`mktemp -d /tmp/cado-check.XXXXXXX`
-${CADO_NFS_SOURCE_DIR}/factor.sh 100000000000000000039 -dlp -gfpext 2 -t 2 -ell 164354743277891 && rm -rf $t
