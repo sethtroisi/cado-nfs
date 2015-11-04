e7083ccd43c4fa814e191a7c695fa69cd842e714
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2015-11-04 11:17:34 +0100

    Separate the "make show" functionality from scripts/call_cmake.sh proper

--- scripts/call_cmake.sh
+++ scripts/call_cmake.sh
@@ -31,104 +31,21 @@ if echo "${MAKEFLAGS}" | grep -q "jobserver-fds=0," ; then
     exit 42
 fi
 
-########################################################################
-# The location of the build tree is, by default,
-# $cado_source_tree/build/`hostname`, but customizing it is easy.
-
-up_path="${0%%scripts/call_cmake.sh}"
-if [ "$up_path" = "$0" ] ; then
-    echo "Error: call_cmake.sh must be called with a path ending in scripts/call_cmake.sh" >&2
-    exit 1
-elif [ "$up_path" = "" ] ; then
-    up_path=./
+args=("$@")
+if [ "$1" = "show" ] ; then
+    if [ "$#" != 1 ] ; then
+        echo "Argument 'show' must be alone on the command line of $0" >&2
+        fi
+    set -- --show
+else
+    set --
 fi
-pwdP="pwd -P"
-if ! $pwdP >/dev/null 2>&1 ; then
-    pwdP=pwd
+source "$(dirname $0)/build_environment.sh"
+if [ "$1" ] ; then
+    # we've done our deeds, finish.
+    exit 0
 fi
-called_from="`pwd`"
-absolute_path_of_source="`cd "$up_path" ; pwd`"
-relative_path_of_cwd="${called_from##$absolute_path_of_source}"
-
-########################################################################
-# Set some default variables which can be overridden from local.sh
-
-# The default behaviour
-# build_tree="/tmp/cado-build-`id -un`"
-: ${build_tree:="${up_path}build/`hostname`"}
-
-# Joe gets confused by " in prev line; this line fixes it
-
-# By default, we also avoid /usr/local ; of course, it may be overridden.
-# Note that cmake does not really have phony targets, so one must avoid
-# basenames which correspond to targets !
-: ${PREFIX:="$absolute_path_of_source/installed"}
-
-# XXX XXX XXX LOOK LOOK LOOK: here you've got an entry point for customizing.
-# The source directory may contain a hint script with several useful
-# preferences. Its job is to put environment variables. Quite notably,
-# $build_tree is amongst these.
-if [ -f "${up_path}local.sh" ] ; then
-    . "${up_path}local.sh"
-fi
-
-# If no CFLAGS have been set yet, set something sensible: get optimization by
-# default, as well as asserts.  If you want to disable this, use either
-# local.sh or the environment to set an environment variable CFLAGS to be
-# something non-empty, like CFLAGS=-g or CFLAGS=-O0 ; Simply having CFLAGS=
-# <nothing> won't do, because bash makes no distinction between null and unset
-# here.
-: ${CFLAGS:=-O2}
-: ${CXXFLAGS:=-O2}
-: ${PTHREADS:=1}
-
-########################################################################
-# Arrange so that relevant stuff is passed to cmake -- the other end of
-# the magic is in CMakeLists.txt. The two lists must agree.
-# (no, it's not as simple. As long as the cmake checks care about
-# *environment variables*, we are here at the right place for setting
-# them. However, we might also be interested in having cmake export test
-# results to scripts. This is done by cmake substitutions, but the
-# corresponding names need not match the ones below).
-
-export CC
-export CXX
-export CFLAGS
-export CXXFLAGS
-export FLAGS_SIZE
-export LDFLAGS
-export PREFIX
-export CADO_DIST_ARCHIVE_NAME
-export MPI
-export GF2X_CONFIGURE_EXTRA
-export GMP
-export GMP_INCDIR
-export GMP_LIBDIR
-export MPIR
-export MPIR_INCDIR
-export MPIR_LIBDIR
-export PTHREADS
-export CURL
-export CURL_INCDIR
-export CURL_LIBDIR
-export HWLOC
-export HWLOC_INCDIR
-export HWLOC_LIBDIR
-export NUMA
-export NUMA_INCDIR
-export NUMA_LIBDIR
-export GF2X_CONFIGURE_EXTRA_FLAGS
-export CMAKE_DUMP_VARIABLES
-export ENABLE_SHARED
-export NO_PYTHON_CHECK
-export NO_SSE
-export NO_INLINE_ASSEMBLY
-export CHECKS_EXPENSIVE
-export BWC_GF2_MATMUL_BACKENDS
-export BWC_GFP_MATMUL_BACKENDS
-export BWC_GF2_ARITHMETIC_BACKENDS
-export BWC_GFP_ARITHMETIC_BACKENDS
-export BWC_EXTRA_BACKENDS
+set "${args[@]}"
 
 ########################################################################
 # "make ?" or "make help" (when the Makefile does not exist)
@@ -170,37 +87,6 @@ if [ "$1" == "tidy" ] ; then
     else
         echo "$wn no action and quit now"
     fi
-    exit 0
-fi
-
-
-########################################################################
-# make show
-if [ "$1" = "show" ] ; then
-    echo "build_tree=\"$build_tree\""
-    echo "up_path=\"$up_path\""
-    echo "called_from=\"$called_from\""
-    echo "absolute_path_of_source=\"$absolute_path_of_source\""
-    echo "relative_path_of_cwd=\"$relative_path_of_cwd\""
-    echo "CC=\"$CC\""
-    echo "CXX=\"$CXX\""
-    echo "CFLAGS=\"$CFLAGS\""
-    echo "CXXFLAGS=\"$CXXFLAGS\""
-    echo "FLAGS_SIZE=\"$FLAGS_SIZE\""
-    echo "LDFLAGS=\"$CFLAGS\""
-    echo "PREFIX=\"$PREFIX\""
-    echo "CADO_DIST_ARCHIVE_NAME=\"$CADO_DIST_ARCHIVE_NAME\""
-    echo "MPI=\"$MPI\""
-    echo "GF2X_CONFIGURE_EXTRA=\"$GF2X_CONFIGURE_EXTRA\""
-    echo "PTHREADS=\"$PTHREADS\""
-    echo "CMAKE_GENERATOR=\"$CMAKE_GENERATOR\""
-    echo "ENABLE_SHARED=\"$ENABLE_SHARED\""
-    echo "CHECKS_EXPENSIVE=\"$CHECKS_EXPENSIVE\""
-    echo "BWC_GF2_MATMUL_BACKENDS=\"$BWC_GF2_MATMUL_BACKENDS\""
-    echo "BWC_GFP_MATMUL_BACKENDS=\"$BWC_GFP_MATMUL_BACKENDS\""
-    echo "BWC_GF2_ARITHMETIC_BACKENDS=\"$BWC_GF2_ARITHMETIC_BACKENDS\""
-    echo "BWC_GFP_ARITHMETIC_BACKENDS=\"$BWC_GFP_ARITHMETIC_BACKENDS\""
-    echo "BWC_EXTRA_BACKENDS=\"$BWC_EXTRA_BACKENDS\""
     exit 0
 fi
 
