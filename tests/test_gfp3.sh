1ed15567fcc954971754c997d80a460c5a7b6780
F. Morain <morain@lix.polytechnique.fr>
2015-11-04 13:17:27 +0100

    getting rid of abunits, etc. 2nd pass.

--- tests/test_gfp3.sh
+++ tests/test_gfp3.sh
@@ -43,9 +43,7 @@ slaves.hostnames = localhost
 
 tasks.polyselect.import = $POLYFILE
 
-# this should be uncommented for nominal execution, since the computations
-# involve 1 unit on side 1, 1 SM on side 0
-tasks.explicit_units1 = true 
+# for the record, the computations involve 1 unit on side 1, 1 SM on side 0
 tasks.lcideals = true
 
 tasks.I = 11
