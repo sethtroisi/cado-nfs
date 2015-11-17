e7d80624e20f72832d6dfa1555e249ae7b7825ae
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2015-11-17 14:16:58 +0100

    minor changes

--- filter/merge_replay_matrix.c
+++ filter/merge_replay_matrix.c
@@ -404,7 +404,7 @@ remove_j_from_row(filter_matrix_t *mat, int i, int j)
 #endif
 }
 
-/* return the weight of the relation obtained when adding relations i2 and i2
+/* return the weight of the relation obtained when adding relations i1 and i2
 */
 int
 weightSum(filter_matrix_t *mat, int i1, int i2, MAYBE_UNUSED int32_t j)
@@ -417,14 +417,14 @@ weightSum(filter_matrix_t *mat, int i1, int i2, MAYBE_UNUSED int32_t j)
     if((len1 == 0) || (len2 == 0))
         fprintf(stderr, "i1=%d i2=%d len1=%d len2=%d\n", i1, i2, len1, len2);
 #endif
-#ifdef FOR_DL /* look for the exponents of j in i1 i2*/
+#ifdef FOR_DL /* look for the exponents of j in i1 and i2*/
     int e1 = 0, e2 = 0;
     int d;
     unsigned int l;
-    for (l = 1 ; l <= matLengthRow(mat, i1) ; l++)
+    for (l = 1 ; l <= len1 ; l++)
         if ((int) matCell(mat, i1, l) == j)
             e1 = mat->rows[i1][l].e;
-    for (l = 1 ; l <= matLengthRow(mat, i2) ; l++)
+    for (l = 1 ; l <= len2 ; l++)
         if ((int) matCell(mat, i2, l) == j)
             e2 = mat->rows[i2][l].e;
 
