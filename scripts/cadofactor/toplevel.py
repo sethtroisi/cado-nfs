9cc2a6686b45c5ff75777194f4cce7519ed25593
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2015-12-04 09:46:11 +0100

    better like this ?

--- scripts/cadofactor/toplevel.py
+++ scripts/cadofactor/toplevel.py
@@ -739,8 +739,8 @@ class Cado_NFS_toplevel(object):
         # server.threads).
         p="tasks.sqrt.sqrt.threads"
         t=self.parameters.get_simple(p, 0)
-        if self.parameters.locate(p) == "tasks.threads":
-            self.parameters.set_simple(p, min(t, 8))
+        if t > 8 and self.parameters.locate(p) == "tasks.threads":
+            self.parameters.set_simple(p, 8)
 
     def set_slaves_parameters(self):
         ''' sets slaves.nrclients and slaves.scriptpath to default values
