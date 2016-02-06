4b1b1a0a77c9782454e2ba53dd59971883dd2cbe
Laurent Grémy <laurent.gremy@inria.fr>
2016-02-06 19:46:24 +0100

    First version of alpha3d.

---
+++ nfs-hd/alpha3d.h
@@ -0,0 +1,10 @@
+#ifndef ALPHA3D_H
+#define ALPHA3D_H 
+
+#include "cado.h"
+#include "utils.h"
+#include "mpz_poly.h"
+
+double alpha3d(mpz_poly_srcptr f, unsigned long p, unsigned int N);
+
+#endif /* ALPHA3D_H */
