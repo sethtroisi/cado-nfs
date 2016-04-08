efadeb5eefea1df4727297fdda9d6a33c1cb41e0
Laurent Grémy <laurent.gremy@inria.fr>
2016-04-08 11:43:22 +0200

    Update parameters for p6bd40.

--- nfs-hd/automatic_p6bd40.sh
+++ nfs-hd/automatic_p6bd40.sh
@@ -20,13 +20,13 @@ sleep 0.5
 ./makefb-hd -t 3 -fbb 1482911,1482911 -poly p6bd40.poly -lpb 25,25 -out sieve_base_p6bd40
 echo "Launch special-q_sieve (less than 3 days on a single 2GHz processor) for the p6bd40."
 sleep 0.5
-./special-q_sieve -H 7,7,7 -fbb 1482911,1482911 -thresh 55,55 -poly p6bd40.poly -lpb 25,25 -q_side 1 -fb sieve_base_p6bd40 -q_range 1482937,2900828 -out sieve_1482937_2900828 -err /dev/null -gal 6
+./special-q_sieve -H 7,7,7 -fbb 1482911,1482911 -thresh 55,55 -poly p6bd40.poly -lpb 25,25 -q_side 1 -fb sieve_base_p6bd40 -q_range 1482937,2515517 -out sieve_1482937_2515517 -err /dev/null -gal 6
 echo
 echo "Check the relations"
 sleep 0.5
 cd ../../../nfs-hd/check_relations/
 make build_tree=../../build/`hostname`/nfs-hd
 cd ../../build/`hostname`/nfs-hd/
-grep -v "#" sieve_1482937_2900828 > relations.raw
+grep -v "#" sieve_1482937_2515517 > relations.raw
 sort relations.raw | uniq > relations.uniq
 ./check_relations relations.uniq relations.true p6bd40.poly 25,25 relations.err
