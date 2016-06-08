dbbcb23ed3bf86338e32c05176de6844b03931e1
Laurent Grémy <laurent.gremy@inria.fr>
2016-06-08 10:00:21 +0200

    Change automatic computation with the parameters in the article.

--- nfs-hd/automatic_p6bd40.sh
+++ nfs-hd/automatic_p6bd40.sh
@@ -2,7 +2,7 @@
 if [ -f local.sh ]; then
 	length=$( wc -l local.sh | cut -d " " -f 1 )
 	if [ $length -ne 2 ]; then
-		echo "Save your local.sh in local.sh.old."
+		echo "Save previous local.sh in local.sh.old."
 		sleep 0.5
 		cp local.sh local.sh.old
 	fi
@@ -17,16 +17,17 @@ cd build/`hostname`/nfs-hd
 echo
 echo "Launch makefb-hd (less than 10 minutes on a single 2GHz processor) for the p6bd40."
 sleep 0.5
-./makefb-hd -t 3 -fbb 1482911,1482911 -poly p6bd40.poly -lpb 25,25 -out sieve_base_p6bd40
-echo "Launch special-q_sieve (less than 3 days on a single 2GHz processor) for the p6bd40."
+./makefb-hd -t 3 -fbb 524288,524288 -poly p6bd40.poly -lpb 23,23 -out sieve_base_p6bd40
+echo "Launch special-q_sieve (less than 1 days on a single 2GHz processor) for the p6bd40."
 sleep 0.5
-./special-q_sieve -H 7,7,7 -fbb 1482911,1482911 -thresh 55,55 -poly p6bd40.poly -lpb 25,25 -q_side 1 -fb sieve_base_p6bd40 -q_range 1482937,2515517 -out sieve_1482937_2515517 -err /dev/null -gal 6
+./special-q_sieve -H 6,6,6 -fbb 524288,524288 -thresh 65,65 -poly p6bd40.poly -lpb 23,23 -q_side 1 -fb sieve_base_p6bd40 -q_range 524341,2354820 -out sieve_524341_2354820 -err /dev/null -gal 6
 echo
 echo "Check the relations"
 sleep 0.5
 cd ../../../nfs-hd/check_relations/
 make build_tree=../../build/`hostname`/nfs-hd
 cd ../../build/`hostname`/nfs-hd/
-grep -v "#" sieve_1482937_2515517 > relations.raw
+grep -v "#" sieve_524341_2354820 > relations.raw
 sort relations.raw | uniq > relations.uniq
-./check_relations relations.uniq relations.true p6bd40.poly 25,25 relations.err
+echo "Needed relations: 1128604"
+./check_relations relations.uniq relations.true p6bd40.poly 23,23 relations.err
