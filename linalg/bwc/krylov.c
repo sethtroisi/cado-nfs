7196f0920f5dd0f3b7295267519b207d052e6a5c
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2016-06-15 19:19:07 +0200

    use mmt_vec_reduce_mod_p for krylov too

--- linalg/bwc/krylov.c
+++ linalg/bwc/krylov.c
@@ -120,6 +120,7 @@ void * krylov_prog(parallelizing_info_ptr pi, param_list pl, void * arg MAYBE_UN
     if (!fake) {
         if (tcan_print) { printf("Loading %s.%u ...", v_name, bw->start); fflush(stdout); }
         mmt_vec_load(ymy[0], v_name, bw->start, unpadded);
+        mmt_vec_reduce_mod_p(ymy[0]);
         if (tcan_print) { printf("done\n"); }
     } else {
         gmp_randstate_t rstate;
