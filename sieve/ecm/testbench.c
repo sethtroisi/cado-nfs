b69cb235b18dc92a11dc4eb1c613daa2ba3a32a1
Laurent Imbert <Laurent.Imbert@lirmm.fr>
2016-10-12 16:30:27 +0200

    Added option -poe16 in testbench

--- sieve/ecm/testbench.c
+++ sieve/ecm/testbench.c
@@ -146,7 +146,7 @@ void print_help (char *programname)
   printf ("-inp <f> Read decimal numbers to factor from file <f>\n");
   printf ("-inpraw <f>  Read numbers in GMP raw format from file <f>\n");
   printf ("-inpstop <n> Stop after reading <n> numbers\n");
-  printf ("-po <s>, -pom12 <s>, -pom16 <s> Compute order of starting point. Use -vf\n");
+  printf ("-po <s>, -pom12 <s>, -pom16 <s>, -poe16 <s> Compute order of starting point. Use -vf\n");
 }
 
 int main (int argc, char **argv)
