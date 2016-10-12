f940a1558a4f5364c52360c0eb3166f24c4ffbe8
Cyril Bouvier <cyril.bouvier@lirmm.fr>
2016-10-12 13:55:36 +0200

    addchain: change m to q

--- sieve/ecm/addchain_bc.c
+++ sieve/ecm/addchain_bc.c
@@ -14,32 +14,32 @@
 #include "addchain_bc.h"
 
 char
-addchain_find_best_r (mpz_srcptr k, const unsigned char m)
+addchain_find_best_r (mpz_srcptr k, const unsigned char q)
 {
-  int l = nbits(2*m);
+  int l = nbits(2*q);
   unsigned int r = k->_mp_d[0] & ((1 << l) - 1);
-  if (r < m)
+  if (r < q)
     return (char) r;
-  else if (m - r < m)
-    return (char) ((int) r) - ((int) m);
+  else if (q - r < q)
+    return (char) ((int) r) - ((int) q);
   else
   {
     l--;
     r = k->_mp_d[0] & ((1 << l) - 1);
-    if (r < m)
+    if (r < q)
       return (char) r;
-    else /* we are sure that (m - r < m) */
-    return (char) ((int) r) - ((int) m);
+    else /* we are sure that (q - r < q) */
+    return (char) ((int) r) - ((int) q);
   }
 }
 
 
 static double
-addchain_rec (mpz_t k, const unsigned char m, addchain_cost_srcptr opcost,
+addchain_rec (mpz_t k, const unsigned char q, addchain_cost_srcptr opcost,
               bc_state_t *state)
 {
-  /* Case 0: k is <= m and (2 or odd) [ i.e., one of the precomputed points ] */
-  if (mpz_cmp_ui (k, m) <= 0 && (mpz_cmp_ui (k, 2) == 0 || mpz_odd_p (k)))
+  /* Case 0: k is <= q and (2 or odd) [ i.e., one of the precomputed points ] */
+  if (mpz_cmp_ui (k, q) <= 0 && (mpz_cmp_ui (k, 2) == 0 || mpz_odd_p (k)))
   {
     /* starting point is kP bc[1] = code(k); bc[2] = 0x00 */
     if (state)
@@ -49,19 +49,19 @@ addchain_rec (mpz_t k, const unsigned char m, addchain_cost_srcptr opcost,
     }
     return 0.;
   }
-  /* Case 1: k == m + 2 */
-  else if (mpz_cmp_ui (k, m+2) == 0)
+  /* Case 1: k == q + 2 */
+  else if (mpz_cmp_ui (k, q+2) == 0)
   {
-    /* starting point is kP + 2P: bc[1] = code(2); bc[2] = code(m) */
+    /* starting point is kP + 2P: bc[1] = code(2); bc[2] = code(q) */
     if (state)
     {
       bytecoder ((literal_t) 0xff, state); /* special code for 2 */
-      bytecoder ((literal_t) (m >> 1), state);
+      bytecoder ((literal_t) (q >> 1), state);
     }
     return opcost->add;
   }
-  /* Case 2:  m+4 <= k <= 3*m-2 and k % 6 == 1 */
-  else if (0 <= mpz_cmp_ui (k, m+4) && mpz_cmp_ui (k, 3*m-2)
+  /* Case 2:  q+4 <= k <= 3*q-2 and k % 6 == 1 */
+  else if (0 <= mpz_cmp_ui (k, q+4) && mpz_cmp_ui (k, 3*q-2)
                                     && mpz_congruent_ui_p (k, 1, 6))
   {
     unsigned int kui = mpz_get_ui (k);
@@ -77,8 +77,8 @@ addchain_rec (mpz_t k, const unsigned char m, addchain_cost_srcptr opcost,
     }
     return opcost->dbladd;
   }
-  /* Case 3:  m+4 <= k <= 3*m and k % 6 == 3 */
-  else if (0 <= mpz_cmp_ui (k, m+4) && mpz_cmp_ui (k, 3*m)
+  /* Case 3:  q+4 <= k <= 3*q and k % 6 == 3 */
+  else if (0 <= mpz_cmp_ui (k, q+4) && mpz_cmp_ui (k, 3*q)
                                     && mpz_congruent_ui_p (k, 3, 6))
   {
     unsigned int kui = mpz_get_ui (k);
@@ -95,8 +95,8 @@ addchain_rec (mpz_t k, const unsigned char m, addchain_cost_srcptr opcost,
     }
     return opcost->dbladd;
   }
-  /* Case 4:  m+4 <= k <= 3*m-4 and k % 6 == 5 */
-  else if (0 <= mpz_cmp_ui (k, m+4) && mpz_cmp_ui (k, 3*m-4)
+  /* Case 4:  q+4 <= k <= 3*q-4 and k % 6 == 5 */
+  else if (0 <= mpz_cmp_ui (k, q+4) && mpz_cmp_ui (k, 3*q-4)
                                     && mpz_congruent_ui_p (k, 5, 6))
   {
     unsigned int kui = mpz_get_ui (k);
@@ -112,8 +112,8 @@ addchain_rec (mpz_t k, const unsigned char m, addchain_cost_srcptr opcost,
     }
     return opcost->dbladd;
   }
-  /* Case 5: 4 <= k <= 2*m-2 and k % 4 == 0 */
-  else if (0 <= mpz_cmp_ui (k, m+4) && mpz_cmp_ui (k, 3*m-4)
+  /* Case 5: 4 <= k <= 2*q-2 and k % 4 == 0 */
+  else if (0 <= mpz_cmp_ui (k, 4) && mpz_cmp_ui (k, 3*q-4)
                                     && mpz_congruent_2exp_p (k, 0, 2))
   {
     unsigned int kui = mpz_get_ui (k);
@@ -131,20 +131,20 @@ addchain_rec (mpz_t k, const unsigned char m, addchain_cost_srcptr opcost,
   else if (mpz_even_p (k))
   {
     mpz_fdiv_q_2exp (k, k, 1); /* divides by 2 */
-    double cost = addchain_rec (k, m, opcost, state);
+    double cost = addchain_rec (k, q, opcost, state);
     if (state)
       bytecoder (ADDCHAIN_DBL, state);
     return cost + opcost->dbl;
   }
   else
   {
-    char r = addchain_find_best_r (k, m);
+    char r = addchain_find_best_r (k, q);
     if (r > 0)
       mpz_sub_ui (k, k, r);
     else
       mpz_add_ui (k, k, -r);
     mpz_fdiv_q_2exp (k, k, 1); /* divides by 2 */
-    double cost = addchain_rec (k, m, opcost, state);
+    double cost = addchain_rec (k, q, opcost, state);
     if (state)
     {
       if (r > 0)
@@ -157,29 +157,29 @@ addchain_rec (mpz_t k, const unsigned char m, addchain_cost_srcptr opcost,
 }
 
 double
-addchain (mpz_srcptr E, const unsigned char m, addchain_cost_srcptr opcost,
+addchain (mpz_srcptr E, const unsigned char q, addchain_cost_srcptr opcost,
           bc_state_t *state)
 {
   double cost;
   mpz_t k;
 
-  FATAL_ERROR_CHECK (m > ADDCHAIN_M_MAX,
-                                "m must be smaller or equal to ADDCHAIN_M_MAX");
-  FATAL_ERROR_CHECK (m % 2 == 0, "m must be odd");
+  FATAL_ERROR_CHECK (q > ADDCHAIN_Q_MAX,
+                                "q must be smaller or equal to ADDCHAIN_Q_MAX");
+  FATAL_ERROR_CHECK (q % 2 == 0, "q must be odd");
 
   mpz_init_set (k, E);
 
-  /* We put m as the first byte in the bytecode */
+  /* We put q as the first byte in the bytecode */
   if (state)
-    bytecoder ((literal_t) m, state);
+    bytecoder ((literal_t) q, state);
 
-  /* Cost of the precomputation: 1 DBL (to compute 2P) and (m-1)/2 ADD (to
-   * compute (2k+1)P from (2k-1)P and 2P, for k in [1..(m-1)/2])
+  /* Cost of the precomputation: 1 DBL (to compute 2P) and (q-1)/2 ADD (to
+   * compute (2k+1)P from (2k-1)P and 2P, for k in [1..(q-1)/2])
    */
-  cost = opcost->dbl_precomp + (m >> 2) * opcost->add_precomp;
+  cost = opcost->dbl_precomp + (q >> 2) * opcost->add_precomp;
   
   /* Recursively compute the cost of the addition chain */
-  cost += addchain_rec (k, m, opcost, state);
+  cost += addchain_rec (k, q, opcost, state);
 
   FATAL_ERROR_CHECK (mpz_cmp_ui (k, 1) != 0, "Error, k != 1 at the end");
   mpz_clear (k);
@@ -210,23 +210,23 @@ addchain_bytecode (const unsigned int B1, addchain_cost_srcptr opcost,
   }
   prime_info_clear (pi);
   
-  /* Computes the bytecode for E: try every odd m up to ADDCHAIN_M_MAX and keep
+  /* Computes the bytecode for E: try every odd q up to ADDCHAIN_M_MAX and keep
    * the one with the lowest cost.
    */
   double chaincost, mincost = DBL_MAX;
-  unsigned char best_m = 0;
-  for (unsigned char m = 1 ; m <= ADDCHAIN_M_MAX ; m += 2)
+  unsigned char best_q = 0;
+  for (unsigned char q = 1 ; q <= ADDCHAIN_Q_MAX ; q += 2)
   {
-    double cost = addchain (E, m, opcost, NULL);
+    double cost = addchain (E, q, opcost, NULL);
     if (cost < mincost)
 	  {
 	    mincost = cost;
-	    best_m = m;
+	    best_q = q;
 	  }
   }
 
   /* Put the best addchain into bytecode */
-  chaincost = addchain (E, best_m, opcost, state);
+  chaincost = addchain (E, best_q, opcost, state);
  
   mpz_clear (E);
   return chaincost;
--- sieve/ecm/addchain_bc.h
+++ sieve/ecm/addchain_bc.h
@@ -14,7 +14,10 @@ typedef struct addchain_cost_s addchain_cost_t[1];
 typedef struct addchain_cost_s * addchain_cost_ptr;
 typedef const struct addchain_cost_s * addchain_cost_srcptr;
 
-#define ADDCHAIN_M_MAX 253 /* Must always fit in a char */
+#define ADDCHAIN_Q_MAX 253 /* Must always fit in a char */
 #define ADDCHAIN_DBL ((literal_t) 0x7f)
 
+/* Op cost for Twisted Edwards Curves with a=-1
+ * TODO explain different models and why dbladd > add+dbl*/
+addchain_cost_t TwEdwards_minus1_opcost = [ {.dbl = 7.} ];
 #endif
