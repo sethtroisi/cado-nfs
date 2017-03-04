da4cd80618c188d0dc76bbc413272bad8cde615b
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2017-03-04 23:11:46 +0100

    need MERGE_LEVEL_MAX >= 40 since we have param files with maxlevel=40

--- filter/filter_config.h
+++ filter/filter_config.h
@@ -34,7 +34,7 @@
 
 /* Maximum level for a merge. Such a large value is only useful when not using
  * BW. */
-#define MERGE_LEVEL_MAX 32
+#define MERGE_LEVEL_MAX 64
 
 #define DEFAULT_MERGE_MAXLEVEL 10
 #ifndef FOR_DL
