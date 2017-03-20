01c64c1352790607673a5b350b79710560c56288
Pierrick Gaudry <pierrick.gaudry@loria.fr>
2017-03-20 16:47:50 +0100

    Small improvement. Still dead slow compared to non-sublat version.

--- sieve/las-unsieve.cpp
+++ sieve/las-unsieve.cpp
@@ -475,7 +475,9 @@ search_survivors_in_line(unsigned char * const SS[2],
                 continue;
             }
             const unsigned int i = abs (int(sublat.m)*(x - (1 << (log_I - 1)))+int(sublat.i0));
-            if (bin_gcd_int64_safe (i, sublat.m*j+sublat.j0) != 1) {
+            const unsigned int jj = sublat.m*j+sublat.j0;
+            if ((((jj % 2) == 0) && ((i % 2) == 0)) ||
+                    (bin_gcd_int64_safe (i, jj) != 1)) {
                 SS[0][x] = 255;
             } else {
                 survivors.push_back(x);
