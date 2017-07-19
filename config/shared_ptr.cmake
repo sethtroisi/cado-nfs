f8b672306549045566e3e4e562efd0821867cc56
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2017-07-19 11:03:16 +0200

    Try less hairy code for cflags/cxxflas. Fix a gap with clang (see #21397).

--- config/shared_ptr.cmake
+++ config/shared_ptr.cmake
@@ -35,3 +35,32 @@ endif()
 if (NOT HAVE_STD_SHARED_PTR AND NOT HAVE_BOOST_SHARED_PTR)
     message(FATAL_ERROR "Need either a C++11 compiler, or boost libraries installed. You may simply unpack the boost headers at the root of the cado tree, that will probably do. Also, most OS distributions include packages for boost, which can be easily installed (names may vary, e.g.  libboost-dev or boost-devel")
 endif()
+
+# See bug #21397
+CHECK_CXX_SOURCE_COMPILES("
+#include <memory>
+class nocopy {
+        protected:
+                nocopy() {}
+                ~nocopy() {}
+        private:
+                nocopy(const nocopy&);
+                nocopy& operator=(const nocopy&);
+};
+
+class foo: private nocopy {
+        int val;
+        public:
+        foo(int val) : val(val) {}
+        int value() const { return val; }
+};
+
+std::shared_ptr<foo> blah()
+{
+        const int val = 65536;
+        return std::make_shared<foo>(val);
+}
+" HAS_BUG_21397)
+if(HAS_BUG_21397)
+    message(FATAL_ERROR "Error, see bug #21397")
+endif()
