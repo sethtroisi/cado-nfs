e0f2e16cf3f3c1c9bdc3188f74be936b0424f81d
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2017-08-03 17:28:07 +0200

    workaround for freebsd not having clog() -- it can't use crtalgsqrt

--- config/math.cmake
+++ config/math.cmake
@@ -7,4 +7,8 @@ search_for_function(exp2 HAVE_EXP2)
 
 search_for_function(cabsl HAVE_CABSL)
 
+# clog is c99, but freebsd-11.1 (released in 2017) does not have it.
+# https://www.freebsd.org/cgi/man.cgi?query=complex&manpath=FreeBSD+11.1-RELEASE
+search_for_function(clog HAVE_CLOG)
+
 set(math_libs ${CMAKE_REQUIRED_LIBRARIES_EXTRA})
