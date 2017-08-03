ce7cdcdd72012f2eb0ac3b2173355486f52166b6
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2017-08-04 01:00:28 +0200

    Nice bug.
    https://ci.inria.fr/cado-qualif/job/compile-openbsd-61-i386/12/console

--- config/sync_fetch.c
+++ config/sync_fetch.c
@@ -1,11 +1,15 @@
 #include <stdlib.h>
-int main () {
-  int o, *p;
+#include <stdio.h>
+#include <stdint.h>
+int main (int argc, char * argv[]) {
+  uint64_t o = argc, y, z;
   __sync_fetch_and_add(&o, 1);
   /* according to http://trac.wxwidgets.org/ticket/4542, one should use the
      return value for a complete configure test */
-  p = __sync_sub_and_fetch(&o, 1);
+  y = __sync_sub_and_fetch(&o, 1);
+  z = __sync_add_and_fetch(&o, y);
+
   /* make sure we use the return value */
-  printf ("%lx", p);
+  printf ("%lx", z);
   exit (0);
 }
