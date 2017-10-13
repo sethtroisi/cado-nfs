397e26c79ac4e4b4939aaf01866515a9f548f258
Pierrick Gaudry <pierrick.gaudry@loria.fr>
2017-10-13 21:06:43 +0200

    changed the order in random-sample broke fakerels test

--- tests/sieve/F9.fakerels.sh
+++ tests/sieve/F9.fakerels.sh
@@ -31,7 +31,7 @@ nfake=`$cmd | grep -c "^[^#]"`
 
 # remove leading spaces (for openbsd 5.3)
 let nfake=nfake
-nfake_exp=111
+nfake_exp=98
 if [ "$nfake" != "$nfake_exp" ]; then
     echo "Wrong number of fake relations ($nfake, expected $nfake_exp)"
     exit 1
