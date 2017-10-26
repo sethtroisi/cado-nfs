235c705444b5bfc192a6530ddd34161dcfa756a4
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2017-10-26 09:21:04 +0200

    ahem

--- sieve/las.cpp
+++ sieve/las.cpp
@@ -2191,8 +2191,8 @@ double nprimes_interval(double p0, double p1)
     return std::expint(log(p1)) - std::expint(log(p0));
 #else
     /* that can't be sooo wrong... */
+    double l0 = log(p0);
     double l1 = log(p1);
-    double l2 = log(p2);
     double s1 = p1*(1/l1+1/pow(l1,2)+2/pow(l1,3)+6/pow(l1,4));
     double s0 = p0*(1/l0+1/pow(l0,2)+2/pow(l0,3)+6/pow(l0,4));
     return s1 - s0;
