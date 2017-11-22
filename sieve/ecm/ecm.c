8901c67e7498412ebed85a8d585327ea3c0e5d28
Cyril Bouvier <cyril.bouvier@lirmm.fr>
2017-11-22 11:10:41 +0100

    Fix no break error

--- sieve/ecm/ecm.c
+++ sieve/ecm/ecm.c
@@ -1165,11 +1165,10 @@ ellE_interpret_bytecode (ellE_point_t P, const char *bc, const unsigned int bc_l
     {
       case ADDCHAIN_2DBL:
         ellE_double (Q, Q, m, a);
-        no_break();
-
 #if 1
     ellE_print (Q);
 #endif
+        no_break();
 
       case ADDCHAIN_DBL:
         ellE_double (Q, Q, m, a);
