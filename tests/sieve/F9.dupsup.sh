4a455a04f4dba31055cacd848ba47a22d5798f10
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2018-05-04 10:16:34 +0200

    script fixes

--- tests/sieve/F9.dupsup.sh
+++ tests/sieve/F9.dupsup.sh
@@ -10,9 +10,9 @@ RELSR="${WORKDIR:?missing}/rels_raw"
 RELSF="${WORKDIR:?missing}/rels_filtered"
 
 PHONY_CHECK=1 RELS=$RELSD "`dirname $0`/sievetest.sh" -dup -dup-qmin 0,$lim1 "$@"
-ndup_ref=`grep -wc DUPE $RELSD`
+ndup_ref=`grep -wc DUPE $RELSD || :`
 # remove leading spaces (for openbsd 5.3)
-let ndup_ref=ndup_ref
+ndup_ref=`echo $ndup_ref`
 if [ "$ndup_ref" != "$DUPE_COUNT" ]; then
     echo "Wrong number of duplicates in las -dup"
     echo "Expected $DUPE_COUNT, got $ndup_ref"
@@ -23,9 +23,9 @@ fi
 PHONY_CHECK=1 RELS=$RELSR "`dirname $0`/sievetest.sh" "$@"
 
 PHONY_CHECK=1 LAS_BINARY=$DUPSUP_BINARY RELS=$RELSF "`dirname $0`/sievetest.sh" -skew 1.0 "${RELSR}"
-ndup=`grep -wc DUPE $RELSF`
+ndup=`grep -wc DUPE $RELSF || :`
 # remove leading spaces (for openbsd 5.3)
-let ndup=ndup
+ndup=`echo $ndup`
 if [ "$ndup" != "$DUPE_COUNT" ]; then
     echo "Wrong number of duplicates in dupsup"
     echo "Expected $DUPE_COUNT, got $ndup"
