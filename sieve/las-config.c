ca630e404e06c19bce91861a080c9875b5482240
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2018-07-29 20:39:16 +0200

    get rid of some cruft in las-config.c

--- sieve/las-config.c
+++ sieve/las-config.c
@@ -37,9 +37,6 @@ void las_display_config_flags()
 #ifdef SAFE_BUCKETS
     verbose_output_print(0, 1, " SAFE_BUCKETS");
 #endif
-#ifdef BUCKETS_IN_ONE_BIG_MALLOC
-    verbose_output_print(0, 1, " BUCKETS_IN_ONE_BIG_MALLOC");
-#endif
 #ifdef PROFILE
     verbose_output_print(0, 1, " PROFILE");
 #endif
@@ -55,14 +52,6 @@ void las_display_config_flags()
 #ifdef TRACK_CODE_PATH
     verbose_output_print(0, 1, " TRACK_CODE_PATH");
 #endif
-#ifdef ALG_LAZY
-    verbose_output_print(0, 1, " ALG_LAZY");
-    verbose_output_print(0, 1, " NORM_STRIDE=8 (locked)");
-    verbose_output_print(0, 1, " VERT_NORM_STRIDE=%u (max)", VERT_NORM_STRIDE);
-#endif
-#ifdef ALG_RAT
-    verbose_output_print(0, 1, " ALG_RAT");
-#endif
 #ifdef SUPPORT_LARGE_Q
     verbose_output_print(0, 1, " SUPPORT_LARGE_Q");
 #endif
@@ -72,7 +61,6 @@ void las_display_config_flags()
 #ifdef USE_CACHEBUFFER
     verbose_output_print(0, 1, " USE_CACHEBUFFER");
 #endif
-    verbose_output_print(0, 1, " LOG_BUCKET_REGION=%u", LOG_BUCKET_REGION);
     verbose_output_print(0, 1, " GUARD=%1.2f", (double) GUARD);
     verbose_output_print(0, 1, " LOG_MAX=%.1f", LOG_MAX);
     verbose_output_print(0, 1, "\n");
