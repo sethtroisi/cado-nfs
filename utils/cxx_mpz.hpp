fdad521d5aee4573d315cb0dda2c729b68a7b44a
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2018-08-24 13:35:49 +0200

    std::sort with cxx_mpz without a stable comparison operator -> kaboom

--- utils/cxx_mpz.hpp
+++ utils/cxx_mpz.hpp
@@ -77,6 +77,8 @@ extern void mpq_init(cxx_mpq & pl) __attribute__((error("mpq_init must not be ca
 extern void mpq_clear(cxx_mpq & pl) __attribute__((error("mpq_clear must not be called on a mpq reference -- it is the caller's business (via a dtor)")));
 #endif
 
+inline bool operator<(cxx_mpz const & a, cxx_mpz const & b) { return mpz_cmp(a, b) < 0; }
+inline bool operator<(cxx_mpq const & a, cxx_mpq const & b) { return mpq_cmp(a, b) < 0; }
 inline std::ostream& operator<<(std::ostream& os, cxx_mpz const& x) { return os << (mpz_srcptr) x; }
 inline std::ostream& operator<<(std::ostream& os, cxx_mpq const& x) { return os << (mpq_srcptr) x; }
 inline std::istream& operator>>(std::istream& is, cxx_mpz & x) { return is >> (mpz_ptr) x; }
