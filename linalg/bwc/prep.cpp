525f23bdec20deeef4aeddc5e4029bfcf6673129
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2018-10-19 19:22:50 +0200

    Fix bug with random init vector generation for GF(p). Was mostly harmless I think, but still.

--- linalg/bwc/prep.cpp
+++ linalg/bwc/prep.cpp
@@ -371,11 +371,11 @@ void * prep_prog_gfp(parallelizing_info_ptr pi, param_list pl, void * arg MAYBE_
         vec_file = fopen(vec_name, "wb");
         ASSERT_ALWAYS(vec_file != NULL);
         printf("// Creating %s\n", vec_name);
-        cheating_vec_init(A, &vec, mmt->n0[!bw->dir]);
-        A->vec_random(A, vec, mmt->n0[!bw->dir], rstate);
-        rc = fwrite(vec, A->vec_elt_stride(A,1), mmt->n0[!bw->dir], vec_file);
-        ASSERT_ALWAYS(rc >= 0 && ((unsigned int) rc) == mmt->n0[!bw->dir]);
-        cheating_vec_clear(A, &vec, mmt->n0[!bw->dir]);
+        cheating_vec_init(A, &vec, mmt->n0[bw->dir]);
+        A->vec_random(A, vec, mmt->n0[bw->dir], rstate);
+        rc = fwrite(vec, A->vec_elt_stride(A,1), mmt->n0[bw->dir], vec_file);
+        ASSERT_ALWAYS(rc >= 0 && ((unsigned int) rc) == mmt->n0[bw->dir]);
+        cheating_vec_clear(A, &vec, mmt->n0[bw->dir]);
         fclose(vec_file);
         free(vec_name);
     }
