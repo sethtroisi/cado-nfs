79ad4af0b3918c54d0cb5a36d49ca9d302c4073a
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2018-11-17 23:41:18 +0100

    more analysis of mpz_probab_prime_p

--- utils/gmp_aux.c
+++ utils/gmp_aux.c
@@ -313,8 +313,9 @@ uint64_nextprime (uint64_t q)
                 (then 1803601, 1943521, 2237017, 3604201, 5095177, ...);
    with REPS=2, it is 1943521 (then 16661633, 18790021, 54470491, ...);
    with REPS=3, it is 465658903 (then 2242724851, 5969607379, 6635692801, ...);
-   with REPS=4, none < 10000000000.
-   See also https://en.wikipedia.org/wiki/Miller–Rabin_primality_test
+   with REPS=4, it is 239626837621 (then 277376554153, 537108528133, and no other
+                      <= 549755813888).
+   See also https://en.wikipedia.org/wiki/Miller–Rabin_primality_test.
 */
 int
 ulong_isprime (unsigned long p)
