ae3ba5edae087c26878e1ff5ae5d74f3654f31d8
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2018-12-19 16:15:31 +0100

    update flint to 5c982991d

--- linalg/bwc/flint-fft/fft.h
+++ linalg/bwc/flint-fft/fft.h
@@ -1,44 +1,21 @@
-/* mul_fft -- radix 2 fft routines for MPIR.
+/* 
+ * Copyright (C) 2009, 2011 William Hart
  * 
- * Copyright 2009, 2011 William Hart. All rights reserved.
+ * This file is part of FLINT.
  * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are
- * met:
- * 
- * 1. Redistributions of source code must retain the above copyright notice,
- * this list of conditions and the following disclaimer.
- * 
- * 2. Redistributions in binary form must reproduce the above copyright
- * notice, this list of conditions and the following disclaimer in the
- * documentation and/or other materials provided with the distribution.
- * 
- * THIS SOFTWARE IS PROVIDED BY William Hart ``AS IS'' AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN 
- * NO EVENT SHALL William Hart OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- * 
- * The views and conclusions contained in the software and documentation are
- * those of the authors and should not be interpreted as representing
- * official policies, either expressed or implied, of William Hart.
- * 
- */
-
-/******************************************************************************
-
-    Copyright (C) 2009, 2011 William Hart
- 
-******************************************************************************/
+ * FLINT is free software: you can redistribute it and/or modify it under the 
+ * terms of the GNU Lesser General Public License (LGPL) as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.  See <http://www.gnu.org/licenses/>. */
 
 #ifndef FFT_H
 #define FFT_H
+
+#ifdef FFT_INLINES_C
+#define FFT_INLINE
+#else
+#define FFT_INLINE static __inline__
+#endif
 
 #undef ulong
 #define ulong ulongxx		/* interferes with system includes */
@@ -51,10 +28,13 @@
 #include "flint.h"
 #include "mpn_extras.h"
 
+#if defined(_OPENMP)
+#include <omp.h>		/* must come after flint.h */
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
-
 
 #if defined(__MPIR_VERSION)
 
@@ -66,8 +46,9 @@ mp_limb_t mpn_sumdiff_n(mp_ptr, mp_ptr, mp_srcptr, mp_srcptr, mp_size_t);
 
 #else
 
-static __inline__ mp_limb_t
-mpn_sumdiff_n(mp_ptr s, mp_ptr d, mp_srcptr x, mp_srcptr y, mp_size_t n)
+FFT_INLINE
+    mp_limb_t mpn_sumdiff_n(mp_ptr s, mp_ptr d, mp_srcptr x, mp_srcptr y,
+			    mp_size_t n)
 {
     mp_limb_t ret;
     mp_ptr t;
@@ -94,16 +75,6 @@ mpn_sumdiff_n(mp_ptr s, mp_ptr d, mp_srcptr x, mp_srcptr y, mp_size_t n)
     ret += mpn_sub_n(d, x, y, n);
     return ret;
 }
-
-/* mpn_mulmod_2expp1 is an internal function exposed by mpir, but the
- * real symbol is mpn_mulmod_2expp1_basecase anyway. The tarball we're
- * extracting here has the very same code (with a more permissive
- * license) as flint_mpn_mulmod_2expp1_basecase
- *
- * Bottom line: if we use gmp and not mpir, we may use the code we have
- * here.
- */
-#define mpn_mulmod_2expp1 flint_mpn_mulmod_2expp1_basecase
 
 #endif
 
@@ -135,10 +106,10 @@ mpn_sumdiff_n(mp_ptr s, mp_ptr d, mp_srcptr x, mp_srcptr y, mp_size_t n)
          nn[limbs] = -nn[limbs]; \
    } while (0)
 
-static __inline__
+FFT_INLINE
 /*
- * Adds the signed limb \code{c} to the generalised fermat number \code{r}
- * modulo \code{B^limbs + 1}. The compiler should be able to inline
+ * Adds the signed limb ``c`` to the generalised fermat number ``r``
+ * modulo ``B^limbs + 1``. The compiler should be able to inline
  * this for the case that there is no overflow from the first limb.
  * 
  */
@@ -163,8 +134,8 @@ void fft_combine_limbs(mp_limb_t * res, mp_limb_t ** poly, slong length,
 		       mp_size_t total_limbs);
 
 void fft_addcombine_bits(mp_limb_t * res, mp_limb_t ** poly, slong length,
-		      mp_bitcnt_t bits, mp_size_t output_limbs,
-		      mp_size_t total_limbs);
+			 mp_bitcnt_t bits, mp_size_t output_limbs,
+			 mp_size_t total_limbs);
 
 mp_size_t fft_split_limbs(mp_limb_t ** poly, mp_srcptr limbs,
 			  mp_size_t total_limbs, mp_size_t coeff_limbs,
@@ -295,7 +266,7 @@ void fft_mfa_truncate_sqrt2_inner(mp_limb_t ** ii, mp_limb_t ** jj,
 				  mp_size_t n, mp_bitcnt_t w, mp_limb_t ** t1,
 				  mp_limb_t ** t2, mp_limb_t ** temp,
 				  mp_size_t n1, mp_size_t trunc,
-				  mp_limb_t * tt);
+				  mp_limb_t ** tt);
 
 void ifft_mfa_truncate_sqrt2_outer(mp_limb_t ** ii, mp_size_t n,
 				   mp_bitcnt_t w, mp_limb_t ** t1,
@@ -324,120 +295,20 @@ void flint_mpn_mul_fft_main(mp_ptr r1, mp_srcptr i1, mp_size_t n1,
 
 void fft_convolution(mp_limb_t ** ii, mp_limb_t ** jj, slong depth,
 		     slong limbs, slong trunc, mp_limb_t ** t1,
-		     mp_limb_t ** t2, mp_limb_t ** s1, mp_limb_t * tt);
+		     mp_limb_t ** t2, mp_limb_t ** s1, mp_limb_t ** tt);
 
+/* CADO-NFS addition */
 
+/* mpn_mulmod_2expp1 is an internal function exposed by mpir, but the real
+ * symbol is mpn_mulmod_2expp1_basecase anyway. The tarball we're extracting
+ * here has the very same code (with a more permissive license) as
+ * flint_mpn_mulmod_2expp1_basecase Bottom line: if we use gmp and not mpir, 
+ * we may use the code we have here. */
+#define mpn_mulmod_2expp1 flint_mpn_mulmod_2expp1_basecase
 
-#define xxxDEBUG_FFT
+#include "transform_interface.h"
 
-struct fft_transform_info {
-    mp_bitcnt_t bits1;
-    mp_bitcnt_t bits2;
-    unsigned int nacc;
-    mp_size_t w;        /* Use sqrt(2)^w as a root of unity */
-    mp_size_t depth;    /* Let n=2^depth. We work modulo 2^(wn)+1. Do a
-                           transform length 4n. */
-    mp_bitcnt_t bits;   /* Chunk sizes in bits */
-    mp_size_t trunc0;   /* Number of coeffs of the transform computed.
-                           This is not exactly the fourier transform
-                           truncation point, because the truncation point
-                           is also subject to a few extra criteria. */
-    mp_bitcnt_t ks_coeff_bits;  /* This is used only for kronecker substitution */
-    mp_bitcnt_t minwrap;        /* zero when no wraparound wanted */
-    int alg;            /* alg==1: use matrix fourier algorithm */
-};
-
-/* The transform data is provided as an opaque void* pointer ; in reality
- * its structure may be written as the following pseudo-C code.
- *  struct transform_data {
- *      union {
- *          mp_limb_t * x[4 << depth];
- *          mp_limb_t * xy[2 << depth2][1 << depth1];
- *          // 1 + depth = depth1 + depth2
- *      } p;
- *      mp_limb_t * extra[2];
- *      mp_limb_t data[0];
- *  };
- * with pointers in x[], or xy[][], or extra[], all pointing to areas
- * within the zone beginning at data[]. Those areas are coefficients in
- * R=Z/(2^(nw)+1), each occupying fti_rsize0(fti)+1 limbs. All these
- * coefficient areas are disjoint. xy[] and x[] are of course to ways of
- * accessing the same set of pointers.
-
- * A transform data object represents a sequence of 4n coefficients in R
- * (pointed to by x[]). Before a DFT, this may be intepreted as a
- * polynomial P modulo x^(4n)-1. After, it's a sequence of pointwise
- * evaluations of P.  The layout is to be interpreted as follows.
-
- * with fti->alg == 0:
- *     x[i] == P( sqrt(2)^(bitrev(i, depth+2)) )
- * with fti->alg == 1 (matrix algorithm), The meaning of xy[] differs in
- * the two halves of the array.  We use the notation n1 == 1<<depth1,
- * n2==1<<depth2. The contents of xy[][] are given as follows.  Let
- * i<n2. 
- 
- * xy[bitrev(i,depth2)][j]             == P(sqrt(2)^(    2*(j + i<<depth1))
- *                                        P(             2^(j + i<<depth1))
- * xy[bitrev(i,depth2) + 1<<depth2][j] == P(sqrt(2)^(1 + 2*(j + i<<depth1)))
- *                                        P(   sqrt(2) * 2^(j + i<<depth1))
-
- * In case of truncation, only some of the entries are considered. For
- * fti->alg==0, these are the entries with i < trunc. For fti->alg==1, the
- * rule is instead (i+b<<depth2) < trunc, with b being the most significant
- * bit of the row index.
- */
-
-void fft_get_transform_info(struct fft_transform_info * fti, mp_bitcnt_t bits1, mp_bitcnt_t bits2, unsigned int nacc);
-void fft_get_transform_info_mulmod(struct fft_transform_info * fti, mp_bitcnt_t xbits, mp_bitcnt_t ybits, unsigned int nacc, mp_bitcnt_t minwrap);
-void fft_transform_info_adjust_depth(struct fft_transform_info * fti, unsigned int adj);
-void fft_transform_info_set_first_guess(struct fft_transform_info * fti);
-int fft_transform_info_check(const struct fft_transform_info * fti);
-void fft_get_transform_allocs(size_t sizes[3], const struct fft_transform_info * fti);
-void fft_transform_prepare(void * x, const struct fft_transform_info * fti);
-void fft_do_dft(void * y, const mp_limb_t * x, mp_size_t nx, void * temp, const struct fft_transform_info * fti);
-void fft_do_ift(mp_limb_t * x, mp_size_t nx, void * y, void * temp, const struct fft_transform_info * fti);
-void fft_mul(void * z, const void * y0, const void * y1, void * temp, const struct fft_transform_info * fti);
-void fft_addmul(void * z, const void * y0, const void * y1, void * temp, void * qtemp, const struct fft_transform_info * fti);
-void fft_add(void * z, void * y0, void * y1, const struct fft_transform_info * fti);
-void fft_zero(void * z, const struct fft_transform_info * fti);
-void fft_get_transform_info_fppol(struct fft_transform_info * fti, mpz_srcptr p, mp_size_t n1, mp_size_t n2, unsigned int nacc);
-void fft_get_transform_info_fppol_mp(struct fft_transform_info * fti, mpz_srcptr p, mp_size_t nmin, mp_size_t nmax, unsigned int nacc);
-void fft_do_dft_fppol(void * y, const mp_limb_t * x, mp_size_t nx, void * temp, const struct fft_transform_info * fti, mpz_srcptr p);
-void fft_do_ift_fppol(mp_limb_t * x, mp_size_t nx, void * y, void * temp, const struct fft_transform_info * fti, mpz_srcptr p);
-void fft_do_ift_fppol_mp(mp_limb_t * x, mp_size_t nx, void * y, void * temp, const struct fft_transform_info * fti, mpz_srcptr p, mp_size_t shift);
-
-/* fft_transform_export modifies the transform area in x and makes it
- * position independent, so that the data may be moved, or transferred to
- * another machine.
- *             /=============================================\
- *             | This (reversibly) invalidates the data in x |
- *             \=============================================/
- * fft_transform_import must be called on x to revert the effect of
- * fft_transform_export (possibly after moving/transferring).
- */
-void fft_transform_export(void * x, const struct fft_transform_info * fti);
-void fft_transform_import(void * x, const struct fft_transform_info * fti);
-
-
-/* indicates whether the integer returned is actually reduced modulo some
- * B^n-a, with a=\pm1. Returns n, and sets a. If the result is known to
- * be valid in Z, then n is returned as 0.
- */
-static inline mp_bitcnt_t fft_get_mulmod(const struct fft_transform_info * fti, int * a)
-{
-    *a=1;
-    return fti->minwrap ? (4<<fti->depth)*fti->bits : 0;
-}
-
-static inline mp_size_t fft_get_mulmod_output_minlimbs(const struct fft_transform_info * fti)
-{
-    if (!fti->minwrap) return 0;
-    mp_size_t w = fti->w;
-    mp_size_t n = 1 << fti->depth;
-    mp_bitcnt_t need = (4*n-1)*fti->bits+n*w;
-    return (need + FLINT_BITS - 1) / FLINT_BITS;
-}
-
+/* end CADO-NFS addition */
 
 #ifdef __cplusplus
 }
